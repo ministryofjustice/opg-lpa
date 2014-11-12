f1959dce39fe85f090ead6c64a46580dd2da1d40
Neil Smith <neil.smith@digital.justice.gov.uk>
2014-11-12 10:49:31 +0000

    Updated composer and test file.

--- service-pdf/test.php
+++ service-pdf/test.php
@@ -4,5 +4,6 @@ date_default_timezone_set('UTC');
 
 require_once 'vendor/autoload.php';
 
+$id = (string)time();
 
-\Opg\Lpa\Pdf\Worker\Worker::run( 'xxx', 'xxx', 'xxx' );
+\Opg\Lpa\Pdf\Worker\Worker::run( $id, 'LP1H', 'xxx' );
