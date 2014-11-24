a49284b06c42a3edb77a733b1e8108bc93aee93d
Jianzhong Yu <github@jianzhong.co.uk>
2014-11-24 22:55:25 +0000

    add __destruct() to remove intermediate files; refactoring

--- service-pdf/src/Opg/Lpa/Pdf/Service/Forms/AbstractForm.php
+++ service-pdf/src/Opg/Lpa/Pdf/Service/Forms/AbstractForm.php
@@ -25,8 +25,18 @@ abstract class AbstractForm
      */
     protected $flattenLpa;
     
-    protected $generatedpPdfFilePath = null;
-
+    /**
+     * The path of the pdf file to be generated.
+     * @var string
+     */
+    protected $generatedPdfFilePath = null;
+    
+    /**
+     * Intermediate pdf files - needed for LP1F/H
+     * @var array
+     */
+    protected $intermediatePdfFilePaths = array();
+    
     /**
      * Map model generated data to pdf form fields; add additional mapping 
      * for the forms on extra pages.
@@ -44,35 +54,33 @@ abstract class AbstractForm
         $this->flattenLpa = $lpa->flatten();
     }
 
-    /**
-     * Populate LPA data into PDF forms, generate pdf file and save into file path.
-     * 
-     * @return Form object
-     */
-    public function generate()
-    {
-        
-        $flattenLpaData = $this->mapData();
-        
-        $this->attachAdditionalPages();
-        
-        $this->pdf->fillForm($flattenLpaData)
-            ->needAppearances()
-            ->saveAs($this->generatedpPdfFilePath);
-        
-        return $this;
-    }
-    
     public function getPdfFilePath()
     {
-        return $this->generatedpPdfFilePath;
+        return $this->generatedPdfFilePath;
     }
     
     public function getPdfObject()
     {
         return $this->pdf;
     }
+    
+    protected function fullName($person)
+    {
+        return $person->name->title . ' '. $person->name->first . ' '. $person->name->last; 
+    }
 
-    public function clear()
-    {}
+    public function __destruct()
+    {
+        // remove all generated intermediate pdf files
+        foreach($this->intermediatePdfFilePaths as $type => $paths) {
+            if(is_string($paths)) {
+                unlink($paths);
+            }
+            else {
+                foreach($paths as $path) {
+                    unlink($path);
+                }
+            }
+        }
+    }
 } // class
