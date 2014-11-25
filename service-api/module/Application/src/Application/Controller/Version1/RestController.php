44096635e2ee0602d36bcc1f4d96451b648c3d28
Neil Smith <neil.smith@digital.justice.gov.uk>
2014-11-25 11:37:26 +0000

    Started adding an example resource.

--- service-api/module/Application/src/Application/Controller/Version1/RestController.php
+++ service-api/module/Application/src/Application/Controller/Version1/RestController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Application\Controller\Version1;
 
+use RuntimeException;
+
 use Zend\Mvc\Controller\AbstractRestfulController;
 
 use Application\Model\Resources\ResourceInterface;
@@ -12,26 +14,30 @@ use ZF\ApiProblem\ApiProblemResponse;
 
 class RestController extends AbstractRestfulController {
 
-    protected $resource;
+    private $resource;
 
     public function __construct(){
 
 
     } // function
 
+    //----------------------------------------------------
+
     public function setResource( ResourceInterface $resource ){
-        $this->response = $resource;
+        $this->resource = $resource;
     } // function
 
     public function getResource(){
 
-        if( !isset($this->response) ){
-            //Error!
+        if( !isset($this->resource) || !($this->resource instanceof ResourceInterface) ){
+            throw new RuntimeException('A resource has not been set');
         }
 
-        return $this->response;
+        return $this->resource;
 
     } // function
+
+    //----------------------------------------------------
 
     public function onDispatch(MvcEvent $e) {
         $return = parent::onDispatch($e);
@@ -40,18 +46,170 @@ class RestController extends AbstractRestfulController {
             return new ApiProblemResponse($return);
         }
 
-        $e->setResult($return);
+        //$e->setResult($return);
         return $return;
+
+    } // function
+
+    //----------------------------------------------------
+
+    /**
+     * Create a new resource
+     *
+     * @param  mixed $data
+     * @return mixed
+     */
+    public function create($data)
+    {
+        return new ApiProblem(405, 'The POST method has not been defined');
+    }
+
+    /**
+     * Delete an existing resource
+     *
+     * @param  mixed $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        return new ApiProblem(405, 'The DELETE method has not been defined');
+    }
+
+    /**
+     * Delete the entire resource collection
+     *
+     * Not marked as abstract, as that would introduce a BC break
+     * (introduced in 2.1.0); instead, raises an exception if not implemented.
+     *
+     * @return mixed
+     */
+    public function deleteList()
+    {
+        return new ApiProblem(405, 'The DELETE method has not been defined');
+    }
+
+    /**
+     * Return single resource
+     *
+     * @param  mixed $id
+     * @return mixed
+     */
+    public function get($id)
+    {
+        return new ApiProblem(405, 'The GET method has not been defined');
+    }
+
+    /**
+     * Return list of resources
+     *
+     * @return mixed
+     */
+    public function getList(){
+
+        if( !is_callable( [ $this->getResource(), 'fetchAll' ] ) ){
+            return new ApiProblem(405, 'The GET method has not been defined');
+        }
+
+        $response = $this->getResource()->fetchAll();
+
+        # TODO - Check the response.
+
+        return $response;
 
     }
 
-    public function get($id){
+    /**
+     * Retrieve HEAD metadata for the resource
+     *
+     * Not marked as abstract, as that would introduce a BC break
+     * (introduced in 2.1.0); instead, raises an exception if not implemented.
+     *
+     * @param  null|mixed $id
+     * @return mixed
+     */
+    public function head($id = null)
+    {
+        return new ApiProblem(405, 'The HEAD method has not been defined');
+    }
 
-        //return new ApiProblem("You do not have enough credit.", "http://example.com/probs/out-of-credit");
+    /**
+     * Respond to the OPTIONS method
+     *
+     * Typically, set the Allow header with allowed HTTP methods, and
+     * return the response.
+     *
+     * Not marked as abstract, as that would introduce a BC break
+     * (introduced in 2.1.0); instead, raises an exception if not implemented.
+     *
+     * @return mixed
+     */
+    public function options()
+    {
+        return new ApiProblem(405, 'The OPTIONS method has not been defined');
+    }
 
-        return new ApiProblem(405, 'The PUT method has not been defined for collections');
+    /**
+     * Respond to the PATCH method
+     *
+     * Not marked as abstract, as that would introduce a BC break
+     * (introduced in 2.1.0); instead, raises an exception if not implemented.
+     *
+     * @param  $id
+     * @param  $data
+     */
+    public function patch($id, $data)
+    {
+        return new ApiProblem(405, 'The PATCH method has not been defined');
+    }
 
+    /**
+     * Replace an entire resource collection
+     *
+     * Not marked as abstract, as that would introduce a BC break
+     * (introduced in 2.1.0); instead, raises an exception if not implemented.
+     *
+     * @param  mixed $data
+     * @return mixed
+     */
+    public function replaceList($data)
+    {
+        return new ApiProblem(405, 'The PUT method has not been defined');
+    }
 
+    /**
+     * Modify a resource collection without completely replacing it
+     *
+     * Not marked as abstract, as that would introduce a BC break
+     * (introduced in 2.2.0); instead, raises an exception if not implemented.
+     *
+     * @param  mixed $data
+     * @return mixed
+     */
+    public function patchList($data)
+    {
+        return new ApiProblem(405, 'The PATCH method has not been defined');
+    }
+
+    /**
+     * Update an existing resource
+     *
+     * @param  mixed $id
+     * @param  mixed $data
+     * @return mixed
+     */
+    public function update($id, $data)
+    {
+        return new ApiProblem(405, 'The PUT method has not been defined');
+    }
+
+    /**
+     * Basic functionality for when a page is not available
+     *
+     * @return array
+     */
+    public function notFoundAction()
+    {
+        return new ApiProblem(404, 'Page not found');
     }
 
 } // class
