49c68097aec6463906133303c5e28595687b1b84
Jianzhong Yu <github@jianzhong.co.uk>
2014-11-25 17:17:23 +0000

    bug fix: change parent class

--- service-pdf/src/Opg/Lpa/Pdf/Service/Forms/Lp1h.php
+++ service-pdf/src/Opg/Lpa/Pdf/Service/Forms/Lp1h.php
@@ -4,17 +4,14 @@ namespace Opg\Lpa\Pdf\Service\Forms;
 use Opg\Lpa\DataModel\Lpa\Lpa;
 use mikehaertl\pdftk\pdf as Pdf;
 
-class Lp1h extends AbstractForm
+class Lp1h extends Lp1
 {
 
-    public function __construct(Lpa $lpa)
+    public function __construct (Lpa $lpa)
     {
         parent::__construct($lpa);
-        $this->pdf = new Pdf(array('A' => "assets/v2/LP1H.pdf"));
-    }
-    
-    protected function mapData()
-    {
-        
+        $this->pdf = new Pdf(array(
+                'A' => "assets/v2/LP1H.pdf"
+        ));
     }
 } // class
