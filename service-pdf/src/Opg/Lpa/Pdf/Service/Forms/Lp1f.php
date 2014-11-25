9e6e726c3598dae6593b56326728918f37883bf5
Jianzhong Yu <github@jianzhong.co.uk>
2014-11-25 17:16:13 +0000

    refactoring & bug fix

--- service-pdf/src/Opg/Lpa/Pdf/Service/Forms/Lp1f.php
+++ service-pdf/src/Opg/Lpa/Pdf/Service/Forms/Lp1f.php
@@ -10,93 +10,95 @@ use Opg\Lpa\DataModel\Lpa\Document\Decisions;
 class Lp1f extends Lp1
 {
 
-    public function __construct(Lpa $lpa)
+    public function __construct (Lpa $lpa)
     {
         parent::__construct($lpa);
         
         // generate a file path with lpa id and timestamp;
-        $this->generatedPdfFilePath = 'pdf-'.Formatter::id($this->lpa->id).'-LP1F-'.time().'.pdf';
+        $this->generatedPdfFilePath = 'pdf-' . Formatter::id($this->lpa->id) .
+                 '-LP1F-' . time() . '.pdf';
         
-        $this->pdf = new Pdf(array('A'=>'../assets/v2/LP1F.pdf'));
+        $this->pdf = new Pdf(array(
+                'A' => '../assets/v2/LP1F.pdf'
+        ));
     }
-    
-    protected function hasAdditionalPages()
+
+    protected function hasAdditionalPages ()
     {
-        if(parent::hasAdditionalPages()) {
+        if (parent::hasAdditionalPages()) {
             return true;
         }
         
         // if a trust corp is a primary attorney attorney - CS4
-        if($this->lpa->document->primaryAttorneys[0] instanceof TrustCorporation) {
+        if ($this->lpa->document->primaryAttorneys[0] instanceof TrustCorporation) {
             return true;
         }
         
         // if a trust corp is a replacement attorney - CS4
-        if((count($this->lpa->document->replacementAttorneys)>0) 
-                && ($this->lpa->document->replacementAttorneys[0] instanceof TrustCorporation)) {
-                    return true;
+        if ((count($this->lpa->document->replacementAttorneys) > 0) &&
+                 ($this->lpa->document->replacementAttorneys[0] instanceof TrustCorporation)) {
+            return true;
         }
     }
-    
-    protected function mapData()
+
+    protected function mapData ()
     {
         parent::mapData();
         
-        if($this->lpa->document->primaryAttorneys[0] instanceof TrustCorporation) {
+        if ($this->lpa->document->primaryAttorneys[0] instanceof TrustCorporation) {
             $this->flattenLpa['attorney-0-is-trust-corporation'] = 'On';
             $this->flattenLpa['lpa-document-primaryAttorneys-0-name-last'] = $this->flattenLpa['lpa-document-primaryAttorneys-0-name'];
         }
         
-        if($this->lpa->document->replacementAttorneys[0]  instanceof TrustCorporation) {
+        if ($this->lpa->document->replacementAttorneys[0] instanceof TrustCorporation) {
             $this->flattenLpa['replacement-attorney-0-is-trust-corporation'] = 'On';
             $this->flattenLpa['lpa-document-replacementAttorneys-0-name-last'] = $this->flattenLpa['lpa-document-replacementAttorneys-0-name'];
         }
         
         /**
-         * @todo how replacements step in and act. fill CS2 and tick $this->flattenLpa['change-how-replacement-attorneys-step-in']
-         */
-        
-        /**
          * When attroney can make decisions (Section 5)
          */
-        if($this->lpa->document->decisions->when == Decisions::LPA_DECISION_WHEN_NOW) {
+        if ($this->lpa->document->primaryAttorneyDecisions->when ==
+                 Decisions\PrimaryAttorneyDecisions::LPA_DECISION_WHEN_NOW) {
             $this->flattenLpa['attorneys-may-make-decisions-when-lpa-registered'] = 'On';
-        }
-        elseif($this->lpa->document->decisions->when == Decisions::LPA_DECISION_WHEN_NO_CAPACITY) {
+        } elseif ($this->lpa->document->primaryAttorneyDecisions->when ==
+                 Decisions\PrimaryAttorneyDecisions::LPA_DECISION_WHEN_NO_CAPACITY) {
             $this->flattenLpa['attorneys-may-make-decisions-when-donor-lost-mental-capacity'] = 'On';
         }
         
         return $this->flattenLpa;
     }
-    
-    protected function generateAdditionalPagePdfs()
+
+    protected function generateAdditionalPagePdfs ()
     {
         parent::generateAdditionalPagePdfs();
         
         // CS4
-        if($this->lpa->document->primaryAttorneys[0] instanceof TrustCorporation) {
-            $this->addContinuationSheet4($this->lpa->document->primaryAttorneys[0]->number);
-        }
-        elseif((count($this->lpa->document->replacementAttorneys)>0) && ($this->lpa->document->replacementAttorneys[0] instanceof TrustCorporation)) {
-            $this->addContinuationSheet4($this->lpa->document->replacementAttorneys[0]->number);
+        if ($this->lpa->document->primaryAttorneys[0] instanceof TrustCorporation) {
+            $this->addContinuationSheet4(
+                    $this->lpa->document->primaryAttorneys[0]->number);
+        } elseif ((count($this->lpa->document->replacementAttorneys) > 0) &&
+                 ($this->lpa->document->replacementAttorneys[0] instanceof TrustCorporation)) {
+            $this->addContinuationSheet4(
+                    $this->lpa->document->replacementAttorneys[0]->number);
         }
     }
 
     /**
      * Fill the trust corporation registration number.
      */
-    protected function addContinuationSheet4($trustCorpRegNumber)
+    protected function addContinuationSheet4 ($trustCorpRegNumber)
     {
-        $tmpSavePath = '/tmp/pdf-CS4-'.$this->lpa->id.'-'.microtime().'.pdf';
-        $this->intermediatePdfFilePaths['cs4'] = $tmpSavePath;
-    
+        $tmpSavePath = '/tmp/pdf-CS4-' . $this->lpa->id . '-' . microtime() . '.pdf';
+        $this->intermediatePdfFilePaths['CS4'] = $tmpSavePath;
+        
         $cs2 = new Pdf('../assets/v2/LPC_Continuation_Sheet_4.pdf');
-    
-        $cs2->fillForm(array(
-                'cs-2-donor-full-name' => $trustCorpRegNumber
-        ))->needAppearances()
-        ->saveAs($tmpSavePath);
-    
-    
-    } //  function addContinuationSheet4()
+        
+        $cs2->fillForm(
+                array(
+                        'cs-4-trust-corporation-company-registration-number' => $trustCorpRegNumber
+                ))
+            ->needAppearances()
+            ->saveAs($tmpSavePath);
+    } // function addContinuationSheet4()
 } // class
