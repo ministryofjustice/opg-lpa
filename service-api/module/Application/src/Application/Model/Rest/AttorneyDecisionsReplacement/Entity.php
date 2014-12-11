d64602f3d40074dd04041204d0211246b2c15f9e
Neil Smith <neil.smith@digital.justice.gov.uk>
2014-12-11 15:12:38 +0000

    Added /replacement-attorney-decisions endpoint.

---
+++ service-api/module/Application/src/Application/Model/Rest/AttorneyDecisionsReplacement/Entity.php
@@ -0,0 +1,52 @@
+<?php
+namespace Application\Model\Rest\AttorneyDecisionsReplacement;
+
+use Application\Model\Rest\EntityInterface;
+
+use Application\Model\Rest\Users\Entity as UsersEntity;
+use Application\Model\Rest\Applications\Entity as ApplicationEntity;
+
+use Opg\Lpa\DataModel\Lpa\Lpa;
+use Opg\Lpa\DataModel\Lpa\Document\Decisions\ReplacementAttorneyDecisions;
+
+use Application\Library\Hal\Hal;
+
+class Entity implements EntityInterface {
+
+    protected $lap;
+    protected $decisions;
+
+    public function __construct( ReplacementAttorneyDecisions $decisions = null, Lpa $lpa ){
+
+        $this->lap = $lpa;
+        $this->decisions = $decisions;
+
+    }
+
+    public function lpaId(){
+        return $this->lap->id;
+    }
+
+    public function resourceId(){
+        return null;
+    }
+
+    public function getHal( callable $routeCallback ){
+
+        $hal = new Hal( call_user_func($routeCallback, $this) );
+
+        if( $this->decisions instanceof ReplacementAttorneyDecisions ){
+            $hal->setData( [ 'replacementAttorneyDecisions' => $this->decisions->toArray() ] );
+        } else {
+            $hal->setData( [ 'replacementAttorneyDecisions' => null ] );
+        }
+
+        # TODO - do I want to include these for all entities?
+        $hal->addLink( 'user', call_user_func($routeCallback, new UsersEntity( ['id'=>$this->lap->user] ) ) );
+        $hal->addLink( 'application', call_user_func($routeCallback, new ApplicationEntity($this->lap) ) );
+
+        return $hal;
+
+    } // function
+
+} // class
