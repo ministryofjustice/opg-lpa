07b1e079dec64a89cc7b96016bf285a32cc12860
Jianzhong Yu <github@jianzhong.co.uk>
2014-12-23 09:58:08 +0000

    refactoring

--- service-pdf/tools/testAll.php
+++ service-pdf/tools/testAll.php
@@ -6,15 +6,15 @@ require_once 'vendor/autoload.php';
 
 $id = (string)time();
 
-foreach(glob(__DIR__.'/../test-data/json/*.json') as $filepath) {
+foreach(['LPA120', 'LP3', 'LP1'] as $type) {
+    foreach(glob(__DIR__.'/../test-data/json/*.json') as $filepath) {
     
-    $pathInfo = pathinfo(realpath($filepath));
+        $pathInfo = pathinfo(realpath($filepath));
+        $data = file_get_contents( realpath($filepath) );
     
-    $data = file_get_contents( realpath($filepath) );
-    
-    foreach(['LPA120', 'LP3', 'LP1'] as $type) {
-        if(\file_exists(__DIR__.'/../test-data/output/'.$type.'/'.$type.'-'.$pathInfo['filename'] . '.pdf')) continue;
+        if(\file_exists(__DIR__.'/../test-data/output/'.$type.'-'.$pathInfo['filename'] . '.pdf')) continue;
         \Opg\Lpa\Pdf\Worker\Worker::run( $type.'-'.$pathInfo['filename'], $type, $data );
+        echo PHP_EOL;
     }
 }
 
