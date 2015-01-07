1160388b06a7f9f8a68d7ee68c13364515921469
Neil Smith <github@nsmith.me.uk>
2015-01-07 15:38:04 +0000

    Moved session config into the config directory

--- service-front/config/autoload/global.php
+++ service-front/config/autoload/global.php
@@ -12,5 +12,26 @@
  */
 
 return array(
-    // ...
+
+    'session' => [
+
+        'redis' => [
+            // Set a default (longish) Redis TTL to protect against long term stale data.
+            'ttl' => (60 * 60 * 24 * 28), // 28 days
+            'namespace' => 'session',
+            'server' => [
+                'host' => 'redisfront.local',
+                'port' => 6379
+            ],
+            'database' => 0, // WARNING: this has to be defined last otherwise Zend\Cache has a hissy fit.
+        ],
+
+        'encryption' => [
+            // key has to be a 32 character string
+            'key' => 'insecure-encryption-session-key!'
+        ]
+
+    ],
+
 );
+
--- service-front/module/Application/src/Application/Model/Service/Session/SessionFactory.php
+++ service-front/module/Application/src/Application/Model/Service/Session/SessionFactory.php
@@ -6,6 +6,7 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 use Zend\Session\SessionManager;
 use Zend\Session\SaveHandler\Cache as CacheSaveHandler;
+use Zend\Session\Exception\RuntimeException;
 
 use Zend\Cache\StorageFactory as CacheStorageFactory;
 
@@ -25,32 +26,32 @@ class SessionFactory implements FactoryInterface {
      */
     public function createService(ServiceLocatorInterface $serviceLocator){
 
+        $config = $serviceLocator->get('Config');
+
+        if( !isset( $config['session'] ) ){
+            throw new RuntimeException('Session configuration setting not found ');
+        }
+
+        $config = $config['session'];
+
+        //---
+
         $manager = new SessionManager();
 
         //----------------------------------------
         // Setup Redis as the save handler
 
-        # TODO - pull settings from config...
-
         $redis = CacheStorageFactory::factory([
             'adapter' => [
                 'name' => 'redis',
-                'options' => [
-                    'namespace' => 'session',
-                    'ttl' => (60 * 60 * 24 * 14), // Set a default (longish) ttl to clean up sessions that did not end properly.
-                    'server' => [
-                        'host' => 'redisfront.local',
-                        'port' => '6379',
-                    ]
-                ],
+                'options' => $config['redis'],
             ]
         ]);
 
         //----------------------------------------
         // Setup the encryption save handler
 
-        # TODO - pull this from Config...
-        $key = '0g5vi1m1602uyD5585lKNaUJE0p22p2k';
+        $key = $config['encryption']['key'];
 
         //$saveHandler = new SaveHandler\EncryptedCache( $redis, $key );
         $saveHandler = new CacheSaveHandler( $redis );
