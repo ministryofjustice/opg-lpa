c8dddd7ac7a45935386f10eb67018735e177db06
Neil Smith <github@nsmith.me.uk>
2015-01-07 16:05:57 +0000

    Moved all session settings into PHP
    From php-fpm's config file.

--- service-front/config/autoload/global.php
+++ service-front/config/autoload/global.php
@@ -15,6 +15,18 @@ return array(
 
     'session' => [
 
+        // ini session.* settings...
+        'native_settings' => [
+
+            // The cookie name used in the session
+            'name' => 'seshy',
+
+            // Hash settings
+            'hash_function' => 'sha512',
+            'hash_bits_per_character' => 5,
+
+        ],
+
         'redis' => [
             // Set a default (longish) Redis TTL to protect against long term stale data.
             'ttl' => (60 * 60 * 24 * 28), // 28 days
@@ -29,7 +41,7 @@ return array(
         'encryption' => [
             // key has to be a 32 character string
             'key' => 'insecure-encryption-session-key!'
-        ]
+        ],
 
     ],
 
