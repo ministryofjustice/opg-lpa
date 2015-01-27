06587b8a4a7ae8f9f1129a21e30bfa138b2973c0
Neil Smith <github@nsmith.me.uk>
2015-01-27 11:09:37 +0000

    Added error checking to the ControllerFactory

--- service-front/module/Application/src/Application/ControllerFactory/ControllerFactory.php
+++ service-front/module/Application/src/Application/ControllerFactory/ControllerFactory.php
@@ -1,23 +1,48 @@
 <?php
-
 namespace Application\ControllerFactory;
+
+use RuntimeException;
+
+use Zend\Stdlib\DispatchableInterface as Dispatchable;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
+/**
+ * Creates a controller based on the passed 'controllerName'.
+ *
+ * Class ControllerFactory
+ * @package Application\ControllerFactory
+ */
 class ControllerFactory implements FactoryInterface
 {
     /**
-     * @param ServiceLocatorInterface $serviceLocator 
-     *      - an instance of Zend\Mvc\Controller\ControllerManager
+     * @param ServiceLocatorInterface $controllerManager
+     * @return Dispatchable
      */
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function createService(ServiceLocatorInterface $controllerManager)
     {
-        $sm = $serviceLocator->getServiceLocator();
-        $router = $sm->get('Router');
-        $routerMatch = $router->match($sm->get('Request'));
-        $routeName = $routerMatch->getMatchedRouteName();
-        $controllerName = $routerMatch->getParam('controllerName');
-        return new $controllerName;
-    }
-}
+
+        $locator = $controllerManager->getServiceLocator();
+
+        //---
+
+        $controllerName = $locator->get('Application')->getMvcEvent()->getRouteMatch()->getParam('controllerName');
+
+        // Check the class exists...
+        if( !class_exists($controllerName) ){
+            throw new RuntimeException( 'Unknown controller name' );
+        }
+
+        $controller = new $controllerName;
+
+        // Ensure it's Dispatchable...
+        if( !( $controller instanceof Dispatchable ) ){
+            throw new RuntimeException( 'Passed controller class is not Dispatchable' );
+        }
+
+        return $controller;
+
+    } // function
+
+} // class
