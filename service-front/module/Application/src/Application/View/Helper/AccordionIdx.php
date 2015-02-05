3da9f533a48a57951c28ed83886ca089aaeef987
Jianzhong Yu <github@jianzhong.co.uk>
2015-02-05 10:25:19 +0000

    calculate title index number for the opening bar

--- service-front/module/Application/src/Application/View/Helper/AccordionIdx.php
+++ service-front/module/Application/src/Application/View/Helper/AccordionIdx.php
@@ -2,15 +2,39 @@
 namespace Application\View\Helper;
 
 use Opg\Lpa\DataModel\Lpa\Lpa;
+use Application\Model\FormFlowChecker;
 
 class AccordionIdx extends AbstractAccordion
 {
     public function __invoke (Lpa $lpa = null)
     {
+        if($lpa === null) {
+            return '';
+        }
         
         $this->lpa = $lpa;
         
-        return 'VI';
+        $routeName = $this->getRouteName();
+        $barConfig = $this->getBarConfig($routeName);
         
+        if($barConfig == null) {
+            return '';
+        }
+        
+        $flowChecker = new FormFlowChecker($lpa);
+        $seq = 1;
+        $barList = [];
+        foreach($barConfig as $barRouteName => $barDataValues) {
+            if($barRouteName == $flowChecker->check($barRouteName)) {
+                if($barRouteName == $routeName) {
+                    break;
+                }
+                else {
+                    $seq++;
+                }
+            }
+        }
+        
+        return $seq;
     }
 }
