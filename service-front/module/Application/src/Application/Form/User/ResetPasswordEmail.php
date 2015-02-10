1826a316c2e9f397c49c420ed4f52c6d3b05420f
Chris Moreton <chris@netsensia.com>
2015-02-10 13:56:57 +0000

    Add linking to error messages on password reset form

--- service-front/module/Application/src/Application/Form/User/ResetPasswordEmail.php
+++ service-front/module/Application/src/Application/Form/User/ResetPasswordEmail.php
@@ -1,8 +1,6 @@
 <?php
 namespace Application\Form\User;
 
-use Zend\InputFilter\InputFilter;
-
 /**
  * For to request a password reset email be sent out.
  *
@@ -19,13 +17,16 @@ class ResetPasswordEmail extends AbstractForm {
 
         $this->add(array(
             'options' => [
-                'label' => 'Title',
+                'label' => 'Email address',
             ],
             'name' => 'email',
             'type' => 'Email',
         ));
 
         $this->add(array(
+            'options' => [
+                'label' => 'Re-enter your email address',
+            ],
             'name' => 'email_confirm',
             'type' => 'Email',
         ));
