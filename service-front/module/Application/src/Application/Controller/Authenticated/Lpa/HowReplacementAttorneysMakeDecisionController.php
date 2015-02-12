2820e1bab597bc4b37f0d7a9246789adfad8d2af
Jianzhong Yu <github@jianzhong.co.uk>
2015-02-12 21:21:22 +0000

    complete replacement attorney decisions action/view

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/HowReplacementAttorneysMakeDecisionController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/HowReplacementAttorneysMakeDecisionController.php
@@ -11,6 +11,8 @@ namespace Application\Controller\Authenticated\Lpa;
 
 use Application\Controller\AbstractLpaController;
 use Zend\View\Model\ViewModel;
+use Opg\Lpa\DataModel\Lpa\Document\Decisions\ReplacementAttorneyDecisions;
+use Application\Form\Lpa\HowAttorneysMakeDecisionForm;
 
 class HowReplacementAttorneysMakeDecisionController extends AbstractLpaController
 {
@@ -19,6 +21,48 @@ class HowReplacementAttorneysMakeDecisionController extends AbstractLpaControlle
     
     public function indexAction()
     {
-        return new ViewModel();
+        $form = new HowAttorneysMakeDecisionForm('replacement-attorney-decisions');
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            
+            // set data for validation
+            $form->setData($postData);
+            
+            if($form->isValid()) {
+                
+                $lpaId = $this->getLpa()->id;
+                $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+                
+                $howAttorneyAct = $form->get('how')->getValue();
+                
+                if($this->getLpa()->document->replacementAttorneyDecisions instanceof ReplacementAttorneyDecisions) {
+                    $decision = $this->getLpa()->document->replacementAttorneyDecisions;
+                }
+                else {
+                    $decision = new ReplacementAttorneyDecisions();
+                }
+                
+                $decision->how = $howAttorneyAct;
+                
+                if($howAttorneyAct == ReplacementAttorneyDecisions::LPA_DECISION_HOW_DEPENDS) {
+                    $decision->howDetails = $form->get('howDetails')->getValue();
+                }
+                
+                // persist data
+                if(!$this->getLpaApplicationService()->setReplacementAttorneyDecisions($lpaId, $decision)) {
+                    throw new \RuntimeException('API client failed to set replacement attorney decisions for id: '.$lpaId);
+                }
+                
+                $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+            }
+        }
+        else {
+            if($this->getLpa()->document->replacementAttorneyDecisions instanceof ReplacementAttorneyDecisions) {
+                $form->bind($this->getLpa()->document->replacementAttorneyDecisions->flatten());
+            }
+        }
+        
+        return new ViewModel(['form'=>$form]);
     }
 }
