48474a937863f5eff3204e6214fb6844626c93f1
Jianzhong Yu <github@jianzhong.co.uk>
2015-02-12 16:45:16 +0000

    complete replacement attorney action/form/view

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/ReplacementAttorneyController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/ReplacementAttorneyController.php
@@ -11,6 +11,11 @@ namespace Application\Controller\Authenticated\Lpa;
 
 use Application\Controller\AbstractLpaController;
 use Zend\View\Model\ViewModel;
+use Opg\Lpa\DataModel\Lpa\Document\Attorneys\Human;
+use Application\Form\Lpa\AttorneyForm;
+use Opg\Lpa\DataModel\Lpa\Document\Document;
+use Application\Form\Lpa\TrustCorporationForm;
+use Opg\Lpa\DataModel\Lpa\Document\Attorneys\TrustCorporation;
 
 class ReplacementAttorneyController extends AbstractLpaController
 {
@@ -19,44 +24,232 @@ class ReplacementAttorneyController extends AbstractLpaController
     
     public function indexAction()
     {
-        return new ViewModel();
+        $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+        $lpaId = $this->getLpa()->id;
+             
+        if( count($this->getLpa()->document->replacementAttorneys) > 0 ) {
+            
+            $attorneysParams = [];
+            foreach($this->getLpa()->document->replacementAttorneys as $idx=>$attorney) {
+                $params = [
+                        'attorney' => [
+                                'address'   => $attorney->address->__toString()
+                        ],
+                        'editRoute'     => $this->url()->fromRoute( $currentRouteName.'/edit', ['lpa-id' => $lpaId, 'idx' => $attorney->id ]),
+                        'deleteRoute'   => $this->url()->fromRoute( $currentRouteName.'/delete', ['lpa-id' => $lpaId, 'idx' => $attorney->id ]),
+                ];
+                
+                if($attorney instanceof Human) {
+                    $params['attorney']['name'] = $attorney->name->__toString();
+                }
+                else {
+                    $params['attorney']['name'] = $attorney->name;
+                }
+                
+                $attorneysParams[] = $params;
+            }
+            
+            return new ViewModel([
+                    'addRoute'    => $this->url()->fromRoute( $currentRouteName.'/add', ['lpa-id'=>999] ),
+                    'lpaId'     => $lpaId,
+                    'attorneys' => $attorneysParams,
+                    'nextRoute' => $this->url()->fromRoute( $this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id'=>$lpaId] )
+            ]);
+            
+        }
+        else {
+            
+            return new ViewModel([
+                    'addRoute'    => $this->url()->fromRoute( $currentRouteName.'/add', ['lpa-id'=>$lpaId] ),
+                    'lpaId'     => $lpaId,
+            ]);
+            
+        }
     }
     
     public function addAction()
     {
-        return new ViewModel();
+        $viewModel = new ViewModel();
+        if ( $this->getRequest()->isXmlHttpRequest() ) {
+            $viewModel->setTerminal(true);
+        }
+        
+        $lpaId = $this->getLpa()->id;
+        $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+        
+        $form = new AttorneyForm('replacement-attorney');
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            $form->setData($postData);
+            
+            if($form->isValid()) {
+            
+                // persist data
+                $attorney = new Human($form->getModelizedData());
+                if( !$this->getLpaApplicationService()->addReplacementAttorney($lpaId, $attorney) ) {
+                    throw new \RuntimeException('API client failed to add a replacement attorney for id: '.$lpaId);
+                }
+                
+                if ( $this->getRequest()->isXmlHttpRequest() ) {
+                    return $viewModel;
+                }
+                else {
+                    $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+                }
+            }
+        }
+        
+        $viewModel->form = $form;
+        
+        // only provide add trust corp link if lpa has not a trust already and lpa is of PF type.
+        if(!$this->hasTrust() && ($this->getLpa()->document->type == Document::LPA_TYPE_PF) ) {
+            $viewModel->addTrustCorporationRoute = $this->url()->fromRoute( 'lpa/replacement-attorney/add-trust', ['lpa-id' => $lpaId] );
+        }
+        
+        return $viewModel;
     }
     
     public function editAction()
     {
-        return new ViewModel();
+        $viewModel = new ViewModel();
+        if ( $this->getRequest()->isXmlHttpRequest() ) {
+            $viewModel->setTerminal(true);
+        }
+        
+        $lpaId = $this->getLpa()->id;
+        $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+        
+        $attorneyIdx = $this->getEvent()->getRouteMatch()->getParam('idx');
+        foreach($this->getLpa()->document->replacementAttorneys as $replacementAttorney) {
+            if($replacementAttorney->id == $attorneyIdx) {
+                $attorney = $replacementAttorney;
+            }
+        }
+        
+        // if attorney idx does not exist in lpa, return 404.
+        if(!isset($attorney)) {
+            return $this->notFoundAction();
+        }
+        
+        if($attorney instanceof Human) {
+            $form = new AttorneyForm();
+        }
+        else {
+            $form = new TrustCorporationForm();
+            $viewModel->setTemplate('application/replacement-attorney/edit-trust.phtml');
+        }
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            $form->setData($postData);
+            
+            if($form->isValid()) {
+                // persist data
+                if($attorney instanceof Human) {
+                    $attorney = new Human($form->getModelizedData());
+                }
+                else {
+                    $attorney = new TrustCorporation($form->getModelizedData());
+                }
+                
+                // update attorney
+                if(!$this->getLpaApplicationService()->setReplacementAttorney($lpaId, $attorney, $attorneyIdx)) {
+                    throw new \RuntimeException('API client failed to update a replacement attorney ' . $attorneyIdx . ' for id: ' . $lpaId);
+                }
+                
+                if ( $this->getRequest()->isXmlHttpRequest() ) {
+                    return $viewModel;
+                }
+                else {
+                    $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+                }
+            }
+        }
+        else {
+            $flattenAttorneyData = $attorney->flatten();
+            if($attorney instanceof Human) {
+                $flattenAttorneyData['dob-date'] = $this->getLpa()->document->donor->dob->date->format('Y-m-d');
+            }
+            
+            $form->bind($flattenAttorneyData);
+        }
+        
+        $viewModel->form = $form;
+        
+        return $viewModel;
     }
     
     public function deleteAction()
     {
-        // @todo delete a replacement attorney
+        $lpaId = $this->getLpa()->id;
+        $attorneyIdx = $this->getEvent()->getRouteMatch()->getParam('idx');
         
-        $this->redirect()->toRoute('lpa/replacement-attorney', array('lpa-id'=>$this->getEvent()->getRouteMatch()->getParam('lpa-id')));
+        $deletionFlag = true;
+        foreach($this->getLpa()->document->replacementAttorneys as $attorney) {
+            if($attorney->id == $attorneyIdx) {
+                if(!$this->getLpaApplicationService()->deleteReplacementAttorney($lpaId, $attorneyIdx)) {
+                    throw new \RuntimeException('API client failed to delete a replacement attorney ' . $attorneyIdx . ' for id: ' . $lpaId);
+                }
+                $deletionFlag = true;
+            }
+        }
         
-        return $this->response;
+        // if attorney idx does not exist in lpa, return 404.
+        if(!$deletionFlag) {
+            return $this->notFoundAction();
+        }
+        
+        if ( $this->getRequest()->isXmlHttpRequest() ) {
+            return $this->response;
+        }
+        else {
+            $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+            $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+        }
     }
     
     public function addTrustAction()
     {
-        return new ViewModel();
-    }
-    
-    public function editTrustAction()
-    {
-        return new ViewModel();
-    }
-    
-    public function deleteTrustAction()
-    {
-        // @todo delete trust from primaryAttorneys
-                
-        $this->redirect()->toRoute('lpa/primary-attorney', array('lpa-id'=>$this->getEvent()->getRouteMatch()->getParam('lpa-id')));
+        $viewModel = new ViewModel();
+        if ( $this->getRequest()->isXmlHttpRequest() ) {
+            $viewModel->setTerminal(true);
+        }
         
-        return $this->response;
+        $lpaId = $this->getLpa()->id;
+        $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+        
+        // redirect to add human attorney if lpa is of hw type or a trust was added already.
+        if( ($this->getLpa()->document->type == Document::LPA_TYPE_HW) || $this->hasTrust() ) {
+            $this->redirect()->toRoute('lpa/replacement-attorney/add', ['lpa-id' => $lpaId]);
+        }
+        
+        $form = new TrustCorporationForm();
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            $form->setData($postData);
+            
+            if($form->isValid()) {
+            
+                // persist data
+                $attorney = new TrustCorporation($form->getModelizedData());
+                if( !$this->getLpaApplicationService()->addReplacementAttorney($lpaId, $attorney) ) {
+                    throw new \RuntimeException('API client failed to add a trust corporation replacement attorney for id: '.$lpaId);
+                }
+                
+                if ( $this->getRequest()->isXmlHttpRequest() ) {
+                    return $viewModel;
+                }
+                else {
+                    $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+                }
+            }
+        }
+        
+        $viewModel->form = $form;
+        $viewModel->addAttorneyRoute = $this->url()->fromRoute( 'lpa/replacement-attorney/add', ['lpa-id' => $lpaId] );
+        
+        return $viewModel;
     }
 }
