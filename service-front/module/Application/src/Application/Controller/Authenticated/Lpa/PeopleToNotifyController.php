54097fa7ed13311e8f7a70381b833437eac092ad
Jianzhong Yu <github@jianzhong.co.uk>
2015-02-13 14:48:47 +0000

    complete people to notify controllers, form & views

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/PeopleToNotifyController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/PeopleToNotifyController.php
@@ -11,6 +11,8 @@ namespace Application\Controller\Authenticated\Lpa;
 
 use Application\Controller\AbstractLpaController;
 use Zend\View\Model\ViewModel;
+use Opg\Lpa\DataModel\Lpa\Document\NotifiedPerson;
+use Application\Form\Lpa\PeopleToNotifyForm;
 
 class PeopleToNotifyController extends AbstractLpaController
 {
@@ -19,25 +21,157 @@ class PeopleToNotifyController extends AbstractLpaController
     
     public function indexAction()
     {
-        return new ViewModel();
+        $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+        $lpaId = $this->getLpa()->id;
+        
+        if( count($this->getLpa()->document->peopleToNotify) > 0 ) {
+            
+            $peopleToNotify = [];
+            foreach($this->getLpa()->document->peopleToNotify as $idx=>$peopleToNotify) {
+                $peopleToNotifyParams[] = [
+                        'notifiedPerson' => [
+                                'name'      => $peopleToNotify->name->__toString(),
+                                'address'   => $peopleToNotify->address->__toString()
+                        ],
+                        'editRoute'     => $this->url()->fromRoute( $currentRouteName.'/edit', ['lpa-id' => $lpaId, 'idx' => $peopleToNotify->id ]),
+                        'deleteRoute'   => $this->url()->fromRoute( $currentRouteName.'/delete', ['lpa-id' => $lpaId, 'idx' => $peopleToNotify->id ]),
+                ];
+            }
+            
+            return new ViewModel([
+                    'peopleToNotify' => $peopleToNotifyParams,
+                    'addRoute'  => $this->url()->fromRoute( $currentRouteName.'/add', ['lpa-id' => $lpaId] ),
+                    'nextRoute' => $this->url()->fromRoute( $this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id'=>$lpaId] )
+            ]);
+            
+        }
+        else {
+            
+            return new ViewModel([
+                    'addRoute'    => $this->url()->fromRoute( $currentRouteName.'/add', ['lpa-id'=>$lpaId] ),
+                    'nextRoute' => $this->url()->fromRoute( $this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id'=>$lpaId] ),
+            ]);
+            
+        }
     }
     
     public function addAction()
     {
-        return new ViewModel();
+        $viewModel = new ViewModel();
+        if ( $this->getRequest()->isXmlHttpRequest() ) {
+            $viewModel->setTerminal(true);
+        }
+        
+        $lpaId = $this->getLpa()->id;
+        $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+        
+        $form = new PeopleToNotifyForm();
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            $form->setData($postData);
+            if($form->isValid()) {
+                
+                // persist data
+                $np = new NotifiedPerson($form->getModelizedData());
+                if(!$this->getLpaApplicationService()->addNotifiedPerson($lpaId, $np)) {
+                    throw new \RuntimeException('API client failed to add a notified person for id: '.$lpaId);
+                }
+                
+                if ( $this->getRequest()->isXmlHttpRequest() ) {
+                    return $viewModel;
+                }
+                else {
+                    $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+                }
+            }
+        }
+        
+        $viewModel->form = $form;
+        
+        return $viewModel;
     }
     
     public function editAction()
     {
-        return new ViewModel();
+        $viewModel = new ViewModel();
+        if ( $this->getRequest()->isXmlHttpRequest() ) {
+            $viewModel->setTerminal(true);
+        }
+        
+        $lpaId = $this->getLpa()->id;
+        $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+        
+        $personIdx = $this->getEvent()->getRouteMatch()->getParam('idx');
+        foreach($this->getLpa()->document->peopleToNotify as $peoplToNotify) {
+            if($peoplToNotify->id == $personIdx) {
+                $notifiedPerson = $peoplToNotify;
+            }
+        }
+        
+        // if notified person idx does not exist in lpa, return 404.
+        if(!isset($notifiedPerson)) {
+            return $this->notFoundAction();
+        }
+        
+        $form = new PeopleToNotifyForm();
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            $form->setData($postData);
+            
+            if($form->isValid()) {
+                // persist data
+                $notifiedPerson = new NotifiedPerson($form->getModelizedData());
+                
+                // update attorney
+                if(!$this->getLpaApplicationService()->setNotifiedPerson($lpaId, $notifiedPerson, $personIdx)) {
+                    throw new \RuntimeException('API client failed to update notified person ' . $personIdx . ' for id: ' . $lpaId);
+                }
+                
+                if ( $this->getRequest()->isXmlHttpRequest() ) {
+                    return $viewModel;
+                }
+                else {
+                    $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+                }
+            }
+        }
+        else {
+            $form->bind($notifiedPerson->flatten());
+        }
+        
+        $viewModel->form = $form;
+        
+        return $viewModel;
     }
     
     public function deleteAction()
     {
-        // @todo delete a notifyed person
+        $lpaId = $this->getLpa()->id;
+        $personIdx = $this->getEvent()->getRouteMatch()->getParam('idx');
         
-        $this->redirect()->toRoute('lpa/people-to-notify', array('lpa-id'=>$this->getEvent()->getRouteMatch()->getParam('lpa-id')));
+        $deletionFlag = true;
+        foreach($this->getLpa()->document->peopleToNotify as $peopleToNotify) {
+            if($peopleToNotify->id == $personIdx) {
+                if(!$this->getLpaApplicationService()->deleteNotifiedPerson($lpaId, $personIdx)) {
+                    throw new \RuntimeException('API client failed to delete notified person ' . $personIdx . ' for id: ' . $lpaId);
+                }
+                $deletionFlag = true;
+            }
+        }
         
-        return $this->response;
+        // if notified person idx does not exist in lpa, return 404.
+        if(!$deletionFlag) {
+            return $this->notFoundAction();
+        }
+        
+        if ( $this->getRequest()->isXmlHttpRequest() ) {
+            return $this->response;
+        }
+        else {
+            $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+            $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+        }
     }
 }
