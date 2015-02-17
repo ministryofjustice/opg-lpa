f3a6ab913f99320fd00c4117331c24f282b0a034
Neil Smith <github@nsmith.me.uk>
2015-02-17 12:26:54 +0000

    Added Register account

--- service-front/module/Application/src/Application/Controller/General/RegisterController.php
+++ service-front/module/Application/src/Application/Controller/General/RegisterController.php
@@ -1,15 +1,92 @@
 <?php
-
 namespace Application\Controller\General;
 
 use Zend\View\Model\ViewModel;
 use Application\Controller\AbstractBaseController;
 
+use Application\Form\User\Registration as RegistrationForm;
+
 class RegisterController extends AbstractBaseController {
 
+    /**
+     * Register a new account.
+     *
+     * @return ViewModel
+     */
     public function indexAction(){
 
-        return new ViewModel();
-    }
+        $form = new RegistrationForm();
+        $form->setAttribute( 'action', $this->url()->fromRoute('register') );
 
-}
+        $error = null;
+
+        //---
+
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+
+                // Create a callback for the Model to get the callback URL from.
+                $callback = function( $token ) {
+                    return $this->url()->fromRoute('register/callback', [ 'token'=>$token ], [ 'force_canonical' => true ] );
+                };
+
+                $result = $this->getServiceLocator()->get('Register')->registerAccount(
+                    $form->getData()['email'],
+                    $form->getData()['password'],
+                    $callback
+                );
+
+                if( $result === true ){
+
+                    return (new ViewModel( ['email'=>$form->getData()['email']] ))->setTemplate('application/register/email-sent');
+
+                }
+
+                $error = $result;
+
+            } // if
+
+        } // if
+
+        //---
+
+        return new ViewModel( compact('form', 'error') );
+
+    } // function
+
+    /**
+     * Confirm the email address, activating the account.
+     *
+     * @return ViewModel
+     */
+    public function confirmAction(){
+
+        $token = $this->params()->fromRoute('token');
+
+        if( empty($token) ){
+            return new ViewModel( [ 'error'=>'invalid-token' ] );
+        }
+
+        /**
+         * This returns:
+         *      TRUE - If the user account exists. The account has been activated, or was already activated.
+         *      FALSE - If the user account does not exist.
+         *
+         *  Alas no other details are returned.
+         */
+        $success = $this->getServiceLocator()->get('Register')->activateAccount( $token );
+
+        if( !$success ){
+            return new ViewModel( [ 'error'=>'account-missing' ] );
+        }
+
+        return new ViewModel();
+
+    } // function
+
+} // class
---
+++ service-front/module/Application/src/Application/Form/User/Registration.php
@@ -0,0 +1,90 @@
+<?php
+namespace Application\Form\User;
+
+use Zend\Validator;
+
+class Registration extends SetPassword {
+
+    public function __construct( $formName = 'registration' ){
+
+        parent::__construct($formName);
+
+        //---
+
+        $this->add(array(
+            'name' => 'email',
+            'type' => 'Email',
+        ));
+
+        $this->add(array(
+            'name' => 'email_confirm',
+            'type' => 'Email',
+        ));
+
+        $this->add(array(
+            'name' => 'terms',
+            'type' => 'Checkbox',
+        ));
+
+        //--------------------------------
+
+        $inputFilter = $this->getInputFilter();
+
+        $inputFilter->add(array(
+            'name'     => 'email',
+            'required' => true,
+            'filters'  => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name'    => 'EmailAddress',
+                ),
+            ),
+        ));
+
+        $inputFilter->add(array(
+            'name'     => 'email_confirm',
+            'required' => true,
+            'filters'  => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name'    => 'Identical',
+                    'options' => array(
+                        'token' => 'email',
+                        'messageTemplates' => [
+                            Validator\Identical::NOT_SAME => 'Your email addresses did not match.',
+                        ],
+                    ),
+                ),
+            ),
+        ));
+
+        $inputFilter->add(array(
+            'name'     => 'terms',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name'    => 'Identical',
+                    'options' => array(
+                        'token' => '1',
+                        'literal' => true,
+                        'messageTemplates' => [
+                            Validator\Identical::NOT_SAME => 'must be checked',
+                        ],
+                    ),
+                ),
+            ),
+        ));
+
+        //---
+
+        $this->setInputFilter( $inputFilter );
+
+    } // function
+
+} // class
