d6003f048a80c43d4a3d11503174af1c6f11efc5
Chris Moreton <chris@netsensia.com>
2015-02-23 15:59:45 +0000

    Style Primary Attorney form

--- service-front/module/Application/src/Application/View/Helper/FormElementErrors.php
+++ service-front/module/Application/src/Application/View/Helper/FormElementErrors.php
@@ -7,11 +7,13 @@ class FormElementErrors extends \Zend\Form\View\Helper\FormElementErrors
 {
     public function __invoke(ElementInterface $element = null, array $attributes = array())
     {
+        echo '<p class="form-element-errors">';
         $this->setMessageOpenFormat('<span class="validation-message">');
         $this->setMessageCloseString('</span>');
         $this->setMessageSeparatorString('<br>');
         
         return parent::__invoke($element, $attributes);
+        echo '</p>';
     }
     
 }
