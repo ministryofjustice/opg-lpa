123ff3fca51b11b0010d1a102e38d1cde99f7cb8
Jianzhong Yu <github@jianzhong.co.uk>
2015-02-23 10:15:04 +0000

    change title field type from 'Select' to 'Text'.

--- service-front/module/Application/src/Application/Form/Lpa/CertificateProviderForm.php
+++ service-front/module/Application/src/Application/Form/Lpa/CertificateProviderForm.php
@@ -6,7 +6,7 @@ class CertificateProviderForm extends AbstractActorForm
 {
     protected $formElements = [
             'name-title' => [
-                    'type' => 'Zend\Form\Element\Select',
+                    'type' => 'Text',
             ],
             'name-first' => [
                     'type' => 'Text',
