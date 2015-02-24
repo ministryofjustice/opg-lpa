b7949d001112b0b3ed2757f24f4d8cc3274e2920
Neil Smith <github@nsmith.me.uk>
2015-02-24 15:36:56 +0000

    Added Postcode Anywhere functionality

--- service-front/module/Application/src/Application/Controller/Authenticated/PostcodeController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/PostcodeController.php
@@ -1,14 +1,46 @@
 <?php
-
 namespace Application\Controller\Authenticated;
 
-use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 use Application\Controller\AbstractAuthenticatedController;
 
-class PostcodeController extends AbstractAuthenticatedController
-{
-    public function indexAction()
-    {
-        return new ViewModel();
-    }
-}
+class PostcodeController extends AbstractAuthenticatedController {
+
+    public function indexAction(){
+
+        $service = $this->getServiceLocator()->get('AddressLookup');
+
+        //-----------------------
+        // Postcode lookup
+
+        $postcode = $this->params()->fromQuery('postcode');
+
+        if( isset($postcode) ){
+
+            $result = $service->lookupPostcode( $postcode );
+
+            return new JsonModel( $result );
+
+        }
+
+        //-----------------------
+        // Address lookup
+
+        $addressId = $this->params()->fromQuery('addressId');
+
+        if( isset($addressId) ){
+
+            $result = $service->lookupAddress( $addressId );
+
+            return new JsonModel( $result );
+
+        }
+
+        //---
+
+        // else not found.
+        return $this->notFoundAction();
+
+    } // function
+
+} // class
