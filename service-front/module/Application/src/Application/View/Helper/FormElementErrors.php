b7728bb09c1158be31ea03c65e73417ddb53e1c0
Chris Moreton <chris@netsensia.com>
2015-02-25 11:37:13 +0000

    Update handling of element error messages

--- service-front/module/Application/src/Application/View/Helper/FormElementErrors.php
+++ service-front/module/Application/src/Application/View/Helper/FormElementErrors.php
@@ -7,14 +7,11 @@ class FormElementErrors extends \Zend\Form\View\Helper\FormElementErrors
 {
     public function __invoke(ElementInterface $element = null, array $attributes = array())
     {
-        echo '<p class="form-element-errors">';
-        
-        $this->setMessageOpenFormat('<span class="validation-message">');
-        $this->setMessageCloseString('</span>');
+        $this->setMessageOpenFormat('<div class="group validation"><span class="validation-message">');
+        $this->setMessageCloseString('</span></div>');
         $this->setMessageSeparatorString('<br>');
         
         return parent::__invoke($element, $attributes);
-        echo '</p>';
     }
     
 }
