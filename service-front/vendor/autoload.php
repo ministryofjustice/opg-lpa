aa799f05d189bde159a0d2d246a6b8820c9c4607
Neil Smith <github@nsmith.me.uk>
2015-02-26 17:05:47 +0000

    Composer update

--- service-front/vendor/autoload.php
+++ service-front/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit35f6e8585ee2881a9d73816d56f1bd9e::getLoader();
+return ComposerAutoloaderInitdf5fd9215351a3f69fa3b31f66515f4e::getLoader();
