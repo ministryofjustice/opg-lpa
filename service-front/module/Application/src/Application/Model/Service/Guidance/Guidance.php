61c474a91fca424a774e114c185a42539e6dd063
Chris Moreton <chris@netsensia.com>
2015-03-06 15:47:15 +0000

    Guidance transformation complete

--- service-front/module/Application/src/Application/Model/Service/Guidance/Guidance.php
+++ service-front/module/Application/src/Application/Model/Service/Guidance/Guidance.php
@@ -14,11 +14,9 @@ class Guidance
      */
     function generateHtmlFromMarkdown()
     {
-        $html = '';
         $sectionArray = [];
         $lines = file(self::GUIDANCE_MARKDOWN_FOLDER . '/order.md');
         
-        $navHtml = '';
         foreach ($lines as $line) {
             if (preg_match('/^\*\w*(.*)/', $line, $matches)) {
                 $sectionTitle = trim($matches[1]);
@@ -40,14 +38,12 @@ class Guidance
         }
         
         return [
-            'navHtml' => $navHtml,
-            'bodyHtml' => $html,
             'sections' => $sectionArray,
         ];
     }
     
     /**
-     * Create HTML for a single section of guidance text
+     * Transform markdown of a guidance section into HTML
      * 
      * @param string $filename
      * @param string $sectionId
@@ -57,10 +53,17 @@ class Guidance
     function processSection($filename, $sectionId)
     {
         $md = Markdown::defaultTransform(file_get_contents(self::GUIDANCE_MARKDOWN_FOLDER . '/' . $filename));
-        $retval = '';
-        $retval.= PHP_EOL . '<article id="topic-' . $sectionId . '">';
-        $retval.= preg_replace('/<a href="\/help\/#topic-(.+)">(.+)<\/a>/', '<a href="/help/#topic-${1}" class="js-guidance" data-journey="stageprompt.lpa:help:${1}">${2}</a>', $md);
-        $retval.= PHP_EOL . '</article>';
-        return $retval;
+
+        $html = '<article id="topic-' . $sectionId . '">';
+        
+        $html .= preg_replace(
+                    '/<a href="\/help\/#topic-(.+)">(.+)<\/a>/',
+                    '<a href="/help/#topic-${1}" class="js-guidance" data-journey="stageprompt.lpa:help:${1}">${2}</a>',
+                    $md
+                 );
+                
+        $html .= '</article>';
+        
+        return $html;
     }
 }
