a2603cb3ff6346b5d4ab3659c4a3781c29e8ab2a
Neil Smith <github@nsmith.me.uk>
2015-03-13 12:20:22 +0000

    Decrypt JSON as it comes out of the queue

--- service-pdf/src/Opg/Lpa/Pdf/Worker/ResqueWorker.php
+++ service-pdf/src/Opg/Lpa/Pdf/Worker/ResqueWorker.php
@@ -1,6 +1,10 @@
 <?php
-
 namespace Opg\Lpa\Pdf\Worker;
+
+use Zend\Crypt\BlockCipher;
+use Zend\Crypt\Symmetric\Exception\InvalidArgumentException as CryptInvalidArgumentException;
+
+use Opg\Lpa\Pdf\Config\Config;
 
 class ResqueWorker extends Worker {
 
@@ -18,9 +22,29 @@ class ResqueWorker extends Worker {
             throw new \Exception('Missing field: type');
         }
 
-        echo "Running job\n";
+        //---------------------------------------------
+        // Decrypt the JSON...
 
-        $this->run( $this->args['docId'], $this->args['type'], $this->args['lpa'] );
+        $config = Config::getInstance()['pdf']['encryption'];
+
+        if( !is_string($config['keys']['queue']) || strlen($config['keys']['queue']) != 32 ){
+            throw new CryptInvalidArgumentException('Invalid encryption key');
+        }
+
+        //---
+
+        // We use AES encryption with Cipher-block chaining (CBC); via PHPs mcrypt extension
+        $blockCipher = BlockCipher::factory('mcrypt', $config['options']);
+
+        // Set the secret key
+        $blockCipher->setKey( $config['keys']['queue'] );
+
+        $json = $blockCipher->decrypt( $this->args['lpa'] );
+
+        //---------------------------------------------
+        // Run the job...
+
+        $this->run( $this->args['docId'], $this->args['type'], $json );
 
     } // function
 
