cd5fd6a380fbcf6c8327d03a8e55c6af0f283533
Jianzhong Yu <github@jianzhong.co.uk>
2015-03-20 11:31:19 +0000

    split dob into day, month and year fields

--- service-front/module/Application/src/Application/Form/Lpa/AttorneyForm.php
+++ service-front/module/Application/src/Application/Form/Lpa/AttorneyForm.php
@@ -2,10 +2,14 @@
 namespace Application\Form\Lpa;
 
 use Opg\Lpa\DataModel\Lpa\Document\Attorneys\Human;
+use Zend\Validator;
 
 class AttorneyForm extends AbstractActorForm
 {
-    protected $formElements = [
+    protected $formElements;    
+    public function __construct ($formName = 'primary-attorney')
+    {
+        $this->formElements  = [
             'name-title' => [
                     'type' => 'Text',
             ],
@@ -18,8 +22,59 @@ class AttorneyForm extends AbstractActorForm
             'otherNames' => [
                     'type' => 'Text',
             ],
-            'dob-date' => [
-                    'type' => 'Date',
+            'dob-date-day' => [
+                    'type' => 'Text',
+                    'required' => true,
+                    'filters' => [
+                            ['name' => 'Zend\Filter\Int'],
+                    ],
+                    'validators' => [
+                        [
+                            'name'    => 'Between',
+                            'options' => [
+                                'min' => 1, 'max' => 31,
+                                'messages' => [
+                                    Validator\Between::NOT_BETWEEN => "must be between %min% and %max%",
+                                ],
+                            ],
+                        ],
+                    ],
+            ],
+            'dob-date-month' => [
+                    'type' => 'Text',
+                    'required' => true,
+                    'filters' => [
+                            ['name' => 'Zend\Filter\Int'],
+                    ],
+                    'validators' => [
+                        [
+                            'name'    => 'Between',
+                            'options' => [
+                                'min' => 1, 'max' => 12,
+                                'messages' => [
+                                    Validator\Between::NOT_BETWEEN => "must be between %min% and %max%",
+                                ],
+                            ],
+                        ],
+                    ],
+            ],
+            'dob-date-year' => [
+                    'type' => 'Text',
+                    'required' => true,
+                    'filters' => [
+                            ['name' => 'Zend\Filter\Int'],
+                    ],
+                    'validators' => [
+                        [
+                            'name'    => 'Between',
+                            'options' => [
+                                'min' => (int)date('Y') - 150, 'max' => (int)date('Y'),
+                                'messages' => [
+                                    Validator\Between::NOT_BETWEEN => "must be between %min% and %max%",
+                                ],
+                            ],
+                        ],
+                    ],
             ],
             'email-address' => [
                     'type' => 'Email',
@@ -35,16 +90,12 @@ class AttorneyForm extends AbstractActorForm
             ],
             'address-postcode' => [
                     'type' => 'Text',
-                    
             ],
             'submit' => [
                     'type' => 'Zend\Form\Element\Submit',
             ],
     ];
     
-    public function __construct ($formName = 'primary-attorney')
-    {
-        
         parent::__construct($formName);
         
     }
