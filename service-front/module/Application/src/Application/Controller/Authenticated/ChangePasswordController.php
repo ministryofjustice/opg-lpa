e08c6dde606210bccd8e35137429bb67e20ced6c
Neil Smith <github@nsmith.me.uk>
2015-03-23 10:40:58 +0000

    Added Change Password

--- service-front/module/Application/src/Application/Controller/Authenticated/ChangePasswordController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/ChangePasswordController.php
@@ -5,10 +5,75 @@ namespace Application\Controller\Authenticated;
 use Zend\View\Model\ViewModel;
 use Application\Controller\AbstractAuthenticatedController;
 
+use Application\Form\User\ChangePassword as ChangePasswordForm;
+
 class ChangePasswordController extends AbstractAuthenticatedController
 {
-    public function indexAction()
-    {
-        return new ViewModel();
+    public function indexAction(){
+
+        $currentAddress = (string)$this->getUserDetails()->email;
+
+        //----------------------
+
+        $form = new ChangePasswordForm();
+        $form->setAttribute( 'action', $this->url()->fromRoute('user/change-password') );
+
+        $error = null;
+
+        //----------------------
+
+        // This form needs to check the user's current password,
+        // thus we pass it the Authentication Service
+
+        $authentication =   $this->getServiceLocator()->get('AuthenticationService');
+        $adapter =          $this->getServiceLocator()->get('AuthenticationAdapter');
+
+        // Pass the user's current email address...
+        $adapter->setEmail( $currentAddress );
+
+        $authentication->setAdapter( $adapter );
+
+        $form->setAuthenticationService( $authentication );
+
+        //----------------------
+
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+
+            //---
+
+            $form->setData($request->getPost());
+
+            //---
+
+            if ($form->isValid()) {
+
+                $service = $this->getServiceLocator()->get('AboutYouDetails');
+
+                $result = $service->updatePassword( $form );
+
+                //---
+
+                if( $result === true ){
+
+                    $this->flashMessenger()->addSuccessMessage('Your new password has been saved. Please remember to use this new password to sign in from now on.');
+
+                    return $this->redirect()->toRoute( 'user/about-you' );
+
+                } else {
+                    $error = $result;
+                }
+
+            } // if
+
+        } // if
+
+        //----------------------
+
+        $pageTitle = 'Change your password';
+
+        return new ViewModel( compact( 'form', 'error', 'pageTitle' ) );
+
     }
 }
---
+++ service-front/module/Application/src/Application/Form/User/ChangePassword.php
@@ -0,0 +1,88 @@
+<?php
+namespace Application\Form\User;
+
+use Zend\Validator;
+
+use Application\Model\Service\Authentication\AuthenticationService;
+use Zend\Authentication\Exception\InvalidArgumentException;
+
+class ChangePassword extends SetPassword {
+
+    /**
+     * @var AuthenticationService
+     */
+    private $authenticationService;
+
+    //---
+
+    public function __construct( $formName = 'change-password' ){
+
+        parent::__construct($formName);
+
+        //---
+
+        $this->add(array(
+            'name' => 'password_current',
+            'type' => 'Password',
+        ));
+
+        //--------------------------------
+
+        $inputFilter = $this->getInputFilter();
+
+        //---
+
+        $inputFilter->add(array(
+            'name'     => 'password_current',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name'    => 'Callback',
+                    'options' => array(
+                        'callback' => [ $this, 'validatePassword' ],
+                        'messages' => [
+                            Validator\Callback::INVALID_VALUE => 'is incorrect',
+                        ],
+                    ),
+                ),
+            ),
+        ));
+
+        //---
+
+        $this->setInputFilter( $inputFilter );
+
+    } // function
+
+
+    /**
+     * Set the Authentication Service used to validate the user's password.
+     *
+     * @param AuthenticationService $authenticationService
+     */
+    public function setAuthenticationService( AuthenticationService $authenticationService ){
+        $this->authenticationService = $authenticationService;
+    }
+
+    /**
+     * Validates if a given password is correct.
+     *
+     * The email address MUST already have been set.
+     *
+     * @param $value string The value from the password text field.
+     * @return bool
+     */
+    public function validatePassword( $value ){
+
+        if( !( $this->authenticationService instanceof AuthenticationService ) ){
+            throw new InvalidArgumentException('AuthenticationService not set');
+        }
+
+        // Set the password in teh adapter.
+        $this->authenticationService->getAdapter()->setPassword( $value );
+
+        return $this->authenticationService->verify()->isValid();
+
+    }
+
+} // class
