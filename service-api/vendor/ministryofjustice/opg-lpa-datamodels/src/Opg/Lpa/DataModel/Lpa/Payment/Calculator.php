c18b3aabe2f382ea868b950e2b152695a990184e
Jianzhong Yu <github@jianzhong.co.uk>
2015-03-30 12:44:28 +0100

    update opg-lpa-datamodel with StateChecker fix and Calculator added

---
+++ service-api/vendor/ministryofjustice/opg-lpa-datamodels/src/Opg/Lpa/DataModel/Lpa/Payment/Calculator.php
@@ -0,0 +1,43 @@
+<?php
+namespace Opg\Lpa\DataModel\Lpa\Payment;
+
+use Opg\Lpa\DataModel\Lpa\Lpa;
+
+class Calculator
+{
+    const STANDARD_FEE = 110;
+    
+    /**
+     * Calculate LPA payment amount
+     * 
+     * @param Lpa $lpa
+     * @return NULL|\Opg\Lpa\DataModel\Lpa\Payment
+     */
+    static public function calculate(Lpa $lpa)
+    {
+        if(!($lpa->payment instanceof Payment)) return null;
+        
+        if($lpa->payment->reducedFeeUniversalCredit) {
+            $amount = null;
+        }
+        else {
+            if(($lpa->payment->reducedFeeReceivesBenefits) && ($lpa->payment->reducedFeeAwardedDamages)) {
+                $amount = (float) 0;
+            }
+            elseif($lpa->payment->reducedFeeLowIncome) {
+                $amount = (float) self::STANDARD_FEE/2;
+            }
+            else {
+                $amount = (float) self::STANDARD_FEE;
+            }
+            
+            if($lpa->repeatCaseNumber != null) {
+                $amount = $amount/2;
+            }
+        }
+        
+        $lpa->payment->amount = $amount;
+        
+        return $lpa->payment;
+    }
+}
