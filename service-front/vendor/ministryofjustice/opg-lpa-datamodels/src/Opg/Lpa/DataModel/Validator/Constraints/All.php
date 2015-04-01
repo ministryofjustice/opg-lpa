87ab8c242e08ffd0a1ee494591b651d25b75633d
Neil Smith <github@nsmith.me.uk>
2015-04-01 13:56:24 +0100

    Composer update

--- service-front/vendor/ministryofjustice/opg-lpa-datamodels/src/Opg/Lpa/DataModel/Validator/Constraints/All.php
+++ service-front/vendor/ministryofjustice/opg-lpa-datamodels/src/Opg/Lpa/DataModel/Validator/Constraints/All.php
@@ -1,14 +1,4 @@
 <?php
-
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Opg\Lpa\DataModel\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraints as SymfonyConstraints;
@@ -23,7 +13,6 @@ use Symfony\Component\Validator\Constraints as SymfonyConstraints;
  */
 class All extends SymfonyConstraints\All
 {
-    use ValidatorPathTrait;
 
     public function getDefaultOption()
     {
