fdab87900d4e9c1ec49234f56b29e6cb8a21377b
Jianzhong Yu <github@jianzhong.co.uk>
2015-04-08 16:14:48 +0100

    rewrite getSeedDetails() and add flattenData() into AbstractLpaController

--- service-front/module/Application/src/Application/Controller/AbstractLpaController.php
+++ service-front/module/Application/src/Application/Controller/AbstractLpaController.php
@@ -145,25 +145,148 @@ abstract class AbstractLpaController extends AbstractAuthenticatedController imp
      * Return clone source LPA details from session container, or from the api 
      * if not found in the session container. 
      * 
+     * @param bool $trustOnly - when true, only return trust corporation details
+     * 
      * @return Array|Null;
      */
-    protected function getSeedDetails()
+    protected function getSeedDetails($trustOnly=false)
     {
         if(!($this->lpa instanceof Lpa) || ($this->lpa->seed === null)) return null;
         
         $seedId = $this->lpa->seed;
         $cloneContainer = new Container('clone');
-        
+        unset($cloneContainer->$seedId);
         if(!$cloneContainer->offsetExists($seedId)) {
-            $seedDetails = $this->getLpaApplicationService()->getSeedDetails($this->lpa->id);
-            if($seedDetails) {
-                $cloneContainer->$seedId = $seedDetails;
+            
+            // get seed data from the API
+            $seedData = $this->getLpaApplicationService()->getSeedDetails($this->lpa->id);
+            if(!$seedData) {
+                return null;
+            }
+            
+            // save seed data into session container
+            $cloneContainer->$seedId = $seedData;
+            
+        }
+        
+        $seedData = $cloneContainer->$seedId;
+        
+        // ordering the data
+        $seedDetails = [];
+        foreach($seedData as $type => $actorData) {
+            if($trustOnly) {
+                switch($type) {
+                    case 'primaryAttorneys':
+                        foreach($actorData as $singleActorData) {
+                            if($singleActorData['type'] == 'trust') {
+                                $seedDetails[] = [
+                                        'label' => $singleActorData['name'] . ' (was a Primary Attorney)',
+                                        'data' => $singleActorData,
+                                ];
+                                
+                                // only one trust can be in an LPA
+                                return $seedDetails;
+                            }
+                        }
+                        break;
+                    case 'replacementAttorneys':
+                        foreach($actorData as $singleActorData) {
+                            if($singleActorData['type'] == 'trust') {
+                                $seedDetails[] = [
+                                        'label' => $singleActorData['name'] . ' (was a Replacement Attorney)',
+                                        'data' => $singleActorData,
+                                ];
+                                
+                                // only one trust can be in an LPA
+                                return $seedDetails;
+                            }
+                        }
+                        break;
+                }
             }
             else {
-                return null;
+                switch($type) {
+                    case 'donor':
+                        $seedDetails[] = [
+                        'label' => $actorData['name']['first'].' '.$actorData['name']['last'] . ' (was a Donor)',
+                        'data' => $actorData,
+                        ];
+                        break;
+                    case 'correspondent':
+                        if($actorData['who'] == 'other') {
+                            $seedDetails[] = [
+                            'label' => $actorData['name']['first'].' '.$actorData['name']['last'] . ' (was a Correspondent)',
+                            'data' => $actorData,
+                            ];
+                        }
+                        break;
+                    case 'certificateProvider':
+                        $seedDetails[] = [
+                        'label' => $actorData['name']['first'].' '.$actorData['name']['last'] . ' (was a Certificate Provider)',
+                        'data' => $actorData,
+                        ];
+                        break;
+                    case 'primaryAttorneys':
+                        foreach($actorData as $singleActorData) {
+                            if($singleActorData['type'] == 'trust') continue;
+                            $seedDetails[] = [
+                                    'label' => $singleActorData['name']['first'].' '.$singleActorData['name']['last'] . ' (was a Primary Attorney)',
+                                    'data' => $singleActorData,
+                            ];
+                        }
+                        break;
+                    case 'replacementAttorneys':
+                        foreach($actorData as $singleActorData) {
+                            if($singleActorData['type'] == 'trust') continue;
+                            $seedDetails[] = [
+                                    'label' => $singleActorData['name']['first'].' '.$singleActorData['name']['last'] . ' (was a Replacement Attorney)',
+                                    'data' => $singleActorData,
+                            ];
+                        }
+                        break;
+                    case 'peopleToNotify':
+                        foreach($actorData as $singleActorData) {
+                            $seedDetails[] = [
+                                    'label' => $singleActorData['name']['first'].' '.$singleActorData['name']['last'] . ' (was a person to be notified)',
+                                    'data' => $singleActorData,
+                            ];
+                        }
+                        break;
+                    default: break;
+                }
             }
         }
         
-        return $cloneContainer->$seedId;
+        return $seedDetails;
+    }
+    
+    /**
+     * Convert model/seed data for populating into form
+     * 
+     * @param array $modelData - eg. [name=>[title=>'Mr', first=>'John', last=>'Smith']]
+     * @return array - eg [name-title=>'Mr', name-first=>'John', name-last=>'Smith']
+     */
+    protected function flattenData($modelData)
+    {
+        $formData = [];
+        foreach($modelData as $l1 => $l2) {
+            if(is_array($l2)) {
+                foreach($l2 as $name=>$l3) {
+                    if($l1=='dob') {
+                        $formData['dob-date-day'] = (new \DateTime($l3))->format('d');
+                        $formData['dob-date-month'] = (new \DateTime($l3))->format('m');
+                        $formData['dob-date-year'] = (new \DateTime($l3))->format('Y');
+                    }
+                    else {
+                        $formData[$l1.'-'.$name] = $l3;
+                    }
+                }
+            }
+            else {
+                $formData[$l1] = $l2;
+            }
+        }
+        
+        return $formData;
     }
 }
