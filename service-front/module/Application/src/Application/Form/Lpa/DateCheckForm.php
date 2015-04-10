0f8f5adea84d2b89171fd7bdb55f7732a2c8114d
Chris Moreton <chris@netsensia.com>
2015-04-10 15:32:51 +0100

    Required validation on date check form

--- service-front/module/Application/src/Application/Form/Lpa/DateCheckForm.php
+++ service-front/module/Application/src/Application/Form/Lpa/DateCheckForm.php
@@ -3,59 +3,48 @@ namespace Application\Form\Lpa;
 
 use Opg\Lpa\DataModel\Lpa\Lpa;
 use Zend\Validator\NotEmpty;
+use Zend\Validator\StringLength;
 
 class DateCheckForm extends AbstractForm
 {
-    /**
-     * @var Lpa $lpa
-     */
-    private $lpa;
     
     protected $formElements = [
-        'sign-date-donor' => [
-            'type' => 'Zend\Form\Element'
-        ],
-        'sign-date-certificate-provider' => [
-            'type' => 'Zend\Form\Element'
-        ],
+        'sign-date-donor' => [],
+        'sign-date-certificate-provider' => [],
         'submit' => [
             'type' => 'Zend\Form\Element\Submit',
         ],
     ];
     
-    public function __construct (Lpa $lpa, $formName = 'type-form')
-    {
-        $this->lpa = $lpa;
-        
-        $numAttorneys = count($lpa->get('document')->get('primaryAttorneys'));
-        for ($i=0; $i<$numAttorneys; $i++) {
-            $this->formElements['sign-date-attorney-' . $i] = [
-                'type' => 'Zend\Form\Element'
-            ];
-        }
-        
-        $dateValidator = [
+    private $dateValidators = [
+        [
             'name' => 'Date',
             'options' => [
                 'format' => 'd/m/Y',
             ],
-        ];
-        
-        $notEmptyValidator = [
-            'name'    => 'NotEmpty',
+        ],
+        [
+            'name' => 'StringLength',
             'options' => [
-                'messages' => [
-                    NotEmpty::IS_EMPTY => 'Please enter dates for all the people on the form.',
-                ],
+                'min' => 10,
+                'max' => 10,
             ],
-        ];
+        ]
+    ];
+    
+    public function __construct (Lpa $lpa, $formName = 'type-form')
+    {
+        $numAttorneys = count($lpa->get('document')->get('primaryAttorneys'));
         
+        for ($i=0; $i<$numAttorneys; $i++) {
+            $this->formElements['sign-date-attorney-' . $i] = [];
+        }
+
         foreach ($this->formElements as $key => &$element) {
             if ($key != 'submit') {
-                $element['validators'] = [
-                    $dateValidator,
-                    $notEmptyValidator
-                ];
+                $element['type'] = 'Zend\Form\Element';
+                $element['required'] = true;
+                $element['validators'] = $this->dateValidators;
             }
         }
         
