d4a351cdee7a53839b3963920a039914ee7fb51e
Jianzhong Yu <github@jianzhong.co.uk>
2015-04-14 10:42:27 +0100

    reset session clone data when cloning a new lpa or loading a lpa.

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/IndexController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/IndexController.php
@@ -8,6 +8,11 @@ class IndexController extends AbstractLpaController
     
     public function indexAction()
     {
+        $seedId = $this->getLpa()->seed;
+        if($seedId) {
+            $this->resetSessionCloneData($seedId);
+        }
+        
         $destinationRoute = $this->getFlowChecker()->backToForm('lpa/view-docs');
         $this->redirect()->toRoute($destinationRoute, ['lpa-id'=>$this->getLpa()->id]);
     }
