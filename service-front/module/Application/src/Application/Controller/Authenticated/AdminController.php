b1c631913af62ac5b4bc95deb76fc7f6a1ecfd2c
Chris Moreton <chris@netsensia.com>
2015-04-28 14:34:34 +0100

    Valid admin user check in admin controller

--- service-front/module/Application/src/Application/Controller/Authenticated/AdminController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/AdminController.php
@@ -7,8 +7,32 @@ use Application\Controller\AbstractAuthenticatedController;
 
 class AdminController extends AbstractAuthenticatedController
 {
+    private function adminCheck()
+    {
+        $userEmail = (string)$this->getUserDetails()->email;
+        
+        $adminAccounts = $this->getServiceLocator()->get('config')['admin']['accounts'];
+        
+        $isAdmin = false;
+        foreach ($adminAccounts as $authorisedEmail) {
+            if ($authorisedEmail == $userEmail) {
+                $isAdmin = true;
+                break;
+            }
+        }
+        
+        if (!$isAdmin) {
+            $this->getResponse()->setStatusCode(401);
+            $this->redirect()->toRoute('home');
+        }
+        
+        return true;
+    }
+    
     public function statsAction()
     {
+        $this->adminCheck();
+        
         return new ViewModel();
     }
 }
