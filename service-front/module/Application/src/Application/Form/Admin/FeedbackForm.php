afc6b4485f29b466bf0b9106a766edeb90ce8070
Chris Moreton <chris@netsensia.com>
2015-04-29 16:49:59 +0100

    Add system admin message form

---
+++ service-front/module/Application/src/Application/Form/Admin/FeedbackForm.php
@@ -0,0 +1,62 @@
+<?php
+namespace Application\Form\General;
+
+use Zend\Validator\NotEmpty;
+use Zend\Validator\StringLength;
+/**
+ * For an admin to set the system message
+ *
+ * Class SystemMessageForm
+ * @package Application\Form\General
+ */
+class SystemMessageForm extends AbstractForm {
+
+    const MAX_MESSAGE_LENGTH = 2000;
+    
+    public function __construct( $formName = 'admin-system-message' ){
+
+        parent::__construct( $formName );
+
+        //--- Form elements
+
+        $this->add(array(
+            'name' => 'message',
+            'type' => 'Textarea',
+        ));
+
+        //--------------------------------
+
+        $inputFilter = $this->getInputFilter();
+
+        $inputFilter->add([
+            'name'     => 'message',
+            'filters'  => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                [
+                    'name'    => 'NotEmpty',
+                    'options' => [
+                        'messages' => [
+                            NotEmpty::IS_EMPTY => 'No message was entered.',
+                        ],
+                    ],
+                ],
+                [
+                    'name'    => 'StringLength',
+                    'options' => [
+                        'max' => self::MAX_FEEDBACK_LENGTH,
+                        'messages' => [
+                             StringLength::TOO_LONG => 'Please limit your feedback to ' . self::MAX_MESSAGE_LENGTH . ' chars.',
+                         ],
+                    ],
+                ],
+            ),
+        ]);
+        
+        $this->setInputFilter( $inputFilter );
+
+    } // function
+
+} // class
