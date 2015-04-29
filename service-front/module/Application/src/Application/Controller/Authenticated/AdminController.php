34a80fc2a3eafe3e41be176b7f6c6f43e90f572d
Chris Moreton <chris@netsensia.com>
2015-04-29 10:07:28 +0100

    Move admin checks to onDispatch method

--- service-front/module/Application/src/Application/Controller/Authenticated/AdminController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/AdminController.php
@@ -4,10 +4,17 @@ namespace Application\Controller\Authenticated;
 
 use Zend\View\Model\ViewModel;
 use Application\Controller\AbstractAuthenticatedController;
+use Zend\Mvc\MvcEvent;
 
 class AdminController extends AbstractAuthenticatedController
 {
-    private function adminCheck()
+    /**
+     * Ensure user is allowed to access admin functions
+     *
+     * @param  MvcEvent $e
+     * @return mixed
+     */
+    public function onDispatch(MvcEvent $event)
     {
         $userEmail = (string)$this->getUserDetails()->email;
         
