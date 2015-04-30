3d6a4b6922037c110132de020821060741f5da69
Jianzhong Yu <github@jianzhong.co.uk>
2015-04-30 12:41:28 +0100

    add fee reduction form, controller, view, accordion bar item.

---
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/FeeReductionController.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller\Authenticated\Lpa;
+
+use Application\Controller\AbstractLpaController;
+use Zend\View\Model\ViewModel;
+use Application\Model\Service\Lpa\Metadata;
+
+class FeeReductionController extends AbstractLpaController
+{
+    
+    protected $contentHeader = 'registration-partial.phtml';
+    
+    public function indexAction()
+    {
+        $form = $this->getServiceLocator()->get('FormElementManager')->get('Application\Form\Lpa\FeeReductionForm');
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            
+            // set data for validation
+            $form->setData($postData);
+            
+            if($form->isValid()) {
+                
+                $lpaId = $this->getLpa()->id;
+                $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+                
+                // persist data
+                $this->getServiceLocator()->get('Metadata')->setApplyForFeeReduction($this->getLpa(), (bool)$form->getData()['applyForFeeReduction']);
+                
+                if($form->getData()['applyForFeeReduction'] === '1') {
+                    $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+                }
+                elseif($form->getData()['applyForFeeReduction'] === '0')  {
+                    $this->redirect()->toRoute('lpa/payment', ['lpa-id' => $lpaId]);
+                }
+            }
+        }
+        else {
+            if(array_key_exists(Metadata::APPLY_FOR_FEE_REDUCTION, $this->getLpa()->metadata)) {
+                $form->bind(['applyForFeeReduction' => $this->getLpa()->metadata[Metadata::APPLY_FOR_FEE_REDUCTION]]);
+            }
+        }
+        
+        return new ViewModel([
+                'form'=>$form, 
+        ]);
+    }
+}
