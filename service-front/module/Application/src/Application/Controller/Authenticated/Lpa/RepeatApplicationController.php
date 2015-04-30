0c576f586c53982cf12dc8f9b72335e4118e930f
Jianzhong Yu <github@jianzhong.co.uk>
2015-04-30 12:40:27 +0100

    add repeat application form, controller, view, accordion bar item.

---
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/RepeatApplicationController.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller\Authenticated\Lpa;
+
+use Application\Controller\AbstractLpaController;
+use Zend\View\Model\ViewModel;
+use Application\Model\Service\Lpa\Metadata;
+
+class RepeatApplicationController extends AbstractLpaController
+{
+    
+    protected $contentHeader = 'registration-partial.phtml';
+    
+    public function indexAction()
+    {
+        $form = $this->getServiceLocator()->get('FormElementManager')->get('Application\Form\Lpa\RepeatApplicationForm');
+        
+        if($this->request->isPost()) {
+            $postData = $this->request->getPost();
+            
+            // set data for validation
+            $form->setData($postData);
+            
+            if($form->isValid()) {
+                
+                $lpaId = $this->getLpa()->id;
+                $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
+                
+                // persist data
+                if($form->getData()['isRepeatApplication'] == 'is-repeat') {
+                    if(!$this->getLpaApplicationService()->setRepeatCaseNumber($this->getLpa()->id, $form->getData()['repeatCaseNumber'])) {
+                        throw new \RuntimeException('API client failed to set repeat case number for id: '.$lpaId);
+                    }
+                }
+                
+                // set metadata
+                $this->getServiceLocator()->get('Metadata')->setRepeatApplicationConfirmed($this->getLpa());
+                
+                $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
+            }
+        }
+        else {
+            if(array_key_exists(Metadata::REPEAT_APPLICATION_CONFIRMED, $this->getLpa()->metadata)) {
+                $form->bind([
+                        'isRepeatApplication' => ($this->getLpa()->repeatCaseNumber === null)?'is-new':'is-repeat',
+                        'repeatCaseNumber'    => $this->getLpa()->repeatCaseNumber,
+                        
+                ]);
+            }
+        }
+        
+        return new ViewModel([
+                'form'=>$form,
+        ]);
+    }
+}
---
+++ service-front/module/Application/src/Application/Form/Lpa/RepeatApplicationForm.php
@@ -0,0 +1,62 @@
+<?php
+namespace Application\Form\Lpa;
+
+use Opg\Lpa\DataModel\Lpa\Lpa;
+
+class RepeatApplicationForm extends AbstractForm
+{
+    protected $formElements = [
+            'isRepeatApplication' => [
+                    'type'      => 'Zend\Form\Element\Radio',
+                    'required'  => true,
+                    'options'   => [
+                            'value_options' => [
+                                    'is-new' => [
+                                            'value' => 'is-new',
+                                    ],
+                                    'is-repeat' => [
+                                            'value' => 'is-repeat',
+                                    ],
+                            ],
+                    ],
+            ],
+            'repeatCaseNumber' => [
+                    'type' => 'Text',
+            ],
+            'submit' => [
+                    'type' => 'Zend\Form\Element\Submit',
+            ],
+    ];
+    
+    public function init()
+    {
+        $this->setName('repeat-application');
+        parent::init();
+    }
+    
+   /**
+    * Validate form input data through model validators.
+    * 
+    * @return [isValid => bool, messages => [<formElementName> => string, ..]]
+    */
+    public function validateByModel()
+    {
+        if($this->data['isRepeatApplication'] == 'is-new') {
+            return ['isValid'=>true, 'messages' => []];
+        }
+        elseif($this->data['isRepeatApplication'] == 'is-repeat') {
+            $lpa = new Lpa(['repeatCaseNumber' => (int) $this->data['repeatCaseNumber']]);
+            $validation = $lpa->validate(['repeatCaseNumber']);
+        
+            if(count($validation) == 0) {
+                return ['isValid'=>true, 'messages' => []];
+            }
+            else {
+                return [
+                        'isValid'=>false,
+                        'messages' => $this->modelValidationMessageConverter($validation),
+                ];
+            }
+        }
+    }
+}
