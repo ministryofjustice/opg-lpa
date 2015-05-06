ace1e81b0473bd896ec72b3ae8f2cff93528863b
Jianzhong Yu <github@jianzhong.co.uk>
2015-05-06 16:40:46 +0100

    bug fix: isCorrespondentEditAccessible depends on lpaHasApplicant instead of lpaHasCorrespondent

--- service-front/module/Application/src/Application/Model/FormFlowChecker.php
+++ service-front/module/Application/src/Application/Model/FormFlowChecker.php
@@ -621,7 +621,7 @@ class FormFlowChecker extends StateChecker
 
     private function isCorrespondentEditAccessible()
     {
-        if($this->lpaHasCorrespondent()) {
+        if($this->lpaHasApplicant()) {
             return true;
         }
         else {
