3d8df811063f7cdb9f2dd0a87162d9d6b4aab899
Neil Smith <github@nsmith.me.uk>
2015-05-21 11:58:03 +0100

    Added Redis hostname into dynamic config

--- service-front/config/autoload/global.php
+++ service-front/config/autoload/global.php
@@ -103,4 +103,28 @@ return array(
     
     'sendFeedbackEmailTo' => 'LPADigitalFeedback@PublicGuardian.gsi.gov.uk',
 
+
+    'v1proxy' => [
+
+        // Should we cache the fact there are no v1 LPAs in a user's account.
+        // Should be TRUE in production.
+        'cache-no-laps' => false,
+
+        // Should we allow new v1 LPAs to be created.
+        // Should be FALSE in production.
+        'allow-v1-laps-to-be-created' => true,
+
+        'redis' => [
+            // This data should persist for the length v1 is alive (6 months predicted).
+            'ttl' => (86400 * 365), // 365 days; leave room for delays.
+            'namespace' => 'v1proxy',
+            'server' => [
+                'host' => 'redisfront.local',
+                'port' => 6379
+            ],
+            'database' => 2, // WARNING: this has to be defined last otherwise Zend\Cache has a hissy fit.
+        ],
+
+    ],
+
 );
