1cd1035d950a18fe92e2e3ed4274dbd7b8b93a6a
Jianzhong Yu <github@jianzhong.co.uk>
2015-06-03 18:33:17 +0100

    add handling various situations for generating CS2 for replacement step in and act

--- service-pdf/src/Opg/Lpa/Pdf/Service/Forms/Lp1.php
+++ service-pdf/src/Opg/Lpa/Pdf/Service/Forms/Lp1.php
@@ -121,36 +121,66 @@ abstract class Lp1 extends AbstractForm
         }
         
         // generate a CS2 page if replacement attorneys decision differ to standard arrangement.
-        if((count($this->lpa->document->replacementAttorneys) > 1) && 
-            ($this->lpa->document->replacementAttorneyDecisions->how != ReplacementAttorneyDecisions::LPA_DECISION_HOW_JOINTLY)) {
-            
-            $content = "";
-            switch($this->lpa->document->replacementAttorneyDecisions->when) {
-                case ReplacementAttorneyDecisions::LPA_DECISION_WHEN_FIRST:
-                    $content = "Replacement attorneys are to step in when one of original attorneys can no longer act\r\n";
-                    break;
-                case ReplacementAttorneyDecisions::LPA_DECISION_WHEN_LAST:
-                    $content = "Replacement attorneys are to step in only when all original attorneys can no longer act\r\n";
-                    break;
-                case ReplacementAttorneyDecisions::LPA_DECISION_WHEN_DEPENDS:
-                    $content = "Replacement attorneys are to step in for some decisons, as below\r\n" . $this->lpa->document->replacementAttorneyDecisions->whenDetails;
-                    break;
-            }
-            
+        $content = "";
+        if( ((count($this->lpa->document->primaryAttorneys) == 1) 
+                || ((count($this->lpa->document->primaryAttorneys) > 1) && ($this->lpa->document->primaryAttorneyDecisions->how == PrimaryAttorneyDecisions::LPA_DECISION_HOW_JOINTLY)))  
+            && (count($this->lpa->document->replacementAttorneys) > 1)) {
+                
             switch($this->lpa->document->replacementAttorneyDecisions->how) {
                 case ReplacementAttorneyDecisions::LPA_DECISION_HOW_JOINTLY_AND_SEVERALLY:
-                    $content .= "Replacement attorneys are to act jointly and severally\r\n";
+                    $content = "Replacement attorneys are to act jointly and severally\r\n";
                     break;
                 case ReplacementAttorneyDecisions::LPA_DECISION_HOW_DEPENDS:
-                    $content .= "Replacement attorneys are to act joint for some decisions, joint and several for other decisions, as below\r\n" . $this->lpa->document->replacementAttorneyDecisions->howDetails . "\r\n";
+                    $content = "Replacement attorneys are to act jointly for some decisions and jointly and severally for others, as below:\r\n" . $this->lpa->document->replacementAttorneyDecisions->howDetails . "\r\n";
+                    break;
                 case ReplacementAttorneyDecisions::LPA_DECISION_HOW_JOINTLY:
+                    $content = "Replacement attorneys are to act jointly\r\n";
                     // this is default
                     break;
             }
-            
+        }
+        elseif((count($this->lpa->document->primaryAttorneys) > 1) && ($this->lpa->document->primaryAttorneyDecisions->how == PrimaryAttorneyDecisions::LPA_DECISION_HOW_JOINTLY_AND_SEVERALLY)) {
+            if(count($this->lpa->document->replacementAttorneys) == 1) {
+                
+                switch($this->lpa->document->replacementAttorneyDecisions->when) {
+                    case ReplacementAttorneyDecisions::LPA_DECISION_WHEN_FIRST:
+                        $content = "Replacement attorney to step in as soon as one original attorney can no longer act\r\n";
+                        break;
+                    case ReplacementAttorneyDecisions::LPA_DECISION_WHEN_LAST:
+                        $content = "Replacement attorney to step in only when none of the original attorneys can act\r\n";
+                        break;
+                    case ReplacementAttorneyDecisions::LPA_DECISION_WHEN_DEPENDS:
+                        $content = "How replacement attorneys will replace the original attorneys:\r\n" . $this->lpa->document->replacementAttorneyDecisions->whenDetails;
+                        break;
+                }
+            }
+            elseif(count($this->lpa->document->replacementAttorneys) > 1) { 
+                if($this->lpa->document->replacementAttorneyDecisions->when == ReplacementAttorneyDecisions::LPA_DECISION_WHEN_LAST) {
+                    $content = "Replacement attorneys to step in only when none of the original attorneys can act\r\n";
+                    
+                    switch($this->lpa->document->replacementAttorneyDecisions->how) {
+                        case ReplacementAttorneyDecisions::LPA_DECISION_HOW_JOINTLY_AND_SEVERALLY:
+                            $content .= "Replacement attorneys are to act jointly and severally\r\n";
+                            break;
+                        case ReplacementAttorneyDecisions::LPA_DECISION_HOW_DEPENDS:
+                            $content .= "Replacement attorneys are to act joint for some decisions, joint and several for other decisions, as below:\r\n" . $this->lpa->document->replacementAttorneyDecisions->howDetails . "\r\n";
+                            break;
+                        case ReplacementAttorneyDecisions::LPA_DECISION_HOW_JOINTLY:
+                            $content .= "Replacement attorneys are to act jointly\r\n";
+                            break;
+                    }
+                }
+                elseif($this->lpa->document->replacementAttorneyDecisions->when == ReplacementAttorneyDecisions::LPA_DECISION_WHEN_DEPENDS) {
+                    $content = "How replacement attorneys will replace the original attorneys:\r\n". $this->lpa->document->replacementAttorneyDecisions->whenDetails;
+                }
+            } // endif
+        }
+        
+        if(!empty($content)) {
             $generatedCs2 = (new Cs2($this->lpa, self::CONTENT_TYPE_REPLACEMENT_ATTORNEY_STEP_IN, $content))->generate();
             $this->mergerIntermediateFilePaths($generatedCs2);
-        } // endif
+        }
+        
         
         // generate a CS2 page if preference exceed available space on standard form.
         if(!$this->canFitIntoTextBox($this->lpa->document->preference)) {
