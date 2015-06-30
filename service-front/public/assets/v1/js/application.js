0e3ccf3760401a27954b3c4264e464677522b338
Jianzhong Yu <github@jianzhong.co.uk>
2015-06-30 16:16:25 +0100

    bug fix: recover repeat case number box show/hide function

--- service-front/public/assets/v1/js/application.js
+++ service-front/public/assets/v1/js/application.js
@@ -4686,11 +4686,11 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
     initialiseEvents: function () {
       var self = this;
 
-      $('form#repeat-application').on('change', 'input[type="radio"]', function (evt) {
+      $('form#form-repeat-application').on('change', 'input[type="radio"]', function (evt) {
         self.onRepeatApplicationFormChangeHandler(evt);
       });
 
-      $('form#repeat-application').on('click', 'input[type="submit"]', function (evt) {
+      $('form#form-repeat-application').on('click', 'input[type="submit"]', function (evt) {
         self.onRepeatApplicationFormClickHandler(evt);
       });
 
