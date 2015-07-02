173827df8bd34ca7a83656f40e8ff886801ab817
Neil Smith <github@nsmith.me.uk>
2015-07-02 15:36:46 +0100

    Added queue message size logging

--- service-pdf/src/Opg/Lpa/Pdf/Worker/ResqueWorker.php
+++ service-pdf/src/Opg/Lpa/Pdf/Worker/ResqueWorker.php
@@ -3,7 +3,7 @@ namespace Opg\Lpa\Pdf\Worker;
 
 use Zend\Crypt\BlockCipher;
 use Zend\Crypt\Symmetric\Exception\InvalidArgumentException as CryptInvalidArgumentException;
-
+use Opg\Lpa\Pdf\Logger\Logger;
 use Opg\Lpa\Pdf\Config\Config;
 
 class ResqueWorker extends AbstractWorker {
@@ -31,6 +31,12 @@ class ResqueWorker extends AbstractWorker {
         if( !isset($this->args['type']) ){
             throw new \Exception('Missing field: type');
         }
+
+        //---
+
+        $messageSize = strlen( $this->args['lpa'] );
+
+        Logger::getInstance()->info("New message: $messageSize bytes\n");
 
         //---------------------------------------------
         // Decrypt the JSON...
