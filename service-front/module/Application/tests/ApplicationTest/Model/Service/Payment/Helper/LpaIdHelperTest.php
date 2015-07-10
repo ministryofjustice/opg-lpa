199cd57a6290d44263ae5186383ef62ca3b5510b
Chris Moreton <chris@netsensia.com>
2015-07-10 11:05:19 +0100

    LPA ID Padding

---
+++ service-front/module/Application/tests/ApplicationTest/Model/Service/Payment/Helper/LpaIdHelperTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace ApplicationTest\Model\Service\Payment\Helper;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Application\Model\Service\Payment\Helper\LpaIdHelper;
+
+/**
+ * FormFlowChecker test case.
+ */
+class PaymentTest extends AbstractHttpControllerTestCase
+{
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp ()
+    {
+        parent::setUp();
+    }
+    
+    public function testConstructLpaIdWithStringWhenZeroesNeeded()
+    {
+        $this->assertEquals(
+            LpaIdHelper::constructWorldPayTransactionId('123'),
+            '00000000123'
+        );
+    }
+    
+    public function testConstructLpaIdWithIntegerWhenZeroesNeeded()
+    {
+        $this->assertEquals(
+            LpaIdHelper::constructWorldPayTransactionId(123),
+            '00000000123'
+        );
+    }
+    
+    public function testConstructLpaIdWhenNoZeroesNeeded()
+    {
+        $this->assertEquals(
+            LpaIdHelper::constructWorldPayTransactionId('12345678901'),
+            '12345678901'
+        );
+    }
+    
+    public function testConstructLpaIdWhenLpaIdIsTooBig()
+    {
+        $exceptionThrown = false;
+        try {
+            $paddedId = LpaIdHelper::constructWorldPayTransactionId('123456789011');
+        } catch (\Exception $e) {
+            $exceptionThrown = true;
+        }
+        
+        $this->assertTrue($exceptionThrown);
+    }
+    
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown ()
+    {
+    }
+}
