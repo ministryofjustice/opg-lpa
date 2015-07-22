56332f7a968ac811fd66619e63bb53e1479d6c29
Jianzhong Yu <github@jianzhong.co.uk>
2015-07-22 12:04:25 +0100

    bug fix: payment fee not updated when user made change on repeat application page.

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/RepeatApplicationController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/RepeatApplicationController.php
@@ -12,6 +12,8 @@ namespace Application\Controller\Authenticated\Lpa;
 use Application\Controller\AbstractLpaController;
 use Zend\View\Model\ViewModel;
 use Application\Model\Service\Lpa\Metadata;
+use Opg\Lpa\DataModel\Lpa\Payment\Payment;
+use Opg\Lpa\DataModel\Lpa\Payment\Calculator;
 
 class RepeatApplicationController extends AbstractLpaController
 {
@@ -20,6 +22,7 @@ class RepeatApplicationController extends AbstractLpaController
     
     public function indexAction()
     {
+        $lpa = $this->getLpa();
         $form = $this->getServiceLocator()->get('FormElementManager')->get('Application\Form\Lpa\RepeatApplicationForm');
         
         if($this->request->isPost()) {
@@ -36,33 +39,52 @@ class RepeatApplicationController extends AbstractLpaController
             
             if($form->isValid()) {
                 
-                $lpaId = $this->getLpa()->id;
+                $lpaId = $lpa->id;
+                $repeatCaseNumber = $lpa->repeatCaseNumber;
                 $currentRouteName = $this->getEvent()->getRouteMatch()->getMatchedRouteName();
                 
                 // persist data
                 if($form->getData()['isRepeatApplication'] == 'is-repeat') {
-                    if(!$this->getLpaApplicationService()->setRepeatCaseNumber($this->getLpa()->id, $form->getData()['repeatCaseNumber'])) {
-                        throw new \RuntimeException('API client failed to set repeat case number for id: '.$lpaId);
+                    
+                    // set repeat case number only if case number changed or added
+                    if($form->getData()['repeatCaseNumber'] != $lpa->repeatCaseNumber) {
+                        if(!$this->getLpaApplicationService()->setRepeatCaseNumber($lpa->id, $form->getData()['repeatCaseNumber'])) {
+                            throw new \RuntimeException('API client failed to set repeat case number for id: '.$lpaId);
+                        }
                     }
+                    
+                    $lpa->repeatCaseNumber = $form->getData()['repeatCaseNumber'];
                 }
-                elseif($this->getLpa()->repeatCaseNumber !== null) {
-                    // delete case number if it has been set previousely.
-                    if(!$this->getLpaApplicationService()->deleteRepeatCaseNumber($this->getLpa()->id)) {
-                        throw new \RuntimeException('API client failed to set repeat case number for id: '.$lpaId);
+                else {
+                    if($lpa->repeatCaseNumber !== null) {
+                        // delete case number if it has been set previousely.
+                        if(!$this->getLpaApplicationService()->deleteRepeatCaseNumber($lpa->id)) {
+                            throw new \RuntimeException('API client failed to set repeat case number for id: '.$lpaId);
+                        }
+                    }
+                    
+                    $lpa->repeatCaseNumber = null;
+                }
+                
+                if(($lpa->payment instanceof Payment) && ($lpa->repeatCaseNumber != $repeatCaseNumber)) {
+                    Calculator::calculate($lpa);
+                    
+                    if(!$this->getLpaApplicationService()->setPayment($lpa->id, $lpa->payment)) {
+                        throw new \RuntimeException('API client failed to set payment details for id: '.$lpa->id . ' in FeeReductionController');
                     }
                 }
                 
                 // set metadata
-                $this->getServiceLocator()->get('Metadata')->setRepeatApplicationConfirmed($this->getLpa());
+                $this->getServiceLocator()->get('Metadata')->setRepeatApplicationConfirmed($lpa);
                 
                 return $this->redirect()->toRoute($this->getFlowChecker()->nextRoute($currentRouteName), ['lpa-id' => $lpaId]);
             }
         }
         else {
-            if(array_key_exists(Metadata::REPEAT_APPLICATION_CONFIRMED, $this->getLpa()->metadata)) {
+            if(array_key_exists(Metadata::REPEAT_APPLICATION_CONFIRMED, $lpa->metadata)) {
                 $form->bind([
-                        'isRepeatApplication' => ($this->getLpa()->repeatCaseNumber === null)?'is-new':'is-repeat',
-                        'repeatCaseNumber'    => $this->getLpa()->repeatCaseNumber,
+                        'isRepeatApplication' => ($lpa->repeatCaseNumber === null)?'is-new':'is-repeat',
+                        'repeatCaseNumber'    => $lpa->repeatCaseNumber,
                         
                 ]);
             }
