96721c7b7bbb7261f8764dcd6d02ae48ced0c3a5
Neil Smith <github@nsmith.me.uk>
2015-09-01 15:00:03 +0100

    Remove this hack now that Amazon have accepted my patch

--- service-front/module/Application/src/Application/Model/Service/Session/SaveHandler/DynamoDB.php
+++ service-front/module/Application/src/Application/Model/Service/Session/SaveHandler/DynamoDB.php
@@ -3,37 +3,15 @@ namespace Application\Model\Service\Session\SaveHandler;
 
 use Aws\DynamoDb\SessionHandler as DynamoDbSessionHandler;
 
-// The 3 entries below can be removed if the patch is accepted.
-use Aws\DynamoDb\DynamoDbClient;
-use Aws\DynamoDb\LockingSessionConnection;
-use Aws\DynamoDb\StandardSessionConnection;
-
 use Zend\Session\SaveHandler\SaveHandlerInterface;
 
 /**
- * Extends Amazon's DynamoDb Session Handler to we can apply Zend's Save Handler Interface.
- *
- * We don't need to do anything else here.
- *
- * NOTE - Amazon does not use late static binding in fromClient() which is very naughty.
- *          I've submitted a pull request, but until then, we have to override fromClient().
+ * Extends Amazon's DynamoDb Session Handler (mostly) so we can apply Zend's Save Handler Interface.
  *
  * Class DynamoDB
  * @package Application\Model\Service\Session\SaveHandler
  */
 class DynamoDB extends DynamoDbSessionHandler implements SaveHandlerInterface {
-
-    public static function fromClient(DynamoDbClient $client, array $config = [])
-    {
-        $config += ['locking' => false];
-        if ($config['locking']) {
-            $connection = new LockingSessionConnection($client, $config);
-        } else {
-            $connection = new StandardSessionConnection($client, $config);
-        }
-
-        return new static($connection);
-    }
 
     /**
      * Re-enable GC. This will only ever be called out of hours.
@@ -41,6 +19,8 @@ class DynamoDB extends DynamoDbSessionHandler implements SaveHandlerInterface {
      *
      * It would be nice to do this with a cron as recommend by Amazon, but we don't
      * have anywhere nice to run the cronjob yet.
+     *
+     * #TODO - This will be moved to a cron eventually.
      *
      * @param int $maxLifetime
      * @return bool
