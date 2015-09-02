63b8dfd9c96a5f1dbf532f41218b3f952114fdee
Neil Smith <github@nsmith.me.uk>
2015-09-02 12:05:36 +0100

    Composer update

---
+++ service-api/vendor/ministryofjustice/opg-lpa-datamodels/spec/Opg/Lpa/DataModel/User/AddressSpec.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace spec\Opg\Lpa\DataModel\User;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class AddressSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Opg\Lpa\DataModel\User\Address');
+    }
+    
+    function it_will_reject_a_postcode_with_more_than_eight_chars()
+    {
+        $this->beConstructedWith([
+            'address1' => 'Address One',
+            'address2' => '',
+            'address3' => '',
+            'postcode' => '123456789'
+        ]);
+    
+        $this->validate(['postcode'])->getArrayCopy()->shouldBe([
+            "postcode" =>
+            [
+                "value" => "123456789",
+                "messages" => [ 0 => "must-be-less-than-or-equal:8" ],
+            ]
+        ]);
+    }
+    
+    function it_will_allow_an_empty_postcode()
+    {
+        $this->beConstructedWith([
+            'address1' => 'Address One',
+            'address2' => '',
+            'address3' => '',
+            'postcode' => ''
+        ]);
+    
+        $this->validate(['postcode'])->getArrayCopy()->shouldBe([]);
+    }
+    
+    function it_will_allow_a_postcode_with_one_character()
+    {
+        $this->beConstructedWith([
+            'address1' => 'Address One',
+            'address2' => '',
+            'address3' => '',
+            'postcode' => 'A'
+        ]);
+    
+        $this->validate(['postcode'])->getArrayCopy()->shouldBe([]);
+    }
+}
