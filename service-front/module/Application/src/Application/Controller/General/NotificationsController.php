bd17b71180025c3788f496b6a66a74885e048720
Neil Smith <github@nsmith.me.uk>
2015-09-24 11:14:22 +0100

    Added notifications controller

---
+++ service-front/module/Application/src/Application/Controller/General/NotificationsController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Application\Controller\General;
+
+use Application\Controller\AbstractBaseController;
+
+class NotificationsController extends AbstractBaseController {
+
+    public function expiryNoticeAction(){
+
+
+
+    } // function
+
+} // class
