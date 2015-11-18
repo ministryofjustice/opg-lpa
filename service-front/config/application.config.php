0dc89172a708134b15c94532e96c80472549fbaa
Chris Moreton <chris@netsensia.com>
2015-11-18 16:25:21 +0000

    Enable V1Proxy module

--- service-front/config/application.config.php
+++ service-front/config/application.config.php
@@ -3,7 +3,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-        //'V1Proxy',
+        'V1Proxy',
         'ZfcTwig',
     ),
 
