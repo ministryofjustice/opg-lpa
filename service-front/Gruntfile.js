373f44977c4205ebdbb7b94093cbab2d43684457
Biggs Thorarensen <biggs@devillion.com>
2015-11-18 12:13:07 +0000

    govuk template - css integrated

--- service-front/Gruntfile.js
+++ service-front/Gruntfile.js
@@ -1,13 +1,23 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         watch: {
-            files: 'assets/sass/*.scss',
-            tasks: ['sass']
+            scss: {
+                files: 'assets/**/*.scss',
+                tasks: ['sass']
+            }
         },
         sass: {
             dev: {
+                options: {
+                    loadPath: 'assets/bower/govuk_elements/govuk/public/sass'
+                },
                 files: {
-                    'public/assets/v2/css/main.css': 'assets/sass/main.scss'
+                    'public/assets/v2/css/main.css': 'assets/sass/main.scss',
+                    'public/assets/v2/css/govuk-template.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template.scss',
+                    'public/assets/v2/css/govuk-template-ie8.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie8.scss',
+                    'public/assets/v2/css/govuk-template-ie7.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie7.scss',
+                    'public/assets/v2/css/govuk-template-ie6.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie6.scss',
+                    'public/assets/v2/css/govuk-template-print.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-print.scss'
                 }
             }
         }
