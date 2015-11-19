166e508d1bb0ee42d3f9eca221681c4a52129992
Biggs Thorarensen <biggs@devillion.com>
2015-11-19 10:16:31 +0000

    scss-lint added to workflow

--- service-front/Gruntfile.js
+++ service-front/Gruntfile.js
@@ -1,35 +1,46 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        watch: {
-            scss: {
-                files: 'assets/**/*.scss',
-                tasks: ['sass']
-            }
+  grunt.initConfig({
+    watch: {
+      scss: {
+        files: 'assets/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          loadPath: [
+          'assets/bower/govuk_elements/govuk/public/sass',
+          'assets/bower/govuk_elements/public/sass'
+          ]
         },
-        sass: {
-            dev: {
-                options: {
-                    loadPath: [
-                        'assets/bower/govuk_elements/govuk/public/sass',
-                        'assets/bower/govuk_elements/public/sass'
-                    ]
-                },
-                files: {
-                    'public/assets/v2/css/application.css': 'assets/sass/application.scss',
-                    'public/assets/v2/css/govuk-template.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template.scss',
-                    'public/assets/v2/css/govuk-template-ie8.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie8.scss',
-                    'public/assets/v2/css/govuk-template-ie7.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie7.scss',
-                    'public/assets/v2/css/govuk-template-ie6.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie6.scss',
-                    'public/assets/v2/css/govuk-template-print.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-print.scss'
-                }
-            }
+        files: {
+          'public/assets/v2/css/application.css': 'assets/sass/application.scss',
+          'public/assets/v2/css/govuk-template.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template.scss',
+          'public/assets/v2/css/govuk-template-ie8.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie8.scss',
+          'public/assets/v2/css/govuk-template-ie7.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie7.scss',
+          'public/assets/v2/css/govuk-template-ie6.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-ie6.scss',
+          'public/assets/v2/css/govuk-template-print.css': 'assets/bower/govuk_template/source/assets/stylesheets/govuk-template-print.scss'
         }
-    });
+      }
+    },
+    scsslint: {
+      allFiles: [
+        'assets/sass/*.scss',
+      ],
+      options: {
+        config: null,
+        reporterOutput: null,
+        colorizeOutput: true
+      },
+    }
+  });
 
-    // load npm tasks
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  // load npm tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-scss-lint');
 
-    // define default task
-    grunt.registerTask('default', ['watch']);
+  // define default task
+  grunt.registerTask('default', ['watch']);
 };
