d7b87f7059c792fcdbf2e923e75a4e9a2b6c2141
Biggs Thorarensen <biggs@devillion.com>
2015-11-27 14:54:19 +0000

    grunt concat to application.js, jshint added too

--- service-front/Gruntfile.js
+++ service-front/Gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function (grunt) {
+  'use strict';
+
   grunt.initConfig({
     watch: {
       scss: {
@@ -27,6 +29,16 @@ module.exports = function (grunt) {
         }
       }
     },
+    scsslint: {
+      allFiles: [
+        'assets/sass/*.scss'
+      ],
+      options: {
+        config: null,
+        reporterOutput: null,
+        colorizeOutput: true
+      }
+    },
     replace: {
       image_url: {
         src: ['public/assets/v2/css/*.css'],
@@ -37,15 +49,68 @@ module.exports = function (grunt) {
         }]
       }
     },
-    scsslint: {
-      allFiles: [
-        'assets/sass/*.scss',
-      ],
+    // Join the files
+    concat: {
+      dist: {
+        src: [
+          // Vendor Scripts
+          'assets/js/vendor/handlebars.js',
+          'assets/js/vendor/lodash-2.4.1.min.js',
+
+          // GOVUK Scripts
+          'assets/bower/stageprompt/script/stageprompt.js',
+
+          // UI Framework
+          'assets/js/ui_framework/jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.min.js',
+
+          // OPG Scripts
+          'assets/js/opg/jquery-plugin-opg-hascrollbar.js',
+          'assets/js/opg/jquery-plugin-opg-spinner.js',
+
+          // Polyfills
+          'assets/js/polyfills/jquery-details/jquery.details.min.js',
+
+          // MoJ Scripts - Base
+          'assets/js/moj/moj.js',
+          'assets/js/moj/moj.helpers.js',
+
+          // LPA Scripts - Templates
+          'assets/js/lpa/lpa.templates.js',
+
+          // MoJ Scripts - Modules
+          'assets/js/moj/moj.modules/moj.popup.js',
+          'assets/js/moj/moj.modules/moj.help-system.js',
+          'assets/js/moj/moj.modules/moj.form-popup.js',
+          'assets/js/moj/moj.modules/moj.title-switch.js',
+          'assets/js/moj/moj.modules/moj.reusables.js',
+          'assets/js/moj/moj.modules/moj.postcode-lookup.js',
+          'assets/js/moj/moj.modules/moj.person-form.js',
+          'assets/js/moj/moj.modules/moj.validation.js',
+          'assets/js/moj/moj.modules/moj.user-timeout.js',
+          'assets/js/moj/moj.modules/moj.sticky-nav.js',
+          'assets/js/moj/moj.modules/moj.repeat-application.js',
+
+          // LPA Scripts
+          'assets/js/main.js',
+          'assets/js/lpa/who-are-you.js',
+          'assets/js/lpa/form.js'
+        ],
+        dest: 'public/assets/v2/js/application.js',
+        nonull: true
+      }
+    },
+    // lint js files
+    jshint: {
       options: {
-        config: null,
-        reporterOutput: null,
-        colorizeOutput: true
+        jshintrc: '.jshintrc',
+        ignores: []
       },
+      files: [
+        'Gruntfile.js',
+        'assets/js/moj/**/*.js',
+        'assets/js/lpa/**/*.js',
+        'assets/js/main.js'
+      ]
     }
   });
 
@@ -54,8 +119,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-scss-lint');
   grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // define default task
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('build', ['sass', 'replace:image_url']);
+  grunt.registerTask('test', ['scsslint', 'jshint']);
 };
