ea8c761617ad16f11047a971c6bc5fd62ce7fd4d
Chris Moreton <chris@netsensia.com>
2015-12-29 11:45:05 +0000

    Add dimensions vars to TypeController

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/TypeController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/TypeController.php
@@ -51,6 +51,16 @@ class TypeController extends AbstractLpaController
             }
         }
         
+        if (empty($this->getLpa()->document->type)) {
+            $analyticsDimensions = [
+                'start_date' => date('Y-m-d'),
+                'return_session' => 0,
+            ];
+        
+            $this->layout()->setVariable('analyticsDimensions', json_encode($analyticsDimensions));
+        
+        }
+        
         return new ViewModel([
                 'form'=>$form, 
                 'cloneUrl'=>$this->url()->fromRoute('user/dashboard/create-lpa', ['lpa-id'=>$this->getLpa()->id]),
