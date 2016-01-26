b4e61c6a52601452ef41f25d8a22ae43bbc8c3b8
Chris Moreton <chris@netsensia.com>
2016-01-26 10:36:27 +0000

    Move error messages to view - login form

--- service-front/module/Application/src/Application/Form/User/Login.php
+++ service-front/module/Application/src/Application/Form/User/Login.php
@@ -33,7 +33,7 @@ class Login extends AbstractForm {
         $inputFilter->add(array(
             'name'     => 'email',
             'required' => true,
-            'error_message' => 'You need to enter your email address',
+            'error_message' => 'cannot-be-empty',
             'filters'  => array(
                 array('name' => 'StringTrim'),
                 array('name' => 'StringToLower'),
@@ -43,7 +43,7 @@ class Login extends AbstractForm {
         $inputFilter->add(array(
             'name'     => 'password',
             'required' => true,
-            'error_message' => 'You need to enter your password',
+            'error_message' => 'cannot-be-empty',
         ));
 
     } // function
