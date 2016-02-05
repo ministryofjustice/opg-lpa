a01152357142c72303cafe770b107f2ee79bc21c
Neil Smith <github@nsmith.me.uk>
2016-02-05 11:33:13 +0000

    Remove DynamoDB gc() method
    This is now done via a cron.

--- service-front/module/Application/src/Application/Model/Service/Session/SaveHandler/DynamoDB.php
+++ service-front/module/Application/src/Application/Model/Service/Session/SaveHandler/DynamoDB.php
@@ -13,24 +13,5 @@ use Zend\Session\SaveHandler\SaveHandlerInterface;
  */
 class DynamoDB extends DynamoDbSessionHandler implements SaveHandlerInterface {
 
-    /**
-     * Re-enable GC. This will only ever be called out of hours.
-     * See the global.php config file for details.
-     *
-     * It would be nice to do this with a cron as recommend by Amazon, but we don't
-     * have anywhere nice to run the cronjob yet.
-     *
-     * #TODO - This will be moved to a cron eventually.
-     *
-     * @param int $maxLifetime
-     * @return bool
-     */
-    public function gc($maxLifetime){
-
-        $this->garbageCollect();
-
-        // Garbage collection for a DynamoDB table must be triggered manually.
-        return true;
-    }
 
 } // class
