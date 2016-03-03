4d983b2e4171a7aa132c0c9c70a1d3b1327951bd
Neil Smith <github@nsmith.me.uk>
2016-03-03 15:04:16 +0000

    Tidied up the AccountInto view helper and template

--- service-front/module/Application/src/Application/View/Helper/AccountInfo.php
+++ service-front/module/Application/src/Application/View/Helper/AccountInfo.php
@@ -10,67 +10,54 @@ class AccountInfo extends AbstractHelper
         $serviceLocator = $this->view->getHelperPluginManager()->getServiceLocator();
         
         $auth = $serviceLocator->get('AuthenticationService');
+
+        // Only continue if the user is singed in.
+        if ( !$auth->hasIdentity() ){ return; }
         
         $params = [
-            'links' => [],
+            'view' => $this->view,
         ];
-        
-        if ($auth->hasIdentity()) {
 
-            $details = $this->view->getHelperPluginManager()->getServiceLocator()->get('UserDetailsSession');
-            
-            // do not show user names and links to account details and dashboard if user is logging in the first time.
-            if($details->user->name !== null) { 
-                
-                if( $details->user->name->first != null && $details->user->name->last != null ){
-                    $params['name'] = "{$details->user->name->first} {$details->user->name->last}";
-                }
-    
-                $params['links'][] = array(
-                    'text' => 'Your details',
-                    'url' => $this->view->url('user/about-you'),
-                );
-                $params['links'][] = array(
-                    'text' => 'Your LPAs',
-                    'url' => $this->view->url('user/dashboard'),
-                );
+        //---
+
+        $details = $this->view->getHelperPluginManager()->getServiceLocator()->get('UserDetailsSession');
+
+        // Only include name (and singed in user links) if the user has set their name.
+        // i.e. they've completed the first About You step.
+        if($details->user->name !== null) {
+
+            if( $details->user->name->first != null && $details->user->name->last != null ){
+                $params['name'] = "{$details->user->name->first} {$details->user->name->last}";
             }
-            
-            $params['links'][] = array(
-                'text' => 'Sign out',
-                'url' => $this->view->url('logout'),
-            );
 
-            //-----------------------------------------------------
-            // Include last logged in date if set a view parameter
-
-            $layoutChildren = $this->view->getHelperPluginManager()->getServiceLocator()
-                ->get('ViewManager')->getViewModel()->getIterator();
-
-            if( $layoutChildren->count() > 0 ){
-                $view = $layoutChildren->current();
-
-                if( isset($view->user) && isset($view->user['lastLogin']) ){
-                    $params['lastLogin'] = $view->user['lastLogin'];
-                }
-
-            } // if
-            
-        } else {
-            $params['email'] = '';
-            $params['links'][] = array(
-                'text' => 'Sign in',
-                'url' => '/user/login'
-            );
         }
 
+        //-----------------------------------------------------
+        // Include last logged in date if set a view parameter
+
+        $layoutChildren = $this->view->getHelperPluginManager()->getServiceLocator()
+            ->get('ViewManager')->getViewModel()->getIterator();
+
+        if( $layoutChildren->count() > 0 ){
+            $view = $layoutChildren->current();
+
+            if( isset($view->user) && isset($view->user['lastLogin']) ){
+                $params['lastLogin'] = $view->user['lastLogin'];
+            }
+
+        } // if
+
+        //---
+
+        // Include the name of teh current route.
         $params['route'] = $serviceLocator->get('Application')->getMvcEvent()->getRouteMatch()->getMatchedRouteName();
-        
-        if ($auth->hasIdentity()) {
-            $template = $this->view->getHelperPluginManager()->getServiceLocator()->get('TwigViewRenderer')->loadTemplate('account-info/account-info.twig');
-            $content = $template->render($params);
-        
-            echo $content;
-        }
-    }
-}
+
+        //---
+
+        $template = $this->view->getHelperPluginManager()->getServiceLocator()->get('TwigViewRenderer')->loadTemplate('account-info/account-info.twig');
+
+        echo $template->render($params);
+
+    } // function
+
+} // class
