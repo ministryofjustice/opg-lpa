0207806d849998b4720673eaf09a6ba1fd7039c2
Chris Moreton <chris@netsensia.com>
2016-04-12 11:20:41 +0100

    Remove unused code from DOB fieldset

--- service-front/module/Application/src/Application/Form/Fieldset/Dob.php
+++ service-front/module/Application/src/Application/Form/Fieldset/Dob.php
@@ -10,21 +10,20 @@ class Dob extends Fieldset
         parent::__construct($name, $options);
     
         $this->add([
-                'name' => 'day',
-                'type' => 'Text',
+            'name' => 'day',
+            'type' => 'Text',
+            
         ]);
         
         $this->add([
-                'name' => 'month',
-                'type' => 'Text',
+            'name' => 'month',
+            'type' => 'Text',
         ]);
         
         $this->add([
-                'name' => 'year',
-                'type' => 'Text',
+            'name' => 'year',
+            'type' => 'Text',
         ]);
-        
-        $this->setAttribute('name', 'date-of-birth');
     }
     
     public function setMessages($messages)
