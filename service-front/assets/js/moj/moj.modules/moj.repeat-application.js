41469af3154d2bee7dee506e1e5dfa80f7b2bc80
Biggs Thorarensen <biggs@devillion.com>
2016-06-15 15:29:19 +0100

    LPA-1766 js lint fixes

--- service-front/assets/js/moj/moj.modules/moj.repeat-application.js
+++ service-front/assets/js/moj/moj.modules/moj.repeat-application.js
@@ -36,14 +36,12 @@
       }
 
     },
-    render: function (e, params) {
-      var $el = $(this.selector, $(params.wrap));
-
+    render: function () {
       this.displayCaseNumber(0);
       this.initialiseEvents();
 
     },
-    onRepeatApplicationFormChangeHandler: function (evt) {
+    onRepeatApplicationFormChangeHandler: function () {
       this.displayCaseNumber(500);
     },
     onRepeatApplicationFormClickHandler: function (evt) {
