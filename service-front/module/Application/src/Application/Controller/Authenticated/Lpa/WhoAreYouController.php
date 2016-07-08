4d8ff017b7421efd8b0e8d0a41edb734fd6295bf
Chris Moreton <chris@netsensia.com>
2016-07-08 16:19:10 +0100

    Work in progess for LPA-1815

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/WhoAreYouController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/WhoAreYouController.php
@@ -32,97 +32,98 @@ class WhoAreYouController extends AbstractLpaController
         
         $professional   = $form->get('professional');
         
-           $whoOptions = [];
-    $whoOptions['donor'] = new Element('who', [
-            'label' => "The donor used this online service with little or no help",
-    ]);
-    $whoOptions['donor']->setAttributes([
-            'type' => 'radio',
-            'id' => 'who-donor',
-            'value' => $who->getOptions()['value_options']['donor']['value'],
-            'checked' => (($who->getValue() == 'donor')? 'checked':null),
-    ]);
-    
-    $whoOptions['friend-or-family'] = new Element('who', [
-            'label' => "A friend or family member helped the donor get online, navigate or enter information",
-    ]);
-    $whoOptions['friend-or-family']->setAttributes([
-            'type' => 'radio',
-            'id' => 'who-friend-or-family',
-            'value' => $who->getOptions()['value_options']['friendOrFamily']['value'],
-            'checked' => (($who->getValue() == 'friendOrFamily')? 'checked':null),
-    ]);
-    
-    $whoOptions['professional'] = new Element('who', [
-            'label' => "A paid professional made the LPA on the donor's behalf",
-    ]);
-    $whoOptions['professional']->setAttributes([
-            'type' => 'radio',
-            'id' => 'who-professional',
-            'value' => $who->getOptions()['value_options']['professional']['value'],
-            'checked' => (($who->getValue() == 'professional')? 'checked':null),
-    ]);
-    
-    $whoOptions['digital-partner'] = new Element('who', [
-            'label' => "OPG's Assisted Digital Service helped the donor",
-    ]);
-    $whoOptions['digital-partner']->setAttributes([
-            'type' => 'radio',
-            'id' => 'who-digital-partner',
-            'value' => $who->getOptions()['value_options']['digitalPartner']['value'],
-            'checked' => (($who->getValue() == 'digitalPartner')? 'checked':null),
-    ]);
-    
-    $whoOptions['organisation'] = new Element('who', [
-            'label' => "Another organisation, such as a charity, council or community group, helped the donor",
-    ]);
-    $whoOptions['organisation']->setAttributes([
-            'type' => 'radio',
-            'id' => 'who-organisation',
-            'value' => $who->getOptions()['value_options']['organisation']['value'],
-            'checked' => (($who->getValue() == 'organisation')? 'checked':null),
-    ]);
-    
-    $whoOptions['notSaid'] = new Element('who', [
-            'label' => "I'd prefer not to say",
-    ]);
-    $whoOptions['notSaid']->setAttributes([
-            'type' => 'radio',
-            'id' => 'who-notSaid',
-            'value' => $who->getOptions()['value_options']['notSaid']['value'],
-            'checked' => (($who->getValue() == 'notSaid')? 'checked':null),
-    ]);
-    
-    $professionalOptions = [];
-    $professionalOptions['solicitor'] = new Element('professional', [
-            'label' => "Solicitor",
-    ]);
-    $professionalOptions['solicitor']->setAttributes([
-            'type' => 'radio',
-            'id' => 'professional-solicitor',
-            'value' => $professional->getOptions()['value_options']['solicitor']['value'],
-            'checked' => (($professional->getValue() == 'solicitor')? 'checked':null),
-    ]);
-    
-    $professionalOptions['will-writer'] = new Element('professional', [
-            'label' => "Will-writer",
-    ]);
-    $professionalOptions['will-writer']->setAttributes([
-            'type' => 'radio',
-            'id' => 'professional-will-writer',
-            'value' => $professional->getOptions()['value_options']['will-writer']['value'],
-            'checked' => (($professional->getValue() == 'will-writer')? 'checked':null),
-    ]);
-    
-    $professionalOptions['other'] = new Element('professional', [
-            'label' => "Other",
-    ]);
-    $professionalOptions['other']->setAttributes([
-            'type' => 'radio',
-            'id' => 'professional-other',
-            'value' => $professional->getOptions()['value_options']['other']['value'],
-            'checked' => (($professional->getValue() == 'other')? 'checked':null),
-    ]);
+        $whoOptions = [];
+        
+        $whoOptions['donor'] = new Element('who', [
+                'label' => "The donor used this online service with little or no help",
+        ]);
+        $whoOptions['donor']->setAttributes([
+                'type' => 'radio',
+                'id' => 'who-donor',
+                'value' => $who->getOptions()['value_options']['donor']['value'],
+                'checked' => (($who->getValue() == 'donor')? 'checked':null),
+        ]);
+        
+        $whoOptions['friend-or-family'] = new Element('who', [
+                'label' => "A friend or family member helped the donor get online, navigate or enter information",
+        ]);
+        $whoOptions['friend-or-family']->setAttributes([
+                'type' => 'radio',
+                'id' => 'who-friend-or-family',
+                'value' => $who->getOptions()['value_options']['friendOrFamily']['value'],
+                'checked' => (($who->getValue() == 'friendOrFamily')? 'checked':null),
+        ]);
+        
+        $whoOptions['professional'] = new Element('who', [
+                'label' => "A paid professional made the LPA on the donor's behalf",
+        ]);
+        $whoOptions['professional']->setAttributes([
+                'type' => 'radio',
+                'id' => 'who-professional',
+                'value' => $who->getOptions()['value_options']['professional']['value'],
+                'checked' => (($who->getValue() == 'professional')? 'checked':null),
+        ]);
+        
+        $whoOptions['digital-partner'] = new Element('who', [
+                'label' => "OPG's Assisted Digital Service helped the donor",
+        ]);
+        $whoOptions['digital-partner']->setAttributes([
+                'type' => 'radio',
+                'id' => 'who-digital-partner',
+                'value' => $who->getOptions()['value_options']['digitalPartner']['value'],
+                'checked' => (($who->getValue() == 'digitalPartner')? 'checked':null),
+        ]);
+        
+        $whoOptions['organisation'] = new Element('who', [
+                'label' => "Another organisation, such as a charity, council or community group, helped the donor",
+        ]);
+        $whoOptions['organisation']->setAttributes([
+                'type' => 'radio',
+                'id' => 'who-organisation',
+                'value' => $who->getOptions()['value_options']['organisation']['value'],
+                'checked' => (($who->getValue() == 'organisation')? 'checked':null),
+        ]);
+        
+        $whoOptions['notSaid'] = new Element('who', [
+                'label' => "I'd prefer not to say",
+        ]);
+        $whoOptions['notSaid']->setAttributes([
+                'type' => 'radio',
+                'id' => 'who-notSaid',
+                'value' => $who->getOptions()['value_options']['notSaid']['value'],
+                'checked' => (($who->getValue() == 'notSaid')? 'checked':null),
+        ]);
+        
+        $professionalOptions = [];
+        $professionalOptions['solicitor'] = new Element('professional', [
+                'label' => "Solicitor",
+        ]);
+        $professionalOptions['solicitor']->setAttributes([
+                'type' => 'radio',
+                'id' => 'professional-solicitor',
+                'value' => $professional->getOptions()['value_options']['solicitor']['value'],
+                'checked' => (($professional->getValue() == 'solicitor')? 'checked':null),
+        ]);
+        
+        $professionalOptions['will-writer'] = new Element('professional', [
+                'label' => "Will-writer",
+        ]);
+        $professionalOptions['will-writer']->setAttributes([
+                'type' => 'radio',
+                'id' => 'professional-will-writer',
+                'value' => $professional->getOptions()['value_options']['will-writer']['value'],
+                'checked' => (($professional->getValue() == 'will-writer')? 'checked':null),
+        ]);
+        
+        $professionalOptions['other'] = new Element('professional', [
+                'label' => "Other",
+        ]);
+        $professionalOptions['other']->setAttributes([
+                'type' => 'radio',
+                'id' => 'professional-other',
+                'value' => $professional->getOptions()['value_options']['other']['value'],
+                'checked' => (($professional->getValue() == 'other')? 'checked':null),
+        ]);
         
         if($this->request->isPost()) {
             
