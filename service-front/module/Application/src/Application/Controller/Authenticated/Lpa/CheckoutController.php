e5e09003b7be3104fb5f63a6047d0392ba2eee16
Neil Smith <github@nsmith.me.uk>
2016-07-28 11:32:56 +0100

    Added support for paying by cheque to Checkout

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/CheckoutController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/CheckoutController.php
@@ -2,6 +2,8 @@
 
 namespace Application\Controller\Authenticated\Lpa;
 
+use Opg\Lpa\DataModel\Lpa\Payment\Payment;
+
 use Application\Controller\AbstractLpaController;
 use Zend\View\Model\ViewModel;
 
@@ -13,11 +15,31 @@ class CheckoutController extends AbstractLpaController {
 
     }
 
+    public function chequeAction(){
+
+        $lpa = $this->getLpa();
+
+        $lpa->payment->method = Payment::PAYMENT_TYPE_CHEQUE;
+
+        if(!$this->getLpaApplicationService()->setPayment($lpa->id, $lpa->payment)) {
+            throw new \RuntimeException('API client failed to set payment details for id: '.$lpa->id . ' in CheckoutController');
+        }
+
+        //---
+
+        return $this->getNextSectionRedirect();
+
+    }
+
     public function payAction(){
+
+        die('pay');
 
     }
 
     public function worldpayAction(){
+
+        die('worldpay');
 
     }
 
