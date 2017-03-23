6925b302219d116c32a9c4fe45a390ab3bd363b5
Steven Taylor <taylorsj1980@gmail.com>
2017-03-23 12:42:23 +0000

    Removed unused excluded routes array from accordion helper - this is handled by Twig templates now - and some refactor

--- service-front/module/Application/src/Application/View/Helper/Accordion.php
+++ service-front/module/Application/src/Application/View/Helper/Accordion.php
@@ -1,205 +1,157 @@
 <?php
+
 namespace Application\View\Helper;
 
-use Zend\View\Helper\AbstractHelper;
 use Application\Model\FormFlowChecker;
 use Opg\Lpa\DataModel\Lpa\Lpa;
+use Zend\View\Helper\AbstractHelper;
 
-class Accordion extends AbstractHelper {
-
+class Accordion extends AbstractHelper
+{
     private $lpa;
 
+    /**
+     * Full list of all the routes that can be used as bars in the accordion - in order
+     *
+     * @var array
+     */
     private $bars = [
-        'lpa/form-type'                              ,
-        'lpa/donor'                                  ,
-        'lpa/when-lpa-starts'                        ,
-        'lpa/life-sustaining'                        ,
-        'lpa/primary-attorney'                       ,
-        'lpa/how-primary-attorneys-make-decision'    ,
-        'lpa/replacement-attorney'                   ,
-        'lpa/when-replacement-attorney-step-in'      ,
+        'lpa/form-type',
+        'lpa/donor',
+        'lpa/when-lpa-starts',
+        'lpa/life-sustaining',
+        'lpa/primary-attorney',
+        'lpa/how-primary-attorneys-make-decision',
+        'lpa/replacement-attorney',
+        'lpa/when-replacement-attorney-step-in',
         'lpa/how-replacement-attorneys-make-decision',
-        'lpa/certificate-provider'                   ,
-        'lpa/people-to-notify'                       ,
-        'lpa/instructions'                           ,
-        'lpa/applicant'                              ,
-        'lpa/correspondent'                          ,
-        'lpa/who-are-you'                            ,
-        'lpa/repeat-application'                     ,
-        'lpa/fee-reduction'                          ,
+        'lpa/certificate-provider',
+        'lpa/people-to-notify',
+        'lpa/instructions',
+        'lpa/applicant',
+        'lpa/correspondent',
+        'lpa/who-are-you',
+        'lpa/repeat-application',
+        'lpa/fee-reduction',
     ];
 
-    private $excludedRoutes = [
-        'lpa/summary',
-        'lpa/complete',
-        'lpa/date-check',
-        'lpa/view-docs',
-        'lpa/checkout',
-        'lpa/checkout/pay/response',
-        'lpa/checkout/worldpay',
-        'lpa/checkout/worldpay/return/cancel',
-        'lpa/checkout/worldpay/return/success',
-        'lpa/checkout/worldpay/return/failure',
-    ];
-
-    public function __invoke( Lpa $lpa = null ){
-
+    public function __invoke(Lpa $lpa = null)
+    {
         $this->lpa = $lpa;
+
         return $this;
-
     }
 
-    public function top(){
-
-        if( is_null($this->lpa) ){ return array(); }
-
-        //---
-
-        $flowChecker = new FormFlowChecker( $this->lpa );
-
+    /**
+     * Return an array of route configs for bars that can appear above the page content for the current route
+     *
+     * @return array
+     */
+    public function top()
+    {
         $barsInPlay = array();
 
-        $currentRoute = $this->getRouteName();
-        $includeUpToRoute = $flowChecker->backToForm();
+        if ($this->lpa instanceof Lpa) {
+            $flowChecker = new FormFlowChecker($this->lpa);
+            $includeUpToRoute = $flowChecker->backToForm();
 
-        //---
+            // If the route for us to include up to is earlier than the current route...
+            $currentRoute = $this->getRouteName();
 
-        if ( in_array( $currentRoute, $this->excludedRoutes ) ) {
-            // No accordion summary when viewing table summary
-            return [];
-        }
-
-        //---
-
-        // If the route for us to include up to is earlier than the current route...
-        if( array_search( $includeUpToRoute, $this->bars ) < array_search( $currentRoute, $this->bars ) ){
-            $includeUpToRoute = $currentRoute;
-        }
-
-        foreach( $this->bars as $route ){
-
-            // Break at the route we are up to...
-            if( $includeUpToRoute == $route ){
-                break;
+            if (array_search($includeUpToRoute, $this->bars) < array_search($currentRoute, $this->bars)) {
+                $includeUpToRoute = $currentRoute;
             }
 
-            // Break if we get to the current route...
-            if( $currentRoute == $route ){
-                break;
+            foreach ($this->bars as $route) {
+                // Break at the route we are up to...
+                if ($includeUpToRoute == $route) {
+                    break;
+                }
+
+                // Break if we get to the current route...
+                if ($currentRoute == $route) {
+                    break;
+                }
+
+                // True iff the user is allowed to view this route name...
+                if ($route == $flowChecker->getNearestAccessibleRoute($route)) {
+                    $barsInPlay[] = ['routeName' => $route];
+                }
             }
 
-            // True iff the user is allowed to view this route name...
-            if( $route == $flowChecker->getNearestAccessibleRoute($route) ) {
-
-                $barsInPlay[] = [ 'routeName' =>  $route ];
-
+            // Added the special case bar for the review link.
+            if (array_search($currentRoute, $this->bars) >= array_search('lpa/applicant', $this->bars)) {
+                $barsInPlay[] = ['routeName' => 'review-link'];
             }
-
-        } // foreach
-
-        //---
-
-        // Added the special case bar for the review link.
-        if( array_search( $currentRoute, $this->bars ) >= array_search( 'lpa/applicant', $this->bars ) ){
-            $barsInPlay[] = [ 'routeName' =>  'review-link' ];
         }
-
-        //---
 
         return $barsInPlay;
-
     }
 
-    public function bottom(){
-
-        if( is_null($this->lpa) ){ return array(); }
-
-        //---
-
-        $flowChecker = new FormFlowChecker( $this->lpa );
-
+    /**
+     * Return an array of route configs for bars that can appear below the page content for the current route
+     *
+     * @return array
+     */
+    public function bottom()
+    {
         $barsInPlay = array();
 
-        $currentRoute = $this->getRouteName();
-        $includeUpToRoute = $flowChecker->backToForm();
+        if ($this->lpa instanceof Lpa) {
+            $flowChecker = new FormFlowChecker($this->lpa);
+            $includeUpToRoute = $flowChecker->backToForm();
 
-        //---
+            // Skip all routes before the current route...
+            $currentRoute = $this->getRouteName();
+            $startAt = array_search($currentRoute, $this->bars);
+            $startAt = is_int($startAt) ? $startAt : 0;
+            $bars = array_slice($this->bars, $startAt + 1); // +1 to start one past the page the current page.
 
-        if ( in_array( $currentRoute, $this->excludedRoutes ) ) {
-            // No accordion summary when viewing table summary
-            return [];
-        }
+            // For each possible page, starting from the user's current location...
+            foreach ($bars as $key => $route) {
+                // We only want to include 'this' bar if we can access 'this' page; AND
+                // only if we can also access ANY other page after it.
 
-        //---
-
-        // Skip all routes before the current route...
-        $startAt = array_search( $currentRoute, $this->bars );
-        $startAt = is_int($startAt) ? $startAt : 0;
-        $bars = array_slice($this->bars, $startAt+1 ); // +1 to start one past the page the current page.
-
-        // For each possible page, starting from the user's current location...
-        foreach( $bars as $key=>$route ){
-
-            //echo "Current route to check: $route<br />";
-
-            // We only want to include 'this' bar if we can access 'this' page; AND
-            // only if we can also access ANY other page after it.
-
-            // Check we can access 'this' page...
-            if( $route == $flowChecker->getNearestAccessibleRoute($route) ) {
-
-                //echo "Can access: $route<br />";
-
-                // Then check there are more pages...
-                if( isset($bars[ $key+1 ]) ){
-
-                    // And that we can access at least one of them...
-                    foreach( array_slice($bars, $key+1 ) as $futureRoute ){
-
-                        //$found = ($futureRoute == $flowChecker->getNearestAccessibleRoute($futureRoute));
-                        //echo "From $route, can we access$futureRoute = ".(int)$found."<br />";
-
-                        // If we are able to access a future route, then this page is complete.
-                        if ( $futureRoute == $flowChecker->getNearestAccessibleRoute($futureRoute) ) {
-
-                            // All conditions met, so add the bar.
+                // Check we can access 'this' page...
+                if ($route == $flowChecker->getNearestAccessibleRoute($route)) {
+                    // Then check there are more pages...
+                    if (isset($bars[$key + 1])) {
+                        // And that we can access at least one of them...
+                        foreach (array_slice($bars, $key + 1) as $futureRoute) {
+                            // If we are able to access a future route, then this page is complete.
+                            if ($futureRoute == $flowChecker->getNearestAccessibleRoute($futureRoute)) {
+                                // All conditions met, so add the bar.
+                                // We only need one page, so break when the first is found.
+                                $barsInPlay[] = ['routeName' => $route];
+                                break;
+                            }
+                        }
+                    } elseif ($route == 'lpa/fee-reduction') {
+                        // The last page is a special case as we cannot check past it.
+                        // Therefore we have a custom check.
+                        if ($this->lpa->payment instanceof \Opg\Lpa\DataModel\Lpa\Payment\Payment) {
                             $barsInPlay[] = ['routeName' => $route];
-
-                            // We only need one page, so break when the first is found.
-                            break;
-
-                        } // if
-
-                    } // foreach
-
-                } elseif( $route == 'lpa/fee-reduction' ) {
-
-                    // The last page is a special case as we cannot check past it.
-
-                    // Therefore we have a custom check.
-                    if($this->lpa->payment instanceof \Opg\Lpa\DataModel\Lpa\Payment\Payment) {
-                        $barsInPlay[] = ['routeName' => $route];
+                        }
                     }
+                }
 
-                } // if
-
-            } // if
-
-            // Give up here as we'd never show a bar past where the user has been.
-            if( $includeUpToRoute == $route ){ break; }
-
-        } // foreach
+                // Give up here as we'd never show a bar past where the user has been.
+                if ($includeUpToRoute == $route) {
+                    break;
+                }
+            }
+        }
 
         return $barsInPlay;
-
     }
 
-    //-----------------------------
+    private function getRouteName()
+    {
+        $application = $this->getView()
+                            ->getHelperPluginManager()
+                            ->getServiceLocator()
+                            ->get('application');
 
-    protected function getRouteName(){
-        $serviceManager = $this->getView()->getHelperPluginManager()->getServiceLocator();
-        $application = $serviceManager->get('application');
         return $application->getMvcEvent()->getRouteMatch()->getMatchedRouteName();
     }
-
-} // class
+}
