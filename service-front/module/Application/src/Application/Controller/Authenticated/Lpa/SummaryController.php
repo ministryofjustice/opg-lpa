ff4bc58fe060024d0453206107633c53ea47bddf
Max Edwards <youwontforgetthis@gmail.com>
2017-03-30 17:34:39 +0100

    LPA-2196 summary table changes

--- service-front/module/Application/src/Application/Controller/Authenticated/Lpa/SummaryController.php
+++ service-front/module/Application/src/Application/Controller/Authenticated/Lpa/SummaryController.php
@@ -3,6 +3,7 @@
 namespace Application\Controller\Authenticated\Lpa;
 
 use Application\Controller\AbstractLpaController;
+use Opg\Lpa\DataModel\Lpa\Payment\Calculator;
 use Zend\View\Model\ViewModel;
 
 class SummaryController extends AbstractLpaController
@@ -11,8 +12,18 @@ class SummaryController extends AbstractLpaController
     {
         $fromPage = $this->params()->fromRoute('from-page');
 
+        $isRepeatApplication = ($this->getLpa()->repeatCaseNumber != null);
+
+        $lowIncomeFee = Calculator::getLowIncomeFee( $isRepeatApplication );
+        $lowIncomeFee = (floor( $lowIncomeFee ) == $lowIncomeFee ) ? $lowIncomeFee : money_format('%i', $lowIncomeFee);
+
+        $fullFee = Calculator::getFullFee( $isRepeatApplication );
+        $fullFee = (floor( $fullFee ) == $fullFee ) ? $fullFee : money_format('%i', $fullFee);
+
         $viewParams = [
             'returnRoute' => 'lpa/' . $fromPage,
+            'fullFee' => $fullFee,
+            'lowIncomeFee' => $lowIncomeFee,
         ];
 
         return new ViewModel($viewParams);
