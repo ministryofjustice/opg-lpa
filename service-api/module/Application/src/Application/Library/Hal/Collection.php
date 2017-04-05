47e320a91fe732435815062c85e497cccbc37d3a
Steven Taylor <taylorsj1980@gmail.com>
2017-04-05 09:50:00 +0100

    Refactor RestController and some OPG Hal classes

--- service-api/module/Application/src/Application/Library/Hal/Collection.php
+++ service-api/module/Application/src/Application/Library/Hal/Collection.php
@@ -1,154 +1,95 @@
 <?php
+
 namespace Application\Library\Hal;
 
+use Application\Model\Rest\CollectionInterface;
 use RuntimeException;
 
-use Application\Model\Rest\EntityInterface;
-use Application\Model\Rest\CollectionInterface;
-
-class Collection extends Hal {
-
+class Collection extends Hal
+{
     protected $collection;
+
     protected $collectionName;
 
     private $linksSet = false;
 
-
-    public function __construct( CollectionInterface $collection, $collectionName ){
+    public function __construct(CollectionInterface $collection, $collectionName)
+    {
         $this->collectionName = $collectionName;
         $this->setCollection($collection);
     }
 
-    public function setCollection( CollectionInterface $collection  ){
-
+    public function setCollection(CollectionInterface $collection)
+    {
         $this->linksSet = false;
         $this->collection = $collection;
 
-        //---
-
         $data = $collection->toArray();
 
-        //---
-
         // Add the resources...
-
-        foreach( $data['items'] as $item ){
-            $this->addResource( $this->collectionName, new Entity( $item ) );
+        foreach ($data['items'] as $item) {
+            $this->addResource($this->collectionName, new Entity($item));
         }
 
-        unset( $data['items'] );
+        unset($data['items']);
 
-        //---
-
-        $this->setData( $data );
-
-    } // function
-
-    //---
-
-    public function getLinks(){
-
-        if( !$this->linksSet ){ throw new RuntimeException( 'Cannot return links until they have been set.' ); }
-
-        return parent::getLinks();
-
+        $this->setData($data);
     }
 
-    //---
+    public function getLinks()
+    {
+        if (!$this->linksSet) {
+            throw new RuntimeException('Cannot return links until they have been set.');
+        }
 
-    public function setLinks( callable $routeCallback ){
+        return parent::getLinks();
+    }
 
+    public function setLinks(callable $routeCallback)
+    {
+        $callbackParam = 'api-v1/user/level-1';
         $currentPage = $this->collection->getCurrentPageNumber();
 
-        //-------------------------------------------------------------
         // First
+        $this->addLink('first', call_user_func($routeCallback, $callbackParam, $this->collection));
 
-        $this->addLink( 'first', call_user_func($routeCallback, 'api-v1/user/level-1', $this->collection) );
-
-        //-------------------------------------------------------------
         // Self
-
-        if( $currentPage == 1 ){
-
-            $this->addLink( 'self', call_user_func($routeCallback, 'api-v1/user/level-1', $this->collection) );
-
+        if ($currentPage == 1) {
+            $this->addLink('self', call_user_func($routeCallback, $callbackParam, $this->collection));
         } else {
-
-            $this->addLink( 'self', call_user_func(
-                $routeCallback, 'api-v1/user/level-1',
-                $this->collection,
-                [ 'page' => $currentPage ]
-            ));
-
-        } // if
-
-        //-------------------------------------------------------------
-        // Previous
-
-        if ($currentPage - 1 > 0) {
-
-            if ($currentPage - 1 == 1) {
-
-                $this->addLink( 'prev', call_user_func($routeCallback, 'api-v1/user/level-1', $this->collection) );
-
-            } else {
-
-                $this->addLink( 'prev', call_user_func(
-                    $routeCallback, 'api-v1/user/level-1',
-                    $this->collection,
-                    [ 'page' => $currentPage - 1 ]
-                ));
-
-            }
-
-        } // if
-
-        //-------------------------------------------------------------
-        // Next
-
-        // 'next'...
-        if ($currentPage + 1 <= $this->collection->count()) {
-
-            $this->addLink( 'next', call_user_func(
-                $routeCallback, 'api-v1/user/level-1',
-                $this->collection,
-                [ 'page' => $currentPage + 1 ]
-            ));
-
-        } // if
-
-        //-------------------------------------------------------------
-        // Last
-
-        if( $this->collection->count() <= 1 ){
-
-            $this->addLink( 'last', call_user_func($routeCallback, 'api-v1/user/level-1', $this->collection) );
-
-        } else {
-
-            $this->addLink( 'last', call_user_func(
-                $routeCallback, 'api-v1/user/level-1',
-                $this->collection,
-                [ 'page' => $this->collection->count() ]
-            ));
-
+            $this->addLink('self', call_user_func($routeCallback, $callbackParam, $this->collection, ['page' => $currentPage]));
         }
 
-        //-------------------------------------------------------------
-        // Add Links for resources...
+        // Previous
+        if ($currentPage - 1 > 0) {
+            if ($currentPage - 1 == 1) {
+                $this->addLink('prev', call_user_func($routeCallback, $callbackParam, $this->collection));
+            } else {
+                $this->addLink('prev', call_user_func($routeCallback, $callbackParam, $this->collection, ['page' => $currentPage - 1]));
+            }
+        }
 
+        // Next
+        if ($currentPage + 1 <= $this->collection->count()) {
+            $this->addLink('next', call_user_func($routeCallback, $callbackParam, $this->collection, ['page' => $currentPage + 1]));
+        }
+
+        // Last
+        if ($this->collection->count() <= 1) {
+            $this->addLink('last', call_user_func($routeCallback, $callbackParam, $this->collection));
+        } else {
+            $this->addLink('last', call_user_func($routeCallback, $callbackParam, $this->collection, ['page' => $this->collection->count()]));
+        }
+
+        // Add Links for resources...
         $resources = $this->getResources();
 
-        if( isset($resources[ $this->collectionName ]) ){
-            foreach( $resources[ $this->collectionName ] as $resource ){
-                $resource->setLinks( $routeCallback );
+        if (isset($resources[$this->collectionName])) {
+            foreach ($resources[$this->collectionName] as $resource) {
+                $resource->setLinks($routeCallback);
             }
         }
 
-        //-------------------------------------------------------------
-
         $this->linksSet = true;
-
-    } // function
-
-} // class
+    }
+}
--- service-api/module/Application/src/Application/Library/Hal/Entity.php
+++ service-api/module/Application/src/Application/Library/Hal/Entity.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace Application\Library\Hal;
 
-use RuntimeException;
-
 use Application\Model\Rest\EntityInterface;
+use Application\Model\Rest\Applications\Entity as ApplicationsEntity;
+use Application\Model\Rest\Stats\Entity as StatsEntity;
+use Application\Model\Rest\Users\Entity as UsersEntity;
+use RuntimeException;
 
 /**
  * Hal document representing a Application\Model\Rest\EntityInterface
@@ -11,66 +14,55 @@ use Application\Model\Rest\EntityInterface;
  * Class Entity
  * @package Application\Library\Hal
  */
-class Entity extends Hal {
-
+class Entity extends Hal
+{
     protected $entity;
 
     private $linksSet = false;
 
-    public function __construct( EntityInterface $entity ){
-        $this->setEntity( $entity );
+    public function __construct(EntityInterface $entity)
+    {
+        $this->setEntity($entity);
     }
 
-    public function setEntity( EntityInterface $entity ){
+    public function setEntity(EntityInterface $entity)
+    {
         $this->linksSet = false;
         $this->entity = $entity;
-        $this->setData( $entity->toArray() );
+        $this->setData($entity->toArray());
     }
 
-    //---
-
-    public function getLinks(){
-
-        if( !$this->linksSet ){ throw new RuntimeException( 'Cannot return links until they have been set.' ); }
+    public function getLinks()
+    {
+        if (!$this->linksSet) {
+            throw new RuntimeException('Cannot return links until they have been set.');
+        }
 
         return parent::getLinks();
-
     }
-
-    //---
 
     /**
      * Apply the links using the passed route generator.
      *
      * @param callable $routeCallback
      */
-    public function setLinks( callable $routeCallback ){
+    public function setLinks(callable $routeCallback)
+    {
+        if (!$this->entity instanceof StatsEntity) {
+            if ($this->entity instanceof UsersEntity) {
+                $callbackParam = 'api-v1/user';
+            } elseif ($this->entity instanceof ApplicationsEntity) {
+                $callbackParam = 'api-v1/user/level-1';
+                $this->addLink('user', call_user_func($routeCallback, 'api-v1/user', $this->entity));
+            } else {
+                $callbackParam = 'api-v1/user/level-2';
+                $this->addLink('user', call_user_func($routeCallback, 'api-v1/user', $this->entity));
+                $this->addLink('application', call_user_func($routeCallback, 'api-v1/user/level-1', $this->entity));
+            }
 
-        if( $this->entity instanceof \Application\Model\Rest\Stats\Entity ) {
-
-
-
-        } elseif( $this->entity instanceof \Application\Model\Rest\Users\Entity ) {
-
-            $this->setUri( call_user_func($routeCallback, 'api-v1/user', $this->entity) );
-
-        } elseif( $this->entity instanceof \Application\Model\Rest\Applications\Entity ){
-
-            $this->setUri( call_user_func($routeCallback, 'api-v1/user/level-1', $this->entity) );
-            $this->addLink( 'user', call_user_func($routeCallback, 'api-v1/user', $this->entity) );
-
-        } else {
-
-            $this->setUri( call_user_func($routeCallback, 'api-v1/user/level-2', $this->entity) );
-            $this->addLink( 'user', call_user_func($routeCallback, 'api-v1/user', $this->entity) );
-            $this->addLink( 'application', call_user_func($routeCallback, 'api-v1/user/level-1', $this->entity) );
-
+            $this->setUri(call_user_func($routeCallback, $callbackParam, $this->entity));
         }
 
-        //---
-
         $this->linksSet = true;
-
-    } // function
-
-} // class
+    }
+}
