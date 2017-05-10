ca835ec9dbf92f4ee8369e143edd415caf620107
Steven Taylor <taylorsj1980@gmail.com>
2017-05-10 09:27:01 +0100

    Add new make file for unit tests and skip statements

--- service-pdf/tests/Bootstrap.php
+++ service-pdf/tests/Bootstrap.php
@@ -1,9 +1,13 @@
 <?php
+
 date_default_timezone_set('Europe/London');
 error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', true);
 ini_set('log_errors', false);
 
-if (file_exists('../vendor/autoload.php')) {
-    $loader = include '../vendor/autoload.php';
+//  Autoload the vendor components
+$vendorAutoloadFile = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($vendorAutoloadFile)) {
+    require $vendorAutoloadFile;
 }
