29feba48fc76c20f29ef182977c458df6d205343
Steven Taylor <taylorsj1980@gmail.com>
2017-06-06 12:19:56 +0100

    PSR-2 refactor of AbstractWorker

--- service-pdf/src/Opg/Lpa/Pdf/Worker/AbstractWorker.php
+++ service-pdf/src/Opg/Lpa/Pdf/Worker/AbstractWorker.php
@@ -27,48 +27,43 @@ abstract class AbstractWorker
      * @param $docId
      * @return \Opg\Lpa\Pdf\Service\ResponseInterface
      */
-    abstract protected function getResponseObject( $docId );
+    abstract protected function getResponseObject($docId);
 
     /**
      * @param string $docId Unique ID representing this job/document.
      * @param string $type The type of PDF to generate.
      * @param string $lpa JSON document representing the LPA document.
+     * @throws Exception
      */
-    public function run( $docId, $type, $lpa ){
-
-        if( is_array($lpa) && isset($lpa['id']) ){
-
+    public function run($docId, $type, $lpa)
+    {
+        if (is_array($lpa) && isset($lpa['id'])) {
             $lpaId = $lpa['id'];
-
         } else {
-
             $lpaDecode = json_decode($lpa);
+
             if (property_exists($lpaDecode, 'id')) {
                 $lpaId = $lpaDecode->id;
             } else {
                 throw new \Exception('Missing field: id in JSON for docId: ' . $docId . ' This can be caused by incorrectly configured encryption keys.');
             }
-
         }
-
-        //---
 
         $this->logger->info("${docId}: Generating PDF\n", ['lpaId' => $lpaId]);
 
         try {
-
             $this->logger->info("Creating LPA document from JSON", ['lpaId' => $lpaId]);
 
             // Instantiate an LPA document from the JSON
-            $lpaObj = new Lpa( $lpa );
+            $lpaObj = new Lpa($lpa);
 
             // Create and config the $response object.
-            $response = $this->getResponseObject( $docId );
+            $response = $this->getResponseObject($docId);
 
             $this->logger->info("Creating generator", ['lpaId' => $lpaId]);
 
             // Create an instance of the PDF generator service.
-            $generator = new Generator( $type, $lpaObj, $response );
+            $generator = new Generator($type, $lpaObj, $response);
 
             $this->logger->info("Starting PDF generation", ['lpaId' => $lpaId]);
 
@@ -77,31 +72,27 @@ abstract class AbstractWorker
 
             // Check the result...
             if ($result === true) {
-
                 // All is good.
                 $this->logAndShow($lpaId, "${docId}: PDF successfully generated");
-
             } else {
-
                 // Else there was an error.
                 $this->logAndShow($lpaId, "${docId}: PDF generation failed: $result");
-
             }
-
-        } catch (Exception $e){
-
+        } catch (Exception $e) {
             $this->logAndShow($lpaId, "${docId}: PDF generation failed with exception: " . $e->getMessage());
-
         }
+    }
 
-    } // function
-
-    private function logAndShow($lpaId, $message) {
-
+    /**
+     * Log the message and show at the command line
+     *
+     * @param $lpaId
+     * @param $message
+     */
+    private function logAndShow($lpaId, $message)
+    {
         $this->logger->info($message, ['lpaId' => $lpaId]);
 
         echo $message . "\n";
-
     }
-
-} // class
+}
