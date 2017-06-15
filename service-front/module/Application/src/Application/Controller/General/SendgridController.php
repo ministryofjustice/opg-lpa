f81d1b600c0f14625bc1fc46ab72de29a968cd16
Steven Taylor <taylorsj1980@gmail.com>
2017-06-15 17:12:27 +0100

    PSR-2 refactor of sendgrid classes

--- service-front/module/Application/src/Application/Controller/General/SendgridController.php
+++ service-front/module/Application/src/Application/Controller/General/SendgridController.php
@@ -4,97 +4,86 @@ namespace Application\Controller\General;
 
 use Application\Controller\AbstractBaseController;
 use Application\Model\Service\Mail\Message as MessageService;
+use Zend\Mime\Message;
 use Zend\Mime\Part;
+use Exception;
 
 class SendgridController extends AbstractBaseController
 {
     public function bounceAction()
     {
-
-        $config = $this->getServiceLocator()->get('config');
-
-        //---
-
         $token = $this->params()->fromRoute('token');
 
-        if( !$token || $token !== $config['email']['sendgrid']['webhook']['token'] ){
+        $config = $this->getServiceLocator()->get('config');
+        $emailConfig = $config['email'];
+
+        if (!$token || $token !== $emailConfig['sendgrid']['webhook']['token']) {
             $response = $this->getResponse();
             $response->setStatusCode(403);
             $response->setContent('Invalid Token');
+
             return $response;
         }
 
-        //---
-
         $blackHoleAddress = 'blackhole@lastingpowerofattorney.service.gov.uk';
 
+        $fromAddress = $this->request->getPost('from');
         $originalToAddress = $this->request->getPost('to');
 
-        // If sent to this address, don't respond.
-        if( !is_string($originalToAddress) || strpos( strtolower($originalToAddress), $blackHoleAddress ) !== false ){
+        //  If there is no from email address, or the user has responded to the blackhole email address then do nothing
+        if (!is_string($fromAddress) || !is_string($originalToAddress) || strpos(strtolower($originalToAddress), $blackHoleAddress) !== false) {
             return $this->getResponse();
         }
 
-        //---
-
         $messageService = new MessageService();
-
-        $messageService->addFrom($blackHoleAddress, $config['email']['sender']['default']['name']);
-
-        $userEmail = $this->request->getPost('from');
-
+        $messageService->addFrom($blackHoleAddress, $emailConfig['sender']['default']['name']);
         $messageService->addCategory('opg');
         $messageService->addCategory('opg-lpa');
         $messageService->addCategory('opg-lpa-autoresponse');
 
-        if(!$userEmail) {
-            return $this->getResponse();
+        if (preg_match('/\<(.*)\>$/', $fromAddress, $matches)) {
+            $fromAddress = $matches[1];
         }
 
-        if (preg_match('/\<(.*)\>$/', $userEmail, $matches)) {
-            $userEmail = $matches[1];
-        }
+        $messageService->addTo($fromAddress);
 
-        $messageService->addTo( $userEmail );
+        //  Set the subject in the message
+        $content = $this->getServiceLocator()
+                        ->get('TwigEmailRenderer')
+                        ->loadTemplate('bounce.twig')
+                        ->render([]);
 
-        //---
-
-        $content = $this->getServiceLocator()->get('TwigEmailRenderer')->loadTemplate('bounce.twig')->render([]);
+        $subject = 'This mailbox is not monitored';
 
         if (preg_match('/<!-- SUBJECT: (.*?) -->/m', $content, $matches) === 1) {
-            $messageService->setSubject($matches[1]);
-        } else {
-            $messageService->setSubject( 'This mailbox is not monitored' );
+            $subject = $matches[1];
         }
 
-        //---
+        $messageService->setSubject($subject);
 
-        $html = new Part( $content );
+        //  Set the content in a mime message
+        $mimeMessage = new Message();
+        $html = new Part($content);
         $html->type = "text/html";
-
-        $mimeMessage = new \Zend\Mime\Message();
         $mimeMessage->setParts([$html]);
 
         $messageService->setBody($mimeMessage);
 
-        //---
-
         try {
-
-            $this->getServiceLocator()->get('MailTransport')->send($messageService);
+            $this->getServiceLocator()
+                 ->get('MailTransport')
+                 ->send($messageService);
 
             echo 'Email sent';
-
-        } catch ( \Exception $e ){
-
-            $this->log()->alert("Failed sending 'This mailbox is not monitored' email to ".$userEmail." due to:\n".$e->getMessage());
+        } catch (Exception $e) {
+            $this->log()->alert("Failed sending '" . $subject . "' email to " . $fromAddress . " due to:\n" . $e->getMessage());
 
             return "failed-sending-email";
-
         }
 
         $response = $this->getResponse();
         $response->setStatusCode(200);
+
         return $response;
     }
 }
