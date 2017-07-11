ced284509497d06f8e75093bde86b0b806f5764a
David Winchurch <david.winchurch@iterativeit.com>
2017-07-11 11:16:39 +0100

    LPA-2313: Mocked enough of the Application Resource to pass a basic test

--- service-api/module/Application/tests/ApplicationTest/Model/Rest/Applications/ResourceTest.php
+++ service-api/module/Application/tests/ApplicationTest/Model/Rest/Applications/ResourceTest.php
@@ -6,6 +6,7 @@ use Application\Model\Rest\Applications\Entity;
 use Application\Model\Rest\Applications\Resource;
 use Application\Model\Rest\Users\Entity as UserEntity;
 use Mockery;
+use MongoCollection;
 use Opg\Lpa\DataModel\User\User;
 use Opg\Lpa\Logger\Logger;
 use Zend\Log\LoggerInterface;
@@ -30,25 +31,38 @@ class ResourceTest extends \PHPUnit_Framework_TestCase
     {
         parent::setUp();
 
-        $user = new User();
-        $user->set('id', $this->userId);
+        $this->user = new User();
+        $this->user->set('id', $this->userId);
+
+        $authorizationServiceMock = Mockery::mock(AuthorizationService::class);
+        $authorizationServiceMock
+            ->shouldReceive('isGranted')
+            ->with('authenticated')
+            ->andReturn(true);
+        $authorizationServiceMock
+            ->shouldReceive('isGranted')
+            ->with('isAuthorizedToManageUser', $this->userId)
+            ->andReturn(true);
 
         $loggerMock = Mockery::mock(LoggerInterface::class);
         $loggerMock->shouldReceive('info');
-        $authorizationServiceMock = Mockery::mock(AuthorizationService::class);
-        $authorizationServiceMock->shouldReceive('isGranted')->with('authenticated')->andReturn(true);
-        $authorizationServiceMock->shouldReceive('isGranted')->with('isAuthorizedToManageUser', $this->userId)->andReturn(true);
-        $loggerMock->shouldReceive('info');
+
         $serviceLocatorMock = Mockery::mock(ServiceLocatorInterface::class);
         $serviceLocatorMock->shouldReceive('get')->with('Logger')->andReturn($loggerMock);
+
+        $lpaCollection = Mockery::mock(MongoCollection::class);
+        $lpaCollection->shouldReceive('findOne')->andReturn(null);
+        $serviceLocatorMock->shouldReceive('get')->with('MongoDB-Default-lpa')->andReturn($lpaCollection);
+
         $this->resource = new Resource();
         $this->resource->setServiceLocator($serviceLocatorMock);
         $this->resource->setAuthorizationService($authorizationServiceMock);
-        $this->resource->setRouteUser(new UserEntity($user));
+        $this->resource->setRouteUser(new UserEntity($this->user));
     }
 
     public function testCreateNullData()
     {
+        $this->setExpectedException(\RuntimeException::class, 'A malformed LPA object was created');
         $this->resource->create(null);
     }
 }
