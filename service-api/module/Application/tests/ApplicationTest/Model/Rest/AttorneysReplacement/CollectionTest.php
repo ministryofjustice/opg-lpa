2d0580fe3a3f94595de9fb92978cb04bb9bb9599
David Winchurch <david.winchurch@iterativeit.com>
2017-07-13 14:58:04 +0100

    LPA-2313: 100% coverage for AttorneysReplacement

--- service-api/module/Application/tests/ApplicationTest/Model/Rest/AttorneysReplacement/CollectionTest.php
+++ service-api/module/Application/tests/ApplicationTest/Model/Rest/AttorneysReplacement/CollectionTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace ApplicationTest\Model\Rest\AttorneysPrimary;
+namespace ApplicationTest\Model\Rest\AttorneysReplacement;
 
-use Application\Model\Rest\AttorneysPrimary\Collection;
-use Application\Model\Rest\AttorneysPrimary\Entity;
+use Application\Model\Rest\AttorneysReplacement\Collection;
+use Application\Model\Rest\AttorneysReplacement\Entity;
 use OpgTest\Lpa\DataModel\FixturesData;
 use Zend\Paginator\Adapter\ArrayAdapter;
 use Zend\Paginator\Adapter\NullFill;
@@ -38,19 +38,19 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
     public function testToArray()
     {
         $lpa = FixturesData::getPfLpa();
-        $primaryAttorneys = $lpa->document->primaryAttorneys;
-        $collection = new Collection(new ArrayAdapter($primaryAttorneys), $lpa);
+        $replacementAttorneys = $lpa->document->replacementAttorneys;
+        $collection = new Collection(new ArrayAdapter($replacementAttorneys), $lpa);
 
         $array = $collection->toArray();
 
-        $this->assertEquals(count($primaryAttorneys), $array['count']);
-        $this->assertEquals(count($primaryAttorneys), $array['total']);
+        $this->assertEquals(count($replacementAttorneys), $array['count']);
+        $this->assertEquals(count($replacementAttorneys), $array['total']);
         $this->assertEquals(1, $array['pages']);
         /* @var $items Entity[] */
         $items = $array['items'];
-        for ($i = 0; $i < count($primaryAttorneys); $i++) {
+        for ($i = 0; $i < count($replacementAttorneys); $i++) {
             $this->assertTrue($array['items'][$i] instanceof Entity);
-            $this->assertEquals(new Entity($primaryAttorneys[$i], $lpa), $items[$i]);
+            $this->assertEquals(new Entity($replacementAttorneys[$i], $lpa), $items[$i]);
         }
     }
 }
