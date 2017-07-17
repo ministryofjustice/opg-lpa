c00c93ef3ee404981ca7e1e1a05706d42da5a2e8
David Winchurch <david.winchurch@iterativeit.com>
2017-07-17 14:53:48 +0100

    LPA-2313: Fetch unit tests

--- service-api/module/Application/tests/ApplicationTest/Model/Rest/Pdfs/ResourceTest.php
+++ service-api/module/Application/tests/ApplicationTest/Model/Rest/Pdfs/ResourceTest.php
@@ -2,13 +2,17 @@
 
 namespace ApplicationTest\Model\Rest\Pdfs;
 
+use Application\Library\ApiProblem\ApiProblem;
+use Application\Library\ApiProblem\ValidationApiProblem;
 use Application\Model\Rest\AbstractResource;
+use Application\Model\Rest\Pdfs\Entity;
 use Application\Model\Rest\Pdfs\Resource as PdfsResource;
 use Application\Model\Rest\Pdfs\Resource;
 use ApplicationTest\Model\AbstractResourceTest;
 use Aws\S3\S3Client;
 use DynamoQueue\Queue\Client as DynamoQueue;
 use Mockery;
+use OpgTest\Lpa\DataModel\FixturesData;
 
 class ResourceTest extends AbstractResourceTest
 {
@@ -69,6 +73,58 @@ class ResourceTest extends AbstractResourceTest
         /** @var PdfsResource $resource */
         $resource = parent::setUpCheckAccessTest(new ResourceBuilder());
         $resource->fetch(-1);
+    }
+
+    public function testFetchValidationFailed()
+    {
+        //The bad id value on this user will fail validation
+        $lpa = FixturesData::getHwLpa();
+        $lpa->user = 3;
+        $resourceBuilder = new ResourceBuilder();
+        $resource = $resourceBuilder->withUser(FixturesData::getUser())->withLpa($lpa)->build();
+
+        $validationError = $resource->fetch(-1);
+
+        $this->assertTrue($validationError instanceof ValidationApiProblem);
+        $this->assertEquals(400, $validationError->status);
+        $this->assertEquals('Your request could not be processed due to validation error', $validationError->detail);
+        $this->assertEquals('https://github.com/ministryofjustice/opg-lpa-datamodels/blob/master/docs/validation.md', $validationError->type);
+        $this->assertEquals('Bad Request', $validationError->title);
+        $validation = $validationError->validation;
+        $this->assertEquals(1, count($validation));
+        $this->assertTrue(array_key_exists('user', $validation));
+
+        $resourceBuilder->verify();
+    }
+
+    public function testFetchLpa120NotAvailable()
+    {
+        $lpa = FixturesData::getHwLpa();
+        $resourceBuilder = new ResourceBuilder();
+        $resource = $resourceBuilder->withUser(FixturesData::getUser())->withLpa($lpa)->build();
+
+        $entity = $resource->fetch('lpa120');
+
+        $this->assertTrue($entity instanceof Entity);
+        $this->assertEquals(new Entity([
+            'type' => 'lpa120',
+            'complete' => false,
+            'status' => Entity::STATUS_NOT_AVAILABLE
+        ], $lpa), $entity);
+    }
+
+    public function testFetchNotFound()
+    {
+        $resourceBuilder = new ResourceBuilder();
+        $resource = $resourceBuilder->withUser(FixturesData::getUser())->withLpa(FixturesData::getPfLpa())->build();
+
+        $entity = $resource->fetch(-1);
+
+        $this->assertTrue($entity instanceof ApiProblem);
+        $this->assertEquals(404, $entity->status);
+        $this->assertEquals('Document not found', $entity->detail);
+
+        $resourceBuilder->verify();
     }
 
     public function testFetchAllCheckAccess()
