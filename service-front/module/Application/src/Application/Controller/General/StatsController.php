50f658c04bdc971c4e7f82bdfe435c506c9ecfe4
David Winchurch <david.winchurch@iterativeit.com>
2017-07-27 15:11:10 +0100

    LPA-2324: Correspondence stats

--- service-front/module/Application/src/Application/Controller/General/StatsController.php
+++ service-front/module/Application/src/Application/Controller/General/StatsController.php
@@ -29,10 +29,15 @@ class StatsController extends AbstractBaseController
 
         //---
 
+        $welshLanguageStats = $this->getServiceLocator()->get('LpaApplicationService')->getApiStats( 'welshlanguage' );
+
+        //---
+
         return new ViewModel([
             'lpas' => $generalLpaStats,
             'who' => $whoAreYouStats,
             'users' => $userStats,
+            'welshLanguage' => $welshLanguageStats,
         ]);
 
     } // function
