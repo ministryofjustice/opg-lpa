a8c71d008fe7cc9d38c3fe201c1b9a49d058fe81
Steven Taylor <taylorsj1980@gmail.com>
2017-08-09 11:32:23 +0100

    Utilise datamodels updates from feature branch for LPA-2361

--- service-api/vendor/ministryofjustice/opg-lpa-datamodels/tests/Bootstrap.php
+++ service-api/vendor/ministryofjustice/opg-lpa-datamodels/tests/Bootstrap.php
@@ -1,16 +1,34 @@
 <?php
 
 date_default_timezone_set('UTC');
-error_reporting(E_ALL|E_STRICT);
-ini_set('display_errors', true);
-ini_set('log_errors', false);
 
-//  Autoload the vendor components
-$vendorAutoloadFile = __DIR__ . '/../vendor/autoload.php';
+/**
+ * Simple autoloader function to dynamically load
+ * the required files as they are instantiated
+ */
+spl_autoload_register(function ($class) {
 
-if (file_exists($vendorAutoloadFile)) {
-    require $vendorAutoloadFile;
+    //  Base directories where namespaced files reside
+    $baseDirs = [
+        __DIR__ . '/',
+        __DIR__ . '/../src/',
+    ];
+
+    //  Loop through the base directories to try to find the requested class
+    foreach ($baseDirs as $baseDir) {
+        //  Replace the separators with directory separators in the relative class name, append and with .php
+        $file = $baseDir . str_replace('\\', '/', $class) . '.php';
+
+        // if the file exists, require it
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+});
+
+//  If it exists, hook into the composer autoload file too
+$composerAutoloadFile = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($composerAutoloadFile)) {
+    require_once $composerAutoloadFile;
 }
-
-//Autoload test components
-require_once 'autoload.php';
