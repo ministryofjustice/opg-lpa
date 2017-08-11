95f8e1a401a21f285a9869e6892e9c2e78b52ee9
David Winchurch <david.winchurch@iterativeit.com>
2017-08-11 13:24:33 +0100

    LPA-2316: Working pattern for testing controllers with authentication and redirects

--- service-front/module/Application/tests/ApplicationTest/Controller/AbstractControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/AbstractControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace ApplicationTest\Controller;
 
+use Application\Model\Service\Authentication\AuthenticationService;
 use Mockery;
 use Mockery\MockInterface;
 use Opg\Lpa\Logger\Logger;
 use PHPUnit_Framework_Error_Deprecated;
+use Zend\Mvc\Controller\Plugin\Redirect;
+use Zend\Mvc\Controller\PluginManager;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 abstract class AbstractControllerTest extends \PHPUnit_Framework_TestCase
@@ -18,6 +21,18 @@ abstract class AbstractControllerTest extends \PHPUnit_Framework_TestCase
      * @var MockInterface|Logger
      */
     protected $logger;
+    /**
+     * @var MockInterface|AuthenticationService
+     */
+    protected $authenticationService;
+    /**
+     * @var MockInterface|PluginManager
+     */
+    protected $pluginManager;
+    /**
+     * @var MockInterface|Redirect
+     */
+    protected $redirect;
 
     public function setUp()
     {
@@ -32,6 +47,15 @@ abstract class AbstractControllerTest extends \PHPUnit_Framework_TestCase
         $this->logger = Mockery::mock(Logger::class);
         $this->logger->shouldReceive('info');
         $this->serviceLocator->shouldReceive('get')->with('Logger')->andReturn($this->logger);
+
+        $this->authenticationService = Mockery::mock(AuthenticationService::class);
+        $this->serviceLocator->shouldReceive('get')->with('AuthenticationService')->andReturn($this->authenticationService);
+
+        $this->pluginManager = Mockery::mock(PluginManager::class);
+        $this->pluginManager->shouldReceive('setController');
+
+        $this->redirect = Mockery::mock(Redirect::class);
+        $this->pluginManager->shouldReceive('get')->with('redirect', null)->andReturn($this->redirect);
     }
 
     public function tearDown()
--- service-front/module/Application/tests/ApplicationTest/Controller/General/AuthControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/General/AuthControllerTest.php
@@ -2,7 +2,46 @@
 
 namespace ApplicationTest\Controller\General;
 
-class AuthControllerTest
-{
+use Application\Controller\General\AuthController;
+use Application\Model\Service\Authentication\Identity\User;
+use ApplicationTest\Controller\AbstractControllerTest;
+use Mockery;
+use Zend\Http\Response;
+use Zend\Mvc\Router\RouteStackInterface;
 
+class AuthControllerTest extends AbstractControllerTest
+{
+    /**
+     * @var AuthController
+     */
+    private $controller;
+    /**
+     * @var User
+     */
+    private $identity;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->router = Mockery::mock(RouteStackInterface::class);
+
+        $this->controller = new AuthController();
+        $this->controller->setServiceLocator($this->serviceLocator);
+        $this->controller->setPluginManager($this->pluginManager);
+
+        $this->identity = Mockery::mock(User::class);
+    }
+
+    public function testIndexActionAlreadySignedIn()
+    {
+        $response = new Response();
+
+        $this->authenticationService->shouldReceive('getIdentity')->andReturn($this->identity)->once();
+        $this->redirect->shouldReceive('toRoute')->with('user/dashboard')->andReturn($response)->once();
+
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($response, $result);
+    }
 }
