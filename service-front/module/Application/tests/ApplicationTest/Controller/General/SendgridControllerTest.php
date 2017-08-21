153c7792c02737261061f9d854713a7170648dbd
David Winchurch <david.winchurch@iterativeit.com>
2017-08-21 19:43:04 +0100

    LPA-2316: Starting on Sendgrid tests

--- service-front/module/Application/tests/ApplicationTest/Controller/General/SendgridControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/General/SendgridControllerTest.php
@@ -4,6 +4,8 @@ namespace ApplicationTest\Controller\General;
 
 use Application\Controller\General\SendgridController;
 use ApplicationTest\Controller\AbstractControllerTest;
+use Zend\Http\Response;
+use Zend\View\Model\ViewModel;
 
 class SendgridControllerTest extends AbstractControllerTest
 {
@@ -20,6 +22,19 @@ class SendgridControllerTest extends AbstractControllerTest
 
     public function testIndexAction()
     {
-        $this->controller->indexAction();
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals('Placeholder page', $result->getVariable('content'));
+    }
+
+    public function testBounceActionBlanckFromAddress()
+    {
+        /** @var Response $result */
+        $result = $this->controller->bounceAction();
+
+        $this->assertInstanceOf(Response::class, $result);
     }
 }
