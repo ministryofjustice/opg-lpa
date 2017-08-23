53c9a233a7eba6813a4db630ef03377e0de322b8
David Winchurch <david.winchurch@iterativeit.com>
2017-08-23 09:22:04 +0100

    LPA-2316: 100% coverage for ChangePasswordController

--- service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/ChangePasswordControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/ChangePasswordControllerTest.php
@@ -4,10 +4,12 @@ namespace ApplicationTest\Controller\Authenticated;
 
 use Application\Controller\Authenticated\ChangePasswordController;
 use Application\Form\User\ChangePassword;
+use Application\Model\Service\User\Details;
 use ApplicationTest\Controller\AbstractControllerTest;
 use Mockery;
 use Mockery\MockInterface;
 use OpgTest\Lpa\DataModel\FixturesData;
+use Zend\Http\Response;
 use Zend\View\Model\ViewModel;
 
 class ChangePasswordControllerTest extends AbstractControllerTest
@@ -20,6 +22,13 @@ class ChangePasswordControllerTest extends AbstractControllerTest
      * @var MockInterface|ChangePassword
      */
     private $form;
+    /**
+     * @var MockInterface|Details
+     */
+    private $aboutYouDetails;
+    private $postData = [
+
+    ];
 
     public function setUp()
     {
@@ -29,11 +38,14 @@ class ChangePasswordControllerTest extends AbstractControllerTest
         $this->form = Mockery::mock(ChangePassword::class);
         $this->formElementManager->shouldReceive('get')->with('Application\Form\User\ChangePassword')->andReturn($this->form);
 
+        $this->aboutYouDetails = Mockery::mock(Details::class);
+        $this->serviceLocator->shouldReceive('get')->with('AboutYouDetails')->andReturn($this->aboutYouDetails);
+
         $this->user = FixturesData::getUser();
         $this->userDetailsSession->user = $this->user;
     }
 
-    public function testIndexAction()
+    public function testIndexActionGet()
     {
         $this->url->shouldReceive('fromRoute')->with('user/change-password')->andReturn('user/change-password')->once();
         $this->form->shouldReceive('setAttribute')->with('action', 'user/change-password')->once();
@@ -41,6 +53,75 @@ class ChangePasswordControllerTest extends AbstractControllerTest
         $this->authenticationService->shouldReceive('setAdapter')->with($this->authenticationAdapter)->once();
         $this->form->shouldReceive('setAuthenticationService')->with($this->authenticationService)->once();
         $this->request->shouldReceive('isPost')->andReturn(false)->once();
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals(null, $result->getVariable('error'));
+        $this->assertEquals('Change your password', $result->getVariable('pageTitle'));
+    }
+
+    public function testIndexActionPostInvalid()
+    {
+        $this->url->shouldReceive('fromRoute')->with('user/change-password')->andReturn('user/change-password')->once();
+        $this->form->shouldReceive('setAttribute')->with('action', 'user/change-password')->once();
+        $this->authenticationAdapter->shouldReceive('setEmail')->with($this->user->email->address)->once();
+        $this->authenticationService->shouldReceive('setAdapter')->with($this->authenticationAdapter)->once();
+        $this->form->shouldReceive('setAuthenticationService')->with($this->authenticationService)->once();
+        $this->request->shouldReceive('isPost')->andReturn(true)->once();
+        $this->request->shouldReceive('getPost')->andReturn($this->postData)->once();
+        $this->form->shouldReceive('setData')->with($this->postData)->once();
+        $this->form->shouldReceive('isValid')->andReturn(false)->once();
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals(null, $result->getVariable('error'));
+        $this->assertEquals('Change your password', $result->getVariable('pageTitle'));
+    }
+
+    public function testIndexActionPostValid()
+    {
+        $response = new Response();
+
+        $this->url->shouldReceive('fromRoute')->with('user/change-password')->andReturn('user/change-password')->once();
+        $this->form->shouldReceive('setAttribute')->with('action', 'user/change-password')->once();
+        $this->authenticationAdapter->shouldReceive('setEmail')->with($this->user->email->address)->once();
+        $this->authenticationService->shouldReceive('setAdapter')->with($this->authenticationAdapter)->once();
+        $this->form->shouldReceive('setAuthenticationService')->with($this->authenticationService)->once();
+        $this->request->shouldReceive('isPost')->andReturn(true)->once();
+        $this->request->shouldReceive('getPost')->andReturn($this->postData)->once();
+        $this->form->shouldReceive('setData')->with($this->postData)->once();
+        $this->form->shouldReceive('isValid')->andReturn(true)->once();
+        $this->aboutYouDetails->shouldReceive('updatePassword')->with($this->form)->andReturn(true)->once();
+        $this->flashMessenger->shouldReceive('addSuccessMessage')->with('Your new password has been saved. Please remember to use this new password to sign in from now on.')->once();
+        $this->redirect->shouldReceive('toRoute')->with('user/about-you')->andReturn($response)->once();
+
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($response, $result);
+    }
+
+    public function testIndexActionUpdateFailes()
+    {
+        $response = new Response();
+
+        $this->url->shouldReceive('fromRoute')->with('user/change-password')->andReturn('user/change-password')->once();
+        $this->form->shouldReceive('setAttribute')->with('action', 'user/change-password')->once();
+        $this->authenticationAdapter->shouldReceive('setEmail')->with($this->user->email->address)->once();
+        $this->authenticationService->shouldReceive('setAdapter')->with($this->authenticationAdapter)->once();
+        $this->form->shouldReceive('setAuthenticationService')->with($this->authenticationService)->once();
+        $this->request->shouldReceive('isPost')->andReturn(true)->once();
+        $this->request->shouldReceive('getPost')->andReturn($this->postData)->once();
+        $this->form->shouldReceive('setData')->with($this->postData)->once();
+        $this->form->shouldReceive('isValid')->andReturn(true)->once();
+        $this->aboutYouDetails->shouldReceive('updatePassword')->with($this->form)->andReturn(false)->once();
 
         /** @var ViewModel $result */
         $result = $this->controller->indexAction();
