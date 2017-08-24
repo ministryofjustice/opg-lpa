df15fd3f1cab5a1f92af0c66b24e581d28a23897
David Winchurch <david.winchurch@iterativeit.com>
2017-08-24 14:53:07 +0100

    LPA-2316: 100% coverage for TypeController

--- service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/TypeControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/TypeControllerTest.php
@@ -4,10 +4,17 @@ namespace ApplicationTest\Controller\Authenticated;
 
 use Application\Controller\Authenticated\TypeController;
 use Application\Form\Lpa\TypeForm;
+use Application\Model\Service\ApiClient\Response\Lpa;
 use ApplicationTest\Controller\AbstractControllerTest;
 use DateTime;
 use Mockery;
 use Mockery\MockInterface;
+use Opg\Lpa\DataModel\Lpa\Document\Document;
+use OpgTest\Lpa\DataModel\FixturesData;
+use RuntimeException;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
 use Zend\View\Model\ViewModel;
 
 class TypeControllerTest extends AbstractControllerTest
@@ -20,6 +27,9 @@ class TypeControllerTest extends AbstractControllerTest
      * @var MockInterface|TypeForm
      */
     private $form;
+    private $postData = [
+        'type' => 'property-and-financial'
+    ];
 
     public function setUp()
     {
@@ -30,7 +40,7 @@ class TypeControllerTest extends AbstractControllerTest
         $this->formElementManager->shouldReceive('get')->with('Application\Form\Lpa\TypeForm')->andReturn($this->form);
     }
 
-    public function testIndexAction()
+    public function testIndexActionGet()
     {
         $this->request->shouldReceive('isPost')->andReturn(false)->once();
 
@@ -46,4 +56,88 @@ class TypeControllerTest extends AbstractControllerTest
             'dimension3' => 0
         ], $result->getVariable('analyticsDimensions'));
     }
+
+    public function testIndexActionPostInvalid()
+    {
+        $this->request->shouldReceive('isPost')->andReturn(true)->once();
+        $this->request->shouldReceive('getPost')->andReturn($this->postData)->once();
+        $this->form->shouldReceive('setData')->with($this->postData)->once();
+        $this->form->shouldReceive('isValid')->andReturn(false)->once();
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('application/type/index', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals(true, $result->getVariable('isChangeAllowed'));
+        $this->assertEquals([
+            'dimension2' => (new DateTime())->format('Y-m-d'),
+            'dimension3' => 0
+        ], $result->getVariable('analyticsDimensions'));
+    }
+
+    public function testIndexActionPostCreationError()
+    {
+        $response = new Response();
+
+        $this->request->shouldReceive('isPost')->andReturn(true)->once();
+        $this->request->shouldReceive('getPost')->andReturn($this->postData)->once();
+        $this->form->shouldReceive('setData')->with($this->postData)->once();
+        $this->form->shouldReceive('isValid')->andReturn(true)->once();
+        $this->lpaApplicationService->shouldReceive('createApplication')->andReturn(null)->once();
+        $this->flashMessenger->shouldReceive('addErrorMessage')->with('Error creating a new LPA. Please try again.')->once();
+        $this->redirect->shouldReceive('toRoute')->with('user/dashboard')->andReturn($response)->once();
+
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($response, $result);
+    }
+
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage API client failed to set LPA type for id: 5531003156
+     */
+    public function testIndexActionPostSetTypeException()
+    {
+        $this->request->shouldReceive('isPost')->andReturn(true)->once();
+        $this->request->shouldReceive('getPost')->andReturn($this->postData)->once();
+        $this->form->shouldReceive('setData')->with($this->postData)->once();
+        $this->form->shouldReceive('isValid')->andReturn(true)->once();
+        $lpa = FixturesData::getHwLpa();
+        $this->lpaApplicationService->shouldReceive('createApplication')->andReturn($lpa)->once();
+        $this->form->shouldReceive('getData')->andReturn($this->postData)->once();
+        $this->lpaApplicationService->shouldReceive('setType')->andReturn($lpa->id, $this->postData['type'])->andReturn(false)->once();
+
+        $this->controller->indexAction();
+    }
+
+    public function testIndexAction()
+    {
+        $response = new Response();
+
+        $this->request->shouldReceive('isPost')->andReturn(true)->once();
+        $this->request->shouldReceive('getPost')->andReturn($this->postData)->once();
+        $this->form->shouldReceive('setData')->with($this->postData)->once();
+        $this->form->shouldReceive('isValid')->andReturn(true)->once();
+        $lpa = new Lpa();
+        $lpa->id = 123;
+        $lpa->document = new Document();
+        $lpa->document->type = $this->postData['type'];
+        $this->lpaApplicationService->shouldReceive('createApplication')->andReturn($lpa)->once();
+        $this->form->shouldReceive('getData')->andReturn($this->postData)->once();
+        $this->lpaApplicationService->shouldReceive('setType')->andReturn($lpa->id, $this->postData['type'])->andReturn(true)->once();
+
+        $event = new MvcEvent();
+        $routeMatch = Mockery::mock(RouteMatch::class);
+        $event->setRouteMatch($routeMatch);
+        $this->controller->setEvent($event);
+        $routeMatch->shouldReceive('getMatchedRouteName')->andReturn('lpa/form-type')->once();
+        $this->redirect->shouldReceive('toRoute')->with('lpa/donor', ['lpa-id' => $lpa->id], ['fragment' => 'current'])->andReturn($response)->once();
+
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($response, $result);
+    }
+
 }
