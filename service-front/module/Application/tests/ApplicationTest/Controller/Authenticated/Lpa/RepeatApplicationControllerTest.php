3b3d3866ef7ac829917f486adaaa45605791f833
David Winchurch <david.winchurch@iterativeit.com>
2017-08-30 15:22:34 +0100

    LPA-2316: 100% 100% coverage for RepeatApplicationController

--- service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/Lpa/RepeatApplicationControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/Lpa/RepeatApplicationControllerTest.php
@@ -13,6 +13,7 @@ use Mockery\MockInterface;
 use Opg\Lpa\DataModel\Lpa\Lpa;
 use OpgTest\Lpa\DataModel\FixturesData;
 use RuntimeException;
+use Zend\Http\Response;
 use Zend\View\Model\ViewModel;
 
 class RepeatApplicationControllerTest extends AbstractControllerTest
@@ -29,6 +30,13 @@ class RepeatApplicationControllerTest extends AbstractControllerTest
      * @var Lpa
      */
     private $lpa;
+    private $postDataNoRepeat = [
+        'isRepeatApplication' => 'no-repeat'
+    ];
+    private $postDataRepeat = [
+        'isRepeatApplication' => 'is-repeat',
+        'repeatCaseNumber' => '12345'
+    ];
 
     public function setUp()
     {
@@ -65,5 +73,124 @@ class RepeatApplicationControllerTest extends AbstractControllerTest
         $this->assertEquals('', $result->getTemplate());
         $this->assertEquals($this->form, $result->getVariable('form'));
         $this->assertEquals(41, $result->getVariable('lpaRepeatFee'));
+    }
+
+    public function testIndexActionGet()
+    {
+        $this->controller->setLpa($this->lpa);
+        $this->request->shouldReceive('isPost')->andReturn(false)->once();
+        $this->form->shouldReceive('bind')->withArgs([[
+            'isRepeatApplication' => 'is-new',
+            'repeatCaseNumber'    => null,
+        ]])->once();
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals(41, $result->getVariable('lpaRepeatFee'));
+    }
+
+    public function testIndexActionPostNoRepeatInvalid()
+    {
+        $this->controller->setLpa($this->lpa);
+        $this->setPostInvalid($this->form, $this->postDataNoRepeat);
+        $this->form->shouldReceive('setValidationGroup')->withArgs(['isRepeatApplication'])->once();
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals(41, $result->getVariable('lpaRepeatFee'));
+    }
+
+    public function testIndexActionPostRepeatInvalid()
+    {
+        $this->controller->setLpa($this->lpa);
+        $this->setPostInvalid($this->form, $this->postDataRepeat);
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals(41, $result->getVariable('lpaRepeatFee'));
+    }
+
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage API client failed to set repeat case number for id: 91333263035
+     */
+    public function testIndexActionPostNoRepeatFailed()
+    {
+        $this->lpa->repeatCaseNumber = 12345;
+        $this->controller->setLpa($this->lpa);
+        $this->setPostValid($this->form, $this->postDataNoRepeat);
+        $this->form->shouldReceive('setValidationGroup')->withArgs(['isRepeatApplication'])->once();
+        $this->form->shouldReceive('getData')->andReturn($this->postDataNoRepeat)->once();
+        $this->lpaApplicationService->shouldReceive('deleteRepeatCaseNumber')->withArgs([$this->lpa->id])->andReturn(false)->once();
+
+        $this->controller->indexAction();
+    }
+
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage API client failed to set repeat case number for id: 91333263035
+     */
+    public function testIndexActionPostRepeatFailed()
+    {
+        $this->controller->setLpa($this->lpa);
+        $this->setPostValid($this->form, $this->postDataRepeat);
+        $this->form->shouldReceive('getData')->andReturn($this->postDataRepeat)->times(3);
+        $this->lpaApplicationService->shouldReceive('setRepeatCaseNumber')->withArgs([$this->lpa->id, $this->postDataRepeat['repeatCaseNumber']])->andReturn(false)->once();
+
+        $this->controller->indexAction();
+    }
+
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage API client failed to set payment details for id: 91333263035 in RepeatApplicationController
+     */
+    public function testIndexActionPostRepeatSetPaymentFailed()
+    {
+        $this->controller->setLpa($this->lpa);
+        $this->setPostValid($this->form, $this->postDataRepeat);
+        $this->form->shouldReceive('getData')->andReturn($this->postDataRepeat)->times(4);
+        $this->lpaApplicationService->shouldReceive('setRepeatCaseNumber')->withArgs([$this->lpa->id, $this->postDataRepeat['repeatCaseNumber']])->andReturn(true)->once();
+        $this->lpaApplicationService->shouldReceive('setPayment')->withArgs(function ($lpaId, $payment) {
+            return $lpaId === $this->lpa->id
+                && $payment->amount === 41.0;
+        })->andReturn(false)->once();
+
+        $this->controller->indexAction();
+    }
+
+    public function testIndexActionPostNoRepeatSuccess()
+    {
+        $response = new Response();
+
+        $this->lpa->repeatCaseNumber = 12345;
+        $this->controller->setLpa($this->lpa);
+        $this->setPostValid($this->form, $this->postDataNoRepeat);
+        $this->form->shouldReceive('setValidationGroup')->withArgs(['isRepeatApplication'])->once();
+        $this->form->shouldReceive('getData')->andReturn($this->postDataNoRepeat)->once();
+        $this->lpaApplicationService->shouldReceive('deleteRepeatCaseNumber')->withArgs([$this->lpa->id])->andReturn(true)->once();
+        $this->lpaApplicationService->shouldReceive('setPayment')->withArgs(function ($lpaId, $payment) {
+            return $lpaId === $this->lpa->id
+                && $payment->amount === 82.0;
+        })->andReturn(true)->once();
+        $this->metadata->shouldReceive('setRepeatApplicationConfirmed')->withArgs([$this->lpa])->once();
+        $this->request->shouldReceive('isXmlHttpRequest')->andReturn(false)->once();
+        $this->setMatchedRouteNameHttp($this->controller, 'lpa/fee-reduction');
+        $this->setRedirectToRoute('lpa/checkout', $this->lpa, $response, false);
+
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($response, $result);
     }
 }
