c066ad29119aa2edfc7420a3e37885390fa5cfcc
David Winchurch <david.winchurch@iterativeit.com>
2017-08-30 16:14:44 +0100

    LPA-2316: 100% coverage for Type and WhenLpaStarts controllers

--- service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/Lpa/WhenLpaStartsControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/Lpa/WhenLpaStartsControllerTest.php
@@ -7,9 +7,11 @@ use Application\Form\Lpa\WhenLpaStartsForm;
 use ApplicationTest\Controller\AbstractControllerTest;
 use Mockery;
 use Mockery\MockInterface;
+use Opg\Lpa\DataModel\Lpa\Document\Decisions\PrimaryAttorneyDecisions;
 use Opg\Lpa\DataModel\Lpa\Lpa;
 use OpgTest\Lpa\DataModel\FixturesData;
 use RuntimeException;
+use Zend\Http\Response;
 use Zend\View\Model\ViewModel;
 
 class WhenLpaStartsControllerTest extends AbstractControllerTest
@@ -26,6 +28,9 @@ class WhenLpaStartsControllerTest extends AbstractControllerTest
      * @var Lpa
      */
     private $lpa;
+    private $postData = [
+        'when' => PrimaryAttorneyDecisions::LPA_DECISION_WHEN_NO_CAPACITY
+    ];
 
     public function setUp()
     {
@@ -58,5 +63,55 @@ class WhenLpaStartsControllerTest extends AbstractControllerTest
         $this->assertInstanceOf(ViewModel::class, $result);
         $this->assertEquals('', $result->getTemplate());
         $this->assertEquals($this->form, $result->getVariable('form'));
+    }
+
+    public function testIndexActionPostInvalid()
+    {
+        $this->controller->setLpa($this->lpa);
+        $this->setPostInvalid($this->form, []);
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+    }
+
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage API client failed to set when LPA starts for id: 91333263035
+     */
+    public function testIndexActionPostFailed()
+    {
+        $this->controller->setLpa($this->lpa);
+        $this->setPostValid($this->form, $this->postData);
+        $this->form->shouldReceive('getData')->andReturn($this->postData)->once();
+        $this->lpaApplicationService->shouldReceive('setPrimaryAttorneyDecisions')->withArgs(function ($lpaId, $primaryAttorneyDecisions) {
+            return $lpaId === $this->lpa->id
+                && $primaryAttorneyDecisions->when === $this->postData['when'];
+        })->andReturn(false)->once();
+
+        $this->controller->indexAction();
+    }
+
+    public function testIndexActionPostSuccess()
+    {
+        $response = new Response();
+
+        $this->controller->setLpa($this->lpa);
+        $this->setPostValid($this->form, $this->postData);
+        $this->form->shouldReceive('getData')->andReturn($this->postData)->once();
+        $this->lpaApplicationService->shouldReceive('setPrimaryAttorneyDecisions')->withArgs(function ($lpaId, $primaryAttorneyDecisions) {
+            return $lpaId === $this->lpa->id
+                && $primaryAttorneyDecisions->when === $this->postData['when'];
+        })->andReturn(true)->once();
+        $this->request->shouldReceive('isXmlHttpRequest')->andReturn(false)->once();
+        $this->setMatchedRouteNameHttp($this->controller, 'lpa/when-lpa-starts');
+        $this->setRedirectToRoute('lpa/primary-attorney', $this->lpa, $response);
+
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($response, $result);
     }
 }
