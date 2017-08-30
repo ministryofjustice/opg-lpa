96ae56f74c6812fe2de33c0164edb4dd4b11e579
David Winchurch <david.winchurch@iterativeit.com>
2017-08-30 19:20:09 +0100

    LPA-2316: 100% coverage for AbstractLpaController

--- service-front/module/Application/tests/ApplicationTest/Controller/TestableAbstractLpaActorController.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/TestableAbstractLpaActorController.php
@@ -3,9 +3,15 @@
 namespace ApplicationTest\Controller;
 
 use Application\Controller\AbstractLpaActorController;
+use Application\Form\Lpa\AbstractActorForm;
 
 class TestableAbstractLpaActorController extends AbstractLpaActorController
 {
+    public function testReuseActorDetails(AbstractActorForm $actorForm)
+    {
+        return parent::reuseActorDetails($actorForm);
+    }
+
     public function testGetActorReuseDetails($includeTrusts = true, $forCorrespondent = false)
     {
         return parent::getActorReuseDetails($includeTrusts, $forCorrespondent);
