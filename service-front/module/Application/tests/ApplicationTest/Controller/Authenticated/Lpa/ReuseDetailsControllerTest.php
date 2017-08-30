00e16c6d6e5c9b434214df6e10aef86a00632726
David Winchurch <david.winchurch@iterativeit.com>
2017-08-30 15:42:00 +0100

    LPA-2316: 100% coverage for ReuseDetailsController

--- service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/Lpa/ReuseDetailsControllerTest.php
+++ service-front/module/Application/tests/ApplicationTest/Controller/Authenticated/Lpa/ReuseDetailsControllerTest.php
@@ -12,6 +12,8 @@ use Mockery\MockInterface;
 use Opg\Lpa\DataModel\Lpa\Lpa;
 use OpgTest\Lpa\DataModel\FixturesData;
 use RuntimeException;
+use Zend\Http\Response;
+use Zend\Mvc\Router\RouteMatch;
 use Zend\View\Model\ViewModel;
 
 class ReuseDetailsControllerTest extends AbstractControllerTest
@@ -28,6 +30,9 @@ class ReuseDetailsControllerTest extends AbstractControllerTest
      * @var Lpa
      */
     private $lpa;
+    private $postData = [
+        'reuse-details' => 1
+    ];
 
     public function setUp()
     {
@@ -103,5 +108,120 @@ class ReuseDetailsControllerTest extends AbstractControllerTest
         $this->assertEquals($this->form, $result->getVariable('form'));
         $this->assertEquals('/lpa/' . $this->lpa->id . '/donor', $result->cancelUrl);
         $this->assertEquals($queryParameters['actor-name'], $result->actorName);
+    }
+
+    public function testIndexActionPostInvalid()
+    {
+        $queryParameters = [
+            'calling-url' => '/lpa/' . $this->lpa->id . '/donor/add',
+            'include-trusts' => '0',
+            'actor-name' => 'Donor',
+        ];
+        $this->controller->setLpa($this->lpa);
+        $this->request->shouldReceive('isXmlHttpRequest')->andReturn(false)->once();
+        $this->params->shouldReceive('fromQuery')->andReturn($queryParameters)->once();
+        $this->userDetailsSession->user = $this->user;
+
+        $this->formElementManager->shouldReceive('get')->with(
+            'Application\Form\Lpa\ReuseDetailsForm',
+            ['actorReuseDetails' => $this->controller->testGetActorReuseDetails(false, false)]
+        )->andReturn($this->form);
+
+        $this->url->shouldReceive('fromRoute')
+            ->with('lpa/reuse-details', ['lpa-id' => $this->lpa->id], ['query' => $queryParameters])
+            ->andReturn('lpa/reuse-details?lpa-id=' . $this->lpa->id)->once();
+
+        $this->form->shouldReceive('setAttribute')->with('action', 'lpa/reuse-details?lpa-id=' . $this->lpa->id)->once();
+        $this->setPostInvalid($this->form, []);
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals('/lpa/' . $this->lpa->id . '/donor', $result->cancelUrl);
+        $this->assertEquals($queryParameters['actor-name'], $result->actorName);
+    }
+
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage Calling controller or action could not be determined for processing reuse details request
+     */
+    public function testIndexActionPostInvalidRouteMatch()
+    {
+        $queryParameters = [
+            'calling-url' => '/lpa/' . $this->lpa->id . '/donor/add',
+            'include-trusts' => '0',
+            'actor-name' => 'Donor',
+        ];
+        $this->controller->setLpa($this->lpa);
+        $this->request->shouldReceive('isXmlHttpRequest')->andReturn(false)->once();
+        $this->params->shouldReceive('fromQuery')->andReturn($queryParameters)->once();
+        $this->userDetailsSession->user = $this->user;
+
+        $this->formElementManager->shouldReceive('get')->with(
+            'Application\Form\Lpa\ReuseDetailsForm',
+            ['actorReuseDetails' => $this->controller->testGetActorReuseDetails(false, false)]
+        )->andReturn($this->form);
+
+        $this->url->shouldReceive('fromRoute')
+            ->with('lpa/reuse-details', ['lpa-id' => $this->lpa->id], ['query' => $queryParameters])
+            ->andReturn('lpa/reuse-details?lpa-id=' . $this->lpa->id)->once();
+
+        $this->form->shouldReceive('setAttribute')->with('action', 'lpa/reuse-details?lpa-id=' . $this->lpa->id)->once();
+        $this->setPostValid($this->form, $this->postData);
+        $this->form->shouldReceive('getData')->andReturn($this->postData);
+        $this->router->shouldReceive('match')->andReturn(null)->once();
+
+        /** @var ViewModel $result */
+        $result = $this->controller->indexAction();
+
+        $this->assertInstanceOf(ViewModel::class, $result);
+        $this->assertEquals('', $result->getTemplate());
+        $this->assertEquals($this->form, $result->getVariable('form'));
+        $this->assertEquals('/lpa/' . $this->lpa->id . '/donor', $result->cancelUrl);
+        $this->assertEquals($queryParameters['actor-name'], $result->actorName);
+    }
+
+    public function testIndexActionPostPrimaryAttorneyAdd()
+    {
+        $response = new Response();
+
+        $queryParameters = [
+            'calling-url' => '/lpa/' . $this->lpa->id . '/primary-attorney/add',
+            'include-trusts' => '0',
+            'actor-name' => 'Donor',
+        ];
+        $this->controller->setLpa($this->lpa);
+        $this->request->shouldReceive('isXmlHttpRequest')->andReturn(false)->once();
+        $this->params->shouldReceive('fromQuery')->andReturn($queryParameters)->once();
+        $this->userDetailsSession->user = $this->user;
+
+        $this->formElementManager->shouldReceive('get')->with(
+            'Application\Form\Lpa\ReuseDetailsForm',
+            ['actorReuseDetails' => $this->controller->testGetActorReuseDetails(false, false)]
+        )->andReturn($this->form);
+
+        $this->url->shouldReceive('fromRoute')
+            ->with('lpa/reuse-details', ['lpa-id' => $this->lpa->id], ['query' => $queryParameters])
+            ->andReturn('lpa/reuse-details?lpa-id=' . $this->lpa->id)->once();
+
+        $this->form->shouldReceive('setAttribute')->with('action', 'lpa/reuse-details?lpa-id=' . $this->lpa->id)->once();
+        $this->setPostValid($this->form, $this->postData);
+        $this->form->shouldReceive('getData')->andReturn($this->postData);
+        $routeMatch = Mockery::mock(RouteMatch::class);
+        $this->router->shouldReceive('match')->andReturn($routeMatch)->once();
+        $routeMatch->shouldReceive('getParam')->withArgs(['controller'])->andReturn('Authenticated\Lpa\PrimaryAttorneyController')->once();
+        $routeMatch->shouldReceive('getParam')->withArgs(['action'])->andReturn('add')->once();
+        $this->forward->shouldReceive('dispatch')->withArgs(['Authenticated\Lpa\PrimaryAttorneyController', [
+            'action'            => 'add',
+            'reuseDetailsIndex' => 1,
+            'callingUrl'        => '/lpa/' . $this->lpa->id . '/primary-attorney/add',
+        ]])->andReturn($response)->once();
+
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($response, $result);
     }
 }
