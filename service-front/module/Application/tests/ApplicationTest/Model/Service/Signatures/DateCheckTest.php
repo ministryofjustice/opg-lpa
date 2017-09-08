895e2ce3a9c7ff9815ca6871e82e527d9349ee1c
David Winchurch <david.winchurch@iterativeit.com>
2017-09-08 15:48:28 +0100

    LPA-1233: Complete implementation

--- service-front/module/Application/tests/ApplicationTest/Model/Service/Signatures/DateCheckTest.php
+++ service-front/module/Application/tests/ApplicationTest/Model/Service/Signatures/DateCheckTest.php
@@ -172,7 +172,7 @@ class DateCheckTest extends AbstractHttpControllerTestCase
 
         $this->dateService->shouldReceive('getToday')->andReturn(new DateTime('2015-01-18'))->once();
 
-        $this->assertEquals('No sign date can be in the future.', DateCheck::checkDates($dates, $this->dateService));
+        $this->assertEquals('No signed date can be in the future.', DateCheck::checkDates($dates, $this->dateService));
     }
 
     /**
