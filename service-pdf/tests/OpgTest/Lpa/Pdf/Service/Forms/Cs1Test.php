40865a636259b8b992078b111e9ffdc39fe0ced4
Steven Taylor <taylorsj1980@gmail.com>
2017-11-10 15:39:02 +0000

    Unit test adjustments from previous fix

--- service-pdf/tests/OpgTest/Lpa/Pdf/Service/Forms/Cs1Test.php
+++ service-pdf/tests/OpgTest/Lpa/Pdf/Service/Forms/Cs1Test.php
@@ -38,7 +38,6 @@ class Cs1Test extends AbstractFormTestClass
             'cs1-0-dob-date-day' => "11",
             'cs1-0-dob-date-month' => "04",
             'cs1-0-dob-date-year' => "1972",
-            'cs1-0-email-address' => "\nopglpademo+HenryTalor@gmail.com",
             'cs1-1-is' => "peopleToNotify",
             'cs1-1-name-title' => "Mrs",
             'cs1-1-name-first' => "Liyana",
@@ -47,10 +46,6 @@ class Cs1Test extends AbstractFormTestClass
             'cs1-1-address-address2' => "Mossley",
             'cs1-1-address-address3' => "Greater Manchester",
             'cs1-1-address-postcode' => "MK47 9WD",
-            'cs1-1-dob-date-day' => "10",
-            'cs1-1-dob-date-month' => "10",
-            'cs1-1-dob-date-year' => "1987",
-            'cs1-1-email-address' => "\nopglpademo+ElliotSanders@gmail.com",
         ];
 
         $this->assertEquals($expectedData, $this->extractPdfFormData($pdf));
@@ -84,21 +79,6 @@ class Cs1Test extends AbstractFormTestClass
             'cs1-0-address-address2' => "Mossley",
             'cs1-0-address-address3' => "Greater Manchester",
             'cs1-0-address-postcode' => "MK47 9WD",
-            'cs1-0-dob-date-day' => "10",
-            'cs1-0-dob-date-month' => "10",
-            'cs1-0-dob-date-year' => "1987",
-            'cs1-0-email-address' => "\nopglpademo+ElliotSanders@gmail.com",
-            'cs1-1-is' => "replacementAttorney",
-            'cs1-1-name-title' => "Ms",
-            'cs1-1-name-first' => "Erica",
-            'cs1-1-name-last' => "Schmidt",
-            'cs1-1-address-address1' => "3 Westway",
-            'cs1-1-address-address2' => "Stapleton, Taunton",
-            'cs1-1-address-address3' => "",
-            'cs1-1-address-postcode' => "TA2 9HP",
-            'cs1-1-dob-date-day' => "11",
-            'cs1-1-dob-date-month' => "04",
-            'cs1-1-dob-date-year' => "1972",
         ];
 
         $this->assertEquals($expectedData, $this->extractPdfFormData($pdf));
