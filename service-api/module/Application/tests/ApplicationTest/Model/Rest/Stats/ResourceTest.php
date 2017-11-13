67fd4b1c06e09f3e8b8f40ae3afe1a88eda9568e
David Winchurch <david.winchurch@iterativeit.com>
2017-11-13 16:01:01 +0000

    LPA-2519: Unit tests

--- service-api/module/Application/tests/ApplicationTest/Model/Rest/Stats/ResourceTest.php
+++ service-api/module/Application/tests/ApplicationTest/Model/Rest/Stats/ResourceTest.php
@@ -2,13 +2,14 @@
 
 namespace ApplicationTest\Model\Rest\Stats;
 
-use Application\Library\ApiProblem\ApiProblem;
 use Application\Model\Rest\AbstractResource;
 use Application\Model\Rest\Stats\Entity;
 use Application\Model\Rest\Stats\Resource;
 use ApplicationTest\AbstractResourceTest;
 use Mockery;
 use MongoCollection;
+use MongoDB\BSON\UTCDateTime as MongoDate;
+use MongoDB\Driver\ReadPreference;
 
 class ResourceTest extends AbstractResourceTest
 {
@@ -30,254 +31,22 @@ class ResourceTest extends AbstractResourceTest
         $this->assertEquals(AbstractResource::TYPE_COLLECTION, $resource->getType());
     }
 
-    public function testFetchTypeNotFound()
+    public function testFetch()
     {
-        $resourceBuilder = new ResourceBuilder();
-        $resource = $resourceBuilder->build();
+        $generated = new MongoDate();
 
-        $entity = $resource->fetch('unknown');
-
-        $this->assertTrue($entity instanceof ApiProblem);
-        $this->assertEquals(404, $entity->status);
-        $this->assertEquals('Stats type not found.', $entity->detail);
-
-        $resourceBuilder->verify();
-    }
-
-    public function testFetchTypeLpa()
-    {
-        $lpaCollection = Mockery::mock(MongoCollection::class);
-        $lpaCollection->shouldReceive('count')->andReturn(1);
-
-        $start = new \DateTime('first day of this month');
-        $start->setTime(0, 0, 0);
-
-        $end = new \DateTime('last day of this month');
-        $end->setTime(23, 59, 59);
-
-        $expectedByMonth = array();
-        for ($i = 1; $i <=4; $i++) {
-            $expectedByMonth[date('Y-m', $start->getTimestamp())] = [
-                'started' => 1,
-                'created' => 1,
-                'completed' => 1
-            ];
-
-            $start->modify("first day of -1 month");
-            $end->modify("last day of -1 month");
-        }
-
-        $resourceBuilder = new ResourceBuilder();
-        $resource = $resourceBuilder->withLpaCollection($lpaCollection)->build();
-
-        $entity = $resource->fetch('lpas');
-
-        $expectedStats = [
-            'all' => [
-                'started' => 2,
-                'created' => 2,
-                'completed' => 2,
-                'deleted' => 1
-            ],
-            'health-and-welfare' => [
-                'started' => 1,
-                'created' => 1,
-                'completed' => 1
-            ],
-            'property-and-finance' => [
-                'started' => 1,
-                'created' => 1,
-                'completed' => 1
-            ],
-            'by-month' => $expectedByMonth
-        ];
-
-        $this->assertEquals(new Entity($expectedStats), $entity);
-
-        $resourceBuilder->verify();
-    }
-
-    public function testFetchWhoAreYou()
-    {
-        $statsWhoCollection = Mockery::mock(MongoCollection::class);
-        $statsWhoCollection->shouldReceive('count')->andReturn(1);
-
-        $start = new \DateTime('first day of this month');
-        $start->setTime(0, 0, 0);
-
-        $end = new \DateTime('last day of this month');
-        $end->setTime(23, 59, 59);
-
-        $expectedByMonth = array();
-        for ($i = 1; $i <=4; $i++) {
-            $expectedByMonth[date('Y-m', $start->getTimestamp())] = [
-                'professional' => [
-                    'count' => 1,
-                    'subquestions' => [
-                        'solicitor' => 1,
-                        'will-writer' => 1,
-                        'other' => 1
-                    ]
-                ],
-                'digitalPartner' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'organisation' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'donor' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'friendOrFamily' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'notSaid' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-            ];
-
-            $start->modify("first day of -1 month");
-            $end->modify("last day of -1 month");
-        }
-
-        $resourceBuilder = new ResourceBuilder();
-        $resource = $resourceBuilder->withStatsWhoCollection($statsWhoCollection)->build();
-
-        $entity = $resource->fetch('whoareyou');
-
-        $expectedStats = [
-            'all' => [
-                'professional' => [
-                    'count' => 1,
-                    'subquestions' => [
-                        'solicitor' => 1,
-                        'will-writer' => 1,
-                        'other' => 1
-                    ]
-                ],
-                'digitalPartner' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'organisation' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'donor' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'friendOrFamily' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-                'notSaid' => [
-                    'count' => 1,
-                    'subquestions' => []
-                ],
-            ],
-            'by-month' => $expectedByMonth
-        ];
-
-        $this->assertEquals(new Entity($expectedStats), $entity);
-
-        $resourceBuilder->verify();
-    }
-
-    public function testFetchLpasPerUser()
-    {
-        $statMongoCursor = new DummyStatMongoCursor([['_id' => 1, 'count' => 2]]);
         $statsLpasCollection = Mockery::mock(MongoCollection::class);
         $statsLpasCollection->shouldReceive('setReadPreference');
-        $statsLpasCollection->shouldReceive('find')->andReturn($statMongoCursor);
+        $statsLpasCollection->shouldReceive('findOne')
+            ->withArgs([[], ['readPreference' => new ReadPreference(ReadPreference::RP_SECONDARY_PREFERRED)]])
+            ->andReturn(['generated' => $generated]);
+
         $resourceBuilder = new ResourceBuilder();
         $resource = $resourceBuilder->withStatsLpasCollection($statsLpasCollection)->build();
 
-        $entity = $resource->fetch('lpasperuser');
+        $entity = $resource->fetch('all');
 
-        $expectedStats = [
-            'byLpaCount' => [1 => 2]
-        ];
-
-        $this->assertEquals(new Entity($expectedStats), $entity);
-
-        $resourceBuilder->verify();
-    }
-
-    public function testFetchCorrespondence()
-    {
-        $lpaCollection = Mockery::mock(MongoCollection::class);
-        $lpaCollection->shouldReceive('setReadPreference');
-        $lpaCollection->shouldReceive('count')->andReturn(1);
-        $resourceBuilder = new ResourceBuilder();
-        $resource = $resourceBuilder->withLpaCollection($lpaCollection)->build();
-
-        $entity = $resource->fetch('correspondence');
-
-        $start = new \DateTime('first day of this month');
-        $start->setTime(0, 0, 0);
-
-        $end = new \DateTime('last day of this month');
-        $end->setTime(23, 59, 59);
-
-        $expectedStats = array();
-        for ($i = 1; $i <=4; $i++) {
-            $expectedStats[date('Y-m', $start->getTimestamp())] = [
-                'completed' => 1,
-                'contactByEmail' => 1,
-                'contactByPhone' => 1,
-                'contactByPost' => 1,
-                'contactInEnglish' => 1,
-                'contactInWelsh' => 1
-            ];
-
-            $start->modify("first day of -1 month");
-            $end->modify("last day of -1 month");
-        }
-
-        $this->assertEquals(new Entity($expectedStats), $entity);
-
-        $resourceBuilder->verify();
-    }
-
-    public function testFetchPreferencesInstructionsStats()
-    {
-        $lpaCollection = Mockery::mock(MongoCollection::class);
-        $lpaCollection->shouldReceive('setReadPreference');
-        $lpaCollection->shouldReceive('count')
-                      ->andReturn(1);
-
-        $resourceBuilder = new ResourceBuilder();
-        $resource = $resourceBuilder->withLpaCollection($lpaCollection)
-                                    ->build();
-
-        $entity = $resource->fetch('preferencesinstructions');
-
-        $start = new \DateTime('first day of this month');
-        $start->setTime(0, 0, 0);
-
-        $end = new \DateTime('last day of this month');
-        $end->setTime(23, 59, 59);
-
-        $expectedStats = array();
-
-        for ($i = 1; $i <= 4; $i++) {
-            $expectedStats[date('Y-m', $start->getTimestamp())] = [
-                'completed' => 1,
-                'preferencesStated' => 1,
-                'instructionsStated' => 1
-            ];
-
-            $start->modify("first day of -1 month");
-            $end->modify("last day of -1 month");
-        }
-
-        $this->assertEquals(new Entity($expectedStats), $entity);
+        $this->assertEquals(new Entity(['generated' => date('d/m/Y H:i:s', $generated->toDateTime()->getTimestamp())]), $entity);
 
         $resourceBuilder->verify();
     }
--- service-api/module/Application/tests/ApplicationTest/Model/Service/System/StatsTest.php
+++ service-api/module/Application/tests/ApplicationTest/Model/Service/System/StatsTest.php
@@ -14,20 +14,35 @@ class StatsTest extends TestCase
     public function testGenerate()
     {
         $lpaCollection = Mockery::mock(MongoCollection::class);
+
+        //Return 1 for all counts to aid mocking mongo calls
+        $lpaCollection->shouldReceive('count')->andReturn(1);
+
         $manager = Mockery::mock();
         $cursor = Mockery::mock(\Traversable::class);
-        $cursor->shouldReceive('toArray')->andReturn(['results' => [['value' => 1], ['value' => 1]]]);
+        $cursor->shouldReceive('toArray')->andReturn([0 => (object)['results' => [(object)['value' => 1], (object)['value' => 1]]]]);
         $manager->shouldReceive('executeCommand')->andReturn($cursor)->once();
         $lpaCollection->shouldReceive('getManager')->andReturn($manager)->once();
         $lpaCollection->shouldReceive('getDatabaseName')->andReturn('test')->once();
         $lpaCollection->shouldReceive('getCollectionName')->andReturn('test')->once();
 
         $statsLpasCollection = Mockery::mock(MongoCollection::class);
-        $statsLpasCollection->shouldReceive('remove')->once();
-        $statsLpasCollection->shouldReceive('batchInsert')->andReturn(['ok' => true])->once();
+        $statsLpasCollection->shouldReceive('deleteMany')->withArgs([[]])->once();
+        $statsLpasCollection->shouldReceive('insertOne')->withArgs(function ($stats) {
+            return isset($stats['generated'])
+                && isset($stats['lpas'])
+                && isset($stats['lpasPerUser'])
+                && isset($stats['who'])
+                && isset($stats['correspondence'])
+                && isset($stats['preferencesInstructions']);
+        })->once();
+
+        $statsWhoCollection = Mockery::mock(MongoCollection::class);
+        $statsWhoCollection->shouldReceive('count')->andReturn(1);
 
         $serviceLocatorMock = Mockery::mock(ServiceLocatorInterface::class);
         $serviceLocatorMock->shouldReceive('get')->with(CollectionFactory::class . '-lpa')->andReturn($lpaCollection);
+        $serviceLocatorMock->shouldReceive('get')->with(CollectionFactory::class . '-stats-who')->andReturn($statsWhoCollection);
         $serviceLocatorMock->shouldReceive('get')->with(CollectionFactory::class . '-stats-lpas')->andReturn($statsLpasCollection);
 
         $stats = new Stats();
