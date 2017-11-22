8471489486f57f009599c0e9522071ce33f28190
Steven Taylor <taylorsj1980@gmail.com>
2017-11-22 14:51:15 +0000

    Check error message key before attempting to use it

--- service-front/module/Application/src/Application/View/Helper/FormErrorTextExchange.php
+++ service-front/module/Application/src/Application/View/Helper/FormErrorTextExchange.php
@@ -7,26 +7,26 @@ class FormErrorTextExchange extends AbstractHelper
 {
     /**
      * Catch-all transformations, ignorant of field name
-     * 
+     *
      * @var array - Common Generic Map
      */
     private $commonMap = [];
-    
+
     /**
      * Generic transformations for named fields
-     * 
+     *
      * $var array - Common Field Map
      */
     private $commonFieldMap = [];
-    
+
     /**
      * Look at each element message on the form. If a transform message exists
-     * in the override map then replace the message with its override. If no 
+     * in the override map then replace the message with its override. If no
      * override message exists, see if there is a transformation in the common map.
-     * 
-     * The override map is merged with a generic override map which provides 
+     *
+     * The override map is merged with a generic override map which provides
      * messages for common field names.
-     *  
+     *
      * @param Form $form
      * @return Form
      */
@@ -36,37 +36,38 @@ class FormErrorTextExchange extends AbstractHelper
             $this->commonFieldMap,
             $overrideMap
         );
-        
+
         $elements = $form->getElements();
-        
+
         foreach ($form->getFieldsets() as $fieldset) {
             foreach ($fieldset->getElements() as $element) {
                 $elements[] = $element;
             }
             $elements[] = $fieldset;
         }
-        
+
         foreach ($elements as $element) {
-            
+
             $name = $element->getName();
-            
+
             if (array_key_exists($name, $overrideMap)) {
                 $elementMap = $overrideMap[$name];
             } else {
                 $elementMap = [];
             }
-            
+
             $messages = $element->getMessages();
-            
+
             foreach ($messages as &$elementMessage) {
-                if (array_key_exists($elementMessage, $elementMap)) {
-                    $elementMessage = $elementMap[$elementMessage];
-                } elseif (array_key_exists($elementMessage, $this->commonMap)) {
-                    $elementMessage = $this->commonMap[$elementMessage];
+                if (is_string($elementMessage) || is_numeric($elementMessage)) {
+                    if (array_key_exists($elementMessage, $elementMap)) {
+                        $elementMessage = $elementMap[$elementMessage];
+                    } elseif (array_key_exists($elementMessage, $this->commonMap)) {
+                        $elementMessage = $this->commonMap[$elementMessage];
+                    }
                 }
-               
             }
-            
+
             $element->setMessages($messages);
         }
 
