c2b7700d629093f9a7a8ced2afcdde04307d2612
David Winchurch <david.winchurch@iterativeit.com>
2018-01-19 14:22:54 +0000

    LPA-2144: Zend 3 parameter swap

--- service-api/module/Application/Module.php
+++ service-api/module/Application/Module.php
@@ -55,7 +55,7 @@ class Module {
 
         return [
             'initializers' => [
-                'InitRestController' => function($controller, $sm) {
+                'InitRestController' => function($sm, $controller) {
                     if ($controller instanceof RestController) {
 
                         $locator = $sm->getServiceLocator();
