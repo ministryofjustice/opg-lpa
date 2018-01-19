4e64f01acac48a62f5485489202ce88e20b25b43
Biggs Thorarensen <biggs@devillion.com>
2018-01-19 09:37:08 +0000

    LPA-2562 analytics modules from the toolkit added. module init script created and added.

---
+++ service-front/assets/js/govuk/analytics.js
@@ -0,0 +1,34 @@
+;GOVUK.analyticsSetup = function(global) {
+  "use strict";
+
+  var $ = global.jQuery
+  var GOVUK = global.GOVUK || {}
+  var gaConfig = global.gaConfig || {}
+
+  // Load Google Analytics libraries
+  GOVUK.Analytics.load();
+
+  // Use document.domain in dev, preview and staging so that tracking works
+  // Otherwise explicitly set the domain as lastingpowerofattorney.service.justice.gov.uk.
+  var cookieDomain = (document.domain === 'lastingpowerofattorney.service.justice.gov.uk') ? '.lastingpowerofattorney.service.justice.gov.uk' : document.domain;
+
+  // Configure profiles and make interface public
+  // for custom dimensions, virtual pageviews and events
+  GOVUK.analytics = new GOVUK.Analytics({
+    universalId: gaConfig.universalId  || '',
+    cookieDomain: cookieDomain,
+    allowLinker: true,
+    allowAnchor: true
+  });
+
+  // Track initial pageview
+  if (typeof GOVUK.pageviewOptions !== 'undefined') {
+    GOVUK.analytics.trackPageview(null, null, GOVUK.pageviewOptions);
+  }
+  else {
+    GOVUK.analytics.trackPageview();
+  }
+
+};
+
+GOVUK.analyticsSetup(window)
