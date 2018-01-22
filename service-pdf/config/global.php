2876d85c97e1be9fdf38a81319728ad61f81c0ca
David Winchurch <david.winchurch@iterativeit.com>
2018-01-22 14:32:25 +0000

    LPA-2485: Correct H&W blank coordinates

--- service-pdf/config/global.php
+++ service-pdf/config/global.php
@@ -95,9 +95,9 @@ return [
     ],
 
     'blanks' => [
-        'applicant-signature-1-hw' => ['x1' => 297, 'y1' => 509, 'x2' => 560, 'y2' => 382],
-        'applicant-signature-2-hw' => ['x1' => 31,  'y1' => 379, 'x2' => 295, 'y2' => 252],
-        'applicant-signature-3-hw' => ['x1' => 297, 'y1' => 379, 'x2' => 560, 'y2' => 252],
+        'applicant-signature-1-hw' => ['x1' => 297, 'y1' => 497, 'x2' => 560, 'y2' => 371],
+        'applicant-signature-2-hw' => ['x1' => 31,  'y1' => 367, 'x2' => 295, 'y2' => 241],
+        'applicant-signature-3-hw' => ['x1' => 297, 'y1' => 367, 'x2' => 560, 'y2' => 241],
         'applicant-signature-1-pf' => ['x1' => 297, 'y1' => 509, 'x2' => 560, 'y2' => 382],
         'applicant-signature-2-pf' => ['x1' => 31,  'y1' => 379, 'x2' => 295, 'y2' => 252],
         'applicant-signature-3-pf' => ['x1' => 297, 'y1' => 379, 'x2' => 560, 'y2' => 252]
