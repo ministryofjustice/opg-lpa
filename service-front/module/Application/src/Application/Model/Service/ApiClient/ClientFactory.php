a565ec3c165e4eb3cb52b84d836eea4949c60ad3
Steven Taylor <taylorsj1980@gmail.com>
2018-03-14 11:34:07 +0000

    ZF3 upgrades to new LPA-2375 classes

--- service-front/module/Application/src/Application/Model/Service/ApiClient/ClientFactory.php
+++ service-front/module/Application/src/Application/Model/Service/ApiClient/ClientFactory.php
@@ -4,8 +4,11 @@ namespace Application\Model\Service\ApiClient;
 
 use Application\Model\Service\Authentication\Identity\User as UserIdentity;
 use Http\Client\HttpClient as HttpClientInterface;
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+use Zend\ServiceManager\Factory\FactoryInterface;
 use Zend\Session\Container;
 
 class ClientFactory implements FactoryInterface
@@ -13,20 +16,26 @@ class ClientFactory implements FactoryInterface
     /**
      * Create and instance of the API Client
      *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return Client
+     * @param  ContainerInterface $container
+     * @param  string $requestedName
+     * @param  null|array $options
+     * @return object
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     *     creating a service.
+     * @throws ContainerException if any other error occurs
      */
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         /** @var HttpClientInterface $httpClient */
-        $httpClient = $serviceLocator->get('HttpClient');
+        $httpClient = $container->get('HttpClient');
 
-        $baseApiUri = $serviceLocator->get('config')['api_client']['api_uri'];
+        $baseApiUri = $container->get('config')['api_client']['api_uri'];
 
         $token = null;
 
         /** @var Container $userDetailsSession */
-        $userDetailsSession = $serviceLocator->get('UserDetailsSession');
+        $userDetailsSession = $container->get('UserDetailsSession');
         $identity = $userDetailsSession->identity;
 
         if ($identity instanceof UserIdentity) {
