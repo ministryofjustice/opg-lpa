ae39b7a21f2c359af075a0ec2118a900509f550d
Steven Taylor <taylorsj1980@gmail.com>
2018-03-14 11:34:07 +0000

    ZF3 upgrades to new LPA-2375 classes

--- service-front/module/Application/src/Application/Model/Service/AuthClient/ClientFactory.php
+++ service-front/module/Application/src/Application/Model/Service/AuthClient/ClientFactory.php
@@ -4,29 +4,38 @@ namespace Application\Model\Service\AuthClient;
 
 use Application\Model\Service\Authentication\Identity\User as UserIdentity;
 use Http\Client\HttpClient as HttpClientInterface;
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+use Zend\ServiceManager\Factory\FactoryInterface;
 use Zend\Session\Container;
 
 class ClientFactory implements FactoryInterface
 {
     /**
-     * Create and instance of the API client for the auth service
+     * Create an object
      *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return Client
+     * @param  ContainerInterface $container
+     * @param  string $requestedName
+     * @param  null|array $options
+     * @return object
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     *     creating a service.
+     * @throws ContainerException if any other error occurs
      */
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         /** @var HttpClientInterface $httpClient */
-        $httpClient = $serviceLocator->get('HttpClient');
+        $httpClient = $container->get('HttpClient');
 
-        $baseAuthUri = $serviceLocator->get('config')['api_client']['auth_uri'];
+        $baseAuthUri = $container->get('config')['api_client']['auth_uri'];
 
         $token = null;
 
         /** @var Container $userDetailsSession */
-        $userDetailsSession = $serviceLocator->get('UserDetailsSession');
+        $userDetailsSession = $container->get('UserDetailsSession');
         $identity = $userDetailsSession->identity;
 
         if ($identity instanceof UserIdentity) {
--- service-front/module/Application/src/Application/Model/Service/Authentication/AuthenticationServiceFactory.php
+++ service-front/module/Application/src/Application/Model/Service/Authentication/AuthenticationServiceFactory.php
@@ -3,24 +3,33 @@
 namespace Application\Model\Service\Authentication;
 
 use Application\Model\Service\Authentication\Adapter\AdapterInterface;
-use Zend\ServiceManager\FactoryInterface;
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+use Zend\ServiceManager\Factory\FactoryInterface;
 use Zend\Authentication\Storage\Session as SessionStorage;
-use Zend\ServiceManager\ServiceLocatorInterface;
 
 class AuthenticationServiceFactory implements FactoryInterface
 {
     /**
-     * Create service
+     * Create an object
      *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return AuthenticationService
+     * @param  ContainerInterface $container
+     * @param  string $requestedName
+     * @param  null|array $options
+     * @return object
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     *     creating a service.
+     * @throws ContainerException if any other error occurs
      */
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $storage = new SessionStorage('UserDetails', 'identity');
 
         /** @var AdapterInterface $adapter */
-        $adapter = $serviceLocator->get('LpaAuthAdapter');
+        $adapter = $container->get('LpaAuthAdapter');
 
         return new AuthenticationService($storage, $adapter);
     }
