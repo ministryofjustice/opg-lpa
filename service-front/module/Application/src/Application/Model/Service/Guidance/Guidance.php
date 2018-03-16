1879d21ec35d1e53a4a945953f10ba73f7302df9
Biggs Thorarensen <biggs@devillion.com>
2018-03-16 08:14:04 +0000

    LPA-2635 guidance navigation links events

--- service-front/module/Application/src/Application/Model/Service/Guidance/Guidance.php
+++ service-front/module/Application/src/Application/Model/Service/Guidance/Guidance.php
@@ -11,45 +11,45 @@ class Guidance extends AbstractService
 
     /**
      * Generate guidance sections and navigation from the guidance markdown files
-     * 
+     *
      * @return array An array of guidance section details
      */
     function parseMarkdown()
     {
         $sectionArray = [];
         $lines = file(self::GUIDANCE_MARKDOWN_FOLDER . '/order.md');
-        
+
         foreach ($lines as $line) {
             if (preg_match('/^\*\w*(.*)/', $line, $matches)) {
                 $sectionTitle = trim($matches[1]);
             }
-    
+
             if (preg_match('/^\s+\*\s*(.*\.md)/', $line, $matches)) {
                 $sectionFilename = trim($matches[1]);
-                
+
                 $sectionId = trim(strtolower(str_replace(' ', '-', $sectionTitle)));
-                
+
                 $sectionArray[] = [
                     'id' => $sectionId,
                     'title' => $sectionTitle,
                     'html' => $this->processSection($sectionFilename, $sectionId),
                     'url' => '/' . self::GUIDANCE_ROUTE . '#topic-' . $sectionId,
-                    'dataJourney' => 'stageprompt.lpa:help:' . $sectionId,
+                    'dataJourney' => 'guidance:link:navigation: ' . $sectionTitle,
                 ];
             }
         }
-        
+
         return [
             'sections' => $sectionArray,
         ];
     }
-    
+
     /**
      * Transform markdown of a guidance section into HTML
-     * 
+     *
      * @param string $filename
      * @param string $sectionId
-     * 
+     *
      * @return string The generated HTML
      */
     function processSection($filename, $sectionId)
@@ -57,15 +57,15 @@ class Guidance extends AbstractService
         $md = Markdown::defaultTransform(file_get_contents(self::GUIDANCE_MARKDOWN_FOLDER . '/' . $filename));
 
         $html = '<article id="topic-' . $sectionId . '">';
-        
+
         $html .= preg_replace(
                     '/<a href="\/help\/#topic-(.+)">(.+)<\/a>/',
-                    '<a href="/' . self::GUIDANCE_ROUTE . '#topic-${1}" class="js-guidance" data-journey="stageprompt.lpa:help:${1}">${2}</a>',
+                    '<a href="/' . self::GUIDANCE_ROUTE . '#topic-${1}" class="js-guidance" data-journey="page:help:${1}">${2}</a>',
                     $md
                  );
-                
+
         $html .= '</article>';
-        
+
         return $html;
     }
 }
