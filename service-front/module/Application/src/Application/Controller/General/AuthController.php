ab9607a30c36f7683dd0854cbf7e5f276a89e3ed
Steven Taylor <taylorsj1980@gmail.com>
2018-04-06 17:23:03 +0100

    Display flash message if user has logged in and removed inactivity flags

--- service-front/module/Application/src/Application/Controller/General/AuthController.php
+++ service-front/module/Application/src/Application/Controller/General/AuthController.php
@@ -24,15 +24,21 @@ class AuthController extends AbstractBaseController
      */
     private $lpaApplicationService;
 
-    public function indexAction(){
-
+    public function indexAction()
+    {
         $check = $this->preventAuthenticatedUser();
-        if( $check !== true ){ return $check; }
+
+        if ($check !== true) {
+            return $check;
+        }
 
         //---
 
-        $check = $this->checkCookie( 'login' );
-        if( $check !== true ){ return $check; }
+        $check = $this->checkCookie('login');
+
+        if ($check !== true) {
+            return $check;
+        }
 
         //-----------------------
 
@@ -40,8 +46,6 @@ class AuthController extends AbstractBaseController
         $form = $this->getLoginForm();
 
         //-----------------------
-
-        $authenticationService = $this->getAuthenticationService();
 
         //---
 
@@ -52,25 +56,21 @@ class AuthController extends AbstractBaseController
         $request = $this->getRequest();
 
         if ($request->isPost()) {
-
             $form->setData($request->getPost());
 
             // If the form is valid...
             if ($form->isValid()) {
-
                 // Check if we're going to redirect to a deep(er) link (before we kill the session)
                 $preAuthRequest = new Container('PreAuthRequest');
 
-                if( $preAuthRequest->url ){
+                if ($preAuthRequest->url) {
                     $nextUrl = $preAuthRequest->url;
                 }
 
                 //---
 
                 // Ensure no user is logged in and ALL session data is cleared then re-initialise it.
-
                 $session = $this->getSessionManager();
-
                 $session->getStorage()->clear();
                 $session->initialise();
 
@@ -79,27 +79,23 @@ class AuthController extends AbstractBaseController
                 $email = $form->getData()['email'];
                 $password = $form->getData()['password'];
 
-                $authenticationAdapter = $this->authenticationAdapter;
-
                 // Pass the user's email address and password...
-                $authenticationAdapter->setEmail( $email )->setPassword( $password );
+                $this->authenticationAdapter->setEmail($email)->setPassword($password);
 
                 // Perform the authentication..
-                $result = $authenticationService->authenticate( $authenticationAdapter );
-
+                $result = $this->getAuthenticationService()->authenticate($this->authenticationAdapter);
 
                 // If all went well...
-                if( $result->isValid() ){
-
+                if ($result->isValid()) {
                     // Regenerate the session ID post authentication
                     $session->regenerateId(true);
 
                     // is there a return url stored in the session?
-                    if( isset($nextUrl) ){
-                        $pathArray = explode("/", parse_url($nextUrl, PHP_URL_PATH));
+                    if (isset($nextUrl)) {
+                        $pathArray = explode('/', parse_url($nextUrl, PHP_URL_PATH));
 
                         //  Does that url refer to an LPA?
-                        if (count($pathArray) > 2 && $pathArray[1] == "lpa" && is_numeric($pathArray[2])) {
+                        if (count($pathArray) > 2 && $pathArray[1] == 'lpa' && is_numeric($pathArray[2])) {
                             //  It does but check if the requested URL is the date check page
                             if (isset($pathArray[3]) && $pathArray[3] == 'date-check') {
                                 return $this->redirect()->toUrl($nextUrl);
@@ -113,104 +109,109 @@ class AuthController extends AbstractBaseController
                                 $formFlowChecker = new FormFlowChecker($lpa);
                                 $destinationRoute = $formFlowChecker->backToForm();
 
-                                return $this->redirect()->toRoute($destinationRoute, ['lpa-id' => $lpa->id], $formFlowChecker->getRouteOptions($destinationRoute));
+                                return $this->redirect()->toRoute($destinationRoute, [
+                                    'lpa-id' => $lpa->id
+                                ], $formFlowChecker->getRouteOptions($destinationRoute));
                             }
                         }
 
                         //not an LPA url so redirect directly to it
-                        return $this->redirect()->toUrl( $nextUrl );
+                        return $this->redirect()->toUrl($nextUrl);
                     }
 
-                    // Else Send them to the dashboard...
-                    return $this->redirect()->toRoute( 'user/dashboard' );
+                    //  If there was no nextUrl then just go to the dashboard
 
-                } // if
+                    //  If necessary set a flash message showing that the user account will now remain active
+                    if (in_array('inactivity-flags-cleared', $result->getMessages())) {
+                        $this->flashMessenger()->addWarningMessage('Thanks for logging in. Your LPA account will stay open for another 9 months.');
+                    }
 
-                // else authentication failed...
+                    return $this->redirect()->toRoute('user/dashboard');
+                }
 
-                // Create a new instance of the login form as we'll need a new Csrf token.
+                //  Authentication failed...
+
+                //  Create a new instance of the login form as we'll need a new Csrf token
                 $form = $this->getLoginForm();
 
                 // Keep the entered email address.
-                $form->setData( [ 'email' => $email ] );
+                $form->setData([
+                    'email' => $email
+                ]);
 
                 //---
 
                 $authError = $result->getMessages();
 
                 // If there is a message, extract it (there will only ever be one).
-                if( is_array($authError) && count($authError) > 0 ){
+                if (is_array($authError) && count($authError) > 0) {
                     $authError = array_pop($authError);
                 }
 
-
                 // Help mitigate brute force attacks.
                 sleep(1);
-
-            } // if form is valid
-
-        } // if is post
+            }
+        }
 
         //---
 
-        $isTimeout = ( $this->params('state') == 'timeout' );
+        $isTimeout = ($this->params('state') == 'timeout');
 
-        //---
-
-        return new ViewModel( [ 'form'=>$form, 'authError'=>$authError, 'isTimeout'=>$isTimeout ] );
-
-    } // function
+        return new ViewModel([
+            'form'      => $form,
+            'authError' => $authError,
+            'isTimeout' => $isTimeout
+        ]);
+    }
 
     /**
      * Returns a new instance of the Login form.
      *
      * @return LoginForm
      */
-    private function getLoginForm(){
-
+    private function getLoginForm()
+    {
         $form = $this->getFormElementManager()->get('Application\Form\User\Login');
-        $form->setAttribute( 'action', $this->url()->fromRoute('login') );
+
+        $form->setAttribute('action', $this->url()->fromRoute('login'));
 
         return $form;
-
-    } // function
+    }
 
     /**
      * Logs the user out by clearing the identity from the session.
      *
      * @return \Zend\Http\Response
      */
-    public function logoutAction(){
-
+    public function logoutAction()
+    {
         $this->clearSession();
 
-        return $this->redirect()->toUrl( $this->config()['redirects']['logout'] );
-
-    } // function
+        return $this->redirect()->toUrl($this->config()['redirects']['logout']);
+    }
 
     /**
      * Wipes all session details post-account deletion.
      *
      * @return ViewModel
      */
-    public function deletedAction(){
-
+    public function deletedAction()
+    {
         $this->clearSession();
 
         return new ViewModel();
-
-    } // function
-
+    }
 
     /**
      * Destroys the current session.
      */
-    private function clearSession(){
-
+    private function clearSession()
+    {
         $this->getAuthenticationService()->clearIdentity();
-        $this->getSessionManager()->destroy([ 'clear_storage'=>true ]);
-
-    } // function
+        $this->getSessionManager()->destroy([
+            'clear_storage' => true
+        ]);
+    }
 
     public function setAuthenticationAdapter(AdapterInterface $authenticationAdapter)
     {
@@ -221,4 +222,4 @@ class AuthController extends AbstractBaseController
     {
         $this->lpaApplicationService = $lpaApplicationService;
     }
-} // class
+}
