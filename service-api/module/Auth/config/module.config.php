2cee477c1cb9a5fa06e482ffb6124e570ff94d78
Steven Taylor <taylorsj1980@gmail.com>
2018-05-04 12:51:00 +0100

    Removed view components from auth module

--- service-api/module/Auth/config/module.config.php
+++ service-api/module/Auth/config/module.config.php
@@ -317,37 +317,6 @@ return [
         ],
     ],
 
-    'translator' => [
-        'locale' => 'en_US',
-        'translation_file_patterns' => [
-            [
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ],
-        ],
-    ],
-
-    'view_manager' => [
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => [
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'auth/index/index'        => __DIR__ . '/../view/auth/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ],
-        'template_path_stack' => [
-            __DIR__ . '/../view',
-        ],
-        'strategies' => [
-            'ViewJsonStrategy',
-        ],
-    ],
-
     // Placeholder for console routes
     'console' => [
         'router' => [
