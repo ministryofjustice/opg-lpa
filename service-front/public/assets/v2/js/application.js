1df028a73aa6fbe067fff6d63c29eaa26490c267
Biggs Thorarensen <biggs@devillion.com>
2018-05-14 17:37:15 +0100

    LPA-2621 rendered assets

--- service-front/public/assets/v2/js/application.js
+++ service-front/public/assets/v2/js/application.js
@@ -4852,7 +4852,7 @@ return /******/ (function(modules) { // webpackBootstrap
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.5';
+  var VERSION = '4.17.10';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;
@@ -5276,6 +5276,14 @@ return /******/ (function(modules) { // webpackBootstrap
   /** Used to access faster Node.js helpers. */
   var nodeUtil = (function() {
     try {
+      // Use `util.types` for Node.js 10+.
+      var types = freeModule && freeModule.require && freeModule.require('util').types;
+
+      if (types) {
+        return types;
+      }
+
+      // Legacy `process.binding('util')` for Node.js < 10.
       return freeProcess && freeProcess.binding && freeProcess.binding('util');
     } catch (e) {}
   }());
@@ -21959,53 +21967,62 @@ return /******/ (function(modules) { // webpackBootstrap
 //
 //     <a class="help-button" href="#" data-journey-click="stage:help:info">See more info...</a>
 
-var GOVUK = GOVUK || {};
-
-GOVUK.performance = GOVUK.performance || {};
-
-GOVUK.performance.stageprompt = (function () {
-
-  var setup, setupForGoogleAnalytics, splitAction;
-
-  splitAction = function (action) {
-    var parts = action.split(':');
-    if (parts.length <= 3) return parts;
-    return [parts.shift(), parts.shift(), parts.join(':')];
-  };
-
-  setup = function (analyticsCallback) {
-    var journeyStage = $('[data-journey]').attr('data-journey'),
-        journeyHelpers = $('[data-journey-click]');
-
-    if (journeyStage) {
-      analyticsCallback.apply(null, splitAction(journeyStage));
-    }
-
-    journeyHelpers.on('click', function (event) {
-      analyticsCallback.apply(null, splitAction($(this).data('journey-click')));
-    });
-  };
-
-  setupForGoogleAnalytics = function () {
-    setup(GOVUK.performance.sendGoogleAnalyticsEvent);
-  };
-
-  return {
-    setup: setup,
-    setupForGoogleAnalytics: setupForGoogleAnalytics
-  };
-}());
-
-GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
-  if (window.ga && typeof(window.ga) === 'function') {
-    ga('send', 'event', category, event, label);
-  } else {
-    _gaq.push(['_trackEvent', category, event, label, undefined, true]);
-  }
-};
-;
 ;(function (global) {
   'use strict'
+
+  var $ = global.jQuery
+  var GOVUK = global.GOVUK || {}
+
+  GOVUK.performance = GOVUK.performance || {}
+
+  GOVUK.performance.stageprompt = (function () {
+    var setup, setupForGoogleAnalytics, splitAction
+
+    splitAction = function (action) {
+      var parts = action.split(':')
+      if (parts.length <= 3) return parts
+      return [parts.shift(), parts.shift(), parts.join(':')]
+    }
+
+    setup = function (analyticsCallback) {
+      var journeyStage = $('[data-journey]').attr('data-journey')
+      var journeyHelpers = $('[data-journey-click]')
+
+      if (journeyStage) {
+        analyticsCallback.apply(null, splitAction(journeyStage))
+      }
+
+      journeyHelpers.on('click', function (event) {
+        analyticsCallback.apply(null, splitAction($(this).data('journey-click')))
+      })
+    }
+
+    setupForGoogleAnalytics = function () {
+      setup(GOVUK.performance.sendGoogleAnalyticsEvent)
+    }
+
+    return {
+      setup: setup,
+      setupForGoogleAnalytics: setupForGoogleAnalytics
+    }
+  }())
+
+  GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
+    global._gaq.push(['_trackEvent', category, event, label, undefined, true])
+  }
+
+  global.GOVUK = GOVUK
+})(window)
+;
+// DEPRECATED
+// This isn’t needed if you’re using GOV.UK Elements 3.0.0 or above
+
+;(function (global) {
+  'use strict'
+
+  if (window.console && window.console.warn) {
+    window.console.warn('Deprecation warning: Custom radio buttons and checkboxes (released in GOV.UK Elements 3.0.0) no longer require this JavaScript.')
+  }
 
   var $ = global.jQuery
   var GOVUK = global.GOVUK || {}
@@ -24978,11 +24995,13 @@ this["lpa"]["templates"]["shared.loading-popup"] = Handlebars.template({"compile
     },
 
     checkErrors: function(){
+      // Iterating through link errors
       var errorsLinked = $('.error-summary-list li a')
       for (var i = 0; i < errorsLinked.length; i++) {
         moj.Modules.formErrorTracker.trackError(errorsLinked[i])
       }
 
+      // Iterating through text errors
       var errorsText = $('.error-summary-text p')
       for (var i = 0; i < errorsText.length; i++) {
         moj.Modules.formErrorTracker.trackErrorText(errorsText[i])
