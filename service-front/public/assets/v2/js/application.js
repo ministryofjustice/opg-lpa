7a0bce197f4bb16bb99e5756075c7df5c76f61f9
Biggs Thorarensen <biggs@devillion.com>
2018-05-15 09:12:39 +0100

    LPA-2621 alphagov stageprompt.js added to assets/js and replaces the node_modules one. Slight versioning difference, not worth debugging.

--- service-front/public/assets/v2/js/application.js
+++ service-front/public/assets/v2/js/application.js
@@ -21967,52 +21967,50 @@ return /******/ (function(modules) { // webpackBootstrap
 //
 //     <a class="help-button" href="#" data-journey-click="stage:help:info">See more info...</a>
 
-;(function (global) {
-  'use strict'
+var GOVUK = GOVUK || {};
 
-  var $ = global.jQuery
-  var GOVUK = global.GOVUK || {}
+GOVUK.performance = GOVUK.performance || {};
 
-  GOVUK.performance = GOVUK.performance || {}
+GOVUK.performance.stageprompt = (function () {
 
-  GOVUK.performance.stageprompt = (function () {
-    var setup, setupForGoogleAnalytics, splitAction
+  var setup, setupForGoogleAnalytics, splitAction;
 
-    splitAction = function (action) {
-      var parts = action.split(':')
-      if (parts.length <= 3) return parts
-      return [parts.shift(), parts.shift(), parts.join(':')]
+  splitAction = function (action) {
+    var parts = action.split(':');
+    if (parts.length <= 3) return parts;
+    return [parts.shift(), parts.shift(), parts.join(':')];
+  };
+
+  setup = function (analyticsCallback) {
+    var journeyStage = $('[data-journey]').attr('data-journey'),
+        journeyHelpers = $('[data-journey-click]');
+
+    if (journeyStage) {
+      analyticsCallback.apply(null, splitAction(journeyStage));
     }
 
-    setup = function (analyticsCallback) {
-      var journeyStage = $('[data-journey]').attr('data-journey')
-      var journeyHelpers = $('[data-journey-click]')
+    journeyHelpers.on('click', function (event) {
+      analyticsCallback.apply(null, splitAction($(this).data('journey-click')));
+    });
+  };
 
-      if (journeyStage) {
-        analyticsCallback.apply(null, splitAction(journeyStage))
-      }
+  setupForGoogleAnalytics = function () {
+    setup(GOVUK.performance.sendGoogleAnalyticsEvent);
+  };
 
-      journeyHelpers.on('click', function (event) {
-        analyticsCallback.apply(null, splitAction($(this).data('journey-click')))
-      })
-    }
+  return {
+    setup: setup,
+    setupForGoogleAnalytics: setupForGoogleAnalytics
+  };
+}());
 
-    setupForGoogleAnalytics = function () {
-      setup(GOVUK.performance.sendGoogleAnalyticsEvent)
-    }
-
-    return {
-      setup: setup,
-      setupForGoogleAnalytics: setupForGoogleAnalytics
-    }
-  }())
-
-  GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
-    global._gaq.push(['_trackEvent', category, event, label, undefined, true])
+GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
+  if (window.ga && typeof(window.ga) === 'function') {
+    ga('send', 'event', category, event, label);
+  } else {
+    _gaq.push(['_trackEvent', category, event, label, undefined, true]);
   }
-
-  global.GOVUK = GOVUK
-})(window)
+};
 ;
 // DEPRECATED
 // This isn’t needed if you’re using GOV.UK Elements 3.0.0 or above
