82afe8b28dc3e8e934793d349041e42b880c45e8
Neil Smith <github@nsmith.me.uk>
2018-08-24 18:32:10 +0100

    Added expected helper methods for Application repo

--- service-api/module/Application/src/Model/DataAccess/Repository/Application/ApplicationRepositoryTrait.php
+++ service-api/module/Application/src/Model/DataAccess/Repository/Application/ApplicationRepositoryTrait.php
@@ -1,6 +1,10 @@
 <?php
 namespace Application\Model\DataAccess\Repository\Application;
 
+use RuntimeException;
+use Opg\Lpa\DataModel\Lpa\Lpa;
+use Opg\Lpa\Logger\Logger;
+
 trait ApplicationRepositoryTrait {
 
     /**
@@ -14,6 +18,52 @@ trait ApplicationRepositoryTrait {
     public function setApplicationRepository(ApplicationRepositoryInterface $repo)
     {
         $this->applicationRepository = $repo;
+    }
+
+    /**
+     * @return ApplicationRepositoryInterface
+     */
+    private function getApplicationRepository() : ApplicationRepositoryInterface
+    {
+        if (!($this->applicationRepository instanceof WhoRepositoryInterface)) {
+            throw new \RuntimeException("Instance of ApplicationRepository not set");
+        }
+
+        return $this->applicationRepository;
+    }
+
+    protected function getLpa(int $lpaId) : ?Lpa
+    {
+        $result = $this->getApplicationRepository()->getById($lpaId);
+
+        if (is_null($result)) {
+            return null;
+        }
+
+        return new Lpa($result);
+    }
+
+    protected function updateLpa(Lpa $lpa) : bool
+    {
+        $logger = Logger::getInstance();
+
+        $logger->info('Updating LPA', [
+            'lpaid' => $lpa->id
+        ]);
+
+        // Check LPA is (still) valid.
+        if ($lpa->validateForApi()->hasErrors()) {
+            throw new RuntimeException('LPA object is invalid');
+        }
+
+        $result = $this->getApplicationRepository()->update($lpa);
+
+        $logger->info('LPA updated successfully', [
+            'lpaid' => $lpa->id,
+            'updatedAt' => $lpa->updatedAt,
+        ]);
+
+        return $result;
     }
 
 }
