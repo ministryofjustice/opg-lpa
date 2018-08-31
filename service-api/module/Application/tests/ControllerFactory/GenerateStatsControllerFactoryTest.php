df67dd229c731155279b4cc21bb3cba91ac3c44a
Steven Taylor <taylorsj1980@gmail.com>
2018-08-31 12:06:13 +0100

    Unit tests for API controller factories

---
+++ service-api/module/Application/tests/ControllerFactory/GenerateStatsControllerFactoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ApplicationTest\ControllerFactory;
+
+use Application\Controller\Console\GenerateStatsController;
+use Application\ControllerFactory\GenerateStatsControllerFactory;
+use Application\Model\Service\System\Stats as StatsService;
+use Application\Model\Service\System\DynamoCronLock;
+use Interop\Container\ContainerInterface;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\MockInterface;
+
+class GenerateStatsControllerFactoryTest extends MockeryTestCase
+{
+    /**
+     * @var GenerateStatsControllerFactory
+     */
+    private $factory;
+
+    /**
+     * @var MockInterface|ContainerInterface
+     */
+    private $container;
+
+    public function setUp()
+    {
+        $this->factory = new GenerateStatsControllerFactory();
+        $this->container = Mockery::mock(ContainerInterface::class);
+    }
+
+    public function testInvoke()
+    {
+        $this->container->shouldReceive('get')
+            ->with('DynamoCronLock')
+            ->andReturn(Mockery::mock(DynamoCronLock::class))
+            ->once();
+        $this->container->shouldReceive('get')
+            ->with(StatsService::class)
+            ->andReturn(Mockery::mock(StatsService::class))
+            ->once();
+
+        $controller = $this->factory->__invoke($this->container, GenerateStatsController::class);
+
+        $this->assertNotNull($controller);
+        $this->assertInstanceOf(GenerateStatsController::class, $controller);
+    }
+}
