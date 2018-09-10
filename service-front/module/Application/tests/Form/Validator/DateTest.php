c8cf28c7ed4bd4909158ddd8edaf83f194e67895
Steven Taylor <taylorsj1980@gmail.com>
2018-09-10 10:47:26 +0100

    Unit tests for form validators

--- service-front/module/Application/tests/Form/Validator/DateTest.php
+++ service-front/module/Application/tests/Form/Validator/DateTest.php
@@ -4,28 +4,27 @@ namespace ApplicationTest\Form\Lpa;
 
 use Application\Form\Validator\Date;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
-use PHPUnit\Framework\TestCase;
 
 class DateTest extends MockeryTestCase
 {
     /**
-     * @dataProvider getTestDates
+     * @dataProvider dataProvider
      */
-    public function testIsValid($date, array $errors)
+    public function testIsValid($data, array $errors)
     {
         $validator = new Date();
 
-        $result = $validator->isValid($date);
+        $result = $validator->isValid($data);
 
         $this->assertSame(empty($errors), $result);
         $this->assertEquals($errors, $validator->getMessages());
     }
 
-    public function getTestDates()
+    public function dataProvider()
     {
         return [
             [
-                'date' => [
+                'data' => [
                     'day'   => '2',
                     'month' => '7',
                     'year'  => '1980',
@@ -33,7 +32,7 @@ class DateTest extends MockeryTestCase
                 'errors' => [],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => 3,
                     'month' => 3,
                     'year'  => 2004,
@@ -41,7 +40,7 @@ class DateTest extends MockeryTestCase
                 'errors' => [],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '02',
                     'month' => '07',
                     'year'  => '1980',
@@ -49,7 +48,7 @@ class DateTest extends MockeryTestCase
                 'errors' => [],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '02',
                     'month' => '07',
                     'year'  => '1980',
@@ -57,7 +56,7 @@ class DateTest extends MockeryTestCase
                 'errors' => [],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => 3,
                     'month' => '4',
                     'year'  => '1979',
@@ -65,14 +64,14 @@ class DateTest extends MockeryTestCase
                 'errors' => [],
             ],
             [
-                'date' => null,
+                'data' => null,
                 'errors' => [
                     'dateInvalid' => 'Invalid type given. String, integer, array or DateTime expected',
                     'dateInvalidDate' => 'The input does not appear to be a valid date',
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '32',
                     'month' => '2',
                     'year'  => '1988',
@@ -82,7 +81,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '1',
                     'month' => '15',
                     'year'  => '1999',
@@ -92,7 +91,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '1',
                     'month' => '2',
                     'year'  => '2',
@@ -102,7 +101,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => 'cd',
                     'month' => '2',
                     'year'  => '1965',
@@ -112,7 +111,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '12',
                     'month' => '12',
                     'year'  => '999',
@@ -122,7 +121,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '34',
                     'month' => '1',
                     'year'  => '2001',
@@ -132,7 +131,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => 3,
                     'month' => 3,
                     'year'  => 2004,
@@ -140,7 +139,7 @@ class DateTest extends MockeryTestCase
                 'errors' => [],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => true,
                     'month' => null,
                     'year'  => 1999,
@@ -150,7 +149,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '32cd',
                     'month' => '2',
                     'year'  => '1965',
@@ -160,7 +159,7 @@ class DateTest extends MockeryTestCase
                 ],
             ],
             [
-                'date' => [
+                'data' => [
                     'day'   => '1cd',
                     'month' => '2',
                     'year'  => '1965',
