5e2addd2f774f325d7831b49fb40e65a87bd3297
Neil Smith <github@nsmith.me.uk>
2018-09-21 10:50:10 +0100

    Added applications table definition.

---
+++ service-api/db/migrations/20180920165434_applications_table.php
@@ -0,0 +1,66 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class ApplicationsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+
+        $applications = $this->table('applications', ['id' => false, 'primary_key'=>'id']);
+
+        $applications->addColumn('id', 'biginteger')
+
+            ->addColumn('user', 'text', ['null' => true])
+
+            ->addColumn('updatedAt', 'datetime', ['timezone'=>true])    // NULL is allowed
+            ->addColumn('startedAt', 'datetime', ['null' => true, 'timezone'=>true])
+            ->addColumn('createdAt', 'datetime', ['null' => true, 'timezone'=>true])
+            ->addColumn('completedAt', 'datetime', ['null' => true, 'timezone'=>true])
+            ->addColumn('lockedAt', 'datetime', ['null' => true, 'timezone'=>true])
+
+            ->addColumn('locked', 'boolean', ['null' => true])
+            ->addColumn('whoAreYouAnswered', 'boolean', ['null' => true])
+
+            ->addColumn('seed', 'biginteger', ['null' => true])
+            ->addColumn('repeatCaseNumber', 'biginteger', ['null' => true])
+
+            ->addColumn('document', 'jsonb', ['null' => true])
+            ->addColumn('payment', 'jsonb', ['null' => true])
+            ->addColumn('metadata', 'jsonb', ['null' => true])
+
+            ->addIndex(['user'])
+            ->addIndex(['startedAt'])
+            ->addIndex(['createdAt'])
+            ->addIndex(['completedAt'])
+
+            ->create();
+
+    }
+}
