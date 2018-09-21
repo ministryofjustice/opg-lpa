fd255e6e6a7257b4ef3be6bb84aa1536a1e32e37
Neil Smith <github@nsmith.me.uk>
2018-09-21 09:29:38 +0100

    Disable Postgres

--- service-api/module/Application/src/Module.php
+++ service-api/module/Application/src/Module.php
@@ -54,8 +54,8 @@ class Module
                 // Map the Repository Interfaces to concrete implementations.
                 Repository\User\LogRepositoryInterface::class => Mongo\Collection\AuthLogCollection::class,
                 Repository\User\UserRepositoryInterface::class => Mongo\Collection\AuthUserCollection::class,
-                Repository\Stats\StatsRepositoryInterface::class => Postgres\StatsData::class,
-                Repository\Application\WhoRepositoryInterface::class => Postgres\WhoAreYouData::class,
+                Repository\Stats\StatsRepositoryInterface::class => Mongo\Collection\ApiStatsLpasCollection::class,
+                Repository\Application\WhoRepositoryInterface::class => Mongo\Collection\ApiWhoCollection::class,
                 Repository\Application\ApplicationRepositoryInterface::class => Mongo\Collection\ApiLpaCollection::class,
             ],
             'invokables' => [
