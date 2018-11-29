69a591d8af73c86550da7be4844b68a1a1ed17a3
Neil Smith <github@nsmith.me.uk>
2018-11-29 13:40:43 +0000

    Added localstack S3 endpoint

--- service-api/config/autoload/global.php
+++ service-api/config/autoload/global.php
@@ -129,6 +129,8 @@ return [
                     'Bucket' => getenv('OPG_LPA_COMMON_PDF_CACHE_S3_BUCKET') ?: null,
                 ],
                 'client' => [
+                    'endpoint' => getenv('OPG_LPA_COMMON_S3_ENDPOINT') ?: null,
+                    'use_path_style_endpoint' => true,
                     'version' => '2006-03-01',
                     'region' => 'eu-west-1',
                 ],
