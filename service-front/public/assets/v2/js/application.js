b34552a243b685ad87980a28b09052ec2aa47564
digitalali-moj <javid.ali@digital.justice.gov.uk>
2018-12-06 17:08:09 +0000

    LPA-2945 Form validation fixes required for version 3.0+ of gov-uk-elements

--- service-front/public/assets/v2/js/application.js
+++ service-front/public/assets/v2/js/application.js
@@ -22012,15 +22012,12 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
   }
 };
 ;
-// DEPRECATED
-// This isn’t needed if you’re using GOV.UK Elements 3.0.0 or above
+// NOTICE
+// IF you are using GOV.UK Elements 3.0,0 or above then you do not need to use
+// this script anymore as it uses a pure CSS solution.
 
 ;(function (global) {
   'use strict'
-
-  if (window.console && window.console.warn) {
-    window.console.warn('Deprecation warning: Custom radio buttons and checkboxes (released in GOV.UK Elements 3.0.0) no longer require this JavaScript.')
-  }
 
   var $ = global.jQuery
   var GOVUK = global.GOVUK || {}
@@ -22314,11 +22311,6 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
   var GOVUKTracker = function (gifUrl) {
     this.gifUrl = gifUrl
     this.dimensions = []
-    if (global.ga) {
-      global.ga(function (tracker) {
-        this.gaClientId = tracker.get('clientId')
-      }.bind(this))
-    }
   }
 
   GOVUKTracker.load = function () {}
@@ -22373,12 +22365,16 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
     this.sendToTracker('event', evt)
   }
 
-  GOVUKTracker.prototype.trackSocial = function (network, action, target) {
-    this.sendToTracker('social', {
+  GOVUKTracker.prototype.trackSocial = function (network, action, target, options) {
+    var trackingOptions = {
       'socialNetwork': network,
       'socialAction': action,
       'socialTarget': target
-    })
+    }
+
+    $.extend(trackingOptions, options)
+
+    this.sendToTracker('social', trackingOptions)
   }
 
   GOVUKTracker.prototype.addLinkedTrackerDomain = function () { /* noop */ }
@@ -22425,7 +22421,14 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
 
   GOVUKTracker.prototype.sendToTracker = function (type, payload) {
     $(global.document).ready(function () {
-      this.sendData(this.payloadParams(type, payload))
+      if (global.ga) {
+        global.ga(function (tracker) {
+          this.gaClientId = tracker.get('clientId')
+          this.sendData(this.payloadParams(type, payload))
+        }.bind(this))
+      } else {
+        this.sendData(this.payloadParams(type, payload))
+      }
     }.bind(this))
   }
 
@@ -22451,6 +22454,15 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
       sendToGa('set', 'anonymizeIp', true)
     }
 
+    function disableAdTracking () {
+      // https://support.google.com/analytics/answer/2444872?hl=en
+      sendToGa('set', 'displayFeaturesTask', null)
+    }
+
+    function stripLocationPII () {
+      sendToGa('set', 'location', stripEmailAddressesFromString(window.location.href))
+    }
+
     // Support legacy cookieDomain param
     if (typeof fieldsObject === 'string') {
       fieldsObject = { cookieDomain: fieldsObject }
@@ -22458,6 +22470,8 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
 
     configureProfile()
     anonymizeIp()
+    disableAdTracking()
+    stripLocationPII()
   }
 
   GoogleAnalyticsUniversalTracker.load = function () {
@@ -22542,13 +22556,17 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
     target – Specifies the target of a social interaction.
              This value is typically a URL but can be any text.
   */
-  GoogleAnalyticsUniversalTracker.prototype.trackSocial = function (network, action, target) {
-    sendToGa('send', {
+  GoogleAnalyticsUniversalTracker.prototype.trackSocial = function (network, action, target, options) {
+    var trackingOptions = {
       'hitType': 'social',
       'socialNetwork': network,
       'socialAction': action,
       'socialTarget': target
-    })
+    }
+
+    $.extend(trackingOptions, options)
+
+    sendToGa('send', trackingOptions)
   }
 
   /*
@@ -22571,6 +22589,7 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
     sendToGa(name + '.linker:autoLink', [domain])
 
     sendToGa(name + '.set', 'anonymizeIp', true)
+    sendToGa(name + '.set', 'displayFeaturesTask', null)
     sendToGa(name + '.send', 'pageview')
   }
 
@@ -22585,6 +22604,11 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
     }
   }
 
+  function stripEmailAddressesFromString (string) {
+    var stripped = string.replace(/[^\s=/?&]+(?:@|%40)[^\s=/?&]+/g, '[email]')
+    return stripped
+  }
+
   GOVUK.GoogleAnalyticsUniversalTracker = GoogleAnalyticsUniversalTracker
 
   global.GOVUK = GOVUK
@@ -22594,22 +22618,89 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
   'use strict'
 
   var GOVUK = global.GOVUK || {}
+  var EMAIL_PATTERN = /[^\s=/?&]+(?:@|%40)[^\s=/?&]+/g
+  var POSTCODE_PATTERN = /[A-PR-UWYZ][A-HJ-Z]?[0-9][0-9A-HJKMNPR-Y]?(?:[\s+]|%20)*[0-9][ABD-HJLNPQ-Z]{2}/gi
+  var DATE_PATTERN = /\d{4}(-?)\d{2}(-?)\d{2}/g
 
   // For usage and initialisation see:
   // https://github.com/alphagov/govuk_frontend_toolkit/blob/master/docs/analytics.md#create-an-analytics-tracker
 
   var Analytics = function (config) {
+    this.stripDatePII = false
+    if (typeof config.stripDatePII !== 'undefined') {
+      this.stripDatePII = (config.stripDatePII === true)
+      // remove the option so we don't pass it to other trackers - it's not
+      // their concern
+      delete config.stripDatePII
+    }
+    this.stripPostcodePII = false
+    if (typeof config.stripPostcodePII !== 'undefined') {
+      this.stripPostcodePII = (config.stripPostcodePII === true)
+      // remove the option so we don't pass it to other trackers - it's not
+      // their concern
+      delete config.stripPostcodePII
+    }
     this.trackers = []
     if (typeof config.universalId !== 'undefined') {
       var universalId = config.universalId
       delete config.universalId
       this.trackers.push(new GOVUK.GoogleAnalyticsUniversalTracker(universalId, config))
     }
-    if (typeof config.govukTrackerGifUrl !== 'undefined') {
-      var govukTrackerGifUrl = config.govukTrackerGifUrl
-      delete config.govukTrackerGifUrl
-      this.trackers.push(new GOVUK.GOVUKTracker(govukTrackerGifUrl))
+    if (typeof config.govukTrackerUrl !== 'undefined') {
+      var govukTrackerUrl = config.govukTrackerUrl
+      delete config.govukTrackerUrl
+      this.trackers.push(new GOVUK.GOVUKTracker(govukTrackerUrl))
     }
+  }
+
+  var PIISafe = function (value) {
+    this.value = value
+  }
+  Analytics.PIISafe = PIISafe
+
+  Analytics.prototype.stripPII = function (value) {
+    if (typeof value === 'string') {
+      return this.stripPIIFromString(value)
+    } else if (Object.prototype.toString.call(value) === '[object Array]' || Object.prototype.toString.call(value) === '[object Arguments]') {
+      return this.stripPIIFromArray(value)
+    } else if (typeof value === 'object') {
+      return this.stripPIIFromObject(value)
+    } else {
+      return value
+    }
+  }
+
+  Analytics.prototype.stripPIIFromString = function (string) {
+    var stripped = string.replace(EMAIL_PATTERN, '[email]')
+    if (this.stripDatePII === true) {
+      stripped = stripped.replace(DATE_PATTERN, '[date]')
+    }
+    if (this.stripPostcodePII === true) {
+      stripped = stripped.replace(POSTCODE_PATTERN, '[postcode]')
+    }
+    return stripped
+  }
+
+  Analytics.prototype.stripPIIFromObject = function (object) {
+    if (object instanceof Analytics.PIISafe) {
+      return object.value
+    } else {
+      for (var property in object) {
+        var value = object[property]
+
+        object[property] = this.stripPII(value)
+      }
+      return object
+    }
+  }
+
+  Analytics.prototype.stripPIIFromArray = function (array) {
+    for (var i = 0, l = array.length; i < l; i++) {
+      var elem = array[i]
+
+      array[i] = this.stripPII(elem)
+    }
+    return array
   }
 
   Analytics.prototype.sendToTrackers = function (method, args) {
@@ -22628,8 +22719,19 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
     GOVUK.GOVUKTracker.load()
   }
 
+  Analytics.prototype.defaultPathForTrackPageview = function (location) {
+    // Get the page path including querystring, but ignoring the anchor
+    // as per default behaviour of GA (see: https://developers.google.com/analytics/devguides/collection/analyticsjs/pages#overview)
+    // we ignore the possibility of there being campaign variables in the
+    // anchor because we wouldn't know how to detect and parse them if they
+    // were present
+    return this.stripPIIFromString(location.href.substring(location.origin.length).split('#')[0])
+  }
+
   Analytics.prototype.trackPageview = function (path, title, options) {
-    this.sendToTrackers('trackPageview', arguments)
+    arguments[0] = arguments[0] || this.defaultPathForTrackPageview(window.location)
+    if (arguments.length === 0) { arguments.length = 1 }
+    this.sendToTrackers('trackPageview', this.stripPII(arguments))
   }
 
   /*
@@ -22639,11 +22741,11 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
     options.nonInteraction – Prevent event from impacting bounce rate
   */
   Analytics.prototype.trackEvent = function (category, action, options) {
-    this.sendToTrackers('trackEvent', arguments)
+    this.sendToTrackers('trackEvent', this.stripPII(arguments))
   }
 
-  Analytics.prototype.trackShare = function (network) {
-    this.sendToTrackers('trackSocial', [network, 'share', global.location.pathname])
+  Analytics.prototype.trackShare = function (network, options) {
+    this.sendToTrackers('trackSocial', this.stripPII([network, 'share', global.location.pathname, options]))
   }
 
   /*
@@ -22651,7 +22753,7 @@ GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
     Universal Analytics profile
    */
   Analytics.prototype.setDimension = function (index, value) {
-    this.sendToTrackers('setDimension', arguments)
+    this.sendToTrackers('setDimension', this.stripPII(arguments))
   }
 
   /*
