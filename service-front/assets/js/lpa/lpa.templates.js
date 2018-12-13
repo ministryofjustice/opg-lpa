dfeabdb9ea7be19b0ea177821aca9f7aa492311a
ramchale <richard.mchale@digital.justice.gov.uk>
2018-12-13 08:56:12 +0000

    LPA-2972: Add aria attributes to repeat application dialog and fix links

--- service-front/assets/js/lpa/lpa.templates.js
+++ service-front/assets/js/lpa/lpa.templates.js
@@ -16,15 +16,15 @@ this["lpa"]["templates"]["alert.withinForm"] = Handlebars.template({"compiler":[
 this["lpa"]["templates"]["dialog.confirmRepeatApplication"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"dialog-container\">\n\n    <div class=\"dialog-title-block\">"
+  return "<div class=\"dialog-container\" role=\"dialog\" aria-labelledby=\"dialog-title\" aria-describedby=\"dialog-message\">\n\n    <div id=\"dialog-title\" class=\"dialog-title-block\">"
     + alias4(((helper = (helper = helpers.dialogTitle || (depth0 != null ? depth0.dialogTitle : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dialogTitle","hash":{},"data":data}) : helper)))
     + "</div>\n\n    <div id=\"dialog-message\" class=\"dialog-message-block\"><p>"
     + alias4(((helper = (helper = helpers.dialogMessage || (depth0 != null ? depth0.dialogMessage : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dialogMessage","hash":{},"data":data}) : helper)))
-    + "</p></div>\n\n    <div class=\"dialog__button-bar\">\n        <a class=\"button dialog__button--accept "
+    + "</p></div>\n\n    <div class=\"dialog__button-bar\">\n        <a href=\"#\" class=\"button dialog__button--accept "
     + alias4(((helper = (helper = helpers.acceptClass || (depth0 != null ? depth0.acceptClass : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"acceptClass","hash":{},"data":data}) : helper)))
-    + "\" aria-describedby=\"dialog-message\">"
+    + "\">"
     + alias4(((helper = (helper = helpers.acceptButtonText || (depth0 != null ? depth0.acceptButtonText : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"acceptButtonText","hash":{},"data":data}) : helper)))
-    + "</a>\n        <a class=\"button-secondary dialog__button--cancel  "
+    + "</a>\n        <a href=\"#\" class=\"button-secondary dialog__button--cancel  "
     + alias4(((helper = (helper = helpers.cancelClass || (depth0 != null ? depth0.cancelClass : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"cancelClass","hash":{},"data":data}) : helper)))
     + "\">"
     + alias4(((helper = (helper = helpers.cancelButtonText || (depth0 != null ? depth0.cancelButtonText : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"cancelButtonText","hash":{},"data":data}) : helper)))
