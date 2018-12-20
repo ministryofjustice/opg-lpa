2230fb5cbbb6335658d447e9f3b00c6dd50a2a95
Steven Taylor <taylorsj1980@gmail.com>
2018-12-20 15:28:47 +0000

    Feedback entries added to DB table

--- service-front/module/Application/src/Model/Service/Feedback/Feedback.php
+++ service-front/module/Application/src/Model/Service/Feedback/Feedback.php
@@ -3,18 +3,44 @@
 namespace Application\Model\Service\Feedback;
 
 use Application\Model\Service\AbstractEmailService;
+use Application\Model\Service\ApiClient\ApiClientAwareInterface;
+use Application\Model\Service\ApiClient\ApiClientTrait;
+use Application\Model\Service\ApiClient\Exception\ApiException;
 use Application\Model\Service\Mail\Transport\MailTransport;
 use Exception;
 
-class Feedback extends AbstractEmailService
+class Feedback extends AbstractEmailService implements ApiClientAwareInterface
 {
+    use ApiClientTrait;
+
     /**
      * Send feedback data to the feedback inbox using a template
      *
      * @param array $data
      * @return bool|string
      */
-    public function sendMail(array $data)
+    public function add(array $data)
+    {
+        try {
+            $this->apiClient->httpPost('/user-feedback', $data);
+
+            //  Send the feedback via email also
+            //  TODO - Remove this when we want to move fully to the DB stored feedback
+            $this->sendMail($data);
+
+            return true;
+        } catch (ApiException $ex) {}
+
+        return false;
+    }
+
+    /**
+     * Send feedback data to the feedback inbox using a template
+     *
+     * @param array $data
+     * @return bool|string
+     */
+    private function sendMail(array $data)
     {
         $to = $this->getConfig()['sendFeedbackEmailTo'];
 
