299e4799af3afa1cf52dfb1f466e60a75437a189
ramchale <richard.mchale@digital.justice.gov.uk>
2019-01-11 14:42:05 +0000

    LPA-2945: Fix unit test after id and for removed from Application\Form\Element\Type

--- service-front/module/Application/tests/Form/Element/TypeTest.php
+++ service-front/module/Application/tests/Form/Element/TypeTest.php
@@ -23,22 +23,10 @@ class TypeTest extends MockeryTestCase
             'property-and-financial' => [
                 'label' => 'Property and financial affairs',
                 'value' => 'property-and-financial',
-                'label_attributes' => [
-                    'for' => 'property-and-financial',
-                ],
-                'attributes' => [
-                    'id' => 'property-and-financial',
-                ],
             ],
             'health-and-welfare' => [
                 'label' => 'Health and welfare',
                 'value' => 'health-and-welfare',
-                'label_attributes' => [
-                    'for' => 'health-and-welfare',
-                ],
-                'attributes' => [
-                    'id' => 'health-and-welfare',
-                ],
             ],
         ], $this->element->getValueOptions());
     }
