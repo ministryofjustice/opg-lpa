9ac72a0f1c8aa0151cbabac4fd564562ca0d563e
SeemaMenon <seema.workspace@gmail.com>
2019-01-30 15:15:45 +0000

    LPA-2320-Unit-Test-Coverage: wip

---
+++ service-front/module/Application/tests/View/Helper/AccountInfoTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seemamenon
+ * Date: 28/01/2019
+ * Time: 11:42
+ */
+
+namespace ApplicationTest\View\Helper;
+
+
+class AccountInfoTest extends MockeryTestCase
+{
+    public function testInvoke() : void
+    {
+
+    }
+
+}
