a3dbbf12fac3ba4991ce0f3e859497bc34923d5c
Steven Taylor <taylorsj1980@gmail.com>
2019-03-04 21:32:18 +0000

    Unit test fixes

--- service-front/module/Application/src/Model/Service/Feedback/Feedback.php
+++ service-front/module/Application/src/Model/Service/Feedback/Feedback.php
@@ -5,7 +5,6 @@ namespace Application\Model\Service\Feedback;
 use Application\Model\Service\AbstractEmailService;
 use Application\Model\Service\ApiClient\ApiClientAwareInterface;
 use Application\Model\Service\ApiClient\ApiClientTrait;
-use Application\Model\Service\ApiClient\Exception\ApiException;
 use Application\Model\Service\Mail\Transport\MailTransport;
 use Exception;
 
@@ -25,31 +24,13 @@ class Feedback extends AbstractEmailService implements ApiClientAwareInterface
             $this->apiClient->httpPost('/user-feedback', $data);
 
             //  Send the feedback via email also
-            //  TODO - Remove this when we want to move fully to the DB stored feedback
-            $this->sendMail($data);
+            $to = $this->getConfig()['sendFeedbackEmailTo'];
+
+            $this->getMailTransport()->sendMessageFromTemplate($to, MailTransport::EMAIL_FEEDBACK, $data);
 
             return true;
-        } catch (ApiException $ex) {}
+        } catch (Exception $ignore) {}
 
         return false;
-    }
-
-    /**
-     * Send feedback data to the feedback inbox using a template
-     *
-     * @param array $data
-     * @return bool|string
-     */
-    private function sendMail(array $data)
-    {
-        $to = $this->getConfig()['sendFeedbackEmailTo'];
-
-        try {
-            $this->getMailTransport()->sendMessageFromTemplate($to, MailTransport::EMAIL_FEEDBACK, $data);
-        } catch (Exception $e) {
-            return "failed-sending-email";
-        }
-
-        return true;
     }
 }
