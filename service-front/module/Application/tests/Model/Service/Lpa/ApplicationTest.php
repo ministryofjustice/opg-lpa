5b5da42fb64d26576cc7dae8db89ddd79371c613
ramchale <richard.mchale@digital.justice.gov.uk>
2019-04-07 13:58:22 +0100

    LPA-3192 - UI and status mapping changes to track application status

--- service-front/module/Application/tests/Model/Service/Lpa/ApplicationTest.php
+++ service-front/module/Application/tests/Model/Service/Lpa/ApplicationTest.php
@@ -6,10 +6,14 @@ use Application\Model\Service\ApiClient\Client;
 use Application\Model\Service\ApiClient\Exception\ApiException;
 use Application\Model\Service\Authentication\AuthenticationService;
 use Application\Model\Service\Lpa\Application;
+use ArrayObject;
+use DateTime;
+use Http\Client\Exception;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Mockery\MockInterface;
 use Opg\Lpa\DataModel\Lpa\Lpa;
+use OpgTest\Lpa\DataModel\FixturesData;
 use Psr\Http\Message\ResponseInterface;
 
 class ApplicationTest extends MockeryTestCase
@@ -39,7 +43,9 @@ class ApplicationTest extends MockeryTestCase
 
         $this->apiClient = Mockery::mock(Client::class);
 
-        $this->service = new Application($this->authenticationService, []);
+        $this->service = new Application($this->authenticationService, [
+            'processing-status' => ['track-from-date' => '2019-01-01']
+        ]);
         $this->service->setApiClient($this->apiClient);
     }
 
@@ -99,7 +105,6 @@ class ApplicationTest extends MockeryTestCase
         $result = $this->service->getStatuses('4321');
 
         $this->assertEquals(['4321' => ['found'=>false]], $result);
-
     }
 
     public function testGetStatusesException()
@@ -114,5 +119,130 @@ class ApplicationTest extends MockeryTestCase
         $result = $this->service->getStatuses(4321);
 
         $this->assertEquals(['4321' => ['found'=>false]], $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetLpaSummariesNoApplications()
+    {
+        $this->apiClient->shouldReceive('httpGet')
+            ->withArgs(['/v2/user/4321/applications', ['search' => null]])
+            ->once()
+            ->andReturn(['applications' => []]);
+
+        $result = $this->service->getLpaSummaries();
+
+        $this->assertEquals(['applications' => []], $result);
+    }
+
+    private function modifiedLPA($id = 5531003157, $completedAt = null, $processingStatus = null)
+    {
+        $lpaJson = FixturesData::getHwLpaJson();
+
+        $lpaJson = str_replace('"id" : 5531003156', '"id" : ' . $id, $lpaJson);
+
+        if ($completedAt != null) {
+            $lpaJson = str_replace(
+                '"completedAt" : "2017-03-24T16:21:52.804Z"',
+                '"completedAt" : "' . $completedAt . '"',
+                $lpaJson
+            );
+        }
+
+        if ($processingStatus) {
+            $lpaJson = str_replace(
+                '"metadata" : {',
+                '"metadata" : {
+                "' . Lpa::SIRIUS_PROCESSING_STATUS . '" : "' . $processingStatus . '",',
+                $lpaJson
+            );
+        }
+
+        return $lpaJson;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetLpaSummariesMultipleApplications()
+    {
+        $this->apiClient->shouldReceive('httpGet')
+            ->withArgs(['/v2/user/4321/applications', ['search' => null]])
+            ->once()
+            ->andReturn(['applications' => [FixturesData::getHwLpaJson(), $this->modifiedLPA()]]);
+
+        $result = $this->service->getLpaSummaries();
+
+        $this->assertEquals(['applications' => [
+            new ArrayObject([
+                'id' => 5531003156,
+                'version' => 2,
+                'donor' => 'Hon Ayden Armstrong',
+                'type' => 'health-and-welfare',
+                'updatedAt' => new DateTime('2017-03-24T16:21:52.804000+0000'),
+                'progress' => 'Completed',
+                'refreshId' => null
+            ]),
+            new ArrayObject([
+                'id' => 5531003157,
+                'version' => 2,
+                'donor' => 'Hon Ayden Armstrong',
+                'type' => 'health-and-welfare',
+                'updatedAt' => new DateTime('2017-03-24T16:21:52.804000+0000'),
+                'progress' => 'Completed',
+                'refreshId' => null
+            ])
+        ]], $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetLpaSummariesCanTrackWithNoTrackingStatus()
+    {
+        $this->apiClient->shouldReceive('httpGet')
+            ->withArgs(['/v2/user/4321/applications', ['search' => null]])
+            ->once()
+            ->andReturn(['applications' => [$this->modifiedLPA(5531003157, '2019-01-02')]]);
+
+        $result = $this->service->getLpaSummaries();
+
+        $this->assertEquals(['applications' => [
+            new ArrayObject([
+                'id' => 5531003157,
+                'version' => 2,
+                'donor' => 'Hon Ayden Armstrong',
+                'type' => 'health-and-welfare',
+                'updatedAt' => new DateTime('2017-03-24T16:21:52.804000+0000'),
+                'progress' => 'Waiting',
+                'refreshId' => 5531003157
+            ])
+        ]], $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetLpaSummariesCanTrackWithTrackingStatus()
+    {
+        $this->apiClient->shouldReceive('httpGet')
+            ->withArgs(['/v2/user/4321/applications', ['search' => null]])
+            ->once()
+            ->andReturn(['applications' => [$this->modifiedLPA(5531003157, '2019-01-2', 'Checking')]]);
+
+        $result = $this->service->getLpaSummaries();
+
+        $this->assertEquals(['applications' => [
+            new ArrayObject([
+                'id' => 5531003157,
+                'version' => 2,
+                'donor' => 'Hon Ayden Armstrong',
+                'type' => 'health-and-welfare',
+                'updatedAt' => new DateTime('2017-03-24T16:21:52.804000+0000'),
+                'progress' => 'Checking',
+                'refreshId' => 5531003157
+            ])
+        ]], $result);
     }
 }
