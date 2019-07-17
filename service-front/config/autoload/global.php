c1a27e816ff606d6faca06760d6f3e0e3f1ae9e9
SeemaMenon <seema.workspace@gmail.com>
2019-07-17 12:29:48 +0100

    clean up

--- service-front/config/autoload/global.php
+++ service-front/config/autoload/global.php
@@ -161,8 +161,7 @@ return [
 
     'sendFeedbackEmailTo' => 'LPADigitalFeedback@PublicGuardian.gov.uk',
     'processing-status' => [
-        //'track-from-date' => getenv('OPG_LPA_FRONT_TRACK_FROM_DATE') ?: '2030-01-01',
-        'track-from-date' => '2019-07-08',
+        'track-from-date' => getenv('OPG_LPA_FRONT_TRACK_FROM_DATE') ?: '2030-01-01',
         'endpoint' => getenv('OPG_LPA_PROCESSING_STATUS_ENDPOINT') ?: 'http://gateway:5000/v1/lpa-online-tool/lpas/'
     ],
 ];
