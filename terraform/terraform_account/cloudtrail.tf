7b7a09d8eaf712e38615b160dc0cbb5b2339e487
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-07-30 16:53:21 +0100

    configure cloudtrail with data events for s3

---
+++ terraform/terraform_account/cloudtrail.tf
@@ -0,0 +1,55 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudtrail" "cloudtrail" {
+  name                          = "online_lpa_cloudtrail_${terraform.workspace}"
+  s3_bucket_name                = "${aws_s3_bucket.cloudtrail.id}"
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = true
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["arn:aws:s3:::"]
+    }
+  }
+}
+
+resource "aws_s3_bucket" "cloudtrail" {
+  bucket        = "online_lpa_cloudtrail_${terraform.workspace}"
+  force_destroy = true
+
+  policy = data.aws_iam_policy_document.cloudtrail_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "cloudtrail_bucket_policy" {
+  statement {
+    sid       = "AWSCloudTrailAclCheck"
+    actions   = ["s3:GetBucketAcl"]
+    resources = ["arn:aws:s3:::online_lpa_cloudtrail_${terraform.workspace}"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+  statement {
+    sid       = "AWSCloudTrailWrite"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::online_lpa_cloudtrail_${terraform.workspace}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control",
+      ]
+    }
+
+  }
+}
