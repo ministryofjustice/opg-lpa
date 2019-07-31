057c0d45e7a760b3a2349ac8c818b3576fc317fc
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-07-31 10:15:44 +0100

    clean up locals

--- terraform/terraform_account/locals.tf
+++ terraform/terraform_account/locals.tf
@@ -19,20 +19,6 @@ locals {
 
   account_id = var.accounts[local.account_name].account_id
 
-  # vpc_cidr_ranges = {
-  #   development = "10.172.0.0/16"
-  #   production  = "10.72.0.0/16"
-  # }
-  # domains = {
-  #   development = "dev.lpa.opg.digital"
-  #   production  = "lpa.opg.digital"
-  # }
-
-  # vpc_cidr_range       = lookup(local.vpc_cidr_ranges, local.account_name)
-  # vpc_name             = lookup(local.vpc_names, local.account_name)
-  # domain               = lookup(local.domains, local.account_name)
-  # opg_hosted_zone_name = "${lookup(local.domains, local.account_name)}."
-
   mandatory_moj_tags = {
     business-unit = "OPG"
     application   = "Online LPA Service"
