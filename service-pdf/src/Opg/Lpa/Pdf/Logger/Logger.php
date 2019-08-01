a38578f78812a3302d9e7dd69fa74f9c31f49654
Neil Smith <github@nsmith.me.uk>
2019-08-01 12:07:51 +0100

    Setup for ECS

--- service-pdf/src/Opg/Lpa/Pdf/Logger/Logger.php
+++ service-pdf/src/Opg/Lpa/Pdf/Logger/Logger.php
@@ -3,41 +3,7 @@
 namespace Opg\Lpa\Pdf\Logger;
 
 use Opg\Lpa\Logger\Logger as LpaLogger;
-use Opg\Lpa\Pdf\Config\Config;
 
 class Logger extends LpaLogger
 {
-    private static $instance = null;
-
-    /**
-     * Construct this logger with values from config
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $logConfig = Config::getInstance()['log'];
-
-        $this->setFileLogPath($logConfig['path']);
-        $this->setSentryUri($logConfig['sentry-uri']);
-    }
-
-    /**
-     * Singleton provider
-     *
-     * @return \Opg\Lpa\Pdf\Logger\Logger
-     */
-    public static function getInstance()
-    {
-        if (self::$instance === null) {
-            self::$instance = new self();
-        }
-
-        return self::$instance;
-    }
-
-    public static function destroy()
-    {
-        self::$instance = null;
-    }
 }
