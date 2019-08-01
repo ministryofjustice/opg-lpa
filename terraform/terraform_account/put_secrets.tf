4cb0797b4f2d452d43cb600101911bd7ec6c6e8c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-08-01 09:51:41 +0100

    remove unused secrets

--- terraform/terraform_account/put_secrets.tf
+++ terraform/terraform_account/put_secrets.tf
@@ -1,20 +1,4 @@
 # common
-resource "aws_secretsmanager_secret" "opg_base_ssl_cert" {
-  name = "${local.account_name}/opg_base_ssl_cert"
-}
-
-resource "aws_secretsmanager_secret" "opg_base_ca_cert" {
-  name = "${local.account_name}/opg_base_ca_cert"
-}
-
-resource "aws_secretsmanager_secret" "opg_base_ssl_key" {
-  name = "${local.account_name}/opg_base_ssl_key"
-}
-
-resource "aws_secretsmanager_secret" "opg_lpa_common_sentry_api_uri" {
-  name = "${local.account_name}/opg_lpa_common_sentry_api_uri"
-}
-
 resource "aws_secretsmanager_secret" "opg_lpa_common_admin_accounts" {
   name = "${local.account_name}/opg_lpa_common_admin_accounts"
 }
@@ -24,14 +8,6 @@ resource "aws_secretsmanager_secret" "opg_lpa_common_account_cleanup_notificatio
 }
 
 # api secrets
-resource "aws_secretsmanager_secret" "opg_lpa_api_pdf_encryption_key_queue" {
-  name = "${local.account_name}/opg_lpa_api_pdf_encryption_key_queue"
-}
-
-resource "aws_secretsmanager_secret" "opg_lpa_api_pdf_encryption_key_document" {
-  name = "${local.account_name}/opg_lpa_api_pdf_encryption_key_document"
-}
-
 resource "aws_secretsmanager_secret" "opg_lpa_front_csrf_salt" {
   name = "${local.account_name}/opg_lpa_front_csrf_salt"
 }
@@ -46,32 +22,12 @@ resource "aws_secretsmanager_secret" "opg_lpa_admin_jwt_secret" {
 }
 
 # front secrets
-resource "aws_secretsmanager_secret" "opg_lpa_front_session_encryption_key" {
-  name = "${local.account_name}/opg_lpa_front_session_encryption_key"
-}
-
-resource "aws_secretsmanager_secret" "opg_lpa_front_session_encryption_keys" {
-  name = "${local.account_name}/opg_lpa_front_session_encryption_keys"
-}
-
 resource "aws_secretsmanager_secret" "opg_lpa_front_email_sendgrid_webhook_token" {
   name = "${local.account_name}/opg_lpa_front_email_sendgrid_webhook_token"
 }
 
 resource "aws_secretsmanager_secret" "opg_lpa_front_email_sendgrid_api_key" {
   name = "${local.account_name}/opg_lpa_front_email_sendgrid_api_key"
-}
-
-resource "aws_secretsmanager_secret" "opg_lpa_front_postcode_license_key" {
-  name = "${local.account_name}/opg_lpa_front_postcode_license_key"
-}
-
-resource "aws_secretsmanager_secret" "opg_lpa_front_postcode_info_token" {
-  name = "${local.account_name}/opg_lpa_front_postcode_info_token"
-}
-
-resource "aws_secretsmanager_secret" "opg_lpa_front_nginx_auth" {
-  name = "${local.account_name}/opg_lpa_front_nginx_auth"
 }
 
 resource "aws_secretsmanager_secret" "opg_lpa_front_gov_pay_key" {
@@ -83,14 +39,6 @@ resource "aws_secretsmanager_secret" "opg_lpa_front_ordnance_survey_license_key"
 }
 
 # pdf secrets
-resource "aws_secretsmanager_secret" "opg_lpa_pdf_encryption_key_queue" {
-  name = "${local.account_name}/opg_lpa_pdf_encryption_key_queue"
-}
-
-resource "aws_secretsmanager_secret" "opg_lpa_pdf_encryption_key_document" {
-  name = "${local.account_name}/opg_lpa_pdf_encryption_key_document"
-}
-
 resource "aws_secretsmanager_secret" "opg_lpa_pdf_owner_password" {
   name = "${local.account_name}/opg_lpa_pdf_owner_password"
 }
