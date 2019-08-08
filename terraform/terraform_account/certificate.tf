d824eb3d4c18ad0debee8d7239181b83840fd99d
Tom Withers <thomas.withers@digital.justice.gov.uk>
2019-08-08 08:22:35 +0100

    LPA-3321 - Add certificate for production ALB (#17)
    * LPA-3321 - Add certificate for production ALB
    
    * LPA-3321: Add count to data source

--- terraform/terraform_account/certificate.tf
+++ terraform/terraform_account/certificate.tf
@@ -3,6 +3,11 @@ data "aws_route53_zone" "opg_service_justice_gov_uk" {
   name     = "opg.service.justice.gov.uk"
 }
 
+data "aws_route53_zone" "lastingpowerofattorney_service_gov_uk" {
+  provider = aws.opg-lpa-prod
+  name     = "lastingpowerofattorney.service.gov.uk"
+}
+
 //------------------------
 // Front Certificates
 
@@ -44,5 +49,30 @@ resource "aws_acm_certificate_validation" "certificate_admin" {
 
 resource "aws_acm_certificate" "certificate_admin" {
   domain_name       = var.accounts[local.account_name].admin_certificate_domain_name
+  validation_method = "DNS"
+}
+
+//------------------------
+// Live Service Certificate
+
+resource "aws_route53_record" "certificate_validation_live_service" {
+  count    = terraform.workspace == "production" ? 1 : 0
+  provider = aws.opg-lpa-prod
+  name     = aws_acm_certificate.certificate_live_service[count.index].domain_validation_options.0.resource_record_name
+  type     = aws_acm_certificate.certificate_live_service[count.index].domain_validation_options.0.resource_record_type
+  zone_id  = data.aws_route53_zone.lastingpowerofattorney_service_gov_uk.id
+  records  = [aws_acm_certificate.certificate_live_service[count.index].domain_validation_options.0.resource_record_value]
+  ttl      = 60
+}
+
+resource "aws_acm_certificate_validation" "certificate_live_service" {
+  count                   = terraform.workspace == "production" ? 1 : 0
+  certificate_arn         = aws_acm_certificate.certificate_live_service[count.index].arn
+  validation_record_fqdns = [aws_route53_record.certificate_validation_live_service[count.index].fqdn]
+}
+
+resource "aws_acm_certificate" "certificate_live_service" {
+  count             = terraform.workspace == "production" ? 1 : 0
+  domain_name       = "*.lastingpowerofattorney.service.gov.uk"
   validation_method = "DNS"
 }
