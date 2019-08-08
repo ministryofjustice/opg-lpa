2905d1a1fca9ce3059311b6ee55048dbfd3e90c1
Neil Smith <github@nsmith.me.uk>
2019-08-08 16:37:35 +0100

    Added stats cron

--- terraform/terraform_environment/ecs_api_cron.tf
+++ terraform/terraform_environment/ecs_api_cron.tf
@@ -1,6 +1,6 @@
 
-//-------------------------
-// Permissions
+//------------------------------------------------
+// General Permissions
 
 // What services can assume the role
 data "aws_iam_policy_document" "cloudwatch_events_assume_policy" {
@@ -52,16 +52,26 @@ resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
   policy = data.aws_iam_policy_document.cloudwatch_events_role_policy.json
 }
 
-//---
+//------------------------------------------------
+// Trigger times
+
+resource "aws_cloudwatch_event_rule" "middle_of_the_night" {
+  name                = "${local.environment}-middle-of-the-night-cron"
+  schedule_expression = "rate(10 minutes)"
+  //schedule_expression = "cron(0 3 * * ? *)" // 3am UTC, every day.
+}
 
 resource "aws_cloudwatch_event_rule" "early_morning" {
-  name                = "${local.environment}-early-morning-cron"
+  name = "${local.environment}-early-morning-cron"
   //schedule_expression = "rate(5 minutes)"
   schedule_expression = "cron(0 4 * * ? *)"
   //schedule_expression = "cron(0 10 * * ? *)" // 10am UTC, every day.
 }
 
-resource "aws_cloudwatch_event_target" "api_ecs_cron_event_target" {
+//------------------------------------------------
+// Account Cleanup Task
+
+resource "aws_cloudwatch_event_target" "api_ecs_cron_event_account_cleanup" {
   target_id = "account-cleanup"
   arn       = aws_ecs_cluster.online-lpa.arn
   rule      = aws_cloudwatch_event_rule.early_morning.name
@@ -89,6 +99,43 @@ resource "aws_cloudwatch_event_target" "api_ecs_cron_event_target" {
     {
       "name": "app",
       "command": ["php", "/app/public/index.php", "account-cleanup"]
+    }
+  ]
+}
+DOC
+}
+
+//------------------------------------------------
+// Generate Stats Task
+
+resource "aws_cloudwatch_event_target" "api_ecs_cron_event_generate_stats" {
+  target_id = "generate-stats"
+  arn = aws_ecs_cluster.online-lpa.arn
+  rule = aws_cloudwatch_event_rule.middle_of_the_night.name
+  role_arn = aws_iam_role.cloudwatch_events_ecs_role.arn
+
+  ecs_target {
+    task_count = 1
+    task_definition_arn = aws_ecs_task_definition.api.arn
+    launch_type = "FARGATE"
+    platform_version = "LATEST"
+
+    network_configuration {
+      security_groups = [
+        aws_security_group.api_ecs_service.id,
+        aws_security_group.rds-client.id,
+      ]
+      subnets = data.aws_subnet_ids.private.ids
+      assign_public_ip = false
+    }
+  }
+
+  input = <<DOC
+{
+  "containerOverrides": [
+    {
+      "name": "app",
+      "command": ["php", "/app/public/index.php", "generate-stats"]
     }
   ]
 }
