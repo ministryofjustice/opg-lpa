c026dcf23801a34ac85565b5da2946fb372a80a3
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-08-09 16:56:19 +0100

    generate new certificate that includes a maintenance domain name [ci skip]

--- terraform/terraform_maintenance/ssl-certificate.tf
+++ terraform/terraform_maintenance/ssl-certificate.tf
@@ -1,10 +1,8 @@
 data "aws_route53_zone" "lastingpowerofattorney_service_gov_uk" {
-  # provider = aws.opg-lpa-prod
   name = "lastingpowerofattorney.service.gov.uk"
 }
 
 resource "aws_route53_record" "certificate_validation_maintenance_cloudfront_dn" {
-  # provider = aws.us_east_1
   name    = aws_acm_certificate.certificate_maintenance_cloudfront.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.certificate_maintenance_cloudfront.domain_validation_options.0.resource_record_type
   zone_id = data.aws_route53_zone.lastingpowerofattorney_service_gov_uk.id
@@ -20,21 +18,13 @@ resource "aws_route53_record" "certificate_validation_maintenance_cloudfront_san
   ttl     = 300
 }
 
-# resource "aws_route53_record" "certificate_validation_maintenance_cloudfront_san_maintenance" {
-#   name    = aws_acm_certificate.certificate_maintenance_cloudfront.domain_validation_options.2.resource_record_name
-#   type    = aws_acm_certificate.certificate_maintenance_cloudfront.domain_validation_options.2.resource_record_type
-#   zone_id = data.aws_route53_zone.lastingpowerofattorney_service_gov_uk.id
-#   records = [aws_acm_certificate.certificate_maintenance_cloudfront.domain_validation_options.2.resource_record_value]
-#   ttl     = 300
-# }
-
+# OLD CERT
 resource "aws_acm_certificate_validation" "certificate_maintenance_cloudfront" {
   provider        = aws.us_east_1
   certificate_arn = aws_acm_certificate.certificate_maintenance_cloudfront.arn
   validation_record_fqdns = [
     aws_route53_record.certificate_validation_maintenance_cloudfront_dn.fqdn,
     aws_route53_record.certificate_validation_maintenance_cloudfront_san_www.fqdn,
-    # aws_route53_record.certificate_validation_maintenance_cloudfront_san_maintenance.fqdn,
   ]
 
 }
@@ -43,8 +33,39 @@ resource "aws_acm_certificate" "certificate_maintenance_cloudfront" {
   provider                  = aws.us_east_1
   domain_name               = "lastingpowerofattorney.service.gov.uk"
   subject_alternative_names = ["www.lastingpowerofattorney.service.gov.uk"]
-  # subject_alternative_names = ["www.lastingpowerofattorney.service.gov.uk", "maintenance.lastingpowerofattorney.service.gov.uk"]
-  validation_method = "DNS"
+  validation_method         = "DNS"
+  options {
+    certificate_transparency_logging_preference = "ENABLED"
+  }
+  tags = local.default_tags
+}
+
+
+# NEW CERT
+resource "aws_route53_record" "cv_maintenance_cloudfront_san_maintenance" {
+  name    = aws_acm_certificate.maintenance_cloudfront.domain_validation_options.2.resource_record_name
+  type    = aws_acm_certificate.maintenance_cloudfront.domain_validation_options.2.resource_record_type
+  zone_id = data.aws_route53_zone.lastingpowerofattorney_service_gov_uk.id
+  records = [aws_acm_certificate.maintenance_cloudfront.domain_validation_options.2.resource_record_value]
+  ttl     = 300
+}
+
+resource "aws_acm_certificate_validation" "maintenance_cloudfront" {
+  provider        = aws.us_east_1
+  certificate_arn = aws_acm_certificate.maintenance_cloudfront.arn
+  validation_record_fqdns = [
+    aws_route53_record.certificate_validation_maintenance_cloudfront_dn.fqdn,
+    aws_route53_record.certificate_validation_maintenance_cloudfront_san_www.fqdn,
+    aws_route53_record.cv_maintenance_cloudfront_san_maintenance.fqdn,
+  ]
+
+}
+
+resource "aws_acm_certificate" "maintenance_cloudfront" {
+  provider                  = aws.us_east_1
+  domain_name               = "lastingpowerofattorney.service.gov.uk"
+  subject_alternative_names = ["www.lastingpowerofattorney.service.gov.uk", "maintenance.lastingpowerofattorney.service.gov.uk"]
+  validation_method         = "DNS"
   options {
     certificate_transparency_logging_preference = "ENABLED"
   }
