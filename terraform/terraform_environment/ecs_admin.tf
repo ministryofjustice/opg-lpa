9d8d2978c8a541057f27b99fbc3c5fc4eb2664ff
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-08-13 10:46:35 +0100

    revert admin performance changes

--- terraform/terraform_environment/ecs_admin.tf
+++ terraform/terraform_environment/ecs_admin.tf
@@ -59,8 +59,8 @@ resource "aws_ecs_task_definition" "admin" {
   family                   = "${local.environment}-admin"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 2048
-  memory                   = 4096
+  cpu                      = 512
+  memory                   = 1024
   container_definitions    = "[${local.admin_web}, ${local.admin_app}]"
   task_role_arn            = aws_iam_role.admin_task_role.arn
   execution_role_arn       = aws_iam_role.execution_role.arn
