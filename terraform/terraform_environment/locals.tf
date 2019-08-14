2a1ac97b336a9aea6ece51e2f868ff8abe40aa76
Neil Smith <github@nsmith.me.uk>
2019-08-14 17:07:38 +0100

    Set minimum values

--- terraform/terraform_environment/locals.tf
+++ terraform/terraform_environment/locals.tf
@@ -37,12 +37,11 @@ locals {
   sirius_api_gateway_endpoint = var.accounts[local.account_name].sirius_api_gateway_endpoint
   sirius_api_gateway_arn      = var.accounts[local.account_name].sirius_api_gateway_arn
 
-  // Minimum count for each service.
-  // Autoscaling may increase these values.
+  // Set minimum count for each service.
   ecs_minimum_task_count_front = local.account_name == "development" ? 1 : 3
   ecs_minimum_task_count_api   = local.account_name == "development" ? 1 : 3
   ecs_minimum_task_count_pdf   = local.account_name == "development" ? 1 : 2
-  ecs_minimum_task_count_admin = local.account_name == "development" ? 1 : 1
+  ecs_minimum_task_count_admin = local.account_name == "development" ? 1 : 2
 
   opg_project = "lpa"
   mandatory_moj_tags = {
