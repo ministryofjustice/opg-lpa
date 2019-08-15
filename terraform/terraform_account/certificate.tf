9f009f9cd94f925415b4b18fe75441fd7caa668d
Tom Withers <thomas.withers@digital.justice.gov.uk>
2019-08-16 00:50:11 +0100

    Removed validation record not needed

--- terraform/terraform_account/certificate.tf
+++ terraform/terraform_account/certificate.tf
@@ -65,22 +65,11 @@ resource "aws_route53_record" "certificate_validation_live_service" {
   ttl      = 60
 }
 
-resource "aws_route53_record" "certificate_validation_live_service_root" {
-  count    = terraform.workspace == "production" ? 1 : 0
-  provider = aws.opg-lpa-prod
-  name     = aws_acm_certificate.certificate_live_service[count.index].domain_validation_options.1.resource_record_name
-  type     = aws_acm_certificate.certificate_live_service[count.index].domain_validation_options.1.resource_record_type
-  zone_id  = data.aws_route53_zone.lastingpowerofattorney_service_gov_uk.id
-  records  = [aws_acm_certificate.certificate_live_service[count.index].domain_validation_options.1.resource_record_value]
-  ttl      = 60
-}
-
 resource "aws_acm_certificate_validation" "certificate_live_service" {
   count           = terraform.workspace == "production" ? 1 : 0
   certificate_arn = aws_acm_certificate.certificate_live_service[count.index].arn
   validation_record_fqdns = [
     aws_route53_record.certificate_validation_live_service[count.index].fqdn,
-    aws_route53_record.certificate_validation_live_service_root[count.index].fqdn,
   ]
 }
 
