6b97e3d5df6a3224637540e6f04d6bf1b2ac511f
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-08-15 23:30:24 +0100

    point live service url at new production [ci skip]

--- terraform/terraform_maintenance/live_service_url.tf
+++ terraform/terraform_maintenance/live_service_url.tf
@@ -20,12 +20,12 @@ resource "aws_route53_record" "lastingpowerofattorney_service_gov_uk" {
 
   alias {
     evaluate_target_health = false
-    name                   = data.aws_elb.old_production04_front.dns_name
-    zone_id                = data.aws_elb.old_production04_front.zone_id
+    # name                   = data.aws_elb.old_production04_front.dns_name
+    # zone_id                = data.aws_elb.old_production04_front.zone_id
     # name    = aws_cloudfront_distribution.maintenance.domain_name
     # zone_id = aws_cloudfront_distribution.maintenance.hosted_zone_id
-    # name    = data.aws_lb.new_lpa_production_front.dns_name
-    # zone_id = data.aws_lb.new_lpa_production_front.zone_id
+    name    = data.aws_lb.new_lpa_production_front.dns_name
+    zone_id = data.aws_lb.new_lpa_production_front.zone_id
   }
 
   lifecycle {
