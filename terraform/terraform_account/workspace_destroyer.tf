fa8445c5aab5ee08180b053b84d6db6bbaee3395
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-08-17 16:07:10 +0100

    create a queue and a lambda function for destroying dev account workspaces

--- terraform/terraform_account/workspace_destroyer.tf
+++ terraform/terraform_account/workspace_destroyer.tf
@@ -1,8 +1,8 @@
-resource "aws_sqs_queue" "workspace_destoryer_queue" {
+resource "aws_sqs_queue" "workspace_destroyer" {
   count            = local.account_name == "development" ? 1 : 0
   name             = "${local.account_name}-opg-lpa-workspace-destroyer-queue"
   max_message_size = 2048
-  tags             = local.tags
+  tags             = local.default_tags
 }
 
 resource "local_file" "queue_config" {
@@ -13,7 +13,63 @@ resource "local_file" "queue_config" {
 
 locals {
   queue_config = {
-    account_id                  = "${local.account_id}"
-    workspace_destory_queue_url = "${aws_sqs_queue.workspace_destoryer_queue.id}"
+    account_id                  = local.account_id
+    workspace_destory_queue_url = aws_sqs_queue.workspace_destroyer[0].id
   }
+}
+
+resource "aws_iam_role" "iam_for_workspace_destroyer_lambda" {
+  name               = "iam_for_workspace_destroyer_lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+  tags               = local.default_tags
+}
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "iam_for_workspace_destroyer_lambda_execution_role" {
+  name   = "WorkspaceDeestroyerPermissions"
+  policy = data.aws_iam_policy_document.iam_for_workspace_destroyer_lambda_inline_execution_role.json
+  role   = aws_iam_role.iam_for_workspace_destroyer_lambda.id
+}
+
+data "aws_iam_policy_document" "iam_for_workspace_destroyer_lambda_inline_execution_role" {
+  statement {
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+    ]
+    resources = [
+      aws_sqs_queue.workspace_destroyer[0].arn,
+    ]
+  }
+}
+
+resource "aws_lambda_function" "workspace_destroyer" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "workspace_destroyer"
+  role             = aws_iam_role.iam_for_workspace_destroyer_lambda.arn
+  handler          = "service.lambda_handler"
+  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+  runtime          = "python3.7"
+  timeout          = 900
+  memory_size      = 128
+  # TODO: provide credentials for terraform to the lambda function
+  environment {
+    variables = {
+      GIT_URL           = "https://github.com/ministryofjustice/opg-lpa.git"
+      REPO_DIR          = "/tmp/opg-lpa"
+      TF_CONFIG_PATH    = "terraform/terraform_environment"
+      TERRAFORM_VERSION = "0.12.5"
+    }
+  }
+  tags = local.default_tags
 }
