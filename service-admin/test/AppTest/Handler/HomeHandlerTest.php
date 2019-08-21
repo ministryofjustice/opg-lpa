dd82fab27bbb89ac3b41897b729c13942ab26181
SeemaMenon <seema.workspace@gmail.com>
2019-08-21 12:55:53 +0100

    LPA-3345 wip: fixing admin unit tests

--- service-admin/test/AppTest/Handler/HomeHandlerTest.php
+++ service-admin/test/AppTest/Handler/HomeHandlerTest.php
@@ -14,6 +14,7 @@ use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Expressive\Router\RouterInterface;
 use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\Expressive\Helper\UrlHelper;
 
 class HomeHandlerTest extends TestCase
 {
@@ -29,36 +30,51 @@ class HomeHandlerTest extends TestCase
         $this->router    = $this->prophesize(RouterInterface::class);
     }
 
-    public function testReturnsJsonResponseWhenNoTemplateRendererProvided()
-    {
-        $homePage = new HomeHandler(
-            get_class($this->container->reveal()),
-            $this->router->reveal(),
-            null
-        );
-        $response = $homePage->handle(
-            $this->prophesize(ServerRequestInterface::class)->reveal()
-        );
-
-        $this->assertInstanceOf(JsonResponse::class, $response);
-    }
+//    public function testReturnsJsonResponseWhenNoTemplateRendererProvided()
+//    {
+//        $homePage = new HomeHandler(
+//            get_class($this->container->reveal()),
+//            $this->router->reveal(),
+//            null
+//        );
+//        $response = $homePage->handle(
+//            $this->prophesize(ServerRequestInterface::class)->reveal()
+//        );
+//
+//        $this->assertInstanceOf(JsonResponse::class, $response);
+//    }
 
     public function testReturnsHtmlResponseWhenTemplateRendererProvided()
     {
-        $renderer = $this->prophesize(TemplateRendererInterface::class);
-        $renderer
-            ->render('app::home-page', Argument::type('array'))
+//        $renderer = $this->prophesize(TemplateRendererInterface::class);
+//        $renderer
+//            ->render('app::home-page', Argument::type('array'))
+//            ->willReturn('');
+//
+//        $homePage = new HomeHandler(
+//            get_class($this->container->reveal()),
+//            $this->router->reveal(),
+//            $renderer->reveal()
+//        );
+//
+//        $response = $homePage->handle(
+//            $this->prophesize(ServerRequestInterface::class)->reveal()
+//        );
+//
+//        $this->assertInstanceOf(HtmlResponse::class, $response);
+
+        $rendererProphecy = $this->prophesize(TemplateRendererInterface::class);
+        $rendererProphecy->render('app::home-page')
             ->willReturn('');
 
-        $homePage = new HomeHandler(
-            get_class($this->container->reveal()),
-            $this->router->reveal(),
-            $renderer->reveal()
-        );
+        $urlHelperProphecy = $this->prophesize(UrlHelper::class);
 
-        $response = $homePage->handle(
-            $this->prophesize(ServerRequestInterface::class)->reveal()
-        );
+        //  Set up the handler
+        $handler = new HomeHandler($rendererProphecy->reveal(), $urlHelperProphecy->reveal());
+
+        $requestProphecy = $this->prophesize(ServerRequestInterface::class);
+
+        $response = $handler->handle($requestProphecy->reveal());
 
         $this->assertInstanceOf(HtmlResponse::class, $response);
     }
