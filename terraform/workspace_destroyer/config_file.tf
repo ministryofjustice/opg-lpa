10b13f80411bad0db49ad28a38611fbae28fac3c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-09-15 14:20:05 +0100

    remove ecs content from config file

--- terraform/workspace_destroyer/config_file.tf
+++ terraform/workspace_destroyer/config_file.tf
@@ -7,8 +7,8 @@ locals {
   workspace_detroyer_config = {
     account_id                  = local.account_id
     workspace_destory_queue_url = aws_sqs_queue.workspace_destroyer.id
-    aws_ecs_cluster_arn         = aws_ecs_cluster.workspace_destroyer.arn
-    aws_ecs_task_definition_arn = aws_ecs_task_definition.workspace_destroyer.arn
+    # aws_ecs_cluster_arn         = aws_ecs_cluster.workspace_destroyer.arn
+    # aws_ecs_task_definition_arn = aws_ecs_task_definition.workspace_destroyer.arn
   }
 }
 
