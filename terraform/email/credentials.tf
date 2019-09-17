a3089e7e7e889edd25bbd7396f048791556f5884
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-09-17 17:24:52 +0100

    LPA-3410 Create email inbox for functional testing (#63)
    * provide credentials for legacy accounts when working locally
    
    * cleean up local role usage in account level
    
    * renam terraform account path
    
    * creaete bucket and ses recipient for functional testing
    
    * nlc at end of tfvars.json file
    
    * make a more specific bucket name
    
    * add 2 day expiration lifecycle to objects in the bucket
    
    * fix zone ids and bucket name
    
    * set objects to expire after 1 days
    
    * add sid to bucket policy
    
    * update tags
    
    * build email mailbox on pipeline

---
+++ terraform/email/credentials.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "moj-lasting-power-of-attorney-email/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/state_write"
+    dynamodb_table = "remote_lock"
+  }
+}
+
+variable "default_role" {
+  default = "ci"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  alias  = "management"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
---
+++ terraform/email/locals.tf
@@ -0,0 +1,33 @@
+# variables for terraform.tfvars.json
+variable "accounts" {
+  type = map(
+    object({
+      account_id    = string
+      is_production = string
+    })
+  )
+}
+
+locals {
+  opg_project = "lpa"
+
+  account_name = "development"
+
+  account_id = var.accounts[local.account_name].account_id
+
+  mandatory_moj_tags = {
+    business-unit = "OPG"
+    application   = "Online LPA Service - Functional Testing Mailbox"
+    owner         = "Amy Wilson: amy.wilson@digital.justice.gov.uk"
+    is-production = var.accounts[local.account_name].is_production
+  }
+
+  optional_tags = {
+    environment-name       = local.account_name
+    infrastructure-support = "OPG LPA Product Team: opgteam+online-lpa@digital.justice.gov.uk"
+    runbook                = "https://github.com/ministryofjustice/opg-webops-runbooks/tree/master/LPA"
+    source-code            = "https://github.com/ministryofjustice/opg-lpa"
+  }
+
+  default_tags = merge(local.mandatory_moj_tags, local.optional_tags)
+}
