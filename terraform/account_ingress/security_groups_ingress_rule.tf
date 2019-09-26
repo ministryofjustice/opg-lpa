c2380fb29747a600d39ffe0a3ff224cdfe162b0e
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-09-26 17:28:28 +0100

    clean up trraform and use amazon check ip

--- terraform/account_ingress/security_groups_ingress_rule.tf
+++ terraform/account_ingress/security_groups_ingress_rule.tf
@@ -1,20 +1,15 @@
-data "http" "icanhazip" {
-  url = "https://icanhazip.com"
-}
-
 output "public_ip" {
   value = local.local_ip_cidr
 }
 
 locals {
-  local_ip_cidr = "${chomp(data.http.icanhazip.body)}/32"
+  local_ip_cidr = "${chomp(var.docker_remote_ip)}/32"
 }
 
 variable "docker_remote_ip" {
   description = "IP address for CircleCI docker remote"
   default     = ""
 }
-
 
 data "aws_security_group" "admin_loadbalancer" {
   name = "${local.environment}-admin-loadbalancer"
@@ -26,7 +21,7 @@ resource "aws_security_group_rule" "admin_ci_ingress" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["${chomp(var.docker_remote_ip)}/32"]
+  cidr_blocks       = [local.local_ip_cidr]
   security_group_id = data.aws_security_group.admin_loadbalancer.id
   description       = "ci_ingress"
 }
@@ -41,7 +36,7 @@ resource "aws_security_group_rule" "front_ci_ingress" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["${chomp(var.docker_remote_ip)}/32"]
+  cidr_blocks       = [local.local_ip_cidr]
   security_group_id = data.aws_security_group.front_loadbalancer.id
   description       = "ci_ingress"
 }
