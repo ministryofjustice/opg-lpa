afa4c9d77761b636b7486fef59ad2ea32cc31990
GemTay <gemma.taylor@digital.justice.gov.uk>
2019-09-26 13:43:00 +0100

    added policy

--- terraform/terraform_environment/s3.tf
+++ terraform/terraform_environment/s3.tf
@@ -20,3 +20,19 @@ resource "aws_s3_bucket_object" "opg_logo" {
   etag         = filemd5("../../email-assets/opg-logo-v1.png")
   content_type = "image/png"
 }
+
+data "aws_iam_policy_document" "static_email_assets_policy" {
+  statement {
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_email_assets.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_email_assets_policy" {
+  bucket = aws_s3_bucket.static_email_assets.id
+  policy = data.aws_iam_policy_document.static_email_assets_policy.json
+}
