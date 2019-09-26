10ec0b934c53d25685894f3d23e19c2f38e0ca78
GemTay <gemma.taylor@digital.justice.gov.uk>
2019-09-26 13:58:31 +0100

    Moved s3 into correct directory

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -86,41 +86,41 @@ resource "aws_kms_alias" "lpa_pdf_cache" {
   target_key_id = aws_kms_key.lpa_pdf_cache.key_id
 }
 
-//resource "aws_s3_bucket" "static_email_assets" {
-//  bucket = "opg-lpa-email-assets"
-//
-//  tags   = local.default_tags
-//  region = "eu-west-1"
-//}
-//
-//resource "aws_s3_bucket_object" "govuk_logo" {
-//  bucket       = aws_s3_bucket.static_email_assets.id
-//  key          = "govuk-logo-v1.png"
-//  source       = "../../email-assets/govuk-logo-v1.png"
-//  etag         = filemd5("../../email-assets/govuk-logo-v1.png")
-//  content_type = "image/png"
-//}
-//
-//resource "aws_s3_bucket_object" "opg_logo" {
-//  bucket       = aws_s3_bucket.static_email_assets.id
-//  key          = "opg-logo-v1.png"
-//  source       = "../../email-assets/opg-logo-v1.png"
-//  etag         = filemd5("../../email-assets/opg-logo-v1.png")
-//  content_type = "image/png"
-//}
-//
-//data "aws_iam_policy_document" "static_email_assets_policy" {
-//  statement {
-//    principals {
-//      identifiers = ["*"]
-//      type        = "AWS"
-//    }
-//    actions   = ["s3:GetObject"]
-//    resources = ["${aws_s3_bucket.static_email_assets.arn}/*"]
-//  }
-//}
-//
-//resource "aws_s3_bucket_policy" "static_email_assets_policy" {
-//  bucket = aws_s3_bucket.static_email_assets.id
-//  policy = data.aws_iam_policy_document.static_email_assets_policy.json
-//}
+resource "aws_s3_bucket" "static_email_assets" {
+  bucket = "opg-lpa-email-assets"
+
+  tags   = local.default_tags
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket_object" "govuk_logo" {
+  bucket       = aws_s3_bucket.static_email_assets.id
+  key          = "govuk-logo-v1.png"
+  source       = "../../email-assets/govuk-logo-v1.png"
+  etag         = filemd5("../../email-assets/govuk-logo-v1.png")
+  content_type = "image/png"
+}
+
+resource "aws_s3_bucket_object" "opg_logo" {
+  bucket       = aws_s3_bucket.static_email_assets.id
+  key          = "opg-logo-v1.png"
+  source       = "../../email-assets/opg-logo-v1.png"
+  etag         = filemd5("../../email-assets/opg-logo-v1.png")
+  content_type = "image/png"
+}
+
+data "aws_iam_policy_document" "static_email_assets_policy" {
+  statement {
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_email_assets.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_email_assets_policy" {
+  bucket = aws_s3_bucket.static_email_assets.id
+  policy = data.aws_iam_policy_document.static_email_assets_policy.json
+}
