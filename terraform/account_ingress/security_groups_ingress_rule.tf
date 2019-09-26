4f2931edcb7e4074fdb248654d8ced861a38b171
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-09-26 17:07:39 +0100

    make ip a CIDR range

--- terraform/account_ingress/security_groups_ingress_rule.tf
+++ terraform/account_ingress/security_groups_ingress_rule.tf
@@ -21,13 +21,12 @@ data "aws_security_group" "admin_loadbalancer" {
 }
 
 resource "aws_security_group_rule" "admin_ci_ingress" {
-  count       = local.account.allow_ingress_modification ? 1 : 0
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = [var.docker_remote_ip]
-  # cidr_blocks       = [var.docker_remote_ip]
+  count             = local.account.allow_ingress_modification ? 1 : 0
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["${chomp(var.docker_remote_ip)}/32"]
   security_group_id = data.aws_security_group.admin_loadbalancer.id
   description       = "ci_ingress"
 }
@@ -37,13 +36,12 @@ data "aws_security_group" "front_loadbalancer" {
 }
 
 resource "aws_security_group_rule" "front_ci_ingress" {
-  count       = local.account.allow_ingress_modification ? 1 : 0
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = [local.local_ip_cidr]
-  # cidr_blocks       = [local.local_ip_cidr]
+  count             = local.account.allow_ingress_modification ? 1 : 0
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["${chomp(var.docker_remote_ip)}/32"]
   security_group_id = data.aws_security_group.front_loadbalancer.id
   description       = "ci_ingress"
 }
