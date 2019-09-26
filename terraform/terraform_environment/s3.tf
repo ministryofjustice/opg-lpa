c3c2cb7a492331512b8d0f34c854b133e240aefd
GemTay <gemma.taylor@digital.justice.gov.uk>
2019-09-26 13:23:19 +0100

    added asset files

--- terraform/terraform_environment/s3.tf
+++ terraform/terraform_environment/s3.tf
@@ -3,5 +3,20 @@ resource "aws_s3_bucket" "static_email_assets" {
 
   tags   = local.default_tags
   region = "eu-west-1"
+}
 
+resource "aws_s3_bucket_object" "govuk_logo" {
+  bucket       = aws_s3_bucket.static_email_assets.id
+  key          = "govuk-logo-v1.png"
+  source       = "../../email-assets/govuk-logo-v1.png"
+  etag         = filemd5("../../email-assets/govuk-logo-v1.png")
+  content_type = "image/png"
+}
+
+resource "aws_s3_bucket_object" "opg_logo" {
+  bucket       = aws_s3_bucket.static_email_assets.id
+  key          = "opg-logo-v1.png"
+  source       = "../../email-assets/opg-logo-v1.png"
+  etag         = filemd5("../../email-assets/opg-logo-v1.png")
+  content_type = "image/png"
 }
