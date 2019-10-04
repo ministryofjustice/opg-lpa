af3005bc041cb19e24f348b757db93471d4d3328
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2019-10-04 11:21:40 +0100

    Only enable analytics if the cookie says to

--- service-front/assets/js/moj/moj.modules/moj.analytics.js
+++ service-front/assets/js/moj/moj.modules/moj.analytics.js
@@ -12,11 +12,22 @@
   moj.Modules.Analytics = {
 
     init: function () {
-      GOVUK.Analytics.load();
-      this.setup();
+      this.bindEvents();
+
+      // Check if we have permission to enable tracking
+      if (typeof GOVUK.checkConsentCookieCategory === 'function'
+          && GOVUK.checkConsentCookieCategory('analytics', 'usage')) {
+          this.setup();
+      }
+    },
+
+    bindEvents: function() {
+      moj.Events.on('Analytics.start', this.setup)
     },
 
     setup: function() {
+      GOVUK.Analytics.load();
+
       // Use document.domain in dev, preview and staging so that tracking works
       // Otherwise explicitly set the domain as lastingpowerofattorney.service.justice.gov.uk.
       var prodDomain = new RegExp('^(www\.)*lastingpowerofattorney\.service\.gov\.uk$')
