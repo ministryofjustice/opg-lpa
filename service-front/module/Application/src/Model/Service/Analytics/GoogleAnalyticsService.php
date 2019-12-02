1135d189cb2c119b074c0a513e574385d88b74ee
GemTay <gemma.taylor@digital.justice.gov.uk>
2019-12-02 12:18:32 +0000

    Rearranged code to return null if cookie ga param not set, logs message and does nothing else

--- service-front/module/Application/src/Model/Service/Analytics/GoogleAnalyticsService.php
+++ service-front/module/Application/src/Model/Service/Analytics/GoogleAnalyticsService.php
@@ -30,40 +30,41 @@ class GoogleAnalyticsService extends AbstractService
 
         $clientId = $this->getAnalyticsClientId();
 
-        $this->getLogger()->debug('Client ID: ' . $clientId);
+        if (!is_null($clientId)) {
+            $this->getLogger()->debug('Client ID: ' . $clientId);
 
-        $analytics = $this->analyticsClient;
+            $analytics = $this->analyticsClient;
 
-        $analytics->setProtocolVersion('1')
-            ->setTrackingId('UA-33184303-1')
-            ->setClientId($clientId)
-            ->setDocumentHostName($hostName)
-            ->setDocumentPath($pagePath)
-            ->setDocumentTitle($pageTitle)
-            ->setAnonymizeIp(true);
+            $analytics->setProtocolVersion('1')
+                ->setTrackingId('UA-33184303-1')
+                ->setClientId($clientId)
+                ->setDocumentHostName($hostName)
+                ->setDocumentPath($pagePath)
+                ->setDocumentTitle($pageTitle)
+                ->setAnonymizeIp(true);
 
-        $response = $analytics->sendPageview();
+            $response = $analytics->sendPageview();
 
-        $this->getLogger()->debug('Analytics response code: ' . $response->getHttpStatusCode());
+            $this->getLogger()->debug('Analytics response code: ' . $response->getHttpStatusCode());
+
+        } else {
+            $this->getLogger()->notice('GA parameter not found in cookie - the user likely has tracking blocked in their browser');
+        }
     }
 
     /**
      * Get the client ID from the cookie
      *
-     * @return string Client Id
-     * @throws Exception
      */
-    public function getAnalyticsClientId() : string
+    public function getAnalyticsClientId() : ?string
     {
         if (isset($_COOKIE['_ga'])) {
             list($version, $domainDepth, $cid1, $cid2) = explode('.', $_COOKIE['_ga'], 4);
             $contents = array('version' => $version, 'domainDepth' => $domainDepth, 'cid' => $cid1 . '.' . $cid2);
             $cid = $contents['cid'];
-        } else {
-            $this->getLogger()->notice('Could not find google analytics cookie - user likely has tracking blocked in their browser');
         }
 
-        return $cid;
+        return isset($cid) ? $cid : null;
     }
 
     /**
