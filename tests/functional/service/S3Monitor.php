d6910bfa10647faac5937ecc7751880592d6faca
SeemaMenon <seema.menon@digital.justice.gov.uk>
2019-12-20 14:39:05 +0000

    LPA-3302 Add Casper Testing to Pipeline (#53)
    * LPA-3302 copied in the casper test folfer
    * Add env var for OPENSSL_CONF
    
    Running the email monitor returned an error that the libssl_conf.so
    (shared library) could not be found. Adding this env var fixes this.
    
    * LPA-3302 test fix added
    
    * LPA-3302 added steps to circle ci config to run casper tests
    
    * LPA-3302 fixed the terraform version
    
    * LPA-3302 removed unsued code
    
    * LPA-3302 passing in AWS credeticla to container and added to pipeline task
    
    * LPA-3302 added step to run functional tests in parallel with health check
    
    * LPA-3302 removed applying terraform
    
    * LPA-3302 reset the aws credentials
    
    * testing without running the healthcheck
    
    * testing with the absolute path change to autoload
    
    * testing to check if session set on CI
    
    * LPA-3302 resetting back to use ci role
    
    * get docker ip and pass it as a variable into functional test ingress
    
    * take input variable for docker remote ip address
    
    * add missing $ to echo env var
    
    * make ip a CIDR range
    
    * clean up trraform and use amazon check ip
    
    * LPA-3302 commented the pop up window testing in guidance page for time being
    
    * Changes as per review suggestions
    
    * Commented package OpenSSL in Dockerfile to test
    
    * reverted changes
    
    * added openssl dev package to install
    
    * explicitly 7.3 version php
    
    * commented a test partially to see if execution fine
    
    * commented a test partially to see if execution fine
    
    * reverted the tests
    
    * Removed as per review suggestion
    
    * deleted the vendor folder and ammended the dockerfile to build and copy the composer installs
    
    * Added gitignore file
    
    * fixed dockerfile
    
    * LPA-3302 some minor review changes [ci skip]
    
    * LPA-3302 removed some unwanted files [ci skip]
    
    * Delete casperesults.xml
    
    * Apply suggestions from code review
    
    a cleanup
    
    Co-Authored-By: Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
    
    * Update README.md
    
    cleanup and indented well
    
    * Added to gitignore and added the composer files
    
    Co-authored-by: Andrew Pearce <andrew.pearce@digital.justice.gov.uk>

---
+++ tests/functional/service/S3Monitor.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *
+ *
+ */
+
+require_once '/mnt/test/vendor/autoload.php';
+
+use Aws\Sts\StsClient;
+use Aws\Exception\AwsException;
+use Aws\S3\S3Client;
+
+$seenKeys = [];
+
+/**
+ * Assume Role
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+try {
+    $client = new StsClient([
+        'region' => 'eu-west-1',
+        'version' => 'latest'
+    ]);
+
+    if (getenv('CI')) {
+        $roleToAssumeArn = 'arn:aws:iam::050256574573:role/ci';
+    }
+    else{
+        $roleToAssumeArn = 'arn:aws:iam::050256574573:role/operator';
+    }
+
+    try{
+        $result = $client->assumeRole([
+            'RoleArn' => $roleToAssumeArn,
+            'RoleSessionName' => 'session1'
+        ]);
+        // output AssumedRole credentials, you can use these credentials
+        // to initiate a new AWS Service client with the IAM Role's permissions
+
+        $s3Client = new S3Client([
+            'version'     => 'latest',
+            'region'      => 'eu-west-1',
+            'credentials' =>  [
+                'key'    => $result['Credentials']['AccessKeyId'],
+                'secret' => $result['Credentials']['SecretAccessKey'],
+                'token'  => $result['Credentials']['SessionToken']
+            ]
+        ]);
+    }catch (Exception $e) {
+        exit($e->getMessage());
+    }
+
+    $bucketName = 'opg-lpa-casper-mailbox';
+
+    while(true) {
+        $iterator = $s3Client->getIterator('ListObjects', array('Bucket' => $bucketName));
+
+        echo "Checking objects in S3.......". "\n";
+
+        foreach ($iterator as $object) {
+            if (in_array($object['Key'], $seenKeys)) {
+                continue;
+            }
+            $result = $s3Client->getObject([
+                'Bucket' => $bucketName,
+                'Key' => $object['Key'],
+            ]);
+
+            //The content of email is in Quoted-Printable encoding and uses = as an escape character. Hence decoding to match regex
+            $bodyContent = quoted_printable_decode($result["Body"]);
+
+            parseBody(
+                $bodyContent,
+                'Activate your lasting power of attorney account',
+                'activation',
+                'signup\/confirm'
+            );
+
+            parseBody(
+                $bodyContent,
+                'Password reset request',
+                'passwordreset',
+                'forgot-password\/reset'
+            );
+
+            $seenKeys[] = $object['Key'];
+        }
+        sleep(5);
+    }
+
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
+}
+
+/**
+ * @param inbox
+ * @param subject
+ * @param directory
+ * @param overview
+ * @param message
+ * @param activationLink
+ * @param toEmail
+ * @param userId
+ * @param contents
+ */
+
+function parseBody($bodyContent, $subject, $type, $linkRegex)
+{
+    $regex = '|(https:\/\/\S+' . $linkRegex . '\/[a-zA-Z0-9]+)|sim';
+
+    if (preg_match($regex, $bodyContent, $matches) > 0) {
+        $activationLink = $matches[1];
+
+        if (!is_null($activationLink)) {
+            $emailRegex = '|To: (.+)|m'; //to change and add regex
+
+            if (preg_match($emailRegex, $bodyContent, $emailMatches) > 0) {
+                $toEmail = $emailMatches[1];
+            }
+
+            $userId = getPlusPartFromEmailAddress($toEmail);
+            $contents = $toEmail . ',' . $activationLink;
+
+            if (!is_null($contents)) {
+                //TO-DO : CORRECT PATH HERE
+                file_put_contents('/mnt/test/functional/activation_emails/' . $userId . '.' . $type, $contents);
+            }
+            echo 'Found email for user ' . $userId . PHP_EOL;
+
+        } else {
+            echo 'Message: "' . $subject . '" does not match regex ' . $regex . PHP_EOL;
+            echo '----------------------------------------------------------------------------------';
+            //echo $bodyContent . PHP_EOL;
+            echo '----------------------------------------------------------------------------------';
+        }
+    }
+}
+/**
+ * Extract the plus part from emails of the form:
+ * basename+pluspart@example.com
+ *
+ * @param string $email
+ */
+function getPlusPartFromEmailAddress($email)
+{
+    $plusPos = strpos($email, '+');
+    $atPos = strpos($email, '@');
+    $userIdLength = $atPos - $plusPos - 1;
+    $userId = substr($email, $plusPos + 1, $userIdLength);
+
+    return $userId;
+}
+
+
+
+
+
