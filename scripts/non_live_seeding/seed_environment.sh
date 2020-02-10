3a20659366578eb0ced6f4e29d9e5e208ae21cb7
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-02-10 13:20:16 +0000

    scripts to add test user and test applications

--- scripts/non_live_seeding/seed_environment.sh
+++ scripts/non_live_seeding/seed_environment.sh
@@ -12,16 +12,16 @@
 
 AWS_DEFAULT_REGION=eu-west-1
 
-if ${OPG_LPA_STACK_NAME} == "production"; then
+if [ "$OPG_LPA_STACK_NAME" == "production" ]; then
   echo "These scripts must not be run on production."
   exit 0
 fi
 
 API_OPTS="--host=${OPG_LPA_POSTGRES_HOSTNAME} --username=${OPG_LPA_POSTGRES_USERNAME}"
 
-PGPASSWORD=${OPG_LPA_POSTGRES_PASSWORD} psql ${API_OPTS} \
-  ${OPG_LPA_POSTGRES_NAME} \
-  -f clear_tables.sql
+# PGPASSWORD=${OPG_LPA_POSTGRES_PASSWORD} psql ${API_OPTS} \
+#   ${OPG_LPA_POSTGRES_NAME} \
+#   -f clear_tables.sql
 
 PGPASSWORD=${OPG_LPA_POSTGRES_PASSWORD} psql ${API_OPTS} \
   ${OPG_LPA_POSTGRES_NAME} \
