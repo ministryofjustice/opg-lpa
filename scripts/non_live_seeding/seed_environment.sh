de1a5830f1d17cc521e28628b2aab55df43c8cd2
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2020-02-17 10:32:16 +0000

    Seeding test data into environments (#88)
    * add instructions for getting terminal access to the database
    
    * add tool for making requests to the sirius api gateway
    
    * add pre-commit for terraform changes
    
    * add cleanup instructions
    
    * add scripts for seeding an environment
    
    * give instructions for seeding data into an environmen
    
    * add pr template
    
    * containerise environment seeding
    
    * add seeding to docker-compose service for local dev
    
    * add ecs seeding task definitio
    
    * set variable for building a web container so it can be skipped for pdf and seeeding
    
    * build and push a seeding container on pipeline
    
    * run php unit tests by default, and not for the seeding service container build
    
    * add script to run a one of ecs task
    
    * run seeding task on pipeline
    
    * hold env after slack notifications
    
    * seed preproduction too

---
+++ scripts/non_live_seeding/seed_environment.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+# The following variables are set from docker-compose when run locally,
+# from ECS container definitions when run as a task in AWS,
+# or from scripts/non_live_seeding/.envrc when executed manually from a SSH terminal.
+# OPG_LPA_STACK_NAME
+# OPG_LPA_STACK_ENVIRONMENT
+# OPG_LPA_POSTGRES_HOSTNAME
+# OPG_LPA_POSTGRES_PORT
+# OPG_LPA_POSTGRES_NAME
+# OPG_LPA_POSTGRES_USERNAME
+# OPG_LPA_POSTGRES_PASSWORD
+
+AWS_DEFAULT_REGION=eu-west-1
+
+if [ "$OPG_LPA_STACK_NAME" == "production" ]; then
+  echo "These scripts must not be run on production."
+  exit 0
+fi
+
+API_OPTS="--host=${OPG_LPA_POSTGRES_HOSTNAME} --username=${OPG_LPA_POSTGRES_USERNAME}"
+
+# PGPASSWORD=${OPG_LPA_POSTGRES_PASSWORD} psql ${API_OPTS} \
+#   ${OPG_LPA_POSTGRES_NAME} \
+#   -f clear_tables.sql
+
+PGPASSWORD=${OPG_LPA_POSTGRES_PASSWORD} psql ${API_OPTS} \
+  ${OPG_LPA_POSTGRES_NAME} \
+  -f seed_test_users.sql
+
+PGPASSWORD=${OPG_LPA_POSTGRES_PASSWORD} psql ${API_OPTS} \
+  ${OPG_LPA_POSTGRES_NAME} \
+  -f seed_test_applications.sql
