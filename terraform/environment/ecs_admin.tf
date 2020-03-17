040c7825cf970a538a6db8b71cfcca824b53e350
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-03-17 19:39:22 +0000

    modify task cpu and memory allocations based on modelling

--- terraform/environment/ecs_admin.tf
+++ terraform/environment/ecs_admin.tf
@@ -60,7 +60,7 @@ resource "aws_ecs_task_definition" "admin" {
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 512
-  memory                   = 1024
+  memory                   = 512
   container_definitions    = "[${local.admin_web}, ${local.admin_app}]"
   task_role_arn            = aws_iam_role.admin_task_role.arn
   execution_role_arn       = aws_iam_role.execution_role.arn
--- terraform/environment/ecs_api.tf
+++ terraform/environment/ecs_api.tf
@@ -100,8 +100,8 @@ resource "aws_ecs_task_definition" "api" {
   family                   = "${terraform.workspace}-api"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 2048
-  memory                   = 4096
+  cpu                      = 256
+  memory                   = 512
   container_definitions    = "[${local.api_web}, ${local.api_app}]"
   task_role_arn            = aws_iam_role.api_task_role.arn
   execution_role_arn       = aws_iam_role.execution_role.arn
