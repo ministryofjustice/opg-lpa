1ac5009cebbadd539568ec181059a7e9208d7b46
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-03-18 10:02:35 +0000

    remove module paramters that are set as defaults

--- terraform/environment/autoscaling.tf
+++ terraform/environment/autoscaling.tf
@@ -1,47 +1,29 @@
 module "front_ecs_autoscaling" {
-  source                                 = "./modules/ecs_autoscaling"
-  environment                            = local.environment
-  aws_ecs_cluster_name                   = aws_ecs_cluster.online-lpa.name
-  aws_ecs_service_name                   = aws_ecs_service.front.name
-  ecs_autoscaling_service_role_arn       = data.aws_iam_role.ecs_autoscaling_service_role.arn
-  ecs_task_autoscaling_minimum           = local.account.front_autoscaling_minimum
-  ecs_task_autoscaling_maximum           = local.account.front_autoscaling_maximum
-  autoscaling_metric_track_cpu_target    = 80
-  autoscaling_metric_track_memory_target = 80
-  memory_track_metric_scale_in_cooldown  = 60
-  memory_track_metric_scale_out_cooldown = 60
-  cpu_track_metric_scale_in_cooldown     = 60
-  cpu_track_metric_scale_out_cooldown    = 60
+  source                           = "./modules/ecs_autoscaling"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.online-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.front.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = local.account.front_autoscaling_minimum
+  ecs_task_autoscaling_maximum     = local.account.front_autoscaling_maximum
 }
 
 module "api_ecs_autoscaling" {
-  source                                 = "./modules/ecs_autoscaling"
-  environment                            = local.environment
-  aws_ecs_cluster_name                   = aws_ecs_cluster.online-lpa.name
-  aws_ecs_service_name                   = aws_ecs_service.api.name
-  ecs_autoscaling_service_role_arn       = data.aws_iam_role.ecs_autoscaling_service_role.arn
-  ecs_task_autoscaling_minimum           = local.account.api_autoscaling_minimum
-  ecs_task_autoscaling_maximum           = local.account.api_autoscaling_maximum
-  autoscaling_metric_track_cpu_target    = 80
-  autoscaling_metric_track_memory_target = 80
-  memory_track_metric_scale_in_cooldown  = 60
-  memory_track_metric_scale_out_cooldown = 60
-  cpu_track_metric_scale_in_cooldown     = 60
-  cpu_track_metric_scale_out_cooldown    = 60
+  source                           = "./modules/ecs_autoscaling"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.online-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.api.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = local.account.api_autoscaling_minimum
+  ecs_task_autoscaling_maximum     = local.account.api_autoscaling_maximum
 }
 
 module "pdf_ecs_autoscaling" {
-  source                                 = "./modules/ecs_autoscaling"
-  environment                            = local.environment
-  aws_ecs_cluster_name                   = aws_ecs_cluster.online-lpa.name
-  aws_ecs_service_name                   = aws_ecs_service.pdf.name
-  ecs_autoscaling_service_role_arn       = data.aws_iam_role.ecs_autoscaling_service_role.arn
-  ecs_task_autoscaling_minimum           = local.account.pdf_autoscaling_minimum
-  ecs_task_autoscaling_maximum           = local.account.pdf_autoscaling_maximum
-  autoscaling_metric_track_cpu_target    = 80
-  autoscaling_metric_track_memory_target = 80
-  memory_track_metric_scale_in_cooldown  = 60
-  memory_track_metric_scale_out_cooldown = 60
-  cpu_track_metric_scale_in_cooldown     = 60
-  cpu_track_metric_scale_out_cooldown    = 60
+  source                           = "./modules/ecs_autoscaling"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.online-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.pdf.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = local.account.pdf_autoscaling_minimum
+  ecs_task_autoscaling_maximum     = local.account.pdf_autoscaling_maximum
 }
