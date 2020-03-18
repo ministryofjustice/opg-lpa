0ba0eb26ea22a40420a4929d586efcf43ad394c9
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-03-18 18:34:56 +0000

    jsonencode resource group query

--- terraform/environment/resource_group.tf
+++ terraform/environment/resource_group.tf
@@ -2,18 +2,20 @@ resource "aws_resourcegroups_group" "environment" {
   name = "lpa-${local.environment}"
 
   resource_query {
-    query = <<JSON
-{
-  "ResourceTypeFilters": [
-    "AWS::AllSupported"
-  ],
-  "TagFilters": [
-    {
-      "Key": "Name",
-      "Values": ["${local.environment}-online-lpa-tool"]
-    }
-  ]
-}
-JSON
+    query = local.rsource_group_environment_query
   }
+}
+
+locals {
+  rsource_group_environment_query = jsonencode({
+    ResourceTypeFilters = [
+      "AWS::AllSupported"
+    ],
+    TagFilters = [
+      {
+        Key    = "Name",
+        Values = ["${local.environment}-online-lpa-tool"]
+      }
+    ]
+  })
 }
