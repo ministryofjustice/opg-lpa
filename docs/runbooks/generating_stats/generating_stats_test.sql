d748505141d9d4269d7f721edbd460369a6752a5
raffers <chris.rafferty@digital.justice.gov.uk>
2020-04-02 14:10:53 +0100

    Corrected Query to pull down requested stats

--- docs/runbooks/generating_stats/generating_stats_test.sql
+++ docs/runbooks/generating_stats/generating_stats_test.sql
@@ -1,15 +1,13 @@
-SELECT
-	COUNT(1) ,
-	to_char(completedAt,'Mon') as Mon,
-	extract(year from completedAt) as Yr,
-	payment->>'method'
+SELECT COUNT(1),
+	applications."payment"->>'method' AS "Method",
+	TO_CHAR(applications."completedAt", 'Mon') AS "Mon",
+	TO_CHAR(applications."completedAt", 'YYYY') AS "Yr"
 FROM applications
 WHERE
-	completedAt IS NOT NULL AND
-	completedAt BETWEEN
-		'2019-01-01 00:00:00' AND
-		'2020-03-31 23:59:59'
+	applications."payment"->>'method' IS NOT NULL AND
+	applications."completedAt" IS NOT NULL AND
+	applications."completedAt" BETWEEN '2019-01-01 00:00:00' AND '2020-03-31 23:59:59'
 GROUP BY
-	1,
 	2,
-	payment->>'method';
+	3,
+	4;
