37cc674270fa488c178220e19e779588f4bd5dbe
raffers <chris.rafferty@digital.justice.gov.uk>
2020-04-02 14:53:26 +0100

    Completed queries for requested stats & separated into individual files for running on prod

--- docs/runbooks/generating_stats/generating_stats_lpa_application_types.sql
+++ docs/runbooks/generating_stats/generating_stats_lpa_application_types.sql
@@ -1,11 +1,10 @@
+-- Query for LPA types
 SELECT COUNT(1),
-	applications."payment"->>'method' AS "Method",
+    applications."document"->>'type' AS "Type",
 	TO_CHAR(applications."completedAt", 'Mon') AS "Mon",
 	TO_CHAR(applications."completedAt", 'YYYY') AS "Yr"
 FROM applications
 WHERE
-	applications."payment"->>'method' IS NOT NULL AND
-	applications."completedAt" IS NOT NULL AND
 	applications."completedAt" BETWEEN '2019-01-01 00:00:00' AND '2020-03-31 23:59:59'
 GROUP BY
 	2,
---
+++ docs/runbooks/generating_stats/generating_stats_lpa_payment_types.sql
@@ -0,0 +1,13 @@
+-- Query for LPA payment types
+SELECT COUNT(1),
+	applications."payment"->>'method' AS "Method",
+	TO_CHAR(applications."completedAt", 'Mon') AS "Mon",
+	TO_CHAR(applications."completedAt", 'YYYY') AS "Yr"
+FROM applications
+WHERE
+	applications."payment"->>'method' IS NOT NULL AND
+	applications."completedAt" BETWEEN '2019-01-01 00:00:00' AND '2020-03-31 23:59:59'
+GROUP BY
+	2,
+	3,
+	4;
