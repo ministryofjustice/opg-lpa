4e86e26fd3b794724503d0e822493bd14d553ee3
raffers <chris.rafferty@digital.justice.gov.uk>
2020-04-02 16:23:48 +0100

    Adjusted queries so the results are easier to read

--- docs/runbooks/generating_stats/generating_stats_lpa_application_types.sql
+++ docs/runbooks/generating_stats/generating_stats_lpa_application_types.sql
@@ -1,12 +1,18 @@
--- Query for LPA types
-SELECT COUNT(1),
+-- Query for LPA application types
+SELECT
     applications."document"->>'type' AS "Type",
-	TO_CHAR(applications."completedAt", 'Mon') AS "Mon",
-	TO_CHAR(applications."completedAt", 'YYYY') AS "Yr"
+	TO_CHAR(applications."completedAt", 'MM') AS "Mon",
+	TO_CHAR(applications."completedAt", 'YYYY') AS "Yr",
+    COUNT(1)
 FROM applications
 WHERE
 	applications."completedAt" BETWEEN '2019-01-01 00:00:00' AND '2020-03-31 23:59:59'
 GROUP BY
-	2,
+	1,
 	3,
-	4;
+	2
+ORDER BY
+    1 ASC,
+	3 ASC,
+	2 ASC
+;
