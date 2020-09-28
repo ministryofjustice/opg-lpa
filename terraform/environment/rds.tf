fb93ec3637c0aaa93279b48589acde66b8c8b982
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2020-09-28 13:13:35 +0100

    linting

--- terraform/environment/rds.tf
+++ terraform/environment/rds.tf
@@ -1,38 +1,38 @@
 resource "aws_db_instance" "api" {
-  identifier                 = lower("api-${local.environment}")
-  name                       = "api2"
-  allocated_storage          = 10
-  storage_type               = "gp2"
-  storage_encrypted          = true
-  skip_final_snapshot        = local.account.skip_final_snapshot
-  engine                     = "postgres"
-  engine_version             = local.account.psql_engine_version
-  instance_class             = "db.m3.medium"
-  port                       = "5432"
-  username                   = data.aws_secretsmanager_secret_version.api_rds_username.secret_string
-  password                   = data.aws_secretsmanager_secret_version.api_rds_password.secret_string
-  parameter_group_name       = aws_db_parameter_group.postgres-db-params.name
-  vpc_security_group_ids     = [aws_security_group.rds-api.id]
-  auto_minor_version_upgrade = false
-  maintenance_window         = "sun:01:00-sun:01:30"
-  multi_az                   = true
-  backup_retention_period    = local.account.backup_retention_period
-  deletion_protection        = local.account.prevent_db_destroy
-  tags                       = local.default_tags
-  allow_major_version_upgrade= true
+  identifier                  = lower("api-${local.environment}")
+  name                        = "api2"
+  allocated_storage           = 10
+  storage_type                = "gp2"
+  storage_encrypted           = true
+  skip_final_snapshot         = local.account.skip_final_snapshot
+  engine                      = "postgres"
+  engine_version              = local.account.psql_engine_version
+  instance_class              = "db.m3.medium"
+  port                        = "5432"
+  username                    = data.aws_secretsmanager_secret_version.api_rds_username.secret_string
+  password                    = data.aws_secretsmanager_secret_version.api_rds_password.secret_string
+  parameter_group_name        = aws_db_parameter_group.postgres-db-params.name
+  vpc_security_group_ids      = [aws_security_group.rds-api.id]
+  auto_minor_version_upgrade  = false
+  maintenance_window          = "sun:01:00-sun:01:30"
+  multi_az                    = true
+  backup_retention_period     = local.account.backup_retention_period
+  deletion_protection         = local.account.prevent_db_destroy
+  tags                        = local.default_tags
+  allow_major_version_upgrade = true
 }
 
 resource "aws_db_parameter_group" "postgres-db-params" {
   name        = lower("postgres-db-params-${local.environment}")
   description = "default postgres rds parameter group"
   family      = local.account.psql_parameter_group_family
-
+  # -1 is default which would not log anything
   parameter {
     name         = "log_min_duration_statement"
     value        = "500"
     apply_method = "immediate"
   }
-
+  # none is default
   parameter {
     name         = "log_statement"
     value        = "none"
