8ca95618d6d70810c13cbfd9383f12e1cd6c9319
William Falconer <williamfalconeruk@users.noreply.github.com>
2020-10-01 12:36:01 +0100

    Lpa 3764 async workspace removal (#138)
    * dynamodb table for account level changes
    
    * WIP more work on pipeline
    
    * correct executable name
    
    * try changing arch to see if fixes issue..
    
    * circleci config change
    
    * use lpa-dev account. might need a further tweak.
    
    * add workspace manager orb config and refactor
    
    * correct the format of the orb
    
    * correct location for workspace orb namespaces
    
    * sudo not needed
    
    * amd64 is the one that should work
    
    * correct replacement, based on goreleaser.
    
    * install the libc6-compat library
    
    * force push
    
    * add workspace cleanup schedule
    
    * TF_WORKSPACE is a special terraform override
    
    * remove old binaries - not required
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

---
+++ scripts/pipeline/workspace_cleanup/workspace_cleanup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 0 ]
+  then
+    echo "Please provide workspaces to be removed."
+fi
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` [workspaces separated by a space]"
+  exit 0
+fi
+
+function getWorkspaces {
+  terraform workspace list
+}
+
+in_use_workspaces="$@"
+reserved_workspaces="default production preproduction development demo ithc"
+
+protected_workspaces="$in_use_workspaces $reserved_workspaces"
+all_workspaces=$(terraform workspace list|sed 's/*//g')
+
+for workspace in $all_workspaces
+do
+  case "$protected_workspaces" in
+    *$workspace*)
+      echo "protected workspace: $workspace"
+      ;;
+    *)
+      echo "cleaning up workspace $workspace..."
+      terraform workspace select $workspace
+      terraform destroy -auto-approve
+      terraform workspace select default
+      terraform workspace delete $workspace
+      ;;
+  esac
+done
