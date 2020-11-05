cfae69fd79d5b2741624d79b31edd33dd4d2e4be
William Falconer <williamfalconeruk@users.noreply.github.com>
2020-11-05 13:33:21 +0000

    new 5xx cloudwatch alarms with initial pagerduty integration (#162)
    * new 5xx cloudwatch alarms with initial pagerduty integration
    
    * wire up provider and variables properly
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

---
+++ terraform/environment/pagerduty.tf
@@ -0,0 +1,24 @@
+data "pagerduty_service" "pagerduty" {
+  name = local.account.pagerduty_service_name
+}
+
+data "pagerduty_vendor" "cloudwatch" {
+  name = "Cloudwatch"
+}
+
+resource "pagerduty_service_integration" "cloudwatch_integration" {
+  name    = "${data.pagerduty_vendor.cloudwatch.name} ${local.environment} Account"
+  service = data.pagerduty_service.pagerduty.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "aws_sns_topic" "cloudwatch_to_pagerduty" {
+  name = "CloudWatch-to-PagerDuty-${local.environment}"
+}
+
+resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription" {
+  topic_arn              = aws_sns_topic.cloudwatch_to_pagerduty.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration.integration_key}/enqueue"
+}
