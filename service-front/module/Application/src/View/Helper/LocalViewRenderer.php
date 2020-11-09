8a7497962fe162110f8971b21c9db1577fc81107
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-11-09 15:11:08 +0000

    added local renderer

---
+++ service-front/module/Application/src/View/Helper/LocalViewRenderer.php
@@ -0,0 +1,31 @@
+<?php
+// @inheritdoc
+//
+
+namespace Application\View\Helper;
+
+use Twig\Environment as TwigEnvironment;
+
+class LocalViewRenderer implements RendererInterface
+{
+     // this wraps TwigWrapper because TwigWrapper cannot be mocked due to being marked final
+     // in other words some acrobatics to get around incompatibility between Twig and Mockery
+
+    /**
+     * @param TwigEnvironment $viewRenderer
+     */
+    public function __construct(TwigEnvironment $viewRenderer)
+    {
+        $this->viewRenderer = $viewRenderer;
+    }
+
+    public function LoadTemplate(string $templateName)
+    {
+         return $this->viewRenderer->load($templateName)->unwrap();
+    }
+}
+
+interface RendererInterface
+{
+    public function LoadTemplate(string $templateName);
+}
