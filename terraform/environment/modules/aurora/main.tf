1b28040367a06fda98df95dac0d08afb4617dc5b
william Falconer <william.falconer@digital.justice.gov,uk>
2020-11-17 16:28:06 +0000

    consistency in using variables

--- terraform/environment/modules/aurora/main.tf
+++ terraform/environment/modules/aurora/main.tf
@@ -33,10 +33,10 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   auto_minor_version_upgrade      = var.auto_minor_version_upgrade
   db_subnet_group_name            = var.db_subnet_group_name
   depends_on                      = [aws_rds_cluster.cluster]
-  cluster_identifier              = "${var.cluster_identifier}-${terraform.workspace}"
+  cluster_identifier              = "${var.cluster_identifier}-${var.environment}"
   engine                          = var.engine
   engine_version                  = var.engine_version
-  identifier                      = "${var.cluster_identifier}-${terraform.workspace}-${count.index}"
+  identifier                      = "${var.cluster_identifier}-${var.environment}-${count.index}"
   instance_class                  = var.instance_class
   monitoring_interval             = 30
   monitoring_role_arn             = "arn:aws:iam::${var.account_id}:role/rds-enhanced-monitoring"
@@ -58,7 +58,7 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
 
 resource "aws_rds_cluster" "cluster_serverless" {
   count                        = var.aurora_serverless ? 1 : 0
-  cluster_identifier           = "${var.cluster_identifier}-${terraform.workspace}"
+  cluster_identifier           = "${var.cluster_identifier}-${var.environment}"
   apply_immediately            = var.apply_immediately
   availability_zones           = var.availability_zones
   backup_retention_period      = var.backup_retention_period
@@ -67,7 +67,7 @@ resource "aws_rds_cluster" "cluster_serverless" {
   deletion_protection          = var.deletion_protection
   engine                       = var.engine
   engine_mode                  = "serverless"
-  final_snapshot_identifier    = "${var.database_name}-${terraform.workspace}-final-snapshot"
+  final_snapshot_identifier    = "${var.database_name}-${var.environment}-final-snapshot"
   kms_key_id                   = var.kms_key_id
   master_username              = var.master_username
   master_password              = var.master_password
