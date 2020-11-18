39576d2b53e8c1e9b108549561966460300e34a0
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-11-18 14:47:53 +0000

    cypress tests run in docker locally

--- cypress/integration/basic_login.js
+++ cypress/integration/basic_login.js
@@ -1,10 +1,11 @@
 describe('Test user login', () => {
   it('Visits the Make website', () => {
-    //cy.visit('https://www.lastingpowerofattorney.service.gov.uk/login')
-    cy.visit('https://localhost:7002/login')
+    //cy.visit('https://localhost:7002/login')
+    cy.visit('/login')
     cy.contains('Sign in')
     cy.get("input#email.form-control").clear().type("seeded_test_user@digital.justice.gov.uk");
     cy.get("input#password.form-control").clear().type("Pass1234");
     cy.get('input#signin-form-submit.button').click()
+    cy.contains("h1.heading-xlarge", "Your LPAs");
   })
 })
