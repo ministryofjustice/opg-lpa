49d5d2bee284c6681d845b97bbd5e610481c3086
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-01 14:12:02 +0000

    more Cypress steps

---
+++ cypress/integration/common/i_am_taken_to.js
@@ -0,0 +1,5 @@
+import { Given } from "cypress-cucumber-preprocessor/steps";
+ 
+Then(`I am taken to {string}`, (url) => {
+  cy.url().should('eq',Cypress.config().baseUrl + url)
+})
---
+++ cypress/integration/common/i_click_back.js
@@ -0,0 +1,5 @@
+import { When } from "cypress-cucumber-preprocessor/steps";
+
+When(`I click back`, () => {
+    cy.go('back')
+})
