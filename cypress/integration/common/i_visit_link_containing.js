badcb977fa850def22f28e2479db30945dd5fb3c
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-02 12:11:49 +0000

    refactoring of steps & features, more links working

--- cypress/integration/common/i_visit_link_containing.js
+++ cypress/integration/common/i_visit_link_containing.js
@@ -1,6 +1,5 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
  
-Then(`I visit link containing {string}`, (hrefText) => {
-  let searchStr = 'a[href*="' + hrefText + '"]' 
-  cy.get(searchStr).click()
+Then(`I visit link containing {string}`, (linkText) => {
+  cy.contains(linkText).click()
 })
