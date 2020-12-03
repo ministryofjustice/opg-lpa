d936ae7b9c61a4e177805cdb83baece17f608176
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-03 10:47:17 +0000

    ability to check for standard test user email addr in confirmation page

---
+++ cypress/integration/common/i_see_standard_test_user_in_page_text.js
@@ -0,0 +1,5 @@
+import { Given } from "cypress-cucumber-preprocessor/steps";
+ 
+Then(`I see standard test user in the page text`, () => {
+  cy.get('[class="text"]').contains(Cypress.env("email"))
+})
