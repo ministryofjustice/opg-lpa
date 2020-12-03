d5d1ab20b8dfe1f5e851be977bf5d3b0a9708e6e
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-03 09:47:39 +0000

    fix signup with standard user

--- cypress/integration/common/i_sign_up_standard_test_user.js
+++ cypress/integration/common/i_sign_up_standard_test_user.js
@@ -2,5 +2,9 @@ import { Given } from "cypress-cucumber-preprocessor/steps";
  
 Then(`I sign up standard test user`, () => {
   cy.get("input#email.form-control").type(Cypress.env("email"))
+  cy.get("input#email_confirm.form-control").type(Cypress.env("email"))
   cy.get("input#password.form-control").type(Cypress.env("password"))
+  cy.get("input#password_confirm.form-control").type(Cypress.env("password"))
+  cy.get("input#terms").check()
+  cy.get("input#signin-form-submit.button").click()
 })
