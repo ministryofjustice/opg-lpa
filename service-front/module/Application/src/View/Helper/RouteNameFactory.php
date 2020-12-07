9d60ac06eee03222cb729aa6c74773f2b75f862e
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2020-12-07 17:57:41 +0000

    Updated var name to better reflect what is stored. Updates twig to set route outside of if function due to causing issues with functional test.

--- service-front/module/Application/src/View/Helper/RouteNameFactory.php
+++ service-front/module/Application/src/View/Helper/RouteNameFactory.php
@@ -27,6 +27,6 @@ class RouteNameFactory implements FactoryInterface
         /** @var RouteMatch $routeMatch */
         $routeMatch = $application->getMvcEvent()->getRouteMatch();
 
-        return new RouteName($session, $routeMatch);
+        return new RouteName($routeMatch, $session);
     }
 }
