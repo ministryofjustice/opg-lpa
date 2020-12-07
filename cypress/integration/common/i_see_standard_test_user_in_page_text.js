13045a9c823e8704286e21eb3d94211dc3d104a0
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-07 12:05:48 +0000

    WIP work on feedback

--- cypress/integration/common/i_see_standard_test_user_in_page_text.js
+++
@@ -1,5 +0,0 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
- 
-Then(`I see standard test user in the page text`, () => {
-  cy.get('[class="text"]').contains(Cypress.env("email"));
-})
