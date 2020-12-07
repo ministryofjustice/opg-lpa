549bc071035b2e4990ea86d4453a2073b79b1ce5
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-07 10:13:48 +0000

    refactor steps, take php specific stuff out of Dockerfile

--- cypress/integration/common/i_log_in.js
+++ cypress/integration/common/i_log_in.js
@@ -1,7 +1,21 @@
 import { When } from "cypress-cucumber-preprocessor/steps";
 
-Then(`I log in with user {string} password {string}`, (user, password) => {
+When(`I log in with user {string} password {string}`, (user, password) => {
+    logIn(user, password);
+})
+
+When(`I log in as seeded user`, () => {
+    logIn(Cypress.env("seeded_email"),Cypress.env("seeded_password"));
+})
+
+When(`I log in as standard test user`, () => {
+    logIn(Cypress.env("email"),Cypress.env("password"));
+})
+
+function logIn(user, password){
+    cy.visit("/login").title().should('include','Sign in');
     cy.get("input#email.form-control").clear().type(user);
     cy.get("input#password.form-control").clear().type(password);
     cy.get("input#signin-form-submit.button").click();
-})
+    cy.log("Successfully logged in as " + user);
+}
--- cypress/integration/common/i_log_in_as_standard_test_user.js
+++
@@ -1,9 +0,0 @@
-import { When } from "cypress-cucumber-preprocessor/steps";
-
-When(`I log in as standard test user`, () => {
-    cy.visit("/login").title().should('include','Sign in');
-    cy.get("input#email.form-control").clear().type(Cypress.env("email"));
-    cy.get("input#password.form-control").clear().type(Cypress.env("password"));
-    cy.get("input#signin-form-submit.button").click();
-    cy.log("Successfully logged in as " + Cypress.env("email"));
-})
