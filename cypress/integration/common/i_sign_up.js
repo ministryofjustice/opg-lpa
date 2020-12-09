93644e3781fee9abde3cc6c1a73c20f43d4b1a2c
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-09 10:41:36 +0000

    sign up page now using data-cy tags

---
+++ cypress/integration/common/i_sign_up.js
@@ -0,0 +1,15 @@
+import { Given } from "cypress-cucumber-preprocessor/steps";
+ 
+Then(`I sign up standard test user`, () => {
+    signUp(Cypress.env("email"),Cypress.env("password"));
+})
+
+function signUp(user, password){
+    cy.visit("/signup").title().should('include','Create an account');
+    cy.get('[data-cy=signup-email]').clear().type(user);
+    cy.get('[data-cy=signup-email-confirm]').clear().type(user);
+    cy.get('[data-cy=signup-password]').clear().type(password);
+    cy.get('[data-cy=signup-password-confirm]').clear().type(password);
+    cy.get('[data-cy=signup-terms]').check();
+    cy.get('[data-cy=signup-submit-button]').click();
+}
