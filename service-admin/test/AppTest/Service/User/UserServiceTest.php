2b2736a1a13f5785ec4e65fe26fc466ccf936ca0
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2020-12-11 11:24:31 +0000

    Add test for UserService->match() method

--- service-admin/test/AppTest/Service/User/UserServiceTest.php
+++ service-admin/test/AppTest/Service/User/UserServiceTest.php
@@ -38,4 +38,35 @@ class UserServiceTest extends TestCase
         $this->assertEquals($actual['isActive'], TRUE);
         $this->assertEquals($actual['numberOfLpas'], $numLpas);
     }
+
+    public function testMatchUsers()
+    {
+        $query = 'lint';
+
+        $id = 'FFFFFlinstone';
+        $numLpas = 3;
+
+        $client = $this->prophesize(ApiClient::class);
+
+        // initial search; default limit and offset 0
+        $params = [
+            'query' => $query,
+            'offset' => 0,
+            'limit' => 10,
+        ];
+
+        $client->httpGet('/v2/users/match', $params)->willReturn([
+            'userId' => $id,
+            'isActive' => TRUE,
+            'numberOfLpas' => $numLpas,
+        ]);
+
+        // match method on service
+        $userService = new UserService($client->reveal());
+        $actual = $userService->match($params);
+
+        $this->assertEquals($actual['userId'], $id);
+        $this->assertEquals($actual['isActive'], TRUE);
+        $this->assertEquals($actual['numberOfLpas'], $numLpas);
+    }
 }
