84e6de633dc661c11ada73f3dd44a95dec352203
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-15 13:51:51 +0000

    fixes

--- cypress/S3Monitor.py
+++ cypress/S3Monitor.py
@@ -5,93 +5,110 @@ import quopri
 import time
 import re
 
-class S3Monitor:
-    def set_iam_role_session(self):
-        sts = boto3.client(
-            'sts',
-            region_name='eu-west-1',
-        )
+mailbox_bucket = 'opg-lpa-casper-mailbox' # this might get renamed once casper tests are turned off
+activation_emails_path = 'cypress/activation_emails'
+s3Client = boto3.client('s3')
 
-        if os.getenv('CI'):
-            role_arn = 'arn:aws:iam::050256574573:role/opg-lpa-ci'
+def set_iam_role_session():
+    sts = boto3.client(
+        'sts',
+        region_name='eu-west-1',
+    )
+
+    if os.getenv('CI'):
+        role_arn = 'arn:aws:iam::050256574573:role/opg-lpa-ci'
+    else:
+        role_arn = 'arn:aws:iam::050256574573:role/operator'
+
+    result = sts.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName='session1',
+    )
+    #print(result)
+
+# Extract the plus part from emails of the form:
+# basename+pluspart@example.com
+def getPlusPartFromEmailAddress(email):
+    plusPos = email.find('+')
+    atPos = email.find('@')
+    userIdLength = atPos - plusPos - 1
+    userId = email[plusPos + 1:atPos]
+    return userId
+
+def parseBody(bodyContent, subject, thetype, linkRegex):
+    regex = 'https:\/\/\S+' + linkRegex + '\/[a-zA-Z0-9]+'
+
+    match = re.search(regex, bodyContent)
+
+    if match is not None: 
+        s = match.start()
+        e = match.end()
+        activationLink = bodyContent[s:e]
+        print(f'{ thetype } link { activationLink }')
+
+        emailRegex = 'To: (.+)'
+
+        emailMatch = re.search(emailRegex, bodyContent)
+        if emailMatch is not None:
+            es = emailMatch.start()
+            ee = emailMatch.end()
+            toEmail = bodyContent[es:ee]
+
+            userId = getPlusPartFromEmailAddress(toEmail)
+            print(f'userId {userId}')
+            contents = f'{toEmail[:-1]},{activationLink}'
+            filePath = f'{activation_emails_path}/{userId}.{thetype}'
+            emailFile = open(filePath,'w')
+            emailFile.write(contents)
+            emailFile.close()
+    else:
+        print(f'Message: {subject} does not match regex {regex}') 
+        print('----------------------------------------------------------------------------------')
+        #print(bodyContent)
+        print('----------------------------------------------------------------------------------')
+
+def parse_email(bodyContent, s3Key):
+    activate_subject = 'Activate your lasting power of attorney account'
+    reset_password_subject = 'Password reset request'
+    if re.search(activate_subject, bodyContent) is not None:
+        parseBody(bodyContent, activate_subject, 'activation', 'signup\/confirm')
+    else:
+        if re.search(reset_password_subject, bodyContent) is not None:
+            parseBody(bodyContent, reset_password_subject, 'passwordreset', 'forgot-password\/reset')
         else:
-            role_arn = 'arn:aws:iam::050256574573:role/operator'
+            print("Found an email that is not an Activate or Password reset. Don't know what to do with it")
+            fileSuffix = s3Key[s3Key.rfind('/')+1:]
+            filePath = f'{activation_emails_path}/unrecognized.{fileSuffix}'
+            emailFile = open(filePath,'w')
+            emailFile.write(bodyContent)
+            emailFile.close()
 
-        result = sts.assume_role(
-            RoleArn=role_arn,
-            RoleSessionName='session1',
-        )
-        #print(result)
+def process_bucket_object(s3Key):
+        result = s3Client.get_object(Bucket=mailbox_bucket,Key=s3Key)
+        bodyContent = quopri.decodestring(result["Body"].read()).decode('latin-1')
+        #print(f'Parsing {s3Key}')
+        parse_email(bodyContent, s3Key)
 
-        self.s3Client = boto3.client('s3')
+def monitor_bucket():
+    seenkeys = []
 
-    # Extract the plus part from emails of the form:
-    # basename+pluspart@example.com
-    def getPlusPartFromEmailAddress(self,email):
-        plusPos = email.find('+')
-        atPos = email.find('@')
-        userIdLength = atPos - plusPos - 1
-        userId = email[plusPos + 1:atPos]
-        return userId
-
-    def parseBody(self, bodyContent, subject, thetype, linkRegex):
-        regex = 'https:\/\/\S+' + linkRegex + '\/[a-zA-Z0-9]+'
-
-        match = re.search(regex, bodyContent)
-
-        if match is not None: 
-            s = match.start()
-            e = match.end()
-            activationLink = bodyContent[s:e]
-            print(f'Activation link { activationLink }')
-
-            emailRegex = 'To: (.+)'
-
-            emailMatch = re.search(emailRegex, bodyContent)
-            if emailMatch is not None:
-                es = emailMatch.start()
-                ee = emailMatch.end()
-                toEmail = bodyContent[es:ee]
-
-                userId = self.getPlusPartFromEmailAddress(toEmail)
-                print(f'userId {userId}')
-                contents = f'{toEmail[:-1]},{activationLink}'
-                filePath = f'/mnt/test/activation_emails/{userId}.{thetype}'
-                print(f'writing {contents} here')
-                emailFile = open(filePath,'w')
-                emailFile.write(contents)
-                emailFile.close()
-        else:
-            print(f'Message: {subject} does not match regex {regex}') 
-            print('----------------------------------------------------------------------------------')
-            #print(bodyContent)
-            print('----------------------------------------------------------------------------------')
-
-    def monitor_bucket(self):
-        seenkeys = []
-
-        while True:
-            print('Checking S3')
-            bucketContents = self.s3Client.list_objects(Bucket='opg-lpa-casper-mailbox')
-            if 'Contents' in bucketContents:  # handle bucket being empty
-                for s3obj in self.s3Client.list_objects(Bucket='opg-lpa-casper-mailbox')['Contents']:
-                    s3Key = s3obj['Key']
-                    if not s3Key in seenkeys:
-                        result = self.s3Client.get_object(Bucket='opg-lpa-casper-mailbox',Key=s3Key)
-                        bodyContent = quopri.decodestring(result["Body"].read()).decode('latin-1')
-                        self.parseBody(bodyContent, 'Activate your lasting power of attorney account', 'activation', 'signup\/confirm')
-                        self.parseBody(bodyContent, 'Password reset request', 'passwordreset', 'forgot-password\/reset')
-                        seenkeys.append(s3Key)
-                    #else:
-                    #    print(f'Already seen {s3Key}')
-            time.sleep(5)
+    while True:
+        print('Checking S3')
+        bucketContents = s3Client.list_objects(Bucket=mailbox_bucket)
+        if 'Contents' in bucketContents:  # handle bucket being empty
+            for s3obj in s3Client.list_objects(Bucket=mailbox_bucket)['Contents']:
+                s3Key = s3obj['Key']
+                if not s3Key in seenkeys:
+                    process_bucket_object(s3Key)
+                    seenkeys.append(s3Key)
+                #else:
+                #    print(f'Already seen {s3Key}')
+        time.sleep(5)
 
 
 def main():
-    monitor = S3Monitor()
-    monitor.set_iam_role_session()
-    monitor.monitor_bucket()
-
+    set_iam_role_session()
+    monitor_bucket()
 
 if __name__ == "__main__":
     main()
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -27,7 +27,8 @@ var random = Math.floor(Math.random() * 999999999);
 var date = new Date();
 var userNumber = date.getTime() + "" + random;
 Cypress.env("userNumber", userNumber)
-Cypress.env("email","cypresstests+" + userNumber + "@lpa.opg.service.justice.gov.uk")
+// line below, email currently needs to start with caspertests, to get written to the S3 bucket. That is configured in email.tf, and could be changed once casper tests are switched off
+Cypress.env("email","caspertests+" + userNumber + "@lpa.opg.service.justice.gov.uk") 
 Cypress.env("password", "Cypress" + userNumber)
 Cypress.env("seeded_email","seeded_test_user@digital.justice.gov.uk");
 Cypress.env("seeded_password","Pass1234");
