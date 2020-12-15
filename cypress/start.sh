0fb030c3711234e4b5b0cfd50ee27d4cd9d9f0a7
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-15 13:51:51 +0000

    fixes

---
+++ cypress/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo The base domain is $BASE_DOMAIN
+
+echo Starting S3 Monitor
+python3 cypress/S3Monitor.py &
+
+
+echo Starting Cypress Tests
+
+CYPRESS_CMD="cypress open --project /app"
+
+echo "Running cypress command line:"
+echo $CYPRESS_CMD
+$CYPRESS_CMD
+
+RETVAL=$?
+echo printing $RETVAL
+
+if [ $RETVAL -eq 0 ]; then
+    echo OK
+else
+    echo FAIL
+fi
+
+echo Killing S3 Monitor
+#kill $(ps aux | grep '[p]hp' | awk '{print $2}')
+
+exit $RETVAL
