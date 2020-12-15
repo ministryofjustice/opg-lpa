a4faf1cbcb07d3e82e95d5d1a32d0c13f1f2475a
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2020-12-15 15:29:52 +0000

    Base implementation for controller event listener

---
+++ service-front/module/Application/src/Logging/EventLogger.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Application\Logging;
+
+use Laminas\EventManager\EventManagerInterface;
+use Laminas\EventManager\AbstractListenerAggregate;
+use Laminas\EventManager\Event;
+use Laminas\Mvc\MvcEvent;
+use Opg\Lpa\Logger\LoggerTrait;
+
+class EventLogger extends AbstractListenerAggregate
+{
+    use LoggerTrait;
+
+    /**
+     * Magic method so this class can be its own factory
+     * @return EventLogger
+     */
+    public function __invoke(): self
+    {
+        return new self();
+    }
+
+    /**
+     * @param EventManagerInterface $events
+     */
+    public function attach(EventManagerInterface $events, $priority = 1): void
+    {
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH_ERROR, function ($evt) {
+            $this->onError(MvcEvent::EVENT_DISPATCH_ERROR, $evt);
+        }, 100);
+
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_RENDER_ERROR, function ($evt) {
+            $this->onError(MvcEvent::EVENT_RENDER_ERROR, $evt);
+        }, 100);
+    }
+
+    /**
+     * Log an error
+     *
+     * @param string $errorType Identifier for the type of error
+     * @param Event $event
+     *
+     * @return void
+     */
+    private function onError(string $errorType, Event $event): void
+    {
+        $error = [
+            'type' => $errorType,
+            'event' => $event,
+        ];
+
+        $this->getLogger()->err($error);
+    }
+}
