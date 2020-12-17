6e1bb8390b1219a87bb509127c2eea96ac2d1f3b
William Falconer <williamfalconeruk@users.noreply.github.com>
2020-12-17 09:58:33 +0000

    LPAL-193 fix production after prod DNS change (#208)
    * fix up and remove old legacy certs
    
    * fix dns and cert references
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

--- terraform/environment/dns.tf
+++ terraform/environment/dns.tf
@@ -18,7 +18,7 @@ resource "aws_service_discovery_private_dns_namespace" "internal" {
 resource "aws_route53_record" "public_facing_lastingpowerofattorney" {
   provider = aws.management
   zone_id  = data.aws_route53_zone.live_service_lasting_power_of_attorney.zone_id
-  name     = "${local.dns_namespace_env}${data.aws_route53_zone.live_service_lasting_power_of_attorney.name}"
+  name     = "${local.dns_namespace_env_public}${data.aws_route53_zone.live_service_lasting_power_of_attorney.name}"
   type     = "A"
 
   alias {
