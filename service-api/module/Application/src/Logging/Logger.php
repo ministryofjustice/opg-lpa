2be72b68c57dd68f7a76916803aeb0d6f590d66b
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2020-12-21 15:58:09 +0000

    Use our logger throughout service-api
    Use a copy of the custom JSON logger developed for service-front.
    
    *This really needs a refactor so we can share the logger between front-app and api-app.*

---
+++ service-api/module/Application/src/Logging/Logger.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Application\Logging;
+
+use Laminas\Log\Logger as LaminasLogger;
+use Laminas\Log\Writer\Stream as StreamWriter;
+use Laminas\Log\Formatter\Json as JsonFormatter;
+
+use Application\Logging\MvcEventProcessor;
+use Application\Logging\HeadersProcessor;
+
+/**
+ * class Logger
+ *
+ * A simple StreamWriter file logger which converts log events to JSON.
+ */
+class Logger extends LaminasLogger
+{
+    /**
+     * @var Logger
+     */
+    private static $instance = null;
+
+    /**
+     * Logger constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->addProcessor(new MvcEventProcessor());
+        $this->addProcessor(new HeadersProcessor());
+
+        $writer = new StreamWriter('php://stderr');
+        $writer->setFormatter(new JsonFormatter());
+
+        $this->addWriter($writer);
+    }
+
+    /**
+     * Singleton provider for logger
+     * Required so logger can be loaded in all services including none
+     *
+     * @return self
+     */
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Destroy the logger
+     */
+    public static function destroy()
+    {
+        self::$instance = null;
+    }
+}
