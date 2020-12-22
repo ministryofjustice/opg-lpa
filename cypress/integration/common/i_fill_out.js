0955d3232c0efacb5035f92119226fbbe5aaf2fe
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-12-22 10:34:09 +0000

    for now name-title use direct selector until data-cy for that element can be got to work

--- cypress/integration/common/i_fill_out.js
+++ cypress/integration/common/i_fill_out.js
@@ -3,14 +3,28 @@ import { Then } from "cypress-cucumber-preprocessor/steps";
 When("I fill out", (dataTable) => {
     var rawTable = dataTable.rawTable;
 
-    rawTable.forEach(row => { cy.get("[data-cy=" + row[0] + "]").type(row[1]);
-                });
+    rawTable.forEach(row => { 
+                cy.log("field is " + row[0])
+                if (row[0].valueOf() === new String("#name-title").valueOf()) { // we cannot currently label name-title with a data-cy tag
+                    cy.get(row[0]).type(row[1]);
+                }
+                else {
+                    cy.get("[data-cy=" + row[0] + "]").type(row[1]);
+                }
+            });
 });
 
 // this uses force, to forcibly fill out elements even if they're meant to be hidden
 When("I force fill out", (dataTable) => {
     var rawTable = dataTable.rawTable;
 
-    rawTable.forEach(row => { cy.get("[data-cy=" + row[0] + "]").type(row[1], { force: true });
-                });
+    rawTable.forEach(row => { 
+                cy.log("field is " + row[0])
+                if (row[0].valueOf() === new String("#name-title").valueOf()) { // we cannot currently label name-title with a data-cy tag
+                    cy.get(row[0]).type(row[1], { force: true });
+                }
+                else {
+                    cy.get("[data-cy=" + row[0] + "]").type(row[1], { force: true });
+                }
+            });
 });
