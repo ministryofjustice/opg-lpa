2dbd196ef10117953c1874c9681e09b694a51ab9
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2021-01-05 10:21:52 +0000

    Removes sessionManager changes that are no longer required

--- service-front/module/Application/src/Model/Service/Session/SessionManager.php
+++ service-front/module/Application/src/Model/Service/Session/SessionManager.php
@@ -7,11 +7,6 @@ use Laminas\Session\SessionManager as LaminasSessionManager;
 class SessionManager extends LaminasSessionManager {
 
     /**
-     * @var string $lastMatchedRouteName
-     */
-    private $lastMatchedRouteName;
-
-    /**
      * Tracks whether we've seen this session before and does a regenerateId() if not.
      */
     public function initialise(){
@@ -24,23 +19,5 @@ class SessionManager extends LaminasSessionManager {
             $container->init = true;
         }
 
-    }
-
-    /**
-     * @return string
-     */
-    public function getLastMatchedRouteName(): string
-    {
-        return $this->lastMatchedRouteName;
-    }
-
-    /**
-     * @param string $matchedRouteName
-     */
-    public function setLastMatchedRouteName(string $matchedRouteName): void
-    {
-        if (!isset($this->lastMatchedRouteName) || $this->lastMatchedRouteName !== $matchedRouteName) {
-            $this->lastMatchedRouteName = $matchedRouteName;
-        }
     }
 } // class
