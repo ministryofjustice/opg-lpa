9c1a3f9a14cde03f85330991e28353852636fbb9
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2021-01-11 19:34:54 +0000

    Moves PersistentSession Container into it's own Helper class, creates associated tests along with updating existing tests to reflect changes.

---
+++ service-front/module/Application/src/Model/Service/Session/PersistentSessionDetails.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Model\Service\Session;
+
+use Laminas\Router\RouteMatch;
+use Laminas\Session\Container;
+
+class PersistentSessionDetails {
+
+    /**
+     * @var Container
+     */
+    private $sessionDetails;
+
+    /**
+     * @var RouteMatch|null
+     */
+    private $route;
+
+    public function __construct(?RouteMatch $route) {
+        $this->route = $route;
+        $this->sessionDetails = new Container('SessionDetails');
+
+        $this->setRouteDetails();
+    }
+    
+    private function setRouteDetails(): void {
+        // breadcrumb so we can determine user's last visited route.
+        // Also account for any null values, eg activation links or status checks.
+        // Unable to assign to RouteInterface, as RouteMatch does not implement RouteInterface
+        $this->sessionDetails->currentRoute = ($this->route instanceof RouteMatch) ?
+            $this->route->getMatchedRouteName() :
+            '';
+        
+        if (!isset($this->sessionDetails->previousRoute)) {
+            $this->sessionDetails->previousRoute = $this->sessionDetails->currentRoute;
+        }
+
+        if ($this->sessionDetails->routeStore !== $this->sessionDetails->previousRoute) {
+            $this->sessionDetails->previousRoute = $this->sessionDetails->routeStore;
+        }
+
+        $this->sessionDetails->routeStore = $this->sessionDetails->currentRoute;
+    }
+
+    public function getCurrentRoute(): string {
+        return $this->sessionDetails->currentRoute;
+    }
+
+    public function getPreviousRoute(): string {
+        return $this->sessionDetails->previousRoute;
+    }
+}
