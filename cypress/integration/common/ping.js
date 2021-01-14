fbf75c4d9b371f664295900e02b131dbea08a9b1
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-01-14 10:00:37 +0000

    Add tests for /ping endpoint

--- cypress/integration/common/ping.js
+++ cypress/integration/common/ping.js
@@ -4,3 +4,14 @@ Given('I visit the ping page', (path) => {
     // note that cy.visit will follow redirects, and require the status code to be 2xx after that
     cy.visit('/ping');
 })
+
+Given('I visit the ping JSON endpoint', (path) => {
+    cy.request('/ping/json').then((response) => {
+        expect(response.status).to.eq(200);
+
+        // TODO response.body.api.ok is currently false but should
+        // be true in the live environment; we can't test it here as it
+        // will fail in the dev environment
+        expect(response.body.dynamo.ok).to.eq(true);
+    });
+})
