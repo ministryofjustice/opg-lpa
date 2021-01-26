18f393aa79edb3c66d80d0d4731e293588644007
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-01-26 16:02:30 +0000

    sort out type page visit - click there from dashboard only if needed

--- cypress/integration/common/i_click.js
+++ cypress/integration/common/i_click.js
@@ -9,3 +9,19 @@ Then(`I force click {string}`, (clickable) => {
     cy.get("[data-cy=" + clickable + "]").click({ force: true });
     cy.OPGCheckA11y();
 })
+
+Then(`I click element marked {string}`, (text) => {
+    cy.contains(text).click();
+    cy.OPGCheckA11y();
+})
+
+// this step exists because newly signed-up user goes straight to type page whereas existing user may get taken to dashboard
+Then(`If I am on dashboard I click to create lpa`, () => {
+    cy.url().then(urlStr => {
+        if (urlStr.includes('dashboard')) {
+            cy.get("[data-cy=createnewlpa]").click();
+            cy.OPGCheckA11y();
+        }
+    });
+})
+
