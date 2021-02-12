d61a9bc0ebdf8b5af007e589056562dbf36431ea
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2021-02-12 11:51:10 +0000

    Created Cloudwatch alarm to capture CSRF Mismatch

--- terraform/environment/cloudwatch_alarms.tf
+++ terraform/environment/cloudwatch_alarms.tf
@@ -43,3 +43,37 @@ resource "aws_cloudwatch_metric_alarm" "admin_5xx_errors" {
   threshold                 = 2
   treat_missing_data        = "notBreaching"
 }
+
+resource "aws_cloudwatch_log_metric_filter" "csrf_mistmatch_filter" {
+  name           = "CSRFValuesMismatch"
+  pattern        = "{($.priorityName = \"ERROR\") && ($.message = \"Mismatched CSRF provided\")}"
+  log_group_name = data.aws_cloudwatch_log_group.online-lpa.name
+
+  metric_transformation {
+    name      = "EventCount"
+    namespace = "AWS/ApplicationELB"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "front_csrf_mismatch_errors" {
+  actions_enabled     = true
+  alarm_actions       = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
+  alarm_description   = "CSRF Errors returned to front users for ${local.environment}"
+  alarm_name          = "${local.environment} public front CSRF errors"
+  comparison_operator = "GreaterThanThreshold"
+  datapoints_to_alarm = 2
+  dimensions = {
+    "LoadBalancer" = trimprefix(split(":", aws_lb.front.arn)[5], "loadbalancer/")
+  }
+  evaluation_periods        = 2
+  insufficient_data_actions = []
+  metric_name               = "${data.aws_cloudwatch_log_group.online-lpa}:csrf_mistmatch_filter"
+  namespace                 = "AWS/ApplicationELB"
+  ok_actions                = [aws_sns_topic.cloudwatch_to_pagerduty.arn]
+  period                    = 60
+  statistic                 = "Sum"
+  tags                      = {}
+  threshold                 = 2
+  treat_missing_data        = "notBreaching"
+}
