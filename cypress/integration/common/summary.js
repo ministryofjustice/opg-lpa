35c8542b9c85d837eb0e867a2be6ac9917255c47
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-02-16 17:18:21 +0000

    for PF, fully check of summary page

--- cypress/integration/common/summary.js
+++ cypress/integration/common/summary.js
@@ -2,14 +2,24 @@ import { When } from "cypress-cucumber-preprocessor/steps";
 
 Then("I see the following summary information", (dataTable) => {
     var rawTable = dataTable.rawTable;
-    // row[0] is cya-question,  row[1] is optional cya-answer, row[2] is optional change link
     let counter = 0;
     let changeCounter = 0;
+    let addressCounter = 0;
     rawTable.forEach(row => { 
+        // cya-question should always match row[0]
         cy.get("[data-cy=cya-question]").eq(counter).should("contain",row[0]);
-        if (row[1] != "") {
-            cy.get("[data-cy=cya-answer]").eq(counter).should("contain",row[1]);
+        // addresses are broken up for checking
+        if (row[0].replace(/\s+/g, '')  == 'Address') {
+            checkAddress(row[1], addressCounter);
+            addressCounter++
         }
+        else {
+            // all other non-empty cases of cya-answer should match row[1]
+            if (row[1] != "") {
+                cy.get("[data-cy=cya-answer]").eq(counter).should("contain",row[1]);
+            }
+        }
+        // row[2] is optional change link, if specified , we ensure it exists and href points to right place
         if (row[2] != "") {
             cy.getLpaId().then((lpaId) => { 
                 cy.get("[data-cy=cya-change]").eq(changeCounter).invoke('attr', 'href').should('contain','/lpa/' + lpaId + '/' + row[2]);
@@ -20,5 +30,18 @@ Then("I see the following summary information", (dataTable) => {
     });
 });
 
-//function checkAddress() {
-//}
+function checkAddress(addrString, addressCounter) {
+    var addrArray = addrString.split('$');
+    if (addrArray.length > 0) {
+        cy.get("[data-cy=streetAddress]").eq(addressCounter).should("contain", addrArray[0].trim());
+    }
+    if (addrArray.length > 1) {
+        cy.get("[data-cy=addressLocality]").eq(addressCounter).should("contain", addrArray[1].trim());
+    }
+    if (addrArray.length > 2) {
+        cy.get("[data-cy=addressRegion]").eq(addressCounter).should("contain", addrArray[2].trim());
+    }
+    if (addrArray.length > 3) {
+        cy.get("[data-cy=postalCode]").eq(addressCounter).should("contain", addrArray[3].trim());
+    }
+}
