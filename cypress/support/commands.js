5d694529e261cf66bed0404cbbcaae8567ea4b99
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-02-26 13:14:36 +0000

    Refactor axe wrapper
    Some swings and roundabouts required to get cy.task('log', ...)
    to work. This cannot be called inside the then() handler for
    promises being constructed by axe, so instead I wrapped
    the axe call and captured the violations it found. These can
    then be logged to the nodejs console, which is also mirrored
    in the command log in the UI.
    
    Additionally log accessibility violations to the browser console
    so they are easy to spot during UI use.
    
    Also enable runAxe() to be called with arguments that are passed
    to axe.run(). This enables calling a subset of the full axe
    a11y tests for specific cases (e.g. just checking contrast).

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,61 +25,43 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 //
 
+const axeWrapper = require('./axe_wrapper');
+
 Cypress.Commands.add("getLpaId", () => {
     cy.get('@donorPageUrl').then((donorPageUrl) => {
         return donorPageUrl.match(/\/(\d+)\//)[1];
     });
 });
 
-Cypress.Commands.add("OPGCheckA11y", () => {
-    cy.window({ log: false }).then((window) => {
-        if (Cypress.$('#axe').length < 1) {
-            const script = window.document.createElement('script');
-            script.id = 'axe';
-            script.async = false;
-            script.innerHTML = require('axe-core/axe.js').source;
-            window.document.body.appendChild(script);
+// window: DOM window instance
+// options: passed directly to axe
+// stopOnError: boolean, default=false; if true, if any violations are
+//     found, an exception is thrown, stopping the test
+Cypress.Commands.add("runAxe", (window, options, stopOnError) => {
+    stopOnError = !!stopOnError;
+
+    // wrap runAxe so that cypress understands the promise it returns
+    cy.wrap(axeWrapper.runAxe(window, options)).then((violations) => {
+        if (violations != null) {
+            // wrap this so that all the cy.task('log', ...) calls complete before
+            // throwing the error; without this, the error is thrown before
+            // the logging is completed
+            cy.wrap(axeWrapper.logViolations(violations, (msg) => {
+                cy.task('log', msg);
+            }))
+            .then(() => {
+                // throw an error to stop the test if configured to;
+                // otherwise we just see log messages and the test continues
+                if (stopOnError) {
+                    throw new Error('accessibility violations caused test to fail');
+                }
+            });
         }
-
-        let logFn = (msg) => {
-            Cypress.log({name: 'axe', message: msg});
-        };
-
-        window.axe.run().then((results) => {
-            let violations = results.violations;
-
-            if (violations.length > 0) {
-                showAccessibilityViolations(violations, logFn);
-            }
-        });
     });
 });
 
-// Show axe accessibility violations on the terminal
-function showAccessibilityViolations(violations, logFn) {
-    let location = window.location.href;
-
-    // make a set of unique violations; yes, I could have been clever
-    // and done it in one line, but the previous code confused me and
-    // I think this makes it clearer that we're populating a set
-    let reports = new Set();
-
-    violations.forEach((violation) => {
-        reports.add({
-            id: violation.id,
-            impact: violation.impact,
-            description: violation.description,
-            snippets: violation.nodes.map((node) => node.html),
-        });
+Cypress.Commands.add("OPGCheckA11y", () => {
+    cy.window({ log: false }).then((window) => {
+        cy.runAxe(window);
     });
-
-    logFn(`\n************** ACCESSIBILITY VIOLATIONS ON ${location}`);
-
-    reports.forEach((report) => {
-        logFn(`-------------- ID: ${report.id}`);
-        logFn(`Impact: ${report.impact}`);
-        logFn(`Description: ${report.description}`);
-        logFn('HTML elements causing violation:');
-        logFn('* ' + report.snippets.join('\n* '));
-    });
-}
+});
