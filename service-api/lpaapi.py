4f79fdda9a453fed428288290c2e5a3688d20aed
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-03-08 11:18:00 +0000

    remove unnnecessary 2cnd call to authenticate endpoint as we have the userId from the 1st

--- service-api/lpaapi.py
+++ service-api/lpaapi.py
@@ -3,46 +3,35 @@ import requests
 apiRoot = "http://localhost:7001"
 s = requests.Session()
 
-def getTokenByAuthenticating(username = "seeded_test_user@digital.justice.gov.uk", password = "Pass1234"):
-    # authenticate using the suppled creds, and get the resulting token
+def authenticate(username = "seeded_test_user@digital.justice.gov.uk", password = "Pass1234"):
+    # authenticate using the suppled creds, returning the resulting token and the userId
     credentials = {"username":username,"password":password}
     authPath = f'{apiRoot}/v2/authenticate'
     r = requests.get(authPath, data=credentials)
     token = r.json()['token']
     print(f'token : {token}')
-    return token
-
-def getIdOfAuthenticatedUser(token):
-    # given a token, get the ID of the related user
-    tokenData = {"authToken": token}
-    # note we have to call the authenticate endpoint a second time, with the token, to get the userId, which is a little odd
-    authPath = f'{apiRoot}/v2/authenticate'
-    r = s.get(authPath, data=tokenData)
     userId = r.json()['userId']
     print(f'userId : {userId}')
-    return userId
+    return token, userId
 
 def getUserDetails():
-    token = getTokenJsonByAuthenticating()
+    token, userId = getTokenJsonByAuthenticating()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     userPath = f'{apiRoot}/v2/user/{userId}'
     r = s.get(userPath, headers=tokenHdr)
     print(r.json())
 
 def getApplications():
-    token = getTokenByAuthenticating()
+    token, userId = authenticate()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     applicationPath = f'{apiRoot}/v2/user/{userId}/applications'
     r = s.get(applicationPath, headers=tokenHdr)
     print(r.json())
     return r.json()
 
 def makeNewLpa():
-    token = getTokenByAuthenticating()
+    token, userId = authenticate()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     applicationPath = f'{apiRoot}/v2/user/{userId}/applications'
     emptyData = []
     r = s.post(applicationPath, headers=tokenHdr, data=emptyData)
@@ -52,36 +41,32 @@ def makeNewLpa():
     return id
 
 def setLpaType(lpaId, lpaType = 'health-and-welfare'):
-    token = getTokenByAuthenticating()
+    token, userId = authenticate()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     lpatype = {"type": lpaType}
     typePath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/type'
     r = s.put(typePath, headers=tokenHdr, data=lpatype)
     print(r.json())
 
 def setDonor(lpaId):
-    token = getTokenByAuthenticating()
+    token, userId = authenticate()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     donorDetails = '{"name":{"title":"Mrs","first":"Nancy","last":"Garrison"},"otherNames":"","address":{"address1":"Bank End Farm House","address2":"Undercliff Drive","address3":"Ventnor, Isle of Wight","postcode":"PO38 1UL"},"dob":{"date":"1988-10-22T00:00:00.000000+0000"},"email":{"address":"opglpademo+NancyGarrison@gmail.com"},"canSign":false}'
     donorPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/donor'
     r = s.put(donorPath, headers=tokenHdr, data=donorDetails)
     print(r.json())
 
 def setPrimaryAttorneyDecisions(lpaId):
-    token = getTokenByAuthenticating()
+    token, userId = authenticate()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     primaryAttorneyDecisionPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/primary-attorney-decisions'
     primaryAttorneyDecisionDetails = '{"canSustainLife":true,"how":null,"when":null,"howDetails":null}'
     r = s.put(primaryAttorneyDecisionPath, headers=tokenHdr, data=primaryAttorneyDecisionDetails)
     print(r.json())
 
 def setPrimaryAttorney(lpaId):
-    token = getTokenByAuthenticating()
+    token, userId = authenticate()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     primaryAttorneyPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/primary-attorneys'
     primaryAttorneyDetails = {"name":{"title":"Mrs","first":"Amy","last":"Wheeler"},"dob":{"date":"1988-10-22T00:00:00.000000+0000"},"id":None,"address":{"address1":"Brickhill Cottage","address2":"Birch Cross","address3":"Marchington, Uttoxeter, Staffordshire","postcode":"ST14 8NX"},"email":{"address":"opglpademo+AmyWheeler@gmail.com"},"type":"human"}
     #primaryAttorneyDetails = {"name":{"title":"Mr","first":"David","last":"Wheeler"},"dob":{"date":"1972-03-12T00:00:00.000000+0000"},"id":None,"address":{"address1":"Brickhill Cottage","address2":"Birch Cross","address3":"Marchington, Uttoxeter, Staffordshire","postcode":"ST14 8NX"},"email":{"address":"opglpademo+DavidWheeler@gmail.com"},"type":"human"}
@@ -89,9 +74,8 @@ def setPrimaryAttorney(lpaId):
     print(r.json())
 
 def setReplacementAttorney(lpaId):
-    token = getTokenByAuthenticating()
+    token, userId = authenticate()
     tokenHdr = {"Token": token}
-    userId = getIdOfAuthenticatedUser(token)
     replacementAttorneyPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/replacement-attorneys'
     replacementAttorneyDetails = {"name":{"title":"Ms","first":"Isobel","last":"Ward"},"dob":{"date":"1937-02-01T00:00:00.000000+0000"},"id":None,"address":{"address1":"2 Westview","address2":"Staplehay","address3":"Trull, Taunton, Somerset","postcode":"TA3 7HF"},"email":None,"type":"human"}
     r = s.post(replacementAttorneyPath, headers=tokenHdr, json=replacementAttorneyDetails)
