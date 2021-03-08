f9023ba6d087c55b5aad7bf672522b970e254538
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-03-08 16:31:05 +0000

    rename

--- cypress/integration/common/fixtures.js
+++ cypress/integration/common/fixtures.js
@@ -1,5 +1,15 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
- 
+const {
+  Then, After,
+} = require("cypress-cucumber-preprocessor/steps");
+
+// Clean up lpa test fixture used by the scenario
+After({ tags: "@CleanupFixtures" }, () => {
+    cy.log("hit it");
+    cy.exec("python3 service-api/deleteLpa.py").its('stdout').then(response => {
+        cy.log(response);
+    });
+});
+
 Then(`I create PF LPA test fixture`, () => {
     // check wehther testFixtureLpaId is set, if not, create the fixture
     // TODO  use as, to set testFixtureLpaId here
@@ -9,12 +19,6 @@ Then(`I create PF LPA test fixture`, () => {
 })
  
 Then(`I create HW LPA test fixture`, () => {
-    cy.exec("python3 service-api/createHWLpa.py").its('stdout').then(lpaId => {
-        cy.log(lpaId);
-    });
-})
-
-Then(`I create untyped LPA test fixture`, () => {
     cy.exec("python3 service-api/createHWLpa.py").its('stdout').then(lpaId => {
         cy.log(lpaId);
     });
