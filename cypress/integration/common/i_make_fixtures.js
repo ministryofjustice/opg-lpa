5f636f71a2bf37eb47130dc6cbbd4e988d2746ce
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-03-08 14:14:19 +0000

    create lpa test fixtures

--- cypress/integration/common/i_make_fixtures.js
+++ cypress/integration/common/i_make_fixtures.js
@@ -1,5 +1,13 @@
 import { Then } from "cypress-cucumber-preprocessor/steps";
  
-Then(`I make fixtures`, () => {
-    cy.exec("python3 cypress/testFixtures.py");
+Then(`I create PF LPA test fixture`, () => {
+    cy.exec("python3 service-api/createPFLpa.py").its('stdout').then(lpaId => {
+        cy.log(lpaId);
+    });
+})
+ 
+Then(`I create HW LPA test fixture`, () => {
+    cy.exec("python3 service-api/createHWLpa.py").its('stdout').then(lpaId => {
+        cy.log(lpaId);
+    });
 })
