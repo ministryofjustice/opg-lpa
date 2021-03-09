88b17e3772942a4299313423ebba9eae8bc5de61
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-03-09 17:05:30 +0000

    needs requests in docker

--- service-api/testapi.py
+++
@@ -1,9 +0,0 @@
-from lpaapi import *
-
-lpaId = makeNewLpa()
-setLpaType(lpaId, 'property-and-financial')
-setDonor(lpaId)
-setPrimaryAttorneyDecisions(lpaId)
-setPrimaryAttorney(lpaId)
-setReplacementAttorney(lpaId)
-deleteLpa(lpaId)
