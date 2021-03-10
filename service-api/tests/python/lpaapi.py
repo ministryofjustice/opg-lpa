a77861e9656bd9e75bb1c725f002f465ceb67b96
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-03-10 10:33:06 +0000

    delete methods and cert provider added to python api

--- service-api/tests/python/lpaapi.py
+++ service-api/tests/python/lpaapi.py
@@ -48,7 +48,7 @@ def setPrimaryAttorneyDecisions(lpaId):
     r = s.put(primaryAttorneyDecisionPath, headers=token, data=primaryAttorneyDecisionDetails)
     print(r.json())
 
-def setPrimaryAttorney(lpaId):
+def addPrimaryAttorney(lpaId):
     token, userId = authenticate()
     primaryAttorneyPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/primary-attorneys'
     primaryAttorneyDetails = {"name":{"title":"Mrs","first":"Amy","last":"Wheeler"},"dob":{"date":"1988-10-22T00:00:00.000000+0000"},"id":None,"address":{"address1":"Brickhill Cottage","address2":"Birch Cross","address3":"Marchington, Uttoxeter, Staffordshire","postcode":"ST14 8NX"},"email":{"address":"opglpademo+AmyWheeler@gmail.com"},"type":"human"}
@@ -56,12 +56,32 @@ def setPrimaryAttorney(lpaId):
     r = s.post(primaryAttorneyPath, headers=token, json=primaryAttorneyDetails)
     print(r.json())
 
-def setReplacementAttorney(lpaId):
+def deletePrimaryAttorney(lpaId, index = 1):
+    token, userId = authenticate()
+    primaryAttorneyPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/primary-attorneys/{index}'
+    r = s.delete(primaryAttorneyPath, headers=token)
+    print(r)
+
+def addReplacementAttorney(lpaId):
     token, userId = authenticate()
     replacementAttorneyPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/replacement-attorneys'
     replacementAttorneyDetails = {"name":{"title":"Ms","first":"Isobel","last":"Ward"},"dob":{"date":"1937-02-01T00:00:00.000000+0000"},"id":None,"address":{"address1":"2 Westview","address2":"Staplehay","address3":"Trull, Taunton, Somerset","postcode":"TA3 7HF"},"email":None,"type":"human"}
     r = s.post(replacementAttorneyPath, headers=token, json=replacementAttorneyDetails)
+    print(r)
+    print(r.content)
+
+def deleteReplacementAttorney(lpaId, index = 1):
+    token, userId = authenticate()
+    replacementAttorneyPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/replacement-attorneys/{index}'
+    r = s.delete(replacementAttorneyPath, headers=token)
+    print(r)
+
+def setCertificateProvider(lpaId):
+    token, userId = authenticate()
+    certProviderPath = f'{apiRoot}/v2/user/{userId}/applications/{lpaId}/certificate-provider'
+    certProvider = {"name":{"title":"Mr","first":"Reece","last":"Richards"},"address":{"address1":"11 Brookside","address2":"Cholsey","address3":"Wallingford, Oxfordshire","postcode":"OX10 9NN"}}
+    r = s.put(certProviderPath, headers=token, json=certProvider)
     #import pdb; pdb.set_trace()
     print(r)
     print(r.content)
-    #print(r.json())
+
