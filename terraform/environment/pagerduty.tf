6d29df12b1551ccb22a6a975074cdc90c69faf84
william Falconer <william.falconer@digital.justice.gov,uk>
2021-03-12 14:39:29 +0000

    further sns refactor

--- terraform/environment/pagerduty.tf
+++ terraform/environment/pagerduty.tf
@@ -21,19 +21,3 @@ resource "pagerduty_service_integration" "cloudwatch_integration_ops" {
   service = data.pagerduty_service.pagerduty_ops.id
   vendor  = data.pagerduty_vendor.cloudwatch.id
 }
-
-
-resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription" {
-  topic_arn              = aws_sns_topic.cloudwatch_to_pagerduty.arn
-  protocol               = "https"
-  endpoint_auto_confirms = true
-  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration.integration_key}/enqueue"
-}
-
-
-resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription_ops" {
-  topic_arn              = aws_sns_topic.cloudwatch_to_pagerduty_ops.arn
-  protocol               = "https"
-  endpoint_auto_confirms = true
-  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration_ops.integration_key}/enqueue"
-}
