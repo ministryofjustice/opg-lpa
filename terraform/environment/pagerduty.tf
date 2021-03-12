62260fd382aef50139405dffba137d9081dfae18
william Falconer <william.falconer@digital.justice.gov,uk>
2021-03-12 14:23:33 +0000

    non front facing alarms use ops pagerduty. also fixing minor metric issues

--- terraform/environment/pagerduty.tf
+++ terraform/environment/pagerduty.tf
@@ -2,6 +2,10 @@ data "pagerduty_service" "pagerduty" {
   name = local.account.pagerduty_service_name
 }
 
+data "pagerduty_service" "pagerduty_ops" {
+  name = local.pager_duty_ops_service_name
+}
+
 data "pagerduty_vendor" "cloudwatch" {
   name = "Cloudwatch"
 }
@@ -12,8 +16,19 @@ resource "pagerduty_service_integration" "cloudwatch_integration" {
   vendor  = data.pagerduty_vendor.cloudwatch.id
 }
 
+resource "pagerduty_service_integration" "cloudwatch_integration_ops" {
+  name    = "${data.pagerduty_vendor.cloudwatch.name} ${local.environment} Environment Ops"
+  service = data.pagerduty_service.pagerduty_ops.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
 resource "aws_sns_topic" "cloudwatch_to_pagerduty" {
   name = "CloudWatch-to-PagerDuty-${local.environment}"
+}
+
+
+resource "aws_sns_topic" "cloudwatch_to_pagerduty_ops" {
+  name = "CloudWatch-to-PagerDuty-${local.environment}-Ops"
 }
 
 resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription" {
@@ -21,4 +36,12 @@ resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription" {
   protocol               = "https"
   endpoint_auto_confirms = true
   endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration.integration_key}/enqueue"
+}
+
+
+resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription_ops" {
+  topic_arn              = aws_sns_topic.cloudwatch_to_pagerduty_ops.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration_ops.integration_key}/enqueue"
 }
