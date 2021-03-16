0055a2ac0cd32a8a7b433c04cfd920188f456e59
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-03-16 10:16:11 +0000

    LPAL-77 rework some cloudwatch alarms in environment. (#351)
    * non front facing alarms use ops pagerduty. also fixing minor metric issues
    
    * refactor sns into own file and tag
    
    * further sns refactor
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>
    Co-authored-by: Raffers <Chris.rafferty@digital.justice.gov.uk>

---
+++ terraform/environment/sns.tf
@@ -0,0 +1,23 @@
+resource "aws_sns_topic" "cloudwatch_to_pagerduty" {
+  name = "CloudWatch-to-PagerDuty-${local.environment}"
+  tags = merge(local.default_tags, local.shared_component_tag)
+}
+
+resource "aws_sns_topic" "cloudwatch_to_pagerduty_ops" {
+  name = "CloudWatch-to-PagerDuty-${local.environment}-Ops"
+  tags = merge(local.default_tags, local.shared_component_tag)
+}
+
+resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription" {
+  topic_arn              = aws_sns_topic.cloudwatch_to_pagerduty.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration.integration_key}/enqueue"
+}
+
+resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription_ops" {
+  topic_arn              = aws_sns_topic.cloudwatch_to_pagerduty_ops.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration_ops.integration_key}/enqueue"
+}
