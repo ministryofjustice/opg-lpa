40d58b8480d56a5e7edb7cd7d1d58fa67d8d87d7
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-03-16 15:46:27 +0000

    Attorneys now fully in seperate feature file and missing from Create**LPA.feature.  python api tidied and simplified

--- tests/python-api-client/createLpa.py
+++ tests/python-api-client/createLpa.py
@@ -7,6 +7,12 @@ parser.add_argument('-hw', action='store_true',
 parser.add_argument('-d', action='store_true',
                     default=False,
                     help='Create Donor')
+parser.add_argument('-a1', action='store_true',
+                    default=False,
+                    help='Add 1 attorney')
+parser.add_argument('-a2', action='store_true',
+                    default=False,
+                    help='Add 2 attorneys')
 args = parser.parse_args()
 
 if args.hw :
@@ -15,8 +21,13 @@ else:
     lpaType = 'property-and-financial'
 
 lpaId = makeNewLpa()
+print(lpaId)
 setLpaType(lpaId, lpaType)
 if args.d :
     setDonor(lpaId)
 setPrimaryAttorneyDecisions(lpaId)
-print(lpaId)
+if args.a1 :
+    addPrimaryAttorney(lpaId)
+if args.a2 :
+    addPrimaryAttorney(lpaId)
+    addSecondPrimaryAttorney(lpaId)
