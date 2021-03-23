860457bbfb9fb749848c376e373e419b390a7217
william Falconer <william.falconer@digital.justice.gov,uk>
2021-03-23 13:33:10 +0000

    comment on script as deprecated

--- scripts/run_outdated_scripts/update_outdated.sh
+++ scripts/run_outdated_scripts/update_outdated.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -euo pipefail
-
+# note this is now superseded by a github action that auto runs.
 cd ../..
 cd tests
 composer update --prefer-dist --no-suggest --no-interaction --no-scripts --optimize-autoloader --ignore-platform-reqs
