bb41a6f7e17e6ee027557a3d994f0dd6b2605c8c
william Falconer <william.falconer@digital.justice.gov,uk>
2021-03-24 12:18:12 +0000

    ad SG rule for testing

--- terraform/account_ingress/security_groups_ingress_rule.tf
+++ terraform/account_ingress/security_groups_ingress_rule.tf
@@ -40,3 +40,18 @@ resource "aws_security_group_rule" "front_ci_ingress" {
   security_group_id = data.aws_security_group.front_loadbalancer.id
   description       = "ci_ingress"
 }
+
+data "aws_security_group" "api_ecs_service" {
+  name = "${local.environment}-api-ecs-service"
+}
+
+resource "aws_security_group_rule" "api_ci_ingress" {
+  count             = local.account.allow_ingress_modification ? 1 : 0
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [local.local_ip_cidr]
+  security_group_id = data.aws_security_group.api_ecs_service.id
+  description       = "ci_ingress"
+}
