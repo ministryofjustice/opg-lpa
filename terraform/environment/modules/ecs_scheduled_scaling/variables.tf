8fcdd6c084e0cc6aa75816b88c37242f2fd707e8
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-03-26 13:11:56 +0000

    Lpal 314 dev scale down ooh (#393)
    * add module
    
    * add ecs autoscale control
    
    * stupid fmt
    
    * faulty metric filter
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

---
+++ terraform/environment/modules/ecs_scheduled_scaling/variables.tf
@@ -0,0 +1,27 @@
+variable "ecs_cluster_name" {
+  description = "Name of the ECS Cluster"
+  type        = string
+}
+
+variable "name" {
+  description = "Schedule name if running multiple schedules"
+  type        = string
+  default     = "hibernation"
+}
+
+variable "scale_down_time" {
+  description = "Cron formatted value for scale down trigger"
+}
+
+variable "scale_up_time" {
+  description = "Cron formatted value for scale up trigger"
+}
+
+variable "service_config" {
+  description = "Map of services and task scale down to and up to when regually scaled."
+  type = map(
+    object({
+      scale_down_to = number
+      scale_up_to   = number
+  }))
+}
