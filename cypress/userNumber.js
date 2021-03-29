198c3f3c3c9e1b21d4b274f9a03687a72d1c814e
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-03-29 10:37:37 +0100

    Revert changes to userNumber.js
    Old version of this file was clearer, so revert changes.

--- cypress/userNumber.js
+++ cypress/userNumber.js
@@ -1,7 +1,11 @@
-// This makes a user number for tests.
-// It needs to happen before startup rather than in support/index.js so
-// that we have the same user number for all feature files under CI.
-// This code is therefore called from start.sh.
-// For safety, for now we generate userNumber using
-// the exact same js code that the Casper tests did.
-console.log("" + (new Date()).getTime() + Math.floor(Math.random() * 999999999));
+// This makes a user number which will be used for all tests
+// it needs to happen before startup rather than in support/index.js so
+// that we have the same user number for all feature files
+// This code is therefore called from start.sh.  
+// For safety, for now we generate userNumber using 
+// the exact same js code that the Casper tests did
+var random = Math.floor(Math.random() * 999999999);
+
+var date = new Date();
+var userNumber = date.getTime() + "" + random;
+console.log(userNumber);
