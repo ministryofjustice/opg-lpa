885ac45172ee4b28061b7f46bc5e0703eb5a66eb
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-03-31 10:33:37 +0100

    Simplify dynamodb adapter
    This component references an interface in the laminas-cache
    library, then provides empty implementations of most of the
    methods.
    
    As it is never used in a situation which requires an implementation
    of that interface, remove the dependency on the interface.

--- service-front/module/Application/src/Adapter/DynamoDbKeyValueStore.php
+++ service-front/module/Application/src/Adapter/DynamoDbKeyValueStore.php
@@ -4,13 +4,12 @@ namespace Application\Adapter;
 use Exception;
 
 use RuntimeException;
-use Laminas\Cache\Storage\StorageInterface;
 use Aws\DynamoDb\DynamoDbClient;
 
 /**
  * An adapter to use DynamoDB as a simple key/value store
  */
-class DynamoDbKeyValueStore implements StorageInterface
+class DynamoDbKeyValueStore
 {
 
     /**
@@ -130,174 +129,6 @@ class DynamoDbKeyValueStore implements StorageInterface
         $success = false;
 
         return null;
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::addItem()
-     */
-    public function addItem($key, $value)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::addItems()
-     */
-    public function addItems(array $keyValuePairs)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::checkAndSetItem()
-     */
-    public function checkAndSetItem($token, $key, $value)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::decrementItem()
-     */
-    public function decrementItem($key, $value)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::decrementItems()
-     */
-    public function decrementItems(array $keyValuePairs)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::getCapabilities()
-     */
-    public function getCapabilities()
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::getItems()
-     */
-    public function getItems(array $keys)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::getMetadata()
-     */
-    public function getMetadata($key)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::getMetadatas()
-     */
-    public function getMetadatas(array $keys)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::getOptions()
-     */
-    public function getOptions()
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::hasItem()
-     */
-    public function hasItem($key)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::hasItems()
-     */
-    public function hasItems(array $keys)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::incrementItem()
-     */
-    public function incrementItem($key, $value)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::incrementItems()
-     */
-    public function incrementItems(array $keyValuePairs)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::removeItems()
-     */
-    public function removeItems(array $keys)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::replaceItem()
-     */
-    public function replaceItem($key, $value)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::replaceItems()
-     */
-    public function replaceItems(array $keyValuePairs)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::setItems()
-     */
-    public function setItems(array $keyValuePairs)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::setOptions()
-     */
-    public function setOptions($options)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::touchItem()
-     */
-    public function touchItem($key)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
-    }
-
-     /* (non-PHPdoc)
-     * @see \Laminas\Cache\Storage\StorageInterface::touchItems()
-     */
-    public function touchItems(array $keys)
-    {
-        throw new RuntimeException('The ' . __FUNCTION__ . ' method has not been implemented.');
     }
 
     /**
