b2614592e6b56e91325d4c2997a785af7d7ecad2
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-04-01 16:42:36 +0100

    Urgent rework autoscale to prevent  object not found errors (#405)
    * rework autoscale to prevent  object not found errors
    
    * add refresh step to check status of dangling resources.
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

--- terraform/environment/autoscaling.tf
+++ terraform/environment/autoscaling.tf
@@ -30,3 +30,14 @@ module "pdf_ecs_autoscaling" {
   ecs_task_autoscaling_maximum     = local.account.autoscaling.pdf.maximum
   tags                             = merge(local.default_tags, local.pdf_component_tag)
 }
+
+module "admin_ecs_autoscaling" {
+  source                           = "./modules/ecs_autoscaling"
+  environment                      = local.environment
+  aws_ecs_cluster_name             = aws_ecs_cluster.online-lpa.name
+  aws_ecs_service_name             = aws_ecs_service.admin.name
+  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+  ecs_task_autoscaling_minimum     = 1
+  ecs_task_autoscaling_maximum     = 1
+  tags                             = merge(local.default_tags, local.pdf_component_tag)
+}
