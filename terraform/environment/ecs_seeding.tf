2fb1ad16c1d02ffd13e70b5c2dc6528827898616
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-04-08 11:06:09 +0100

    Lpal 125 reconfigure application logs + minor additional tagging and cleanup. (#404)
    * remove commented alarm
    
    * create new log group
    
    * rewire the application logs on containers
    
    * change log groups on ecs container
    
    * fix retention references
    
    * use correct logs and f' strings
    
    * add shared component tag
    
    * add propagate tags
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

--- terraform/environment/ecs_seeding.tf
+++ terraform/environment/ecs_seeding.tf
@@ -73,7 +73,7 @@ locals {
     "logConfiguration": {
         "logDriver": "awslogs",
         "options": {
-            "awslogs-group": "${data.aws_cloudwatch_log_group.online-lpa.name}",
+            "awslogs-group": "${aws_cloudwatch_log_group.application_logs.name}",
             "awslogs-region": "eu-west-1",
             "awslogs-stream-prefix": "${local.environment}.seeding.online-lpa"
         }
