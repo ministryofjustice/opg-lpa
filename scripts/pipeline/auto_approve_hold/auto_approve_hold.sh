21f1759239c03f740af2f434c418146d6f585c4f
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-12 17:03:17 +0100

    hook circle token up

--- scripts/pipeline/auto_approve_hold/auto_approve_hold.sh
+++ scripts/pipeline/auto_approve_hold/auto_approve_hold.sh
@@ -3,12 +3,13 @@ set -euo pipefail
 
 ID="$1"
 HOLD_JOB_NAME="$2"
+API_KEY="$3"
 echo "ID: ${ID}"
 echo "HOLD_JOB_NAME: ${HOLD_JOB_NAME}"
-url="https://circleci.com/api/v2/workflow/${ID}/job"
+url="https://circleci.com/api/v2/workflow/${ID}/job?circle-token=${API_KEY}"
 
 # Get workflow details
-workflow=$(curl -s -u "${CIRCLE_TOKEN}": -X GET --header "Content-Type: application/json" "$url")
+workflow=$(curl -s -X GET --header "Content-Type: application/json" "$url")
 
 # Get approval job id
 job_id=$(echo "${workflow}" | jq -r '.items[] | select(.name=="${HOLD_JOB_NAME}") | .approval_request_id ')
@@ -18,6 +19,5 @@ echo "allowing approval. Job ID: ${job_id}"
 # Approve
 curl \
   --header "Content-Type: application/json" \
-  -u "${CIRCLE_TOKEN}:" \
   -X POST \
-  "https://circleci.com/api/v2/workflow/${ID}/approve/${job_id}"
+  "https://circleci.com/api/v2/workflow/${ID}/approve/${job_id}?circle-token=${API_KEY}"
