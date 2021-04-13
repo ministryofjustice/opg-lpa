ea104f9987f6632e4b009762df356be4fe673eb0
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-13 11:08:20 +0100

    updated with fixes and a pagination loop

--- scripts/pipeline/auto_approve_hold/auto_approve_hold.sh
+++ scripts/pipeline/auto_approve_hold/auto_approve_hold.sh
@@ -4,22 +4,56 @@ set -euo pipefail
 ID="$1"
 HOLD_JOB_NAME="$2"
 API_KEY="$3"
+
+NEXT_PAGE_TOKEN=
+WORKFLOW=
+APPROVAL_ID=
+
 echo "ID: ${ID}"
 echo "HOLD_JOB_NAME: ${HOLD_JOB_NAME}"
-url="https://circleci.com/api/v2/workflow/${ID}/job?circle-token=${API_KEY}"
 
-# Get workflow details
-workflow=$(curl -s -X GET --header "Content-Type: application/json" "$url")
+function get_workflow_approval_by_name(){
+    # Get workflow details
 
-echo "$workflow"
+    urlbase="https://circleci.com/api/v2/workflow/${ID}/job?circle-token=${API_KEY}"
+    url=urlbase
+    if [[ -n "$1" ]]
+    then
+        echo "looking at next page in workflow..."
+        url="${url}&page_token=$1"
+    fi
 
-# Get approval job id
-job_id=$(echo "${workflow}" | jq -r '.items[] | select(.name=="${HOLD_JOB_NAME}") | .approval_request_id ')
+    WORKFLOW=$(curl -s -X GET --header "Content-Type: application/json" "$url")
+    $NEXT_PAGE_TOKEN=(echo ${WORKFLOW} | jq -r '.next_page_token')
 
-echo "allowing approval. Job ID: ${job_id}"
+    # Get approval job id
+    APPROVAL_ID=$(echo "${WORKFLOW}" | jq -r --arg HOLD_JOB_NAME "${HOLD_JOB_NAME}"  '.items[] | select(.name==$HOLD_JOB_NAME) | .approval_request_id')
+}
 
-# Approve
-curl \
-  --header "Content-Type: application/json" \
-  -X POST \
-  "https://circleci.com/api/v2/workflow/${ID}/approve/${job_id}?circle-token=${API_KEY}"
+function approve_job(){
+    echo "allowing approval ID: ${APPROVAL_ID}"
+
+    # Approve
+    curl \
+    --header "Content-Type: application/json" \
+    -X POST \
+    "https://circleci.com/api/v2/workflow/${ID}/approve/${APPROVAL_ID}?circle-token=${API_KEY}"
+}
+
+until  [[ "${NEXT_PAGE_TOKEN}" == "null" ]]
+do
+    get_workflow_approval_by_name "${NEXT_PAGE_TOKEN}"
+    if [[ -n "${APPROVAL_ID}" ]];
+    then
+        echo "found approval ID: ${APPROVAL_ID}"
+        break
+    fi
+done
+
+if [[ -n "${APPROVAL_ID}" ]]
+then
+    approve_job
+else
+    echo approval step not found for ${HOLD_JOB_NAME}
+    exit 1
+fi
