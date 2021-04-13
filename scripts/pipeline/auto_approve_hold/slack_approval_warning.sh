422278ece16768cbf8fd09f0ea0f612821d97872
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-13 13:24:16 +0100

    invalid payload

--- scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
+++ scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
@@ -4,7 +4,7 @@ WEBHOOK=$1
 echo "notifying slack..."
 curl -X POST -H 'Content-type application/json' ${WEBHOOK}  --data-binary @-  << EOF
 '{
-    attachments :
+    "attachments" :
     [
         "pretext" : ":warning-sign: Approval Needed!",
         "fallback": "Pipeline for ${CIRCLE_PULL_REQUEST} needs approval, as some infra may be destroyed / recreated. go to ${CIRCLE_BUILD_URL}",
