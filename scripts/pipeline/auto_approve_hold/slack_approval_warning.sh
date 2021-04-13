2e171bcc12f3a4304bdfc7dba2767b3c4486b48f
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-13 14:37:24 +0100

    fixed slack build

--- scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
+++ scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env sh
 set -euox pipefail
 WEBHOOK=$1
+PR=$2
+STEP=$3
+BUILD_URL=$4
+
 echo "notifying slack..."
-curl -v POST -H 'Content-type application/json' ${WEBHOOK}  --data-binary @- << EOF
-"{
-    \"attachments\" :[
+
+generate_payload()
+{
+    cat <<EOF
+{
+    "attachments" :
+    [
         {
-            \"blocks\" : [],
-            \"title\" : \":sign-warning: Approval Needed!\",
-            \"text\": \"Pipeline for ${CIRCLE_PULL_REQUEST} needs approval, as some infra may be destroyed / recreated. go to ${CIRCLE_BUILD_URL}\",
-            \"color\":\"#ffff00\"
+            "blocks" : [],
+            "title" : ":sign-warning: Approval Needed!",
+            "text": "Pipeline for $PR needs a manual approval, as some infra may be destroyed / recreated.\n Please check build step $STEP on $BUILD_URL to confirm this is what was intended and approve if ok.",
+            "color": "#ffff00",
+            "emoji": true
         }
     ]
-}"
+}
 EOF
+}
+echo "$(generate_payload)"
+
+curl -0 -v POST  ${WEBHOOK}  \
+-H "Expect:" \
+-H "Content-Type: application/json; charset=utf-8" \
+--data "$(generate_payload)"
