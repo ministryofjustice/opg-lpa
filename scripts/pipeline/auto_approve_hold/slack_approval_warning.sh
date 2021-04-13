4dafe3a1cee3c8d992f208d09f3f2af8534c0cce
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-13 13:55:11 +0100

    escape quotes

--- scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
+++ scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
@@ -2,14 +2,14 @@
 set -euox pipefail
 WEBHOOK=$1
 echo "notifying slack..."
-curl -v -X POST -H 'Content-type application/json' ${WEBHOOK}  --data-binary @- << EOF
+curl -v POST -H 'Content-type application/json' ${WEBHOOK}  --data-binary @- << EOF
 "{
-    "attachments" :[
+    \"attachments\" :[
         {
-            "blocks" : [],
-            "title" : ":sign-warning: Approval Needed!",
-            "text": "Pipeline for ${CIRCLE_PULL_REQUEST} needs approval, as some infra may be destroyed / recreated. go to ${CIRCLE_BUILD_URL}",
-            "color":"#ffff00"
+            \"blocks\" : [],
+            \"title\" : \":sign-warning: Approval Needed!\",
+            \"text\": \"Pipeline for ${CIRCLE_PULL_REQUEST} needs approval, as some infra may be destroyed / recreated. go to ${CIRCLE_BUILD_URL}\",
+            \"color\":\"#ffff00\"
         }
     ]
 }"
