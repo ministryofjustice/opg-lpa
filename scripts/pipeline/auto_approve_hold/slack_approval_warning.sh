0ab019d92145afdbe2bdb6c9e72a20768e4f2c86
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-13 13:49:54 +0100

    verbose curl output

--- scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
+++ scripts/pipeline/auto_approve_hold/slack_approval_warning.sh
@@ -2,15 +2,15 @@
 set -euox pipefail
 WEBHOOK=$1
 echo "notifying slack..."
-curl -X POST -H 'Content-type application/json' ${WEBHOOK}  --data-binary @-  << EOF
-'{
+curl -v -X POST -H 'Content-type application/json' ${WEBHOOK}  --data-binary @- << EOF
+"{
     "attachments" :[
         {
-            blocks : [],
-            "pretext" : ":warning-sign: Approval Needed!",
-            "fallback": "Pipeline for ${CIRCLE_PULL_REQUEST} needs approval, as some infra may be destroyed / recreated. go to ${CIRCLE_BUILD_URL}",
-            "color":"ffff00"
+            "blocks" : [],
+            "title" : ":sign-warning: Approval Needed!",
+            "text": "Pipeline for ${CIRCLE_PULL_REQUEST} needs approval, as some infra may be destroyed / recreated. go to ${CIRCLE_BUILD_URL}",
+            "color":"#ffff00"
         }
     ]
-}'
+}"
 EOF
