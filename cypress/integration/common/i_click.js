fe4e686d73c7de6e0b5935c9779734cd131f380a
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-04-20 17:12:18 +0100

    remove even more dup a11y checks

--- cypress/integration/common/i_click.js
+++ cypress/integration/common/i_click.js
@@ -2,22 +2,30 @@ import { Then } from "cypress-cucumber-preprocessor/steps";
  
 Then(`I click {string}`, (clickable) => {
     cy.get("[data-cy=" + clickable + "]").click();
-    cy.OPGCheckA11y();
+    cy.url().then(urlStr => {
+        cy.OPGCheckA11yWithUrl(urlStr);
+    })
 })
 
 Then(`I click occurrence {int} of {string}`, (number, clickable) => {
     cy.get("[data-cy=" + clickable + "]").eq(number).click();
-    cy.OPGCheckA11y();
+    cy.url().then(urlStr => {
+        cy.OPGCheckA11yWithUrl(urlStr);
+    })
 })
 
 Then(`I force click {string}`, (clickable) => {
     cy.get("[data-cy=" + clickable + "]").click({ force: true });
-    cy.OPGCheckA11y();
+    cy.url().then(urlStr => {
+        cy.OPGCheckA11yWithUrl(urlStr);
+    })
 })
 
 Then(`I click element marked {string}`, (text) => {
     cy.contains(text).click();
-    cy.OPGCheckA11y();
+    cy.url().then(urlStr => {
+        cy.OPGCheckA11yWithUrl(urlStr);
+    })
 })
 
 // this step exists because newly signed-up user goes straight to type page whereas existing user may get taken to dashboard
