abd447e1e3c8eb2d169f036250c842565b821992
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-04-22 15:44:25 +0100

    properly fix deleteApi not running in CI

--- cypress/integration/common/i_click.js
+++ cypress/integration/common/i_click.js
@@ -2,30 +2,22 @@ import { Then } from "cypress-cucumber-preprocessor/steps";
  
 Then(`I click {string}`, (clickable) => {
     cy.get("[data-cy=" + clickable + "]").click();
-    cy.url().then(urlStr => {
-        cy.OPGCheckA11yWithUrl(urlStr);
-    })
+    cy.OPGCheckA11y();
 })
 
 Then(`I click occurrence {int} of {string}`, (number, clickable) => {
     cy.get("[data-cy=" + clickable + "]").eq(number).click();
-    cy.url().then(urlStr => {
-        cy.OPGCheckA11yWithUrl(urlStr);
-    })
+    cy.OPGCheckA11y();
 })
 
 Then(`I force click {string}`, (clickable) => {
     cy.get("[data-cy=" + clickable + "]").click({ force: true });
-    cy.url().then(urlStr => {
-        cy.OPGCheckA11yWithUrl(urlStr);
-    })
+    cy.OPGCheckA11y();
 })
 
 Then(`I click element marked {string}`, (text) => {
     cy.contains(text).click();
-    cy.url().then(urlStr => {
-        cy.OPGCheckA11yWithUrl(urlStr);
-    })
+    cy.OPGCheckA11y();
 })
 
 // this step exists because newly signed-up user goes straight to type page whereas existing user may get taken to dashboard
