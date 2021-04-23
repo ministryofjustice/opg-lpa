a2a211e6618d9ddba5b7131c17097a660a85f2cd
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-23 16:52:11 +0100

    Lpal 352 sanitize commit message for new slack orb (#429)
    * Multiline
    variable substitution
    commit
    
    * add markdown to footer
    
    * cleanup of files
    
    * missed reworked variable
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov.uk>

--- scripts/pipeline/set_environment_variables/set_slack_env_vars.sh
+++ scripts/pipeline/set_environment_variables/set_slack_env_vars.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+
 echo 'export FRONT_DOMAIN="$(jq -r .front_fqdn /tmp/environment_pipeline_tasks_config.json)"'
 echo 'export ADMIN_DOMAIN="$(jq -r .admin_fqdn /tmp/environment_pipeline_tasks_config.json)"'
 echo 'export PUBLIC_FACING_DOMAIN="$(jq -r .public_facing_fqdn /tmp/environment_pipeline_tasks_config.json)"'
