ef197b0efb391ec6e4dda452c0050dc54c05e26c
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-04-23 09:58:43 +0100

    Lpal 352 sanitize commit message for new slack orb (#429)
    * Multiline
    variable substitution
    commit
    
    * add markdown to footer
    
    * cleanup of files
    
    * missed reworked variable
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov.uk>

--- scripts/pipeline/set_environment_variables/set_slack_post_environment_domains.sh
+++ scripts/pipeline/set_environment_variables/set_slack_post_environment_domains.sh
@@ -1,63 +1,68 @@
 #! /bin/bash
+    # needed as circleci did not santize the input for json properly.
+    SANITISED_COMMIT_MESSAGE=$(echo "${COMMIT_MESSAGE//$'\n'/\\\n}"  | sed 's/"/\\""/g')
+
 generate_post_environment_domains()
 {
+
+
+    # needed as circleci did not santize the input for json properly.
     cat <<EOF
 {
     "blocks": [],
-    "attachments": [
-    {
-        "color": "#508c18",
-        "blocks":
-        [
-            {
-                "type": "header",
-                "text": {
-                "type": "plain_text",
-                "text": "Online LPA Development Environment Ready",
-                "emoji": false
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                "type": "mrkdwn",
-                "text": "public facing url: <https://${PUBLIC_FACING_DOMAIN}/home>"
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                "type": "mrkdwn",
-                "text": "front url: <https://${FRONT_DOMAIN}>"
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                "type": "mrkdwn",
-                "text": "admin url: <https://${ADMIN_DOMAIN}>"
-                }
-            },
-            {
-                "type": "context",
-                "elements":
-                [
-                    {
-                        "type": "plain_text",
-                        "text": "by user: ${CIRCLE_USERNAME} - branch: ${CIRCLE_BRANCH} - Commit Message: ${COMMIT_MESSAGE}"
+    "attachments":
+    [
+        {
+            "color": "#508c18",
+            "blocks":
+            [
+                {
+                    "type": "header",
+                    "text": {
+                    "type": "plain_text",
+                    "text": "Online LPA Development Environment Ready",
+                    "emoji": false
                     }
-                ]
-            }
-        ]
-    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                    "type": "mrkdwn",
+                    "text": "public facing url: <https://${PUBLIC_FACING_DOMAIN}/home>"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                    "type": "mrkdwn",
+                    "text": "front url: <https://${FRONT_DOMAIN}>"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                    "type": "mrkdwn",
+                    "text": "admin url: <https://${ADMIN_DOMAIN}>"
+                    }
+                },
+                {
+                    "type": "context",
+                    "elements":
+                    [
+                        {
+                            "type": "mrkdwn",
+                            "text": "by user: ${CIRCLE_USERNAME} - branch: ${CIRCLE_BRANCH} - Commit Message: ${SANITISED_COMMIT_MESSAGE}"
+                        }
+                    ]
+                }
+            ]
+        }
     ]
 }
 EOF
 }
 
 generate_post_environment_domains > /tmp/post_environment_domains.json
-
 echo message sent:
 cat /tmp/post_environment_domains.json
-
 echo 'export SLACK_POST_ENVIRONMENT_DOMAIN_TEMPLATE=$(cat /tmp/post_environment_domains.json)' >> $BASH_ENV
