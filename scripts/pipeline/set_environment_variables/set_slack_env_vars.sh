02307a9ba6748cf211c5442f4a2b1dbdea81e534
william Falconer <william.falconer@digital.justice.gov,uk>
2021-04-23 17:00:04 +0100

    restore file

--- scripts/pipeline/set_environment_variables/set_slack_env_vars.sh
+++ scripts/pipeline/set_environment_variables/set_slack_env_vars.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 echo 'export FRONT_DOMAIN="$(jq -r .front_fqdn /tmp/environment_pipeline_tasks_config.json)"'
 echo 'export ADMIN_DOMAIN="$(jq -r .admin_fqdn /tmp/environment_pipeline_tasks_config.json)"'
 echo 'export PUBLIC_FACING_DOMAIN="$(jq -r .public_facing_fqdn /tmp/environment_pipeline_tasks_config.json)"'
