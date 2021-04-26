815d02b341f18301395661496100a7faef1c2cec
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2021-04-26 15:52:58 +0100

    Corrected resource references

--- terraform/environment/ecs_front.tf
+++ terraform/environment/ecs_front.tf
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "front_ecs_service_elasticache_ingress" {
   from_port                = 0
   to_port                  = 6379
   protocol                 = "tcp"
-  security_group_id        = data.aws_security_group.redis_cache.id
+  security_group_id        = data.aws_security_group.redis_cache_service.id
   source_security_group_id = aws_security_group.front_ecs_service.id
 }
 
@@ -244,7 +244,7 @@ locals {
         { "name" : "OPG_LPA_COMMON_PDF_QUEUE_URL", "value" : aws_sqs_queue.pdf_fifo_queue.id },
         { "name" : "OPG_LPA_ENDPOINTS_API", "value" : "http://${local.api_service_fqdn}" },
         { "name" : "OPG_LPA_OS_PLACES_HUB_ENDPOINT", "value" : "https://api.os.uk/search/places/v1/postcode" },
-        { "name" : "OPG_LPA_COMMON_REDIS_CACHE_URL", "value" : data.aws_elasticache_cluster.redis_cache.cache_nodes.0.address}
+        { "name" : "OPG_LPA_COMMON_REDIS_CACHE_URL", "value" : "tls://${data.aws_elasticache_replication_group.redis_cache.primary_endpoint_address}" }
       ]
     }
   )
