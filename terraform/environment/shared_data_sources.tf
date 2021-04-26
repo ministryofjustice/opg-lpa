3698729f8a4a5a689ef78afcfe17c86aa66392a9
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2021-04-26 16:16:36 +0100

    Removed shared_data_sources and updated id to ids

--- terraform/environment/shared_data_sources.tf
+++
@@ -1,7 +0,0 @@
-data "aws_security_group" "redis_cache_service" {
-  name = "redis-cache-service"
-}
-
-data "aws_elasticache_replication_group" "redis_cache" {
-  replication_group_id = "redis-cache-replication-group"
-}
