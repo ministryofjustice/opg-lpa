4be6335561b14bec53269f6fc4597a719bdd1654
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-05-28 10:23:26 +0100

    LPAL-61 Inject container into SessionManager (#471)
    * Enable SessionManager unit test
    
    * Enable injection of session container into SessionManager
    
    * Rework SessionManager to pass custom storage
    
    Session appears to attempt to use Redis as the storage back-end
    for sessions by default. By passing a storage object to the
    SessionManager constructor instead, we are able to mock this out
    in the tests so that redis is not required.
    
    * Use file save_handler during unit tests
    
    * No need to pass storage into constructor
    
    By using ini_set() to configure storage during tests, we
    no longer need to pass the storage implementation to the
    constructor for SessionManager.

--- service-front/module/Application/src/Model/Service/Session/SessionManager.php
+++ service-front/module/Application/src/Model/Service/Session/SessionManager.php
@@ -4,20 +4,35 @@ namespace Application\Model\Service\Session;
 use Laminas\Session\Container;
 use Laminas\Session\SessionManager as LaminasSessionManager;
 
-class SessionManager extends LaminasSessionManager {
+class SessionManager extends LaminasSessionManager
+{
+    /**
+     * @var Container $container
+     */
+    private $container;
+
+    public function __construct(Container $container = null)
+    {
+        // parent constructor has to be called first, in case we have to
+        // make a container within this constructor
+        parent::__construct();
+
+        if (is_null($container)) {
+            $container = new Container('initialised', $this);
+        }
+
+        $this->container = $container;
+    }
 
     /**
      * Tracks whether we've seen this session before and does a regenerateId() if not.
      */
-    public function initialise(){
-
-        $container = new Container( 'initialised', $this );
-
+    public function initialise()
+    {
         // If it's a new session, regenerate the id.
-        if (!isset($container->init)) {
-            $this->regenerateId(true);
-            $container->init = true;
+        if (!isset($this->container->init)) {
+            $this->regenerateId(TRUE);
+            $this->container->init = TRUE;
         }
-
     }
-} // class
+}
