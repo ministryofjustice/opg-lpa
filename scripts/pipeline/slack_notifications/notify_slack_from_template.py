84ebe45e887422dd02b0d31c2b925697e551752f
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-06-04 11:26:30 +0100

    Lpal 352 slack python notification (#477)
    * add jinja templates
    
    * python script
    
    * python lint and Docustring
    
    * further doc fixes
    
    * consistent naming
    
    * delete old template shell scripts
    
    * config yaml updates
    
    * apk use correct libs, wording update
    
    * typo in orb script
    
    * python3?
    
    * mixing my echoes and exports...
    
    * bug fix and debug
    
    * further bugfix
    
    * more orb changes
    
    * try the echo thing again
    
    * typo
    
    * tyo 2
    
    * echoes and export problems
    
    * direct reference in step that needs it
    
    * /bin/sh: syntax error: unterminated quoted string
    
    * name auto approval
    
    * add replacement var for approval step
    
    * oops wrong commit?
    
    * cloudwatch commented out also...
    
    * remove requires
    
    * mod the inline orb , run post steps always
    
    * dev plan check no longer required, needs approval only
    
    * Revert "cloudwatch commented out also..."
    
    This reverts commit aea543eb48984ec7b25b07aec1b31edb74213e1c.
    
    * Revert "add replacement var for approval step"
    
    This reverts commit d0803376fb197be43d823862285cc135f7397c3e.
    
    * fix spurious $
    
    * put into dev channel
    
    * add default path_to_live for approval
    
    * remove warning shell script
    
    * oopsy - revert this!
    
    * Revert "oopsy - revert this!"
    
    This reverts commit 9ced3621b01078065844d4af84d3ae7f4c7886c2.
    
    * remove excess debug statements
    
    * pin admin to composer 2.0.14
    
    * half asleep! try again
    
    * revert as we'll raise separate PR
    
    * meaningful project level env vars for channels

---
+++ scripts/pipeline/slack_notifications/notify_slack_from_template.py
@@ -0,0 +1,113 @@
+
+import os
+import argparse
+import json
+import jinja2
+from jinja2.loaders import FileSystemLoader
+from slack_sdk import WebClient
+
+
+class TemplateRenderer:
+    """Template renderer class
+    """
+
+    def __init__(self, template_file, template_folder, replacement_vars):
+        """constructor
+
+        Args:
+            template_file (string): filename of the template to use
+            template_folder (string):
+            replacement_vars (dict(string, string)): a dictionary of replacement variables passed on the command line
+        """
+        self.template_file = template_file
+
+        self.template_environment = jinja2.Environment(
+            loader=FileSystemLoader(template_folder))
+
+        # pull in environment variables, and  any replacement variables to use, and merge.
+        self.template_vars = {**dict([(k, self.sanitize(v)) for k, v in os.environ.items()]),
+                              **{k: self.sanitize(v) for k, v in replacement_vars.items()}}
+
+    def sanitize(key, value):
+        """sanitize any strings passed from a dict to make it json compatible.
+
+        Args:
+            key (string): key of the item (unused, but needed for dicts)
+            value (string): value of the item
+
+        Returns:
+            string: a sanitized string useful for json
+        """
+        return json.dumps(value)[1:-1]
+
+    def render(self):
+        """retrieves a template and renders based on the list of variables avaialble
+
+        Returns:
+            string : the rendered json output
+        """
+        tmpl = self.template_environment.get_template(self.template_file)
+        return tmpl.render(**self.template_vars)
+
+
+class SlackNotifier:
+    """notifies slack based on the rendered output, slack token and channel
+    """
+
+    def __init__(self, slack_token, slack_channel):
+        """constructor
+
+        Args:
+            slack_token (string): the slack app token to use
+            slack_channel (string): the slack channel id to post to
+        """
+        self.slack_channel = slack_channel
+        self.slack_token = slack_token
+
+    def notify(self, message_json):
+        """Send the notification based on the rendered output
+
+        Args:
+            message_json (string): the message in block / attachment format to
+        """
+        client = WebClient(token=self.slack_token)
+        message = json.loads(message_json)
+        client.chat_postMessage(channel=self.slack_channel,
+                                attachments=message['attachments'],
+                                blocks=message['blocks'])
+
+
+# https://www.geeksforgeeks.org/python-key-value-pair-using-argparse/
+class keyvalue(argparse.Action):
+    # Constructor calling
+    def __call__(self, parser, namespace,
+                 values, option_string=None):
+        setattr(namespace, self.dest, dict())
+
+        for value in values:
+            # split it into key and value
+            key, value = value.split('=')
+            # assign into dictionary
+            getattr(namespace, self.dest)[key] = value
+
+
+def main():
+    parser = argparse.ArgumentParser(description="runs a slack notification")
+    parser.add_argument(
+        "--template_file", help="defines a template name to look up")
+    parser.add_argument("--template_folder", nargs="?",
+                        default="templates", help="folder for the templates")
+    parser.add_argument("--slack_token", help="slack api token to use")
+    parser.add_argument("--slack_channel", help="slack channel to post to")
+    parser.add_argument("--replacement_vars", help="additional replacement variables to pass in",
+                        nargs="*", action=keyvalue, default=dict())
+    args = parser.parse_args()
+
+    renderer = TemplateRenderer(
+        args.template_file, args.template_folder, args.replacement_vars)
+    notifier = SlackNotifier(args.slack_token, args.slack_channel)
+    notifier.notify(renderer.render())
+
+
+if __name__ == "__main__":
+    main()
