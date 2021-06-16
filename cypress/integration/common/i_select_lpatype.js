1c2ba2f946e549124ce4d1bc48907227ba0a459c
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-06-16 11:18:43 +0100

    use data-cys

--- cypress/integration/common/i_select_lpatype.js
+++ cypress/integration/common/i_select_lpatype.js
@@ -1,11 +1,9 @@
 import { Then } from "cypress-cucumber-preprocessor/steps";
 
 Then(`I choose Property and Finance`, () => {
-    //    ultimately this wants to use a data-cy tag, but that is not straightforward to add in the twig/php
-    cy.get('#type').check();
+    cy.get("[data-cy=type-property-and-financial]").check();
 })
 
 Then(`I choose Health and Welfare`, () => {
-    //    ultimately this wants to use a data-cy tag, but that is not straightforward to add in the twig/php
-    cy.get('#type-health-and-welfare').check();
+    cy.get("[data-cy=type-health-and-welfare]").check();
 })
