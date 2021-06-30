fed103f6a93da89ef091c8a437f3d1a1dd6ae1bb
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-06-30 17:02:17 +0100

    ensure button is not disabled when try to click

--- cypress/integration/common/i_select_lpatype.js
+++ cypress/integration/common/i_select_lpatype.js
@@ -1,9 +1,9 @@
 import { Then } from "cypress-cucumber-preprocessor/steps";
 
 Then(`I choose Property and Finance`, () => {
-    cy.get("[data-cy=type-property-and-financial]").check();
+    cy.get("[data-cy=type-property-and-financial]").should('not.be.disabled').check().should('be.checked');
 })
 
 Then(`I choose Health and Welfare`, () => {
-    cy.get("[data-cy=type-health-and-welfare]").check();
+    cy.get("[data-cy=type-health-and-welfare]").should('not.be.disabled').check().should('be.checked');
 })
