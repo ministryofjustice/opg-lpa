e2494cc92ac25f498032aa26e544127e84250561
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-07-05 09:49:51 +0100

    LPAL-385 Improve LPA application status caching (#505)
    * Fix broken test for LPA statuses
    
    On looking at this again, I realised the test was checking
    the wrong piece of data in the LPA detailed status page.
    
    Fix the data-cy attributes and the tests to ensure we are
    checking that the correct "tab" is highlighted at the top
    of the status page.
    
    * Fix incorrect status detail page test
    
    * Small cleanup of code
    
    Remove unnecessary conditional clauses, fix up comments etc.
    
    * Update idea config
    
    * Retrieve all statuses from db at once
    
    Rather than retrieve the status for each requested LPA one at
    a time, get them all at once.
    
    * Fix unit tests - no more individual retrieval of LPAs from db
    
    * If db call fails return empty Traversable
    
    * Add tests for new Applications\Service method
    
    * Remove separate db queries for LPA metadata
    
    As we're already retrieving the metadata in a query when
    the controller is first evoked, reuse that to populate the
    metadata field when updating an LPA in the db.
    
    Instead of N*2 SELECT queries and N UPDATE queries on each request
    (where N is the number of LPAs the user is viewing in their
    dashboard), we now have 1 SELECT + N UPDATEs.
    
    * Rework unit tests now fetches are no longer executed
    
    * Save metadata if status or any date changed
    
    * Revert "Update idea config"
    
    This reverts commit 797353d2032adcd8326e6746c787c36d93b487f4.
    
    PHPStorm config is machine-specific and probably shouldn't
    be in the repo at all. Revert my local changes.
    
    Co-authored-by: William Falconer <williamfalconeruk@users.noreply.github.com>

--- service-api/module/Application/tests/Model/Service/Applications/ServiceTest.php
+++ service-api/module/Application/tests/Model/Service/Applications/ServiceTest.php
@@ -9,6 +9,7 @@ use Application\Library\DateTime;
 use Application\Model\Service\Applications\Collection;
 use Application\Model\Service\DataModelEntity;
 use ApplicationTest\Model\Service\AbstractServiceTest;
+use \EmptyIterator;
 use Mockery\MockInterface;
 use Opg\Lpa\DataModel\Lpa\Document\Document;
 use Opg\Lpa\DataModel\Lpa\Formatter;
@@ -624,6 +625,46 @@ class ServiceTest extends AbstractServiceTest
         $this->assertEquals(0, $response->count());
     }
 
+    public function testFilterByIdsAndUser_ApplicationRepositoryReturnsEmptyIterator()
+    {
+        $lpaIds = ['1234', '5678'];
+        $userId = 'user1';
+
+        $this->applicationRepository->shouldReceive('getByIdsAndUser')
+            ->withArgs([$lpaIds, $userId])
+            ->andReturn([]);
+
+        $serviceBuilder = new ServiceBuilder();
+        $service = $serviceBuilder
+            ->withApplicationRepository($this->applicationRepository)
+            ->build();
+
+        $response = $service->filterByIdsAndUser($lpaIds, $userId);
+
+        $this->assertEquals([], $response);
+    }
+
+    public function testFilterByIdsAndUser_ApplicationRepositoryReturnsLpas()
+    {
+        $lpaIds = ['1234', '5678'];
+        $userId = 'user1';
+        $hwLpa = FixturesData::getHwLpa();
+        $pfLpa = FixturesData::getPfLpa();
+
+        $this->applicationRepository->shouldReceive('getByIdsAndUser')
+            ->withArgs([$lpaIds, $userId])
+            ->andReturn([$hwLpa->toArray(), $pfLpa->toArray()]);
+
+        $serviceBuilder = new ServiceBuilder();
+        $service = $serviceBuilder
+            ->withApplicationRepository($this->applicationRepository)
+            ->build();
+
+        $response = $service->filterByIdsAndUser($lpaIds, $userId);
+
+        $this->assertEquals([$hwLpa, $pfLpa], $response);
+    }
+
     /**
      * @param User $user
      * @param Lpa $lpa
@@ -638,7 +679,7 @@ class ServiceTest extends AbstractServiceTest
             ->withArgs([$lpa->getId()])
             ->andReturn($lpa->toArray());
     }
-    
+
     /**
      * @param User $user
      */
