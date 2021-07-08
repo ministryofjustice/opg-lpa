3678b9f60ecbadf18207c7079d10184cca74c6ec
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-07-08 11:16:04 +0100

    Fix whitespace (thanks to poor Sublime config)

--- cypress/integration/common/i_can_see.js
+++ cypress/integration/common/i_can_see.js
@@ -1,15 +1,15 @@
 import { Then } from "cypress-cucumber-preprocessor/steps";
 
 Then('I can see a "Reuse LPA details" link for the test fixture lpa', (linkText) => {
-	seeReuseDetailsLink(true);
+    seeReuseDetailsLink(true);
 });
 
 Then('I cannot see a "Reuse LPA details" link for the test fixture lpa', (linkText) => {
-	seeReuseDetailsLink(false);
+    seeReuseDetailsLink(false);
 });
 
 const seeReuseDetailsLink = (shouldExist) => {
-	cy.get('@lpaId').then((lpaId) => { 
+    cy.get('@lpaId').then((lpaId) => {
         const selector = 'a[href*="/user/dashboard/create/' + lpaId + '"]';
         const condition = (shouldExist ? 'exist' : 'not.exist');
         cy.get(selector).should(condition);
