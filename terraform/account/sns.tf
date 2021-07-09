8b5a787201aff6a91ae77af8adeae1c4a98c2da7
william Falconer <william.falconer@digital.justice.gov.uk>
2021-07-09 10:11:11 +0100

    sns topic encryption

--- terraform/account/sns.tf
+++ terraform/account/sns.tf
@@ -1,9 +1,11 @@
 resource "aws_sns_topic" "cloudwatch_to_slack_breakglass_alerts" {
-  name = "CloudWatch-to-PagerDuty-${local.account_name}-Breakglass-alert"
-  tags = merge(local.default_tags, local.shared_component_tag)
+  name              = "CloudWatch-to-PagerDuty-${local.account_name}-Breakglass-alert"
+  tags              = merge(local.default_tags, local.shared_component_tag)
+  kms_master_key_id = "alias/aws/sns"
 }
 
 resource "aws_sns_topic" "cloudwatch_to_slack_elasticache_alerts" {
-  name = "CloudWatch-to-PagerDuty-${local.account_name}-elasticache-alert"
-  tags = merge(local.default_tags, local.front_component_tag)
+  name              = "CloudWatch-to-PagerDuty-${local.account_name}-elasticache-alert"
+  tags              = merge(local.default_tags, local.front_component_tag)
+  kms_master_key_id = "alias/aws/sns"
 }
