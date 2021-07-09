6919512bf22a8b30feb3ff09571f883d9250b7d5
william Falconer <william.falconer@digital.justice.gov.uk>
2021-07-09 14:49:36 +0100

    Add key rotation to cache kms key

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -84,6 +84,7 @@ resource "aws_kms_key" "lpa_pdf_cache" {
   description             = "S3 bucket encryption key for lpa_pdf_cache"
   deletion_window_in_days = 7
   tags                    = merge(local.default_tags, local.pdf_component_tag)
+  enable_key_rotation     = true
 }
 
 resource "aws_kms_alias" "lpa_pdf_cache" {
