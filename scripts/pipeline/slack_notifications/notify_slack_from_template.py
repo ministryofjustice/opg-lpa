f1c27f2003f5ec6f87139fba5ac8c946e402d6ec
william Falconer <william.falconer@digital.justice.gov.uk>
2021-07-12 15:06:18 +0100

    remove debug

--- scripts/pipeline/slack_notifications/notify_slack_from_template.py
+++ scripts/pipeline/slack_notifications/notify_slack_from_template.py
@@ -82,7 +82,6 @@ class keyvalue(argparse.Action):
     def __call__(self, parser, namespace,
                  values, option_string=None):
         setattr(namespace, self.dest, dict())
-        print(f"values:{values}")
         for value in values:
             # split it into key and value
             key, value = value.split(':=')
