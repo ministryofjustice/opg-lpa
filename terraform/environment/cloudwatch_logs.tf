7b6cfdcdb52cfc803a32987b94ab82973bcb6c8d
william Falconer <william.falconer@digital.justice.gov.uk>
2021-07-13 12:21:10 +0100

    push a logs clone

--- terraform/environment/cloudwatch_logs.tf
+++ terraform/environment/cloudwatch_logs.tf
@@ -11,3 +11,17 @@ resource "aws_cloudwatch_log_group" "application_logs" {
     },
   )
 }
+
+resource "aws_cloudwatch_log_group" "application_logs_2" {
+  name              = "${local.environment}_application_logs_2"
+  retention_in_days = local.account.log_retention_in_days
+
+  tags = merge(
+    local.default_tags,
+    local.shared_component_tag,
+    {
+      "Name" = "${local.environment}_application_logs"
+    },
+  )
+}
+
