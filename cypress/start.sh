fbffc87480fb773a5412064322f86d8243df49c8
william Falconer <william.falconer@digital.justice.gov.uk>
2021-07-19 17:13:56 +0100

    browser chrome

--- cypress/start.sh
+++ cypress/start.sh
@@ -34,7 +34,7 @@ if [[ "$CYPRESS_CI" == "true" ]] || [[ "$CYPRESS_headless" == "true" ]] ; then
         ./node_modules/.bin/cypress-tags run -e TAGS='not @SignUp and not @CreateLpa and not @StitchedHW and not @StitchedPF and not @StitchedClone'
     else
         # CYPRESS_TAGS is set so we run those specific tests
-        ./node_modules/.bin/cypress-tags run -e TAGS="$CYPRESS_TAGS" --headed
+        ./node_modules/.bin/cypress-tags run -e TAGS="$CYPRESS_TAGS" --headed --browser chrome
     fi
 else
     echo "Running Cypress"
