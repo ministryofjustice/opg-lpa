97da7bfc327b0dffd4b55c7536e1a337e890dddc
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-07-21 10:09:47 +0100

    Also retry clicks on 'Error' in page title
    On some pages in the site, an error occurs which is only
    visible by the title of the page, which has "Error" preprepended.
    Looking at the logs indicates this is a csrf error.
    
    This is not picked up by the tests and causes failures. We have
    a work-around in the cypress tests to retry button presses
    on csrf errors, but this doesn't come into force for these errors.
    
    Extend the "I click..." function so that it will also retry on
    "Error" being displayed in the page title.

--- cypress/integration/common/i_click.js
+++ cypress/integration/common/i_click.js
@@ -4,12 +4,20 @@ import { Then } from "cypress-cucumber-preprocessor/steps";
 // been temporarily disabled while the page is loading. This may need ultimately to be done for more or even all steps here
  
 Then(`I click {string}`, (clickable) => {
-    // if this results in an Oops, then retry the click
-    // we currently believe this to be caused by the CSRF problem, which we intend to fix fully in future
-    // and this retry step will be able to be removed from the tests
-    cy.get("[data-cy=" + clickable + "]").should('not.be.disabled').click().document().then(docStr => {
-        if (docStr.documentElement.innerHTML.includes('Oops! Something went wrong with the information you entered.')) {
+    // If this results in an Oops, then retry the click.
+    // We currently believe this to be caused by the CSRF problem, which we intend to fix fully in future
+    // and this retry step will be able to be removed from the tests.
+    // Also note that CSRF errors can also occur without "Oops" showing on the page. Examples
+    // are when the CSRF token is incorrect when clicking "Save and continue" on the replacement
+    // attorneys page - the title says "Error" but there is no indication of what the error is
+    // in the page itself.
+    cy.get("[data-cy=" + clickable + "]").should('not.be.disabled').click().document().then(doc => {
+        if (doc.documentElement.innerHTML.includes('Oops! Something went wrong with the information you entered.')) {
             cy.log("Received the Oops! Something went wrong with the information you entered message, so retrying the click");
+            cy.get("[data-cy=" + clickable + "]").click();
+        }
+        else if (doc.title.startsWith('Error')) {
+            cy.log("Saw 'Error' in page title, so retrying the click");
             cy.get("[data-cy=" + clickable + "]").click();
         }
     });
@@ -45,4 +53,3 @@ Then(`If I am on dashboard I click to create lpa`, () => {
         }
     });
 })
-
