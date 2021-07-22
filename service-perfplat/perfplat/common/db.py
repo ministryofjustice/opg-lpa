759de73e24e6bf7e0cdad3b8c4b12ed2bd0caa53
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-07-22 11:48:49 +0100

    LPAL-419 Performance platform worker lambda (#545)
    * Add new local SQS queue for perfplat data generation messages
    
    * First cut of lambda triggered by SQS messages
    
    Currently requires manual sending of messages to the
    perfplat-queue.fifo SQS queue, using script in
    service-perfplat/scratch/ex1.py. So far this produces some
    log output and returns some JSON.
    
    Also no terraform config here to set up the lambda in real AWS:
    this only works with localstack at the moment.
    
    Setup is done from local-config/start.sh, consisting of:
    * Create queue
    * Create lambda
    * Connect events on the queue to lambda trigger
    
    Modified the Dockerfile for local-config so that I can copy
    the lambda code into that docker image, then push it up to
    localstack when run.
    
    * Make deployment scripts more robust and better for dev
    
    * Remove lambda before redeployment
    * Mount local directory into local-config in dev; this makes
    it simpler to redeploy the lambda during dev
    
    * Proper Python project structure for worker
    
    Use pip to install dependencies and package them with the worker
    code.
    
    * Add example of using 3rd party libraries and importing own files
    
    * Explain purpose of script
    
    * Exclude MacOS files from zip
    
    * Rename cli files for clarity
    
    * Reorganise code
    
    * First attempt at db connectivity
    
    This does not work as SQLAlchemy requires native code. The
    current approach, of building a zip with Python packages
    installed by pip, does not package the native code.
    
    The correct approach is to build a docker image deployable to
    a lambda, which can include native code.
    
    * Use statically-compiled psycopg2
    
    This works around issue where SQLAlchemy would not be able
    to create a postgresql connection (via psycopg2) because the
    AWS lambda runtime doesn't contain the postgresql libraries.
    
    This uses a third party git repo which contains a
    statically-compiled version of the psycopg2 library.
    
    If necessary, we would either have to fork this repo or
    manually apply the same steps covered there.
    
    Another option would be to figure out layers.
    
    Note that I also switched local-config over to amazonlinux,
    rather than python:alpine. This is so that, if anything is
    compiled during the build, it is done on the correct platform.
    
    * Strip out lambda gubbins
    
    This is in preparation for deploying the worker as a container
    locally and as an image-based lambda in AWS.
    
    Also fix the Python version used by local-config to 3.8, to
    match the version of the lambda runtime we intend to use.
    
    * Run lambda locally as a docker container
    
    * Add lambda proxy between SQS and real lambda
    
    This puts a small lambda proxy between SQS and the real worker
    lambda. This enables us to hook events from SQS to the proxy,
    which then converts them into a REST request to the worker lambda.
    
    This is necessary now that the worker is running in docker, as
    we can no longer hook SQS to a lambda in docker in localstack.
    
    One consequence of this is that the proxy and the worker have to
    be on an external network, as the proxy container starts outside
    the docker-compose context. Without this external network, the
    proxy can't resolve the domain name of the worker correctly.
    
    In prod, we would simply remove the proxy, as we should be able
    to wire SQS to the worker lambda running as a docker container.
    
    * Reorg and documentation for public consumption
    
    * Remove redundant file
    
    * Remove unused file
    
    * Remove unusued dependency
    
    * Reorg code again
    
    As we're mixing dockerfiles with source code, make the separation
    between the two cleaner.
    
    This also makes a more natural place to put the test folder
    alongside perfplat/ (the Python module code).
    
    * Unit tests for SqsClient
    
    * Additional comments on docker-compose networking
    
    In response to comments during PR Party.
    
    * Clear yum cache after building perfplatworker image

---
+++ service-perfplat/perfplat/common/db.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class Database:
+
+    # example connstr for postgres:
+    # postgresql://scott:tiger@localhost:5432/mydatabase
+    def __init__(self, conn_str):
+        engine = create_engine(conn_str)
+        self.session = sessionmaker(bind=engine)()
+
+    def query(self, *args, **kwargs):
+        return self.session.query(*args, **kwargs)
+
+    def add(self, obj):
+        self.session.add(obj)
+        return self.session.commit()
+
+    def delete(self, modelClass, id):
+        obj = self.session.query(modelClass).filter(modelClass.id==id).first()
+        self.session.delete(obj)
+        return self.session.commit()
