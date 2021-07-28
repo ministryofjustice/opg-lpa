75ea0fa60e5d9d5d3575b3a02d5a77bb9355a1d8
william Falconer <william.falconer@digital.justice.gov.uk>
2021-07-28 15:35:07 +0100

    tfsec fixes

--- terraform/environment/sqs.tf
+++ terraform/environment/sqs.tf
@@ -63,10 +63,12 @@ resource "aws_sqs_queue" "performance_platform_worker" {
 }
 
 resource "aws_sqs_queue_policy" "performance_platform_worker" {
-  count      = local.account.performance_platform_enabled == true ? 1 : 0
-  queue_url  = aws_sqs_queue.performance_platform_worker[0].id
-  policy     = data.aws_iam_policy_document.performance_platform_worker[0].json
-  depends_on = [aws_ecs_service.api, aws_iam_role.api_task_role]
+  count                             = local.account.performance_platform_enabled == true ? 1 : 0
+  queue_url                         = aws_sqs_queue.performance_platform_worker[0].id
+  policy                            = data.aws_iam_policy_document.performance_platform_worker[0].json
+  depends_on                        = [aws_ecs_service.api, aws_iam_role.api_task_role]
+  kms_master_key_id                 = "alias/aws/sqs"
+  kms_data_key_reuse_period_seconds = "300"
 }
 
 data "aws_iam_policy_document" "performance_platform_worker" {
