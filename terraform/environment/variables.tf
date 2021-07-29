ab6e5b46581f609382b8ef9b4117f327ed14960f
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-07-29 15:07:58 +0100

    Lpal 457 add infra and build details for perfplat worker (#565)
    * add lambda module
    
    * SQS
    
    * Add variables support
    
    * lambda function definition
    
    * correct reference
    
    * new secrets for perfplat, for separation
    
    * secrets datasource
    
    * Fix tags
    
    * lambda function and  feature flag
    
    * rejig docker files to support CI.
    we could refactor further later
    
    * add Lambda build / push
    
    * add lambda container version to tf apply
    
    * pre and prod steps
    
    * expand name for clarity
    
    * tf fmt
    
    * tf fmt
    
    * not needed at account level
    
    * tag error
    
    * fixes build trigger filter
    
    * add count to policy doc.
    
    * fix errors to count and names
    
    * fix reference
    
    * tf fmt
    
    * fix ecr reference to account level
    
    * fix security issues found in ECR
    
    * fix security issues on compiled layer section
    
    * enable lambda in dev.
    
    * add -y flag
    
    * correct dependancies on build
    
    * fix char limit and fix ECR Repo uri
    
    * remove fifo requirement
    
    * give lambda additional policy info
    
    * tfsec analysis not quite working
    
    * tfsec fixes
    
    * fix copy pasta error
    
    * fix ignore rules
    
    * add path to live dependancies missed

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -6,26 +6,32 @@ variable "account_mapping" {
   type = map(any)
 }
 
+variable "lambda_container_version" {
+  type    = string
+  default = "latest"
+}
+
 variable "accounts" {
   type = map(
     object({
-      pagerduty_service_name      = string
-      account_id                  = string
-      is_production               = string
-      sirius_api_gateway_endpoint = string
-      sirius_api_gateway_arn      = string
-      deletion_protection         = bool
-      backup_retention_period     = number
-      auth_token_ttl_secs         = number
-      skip_final_snapshot         = bool
-      psql_engine_version         = string
-      psql_parameter_group_family = string
-      aurora_enabled              = bool
-      aurora_serverless           = bool
-      aurora_instance_count       = number
-      deletion_protection         = bool
-      always_on                   = bool
-      log_retention_in_days       = number
+      performance_platform_enabled = bool
+      pagerduty_service_name       = string
+      account_id                   = string
+      is_production                = string
+      sirius_api_gateway_endpoint  = string
+      sirius_api_gateway_arn       = string
+      deletion_protection          = bool
+      backup_retention_period      = number
+      auth_token_ttl_secs          = number
+      skip_final_snapshot          = bool
+      psql_engine_version          = string
+      psql_parameter_group_family  = string
+      aurora_enabled               = bool
+      aurora_serverless            = bool
+      aurora_instance_count        = number
+      deletion_protection          = bool
+      always_on                    = bool
+      log_retention_in_days        = number
       autoscaling = object({
         front = object({
           minimum = number
