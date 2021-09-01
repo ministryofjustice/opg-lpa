940f194dd38427192b2f14d048bd4a326d3aa972
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-09-01 14:06:36 +0100

    base necessary files for flask POC

---
+++ service-front-v2/app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_compress import Compress
+from flask_talisman import Talisman
+from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
+
+app = Flask(__name__, static_url_path="/assets")
+
+app.jinja_loader = ChoiceLoader(
+    [
+        PackageLoader("app"),
+        PrefixLoader({"govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja")}),
+    ]
+)
+
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+csp = {
+    "default-src": "'self'",
+    "script-src": [
+        "'self'",
+        "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+        "'sha256-l1eTVSK8DTnK8+yloud7wZUqFrI0atVo6VlC6PJvYaQ='",
+    ],
+    "img-src": ["data:", "'self'"],
+}
+
+Compress(app)
+Talisman(app, content_security_policy=csp)
+
+from app import routes
---
+++ service-front-v2/app/routes.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+from flask import render_template
+from werkzeug.exceptions import NotFound
+
+from app import app
+
+
+@app.route("/")
+def index():
+    components = os.listdir("govuk_components")
+    components.sort()
+
+    return render_template("index.html", components=components)
+
+
+@app.route("/components/<string:component>")
+def component(component):
+    try:
+        with open("govuk_components/{}/fixtures.json".format(component)) as json_file:
+            fixtures = json.load(json_file)
+    except FileNotFoundError:
+        raise NotFound
+
+    return render_template("component.html", fixtures=fixtures)
+
+@app.route("/feedback")
+def feedback():
+    return render_template("feedback.html")
+
+@app.route("/flask-accessibility")
+def accessibility():
+    return render_template("accessibility.html")
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server(error):
+    return render_template("500.html"), 500
