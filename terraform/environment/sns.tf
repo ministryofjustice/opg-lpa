090d3381de034f4f524e802a60f2847268dd7ac6
william Falconer <william.falconer@digital.justice.gov.uk>
2021-09-06 14:42:58 +0100

    encryption doesn't appear to work for these kind of alerts. will have to ignore for now, and rereview later

--- terraform/environment/sns.tf
+++ terraform/environment/sns.tf
@@ -1,13 +1,13 @@
+#tfsec:ignore:AWS016 - remove for now until we confirm this works with encryption
 resource "aws_sns_topic" "cloudwatch_to_pagerduty" {
-  name              = "CloudWatch-to-PagerDuty-${local.environment}"
-  tags              = merge(local.default_tags, local.shared_component_tag)
-  kms_master_key_id = "alias/aws/sns"
+  name = "CloudWatch-to-PagerDuty-${local.environment}"
+  tags = merge(local.default_tags, local.shared_component_tag)
 }
 
+#tfsec:ignore:AWS016 - remove for now until we confirm this works with encryption
 resource "aws_sns_topic" "cloudwatch_to_pagerduty_ops" {
-  name              = "CloudWatch-to-PagerDuty-${local.environment}-Ops"
-  tags              = merge(local.default_tags, local.shared_component_tag)
-  kms_master_key_id = "alias/aws/sns"
+  name = "CloudWatch-to-PagerDuty-${local.environment}-Ops"
+  tags = merge(local.default_tags, local.shared_component_tag)
 }
 
 resource "aws_sns_topic_subscription" "cloudwatch_sns_subscription" {
