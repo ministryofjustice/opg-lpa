38b9a786177b80bb544d5fabf96af3b1d2e30b4f
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-09-10 09:02:37 +0100

    LPAL-529 Fix Python unit tests and make them consistent (#627)
    * Remove packages not directly referenced in app or test code
    
    Only list packages which are directly referenced. Other packages
    should be pulled in as dependencies, with their versions managed
    by the primary packages we need for imports.
    
    * Use pytest as the test runner
    
    nose is marked as deprecated. Modify the pytest tests so that
    they use the latest pytest-cases API. Remove the nose config.
    Add pytest config.
    
    * Fix typo

--- service-perfplat/test/unit/api/test_dummy_handler.py
+++ service-perfplat/test/unit/api/test_dummy_handler.py
@@ -1,15 +1,14 @@
 import pytest
 
-from pytest_cases import CaseDataGetter, cases_data
+from pytest_cases import parametrize_with_cases
 
-from test.unit.api import cases_handle_dummy
 from perfplat.api.endpoints import handle_dummy
 
-@cases_data(module=cases_handle_dummy)
-def test_dummy(case_data: CaseDataGetter):
-    expected_result, expected_status_code = case_data.get()
+from .cases_handle_dummy import CasesHandleDummy
 
+
+@parametrize_with_cases("expected_result,expected_status_code", cases=CasesHandleDummy)
+def test_dummy(expected_result, expected_status_code):
     result, status_code = handle_dummy()
-
     assert result == expected_result
     assert status_code == expected_status_code
