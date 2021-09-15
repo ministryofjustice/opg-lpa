f6d167a923db6166d5c374bc8c57844cd5c3ab2f
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-09-15 09:55:45 +0100

    LPAL-500 align version (#637)
    * align version
    
    * fix role. will require state moves
    
    * change to data reference
    
    * removed data reference by mistake

--- terraform/environment/rds.tf
+++ terraform/environment/rds.tf
@@ -3,36 +3,13 @@ data "aws_kms_key" "rds" {
   key_id = "alias/aws/rds"
 }
 
+data "aws_iam_role" "rds_enhanced_monitoring" {
+  name = "rds-enhanced-monitoring"
+}
+
 data "aws_sns_topic" "rds_events" {
   name = "${local.account_name}-rds-events"
 }
-
-resource "aws_iam_role" "rds_enhanced_monitoring" {
-  name               = "rds-enhanced-monitoring"
-  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
-  tags               = merge(local.default_tags, local.db_component_tag)
-}
-
-resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
-  role       = aws_iam_role.rds_enhanced_monitoring.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-}
-
-data "aws_iam_policy_document" "rds_enhanced_monitoring" {
-  statement {
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["monitoring.rds.amazonaws.com"]
-    }
-  }
-}
-
 
 resource "aws_db_instance" "api" {
   count                               = local.account.always_on ? 1 : 0
@@ -60,7 +37,7 @@ resource "aws_db_instance" "api" {
   tags                                = merge(local.default_tags, local.db_component_tag)
   allow_major_version_upgrade         = true
   monitoring_interval                 = 30
-  monitoring_role_arn                 = aws_iam_role.rds_enhanced_monitoring.arn
+  monitoring_role_arn                 = data.aws_iam_role.rds_enhanced_monitoring.arn
   enabled_cloudwatch_logs_exports     = ["postgresql", "upgrade"]
   iam_database_authentication_enabled = true
   performance_insights_enabled        = true
