5b5988d85431e6dafaa6aa15be1f4a5b65b25c6d
william Falconer <william.falconer@digital.justice.gov.uk>
2021-09-15 09:06:43 +0100

    fix role. will require state moves

--- terraform/environment/rds.tf
+++ terraform/environment/rds.tf
@@ -3,37 +3,6 @@ data "aws_kms_key" "rds" {
   key_id = "alias/aws/rds"
 }
 
-data "aws_sns_topic" "rds_events" {
-  name = "${local.account_name}-rds-events"
-}
-
-resource "aws_iam_role" "rds_enhanced_monitoring" {
-  name               = "rds-enhanced-monitoring"
-  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
-  tags               = merge(local.default_tags, local.db_component_tag)
-}
-
-resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
-  role       = aws_iam_role.rds_enhanced_monitoring.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-}
-
-data "aws_iam_policy_document" "rds_enhanced_monitoring" {
-  statement {
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["monitoring.rds.amazonaws.com"]
-    }
-  }
-}
-
-
 resource "aws_db_instance" "api" {
   count                               = local.account.always_on ? 1 : 0
   identifier                          = lower("api-${local.environment}")
