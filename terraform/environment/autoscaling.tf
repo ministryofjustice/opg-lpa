bd0445eb4d1b6e39cc171560fc2348a82560e608
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-09-21 16:12:49 +0100

    remove v2 references

--- terraform/environment/autoscaling.tf
+++ terraform/environment/autoscaling.tf
@@ -9,16 +9,16 @@ module "front_ecs_autoscaling" {
   tags                             = merge(local.default_tags, local.front_component_tag)
 }
 
-module "front_v2_ecs_autoscaling" {
-  source                           = "./modules/ecs_autoscaling"
-  environment                      = local.environment
-  aws_ecs_cluster_name             = aws_ecs_cluster.online-lpa.name
-  aws_ecs_service_name             = aws_ecs_service.front_v2.name
-  ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
-  ecs_task_autoscaling_minimum     = local.account.autoscaling.front.minimum
-  ecs_task_autoscaling_maximum     = local.account.autoscaling.front.maximum
-  tags                             = merge(local.default_tags, local.front_component_tag)
-}
+# module "front_v2_ecs_autoscaling" {
+#   source                           = "./modules/ecs_autoscaling"
+#   environment                      = local.environment
+#   aws_ecs_cluster_name             = aws_ecs_cluster.online-lpa.name
+#   aws_ecs_service_name             = aws_ecs_service.front_v2.name
+#   ecs_autoscaling_service_role_arn = data.aws_iam_role.ecs_autoscaling_service_role.arn
+#   ecs_task_autoscaling_minimum     = local.account.autoscaling.front.minimum
+#   ecs_task_autoscaling_maximum     = local.account.autoscaling.front.maximum
+#   tags                             = merge(local.default_tags, local.front_component_tag)
+# }
 
 module "api_ecs_autoscaling" {
   source                           = "./modules/ecs_autoscaling"
