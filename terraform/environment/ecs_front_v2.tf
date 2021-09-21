30c33a7a1bbb4951d685891424db1a285bcf2748
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-09-21 14:08:05 +0100

    service-front web talks via 80 to 8005

--- terraform/environment/ecs_front_v2.tf
+++ terraform/environment/ecs_front_v2.tf
@@ -78,7 +78,7 @@ resource "aws_security_group_rule" "front_v2_ecs_service_ingress" {
 #tfsec:ignore:AWS018 - Adding description is destructive change needing downtime. to be revisited
 resource "aws_security_group_rule" "front_v2_ecs_service_front_ingress" {
   type                     = "ingress"
-  from_port                = 8005
+  from_port                = 80
   to_port                  = 8005
   protocol                 = "tcp"
   security_group_id        = aws_security_group.front_v2_ecs_service.id
