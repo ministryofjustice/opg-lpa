c753ad60d0566fea6d83169a7aa9d056b35b4bf0
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-10-04 15:11:36 +0100

    fix route to feedback page,  remove routes to components for now, add in xray stuff

--- service-front-v2/app/__init__.py
+++ service-front-v2/app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_compress import Compress
 from flask_talisman import Talisman
 from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 app = Flask(__name__, static_url_path="/assets")
+xray_recorder.configure(service='Make An LPA')
+XRayMiddleware(app, xray_recorder)
 
 app.jinja_loader = ChoiceLoader(
     [
