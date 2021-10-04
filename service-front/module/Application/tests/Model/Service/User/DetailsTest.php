243070d28f23bcd6ec12b7de47aca22259e74500
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-04 13:34:46 +0100

    Fix unit tests for Details service

--- service-front/module/Application/tests/Model/Service/User/DetailsTest.php
+++ service-front/module/Application/tests/Model/Service/User/DetailsTest.php
@@ -2,6 +2,7 @@
 
 namespace ApplicationTest\Model\Service\User;
 
+use Hamcrest\MatcherAssert;
 use Laminas\Mail\Exception\InvalidArgumentException;
 use Laminas\Session\Container;
 use Exception;
@@ -476,11 +477,15 @@ class DetailsTest extends AbstractEmailServiceTest
         $this->apiClient->shouldReceive('httpPost')
             ->withArgs(['/v2/authenticate', ['authToken' => 'test-token']])
             ->once()
-            ->andReturn(['test' => 'response']);
+            ->andReturn(['expiresIn' => 10000]);
 
         $result = $this->service->getTokenInfo('test-token');
 
-        $this->assertEquals(['test' => 'response'], $result);
+        $this->assertEquals([
+            'success' => true,
+            'failureCode' => null,
+            'expiresIn' => 10000,
+        ], $result);
     }
 
     public function testGetTokenInfoApiException(): void
@@ -492,7 +497,11 @@ class DetailsTest extends AbstractEmailServiceTest
 
         $result = $this->service->getTokenInfo('test-token');
 
-        $this->assertEquals(false, $result);
+        MatcherAssert::assertThat([
+            'success' => false,
+            'failureCode' => 500,
+            'expiresIn' => null,
+        ], Matchers::equalTo($result));
     }
 
     public function testDelete(): void
