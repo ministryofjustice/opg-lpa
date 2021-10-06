114a3a65c3f5d6589e3557adb0c03b2dbb2574a2
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-10-06 16:30:09 +0100

    put in csrf protection, which was in wtforms demo but not base demo

--- service-front-v2/app/__init__.py
+++ service-front-v2/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_compress import Compress
 from flask_talisman import Talisman
+from flask_wtf.csrf import CSRFProtect
 from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
@@ -31,5 +32,6 @@ csp = {
 
 Compress(app)
 Talisman(app, content_security_policy=csp, force_https=False)
+csrf = CSRFProtect(app)
 
 from app import routes
