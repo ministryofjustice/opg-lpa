507eb3188d0a49906057b044c86444a126b61f6d
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-10-18 14:30:56 +0100

    take out secrets, for now us a very strict csp policy

--- service-front-v2/app/__init__.py
+++ service-front-v2/app/__init__.py
@@ -21,13 +21,7 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 csp = {
-    "default-src": "'self'",
-    "script-src": [
-        "'self'",
-        "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
-        "'sha256-l1eTVSK8DTnK8+yloud7wZUqFrI0atVo6VlC6PJvYaQ='",
-    ],
-    "img-src": ["data:", "'self'"],
+    "default-src": "'self'"
 }
 
 Compress(app)
