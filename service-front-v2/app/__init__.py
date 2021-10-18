b777132cf4edc9a4f8a0e87ea79dde0b7340292e
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-10-18 16:05:07 +0100

    set strict transport policy max age to same as for php site

--- service-front-v2/app/__init__.py
+++ service-front-v2/app/__init__.py
@@ -25,7 +25,7 @@ csp = {
 }
 
 Compress(app)
-Talisman(app, content_security_policy=csp, force_https=False)
+Talisman(app, content_security_policy=csp, force_https=False, strict_transport_security_max_age=3600)
 csrf = CSRFProtect(app)
 
 from app import routes
