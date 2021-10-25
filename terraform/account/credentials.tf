094ad9702502a81d9e2315253590b122eaa7ddb7
william Falconer <william.falconer@digital.justice.gov.uk>
2021-10-25 09:52:29 +0100

    Add alias for EU West 1

--- terraform/account/credentials.tf
+++ terraform/account/credentials.tf
@@ -26,8 +26,17 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "eu_west_2"
+  alias  = "eu-west-1"
+  region = "eu-west-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
   region = "eu-west-2"
+  alias  = "eu-west-2"
   assume_role {
     role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
     session_name = "terraform-session"
