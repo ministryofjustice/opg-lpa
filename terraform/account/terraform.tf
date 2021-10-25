9af7dba06c8896da5a73aa4178d9ef58e3b47fc8
william Falconer <william.falconer@digital.justice.gov.uk>
2021-10-25 12:05:23 +0100

    restructure account terraform to be more canonical

--- terraform/account/terraform.tf
+++ terraform/account/terraform.tf
@@ -9,14 +9,6 @@ terraform {
   }
 }
 
-variable "default_role" {
-  default = "opg-lpa-ci"
-}
-
-variable "legacy_account_role" {
-  default = "opg-lpa-ci"
-}
-
 provider "aws" {
   region = "eu-west-1"
   assume_role {
@@ -73,30 +65,4 @@ provider "aws" {
 
 provider "pagerduty" {
   token = var.pagerduty_token
-}
-
-resource "aws_iam_role" "rds_enhanced_monitoring" {
-  name               = "rds-enhanced-monitoring"
-  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
-  tags               = merge(local.default_tags, local.db_component_tag)
-}
-
-resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
-  role       = aws_iam_role.rds_enhanced_monitoring.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-}
-
-data "aws_iam_policy_document" "rds_enhanced_monitoring" {
-  statement {
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["monitoring.rds.amazonaws.com"]
-    }
-  }
 }
