b9a189aa6cfe0151032e61190659c5e5bca23c5d
Matt Shaw <matthew-shaw@users.noreply.github.com>
2021-11-10 12:41:29 +0000

    only dist assets needed, not fixtures used by demo

--- service-front-v2/build.sh
+++ service-front-v2/build.sh
@@ -1,15 +1,14 @@
+# Remove existing GOV.UK Frontend assets
+rm -rf app/static/fonts
+rm -rf app/static/images
+rm -rf app/static/govuk-frontend*
+
+# Get new release distribution assets and move to static directory
 curl -L https://github.com/alphagov/govuk-frontend/releases/download/v3.14.0/release-v3.14.0.zip > govuk_frontend.zip
-rm -rf app/static
 unzip -o govuk_frontend.zip -d app/static
 mv app/static/assets/* app/static
-rm -rf app/static/assets
-rm -rf govuk_frontend.zip
 
-curl -L https://github.com/alphagov/govuk-frontend/archive/v3.14.0.zip > govuk_frontend_source.zip
-unzip -o govuk_frontend_source.zip -d govuk_frontend_source
-rm -rf govuk_components
-mkdir govuk_components
-mv govuk_frontend_source/govuk-frontend-3.14.0/package/govuk/components/** govuk_components
-find govuk_components -type f ! -name 'fixtures.json' -delete
-rm -rf govuk_frontend_source
-rm -rf govuk_frontend_source.zip
+# Tidy up
+rm -rf app/static/assets
+rm -rf app/static/VERSION.txt
+rm -rf govuk_frontend.zip
