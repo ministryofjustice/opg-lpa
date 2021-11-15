e21f08a764a3d0a0ff7eaf6590236ad3214a9737
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-11-15 17:58:57 +0000

    starting bringing in dependencies for wtforms

--- service-front-v2/app/__init__.py
+++ service-front-v2/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_assets import Bundle, Environment
 from flask_compress import Compress
 from flask_talisman import Talisman
 from flask_wtf.csrf import CSRFProtect
+from govuk_frontend_wtf.main import WTFormsHelpers
 from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
@@ -13,7 +15,12 @@ XRayMiddleware(app, xray_recorder)
 app.jinja_loader = ChoiceLoader(
     [
         PackageLoader("app"),
-        PrefixLoader({"govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja")}),
+        PrefixLoader(
+            {
+                "govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja"),
+                "govuk_frontend_wtf": PackageLoader("govuk_frontend_wtf"),
+            }
+        ),
     ]
 )
 
