11e9a2416c0b80a544d5e0b9e84bd2856267e94c
william Falconer <william.falconer@digital.justice.gov.uk>
2021-11-18 13:53:46 +0000

    update data sources

--- terraform/environment/shared_data_sources.tf
+++ terraform/environment/shared_data_sources.tf
@@ -5,3 +5,31 @@ data "aws_security_group" "front_cache" {
 data "aws_elasticache_replication_group" "front_cache" {
   replication_group_id = "${local.account_name_short}-${local.region_name}-front-cache-rg"
 }
+
+data "aws_s3_bucket" "access_log" {
+  bucket = "online-lpa-${local.account_name}-${local.region_name}-lb-access-logs"
+}
+
+data "aws_s3_bucket" "lpa_pdf_cache" {
+  bucket = lower("online-lpa-pdf-cache-${local.account_name}-${local.region_name}")
+}
+
+data "aws_kms_key" "lpa_pdf_cache" {
+  key_id = "alias/lpa_pdf_cache-${local.account_name}"
+}
+
+data "aws_acm_certificate" "certificate_front" {
+  domain = "${local.cert_prefix_internal}${local.dns_namespace_dev_prefix}front.lpa.opg.service.justice.gov.uk"
+}
+
+data "aws_acm_certificate" "certificate_admin" {
+  domain = "${local.cert_prefix_internal}${local.dns_namespace_dev_prefix}admin.lpa.opg.service.justice.gov.uk"
+}
+
+data "aws_acm_certificate" "public_facing_certificate" {
+  domain = "${local.cert_prefix_public_facing}${local.dns_namespace_dev_prefix}lastingpowerofattorney.service.gov.uk"
+}
+
+data "aws_iam_role" "ecs_autoscaling_service_role" {
+  name = "AWSServiceRoleForApplicationAutoScaling_ECSService"
+}
