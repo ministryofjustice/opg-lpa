9f4ec713f80b32190488c949d32f35e23ea1d425
william Falconer <william.falconer@digital.justice.gov.uk>
2021-11-18 13:46:53 +0000

    update shared data sources

--- terraform/environment/shared_data_sources.tf
+++ terraform/environment/shared_data_sources.tf
@@ -1,7 +1,7 @@
 data "aws_security_group" "front_cache" {
-  name = "front-cache"
+  name = "${local.account_name_short}-${local.region_name}-front-cache"
 }
 
 data "aws_elasticache_replication_group" "front_cache" {
-  replication_group_id = "front-cache-replication-group"
+  replication_group_id = "${local.account_name_short}-${local.region_name}-front-cache-rg"
 }
