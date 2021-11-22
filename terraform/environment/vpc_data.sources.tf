6b9b3acb447a2e75716349c2902be2ed4aeed671
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-11-22 15:57:32 +0000

    Lpal 600 fix up ecs task (#723)
    * Add definitons for region name and shortened name
    
    * update shared data sources
    
    * update data sources
    
    * fix up cache SG, prior to removal of old.
    
    * Add description to SG Rule
    
    * replace hard coded region name for aws-logs

--- terraform/environment/vpc_data.sources.tf
+++ terraform/environment/vpc_data.sources.tf
@@ -21,31 +21,3 @@ data "aws_subnet_ids" "public" {
 module "allowed_ip_list" {
   source = "git@github.com:ministryofjustice/terraform-aws-moj-ip-whitelist.git"
 }
-
-data "aws_s3_bucket" "access_log" {
-  bucket = "online-lpa-${local.account_name}-lb-access-logs"
-}
-
-data "aws_s3_bucket" "lpa_pdf_cache" {
-  bucket = lower("online-lpa-pdf-cache-${local.account_name}")
-}
-
-data "aws_kms_key" "lpa_pdf_cache" {
-  key_id = "alias/lpa_pdf_cache-${local.account_name}"
-}
-
-data "aws_acm_certificate" "certificate_front" {
-  domain = "${local.cert_prefix_internal}${local.dns_namespace_dev_prefix}front.lpa.opg.service.justice.gov.uk"
-}
-
-data "aws_acm_certificate" "certificate_admin" {
-  domain = "${local.cert_prefix_internal}${local.dns_namespace_dev_prefix}admin.lpa.opg.service.justice.gov.uk"
-}
-
-data "aws_acm_certificate" "public_facing_certificate" {
-  domain = "${local.cert_prefix_public_facing}${local.dns_namespace_dev_prefix}lastingpowerofattorney.service.gov.uk"
-}
-
-data "aws_iam_role" "ecs_autoscaling_service_role" {
-  name = "AWSServiceRoleForApplicationAutoScaling_ECSService"
-}
