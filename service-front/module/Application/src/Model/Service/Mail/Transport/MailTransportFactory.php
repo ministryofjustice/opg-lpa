cf4b24abbaf2582e87db5660e3b8ad3d80621482
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-11-30 10:43:48 +0000

    remove further logic around sendgrid/notify

--- service-front/module/Application/src/Model/Service/Mail/Transport/MailTransportFactory.php
+++ service-front/module/Application/src/Model/Service/Mail/Transport/MailTransportFactory.php
@@ -34,24 +34,15 @@ class MailTransportFactory implements FactoryInterface
     {
         $emailConfig = $container->get('Config')['email'];
 
-        $transport = null;
-        if (isset($emailConfig['transport'])) {
-            $transport = $emailConfig['transport'];
+        if (!isset($emailConfig['notify']['key'])) {
+            throw new RuntimeException('Notify API settings not found');
         }
 
-        if ($transport === 'notify') {
-            if (!isset($emailConfig['notify']['key'])) {
-                throw new RuntimeException('Notify API settings not found');
-            }
+        $notifyClient = new NotifyClient([
+            'apiKey' => $emailConfig['notify']['key'],
+            'httpClient' => new Guzzle6Wrapper()
+        ]);
 
-            $notifyClient = new NotifyClient([
-                'apiKey' => $emailConfig['notify']['key'],
-                'httpClient' => new Guzzle6Wrapper()
-            ]);
-
-            return new NotifyMailTransport($notifyClient);
-        }
-
-        throw new RuntimeException('Unable to instantiate email transport; transport is set to ' . $transport);
+        return new NotifyMailTransport($notifyClient);
     }
 }
