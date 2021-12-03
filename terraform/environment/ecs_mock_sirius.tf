b3a0412d24fad54d886a826bea4b2116bedd2ccb
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2021-12-03 13:12:04 +0000

    Adds ecs terraform for containers

---
+++ terraform/environment/ecs_mock_sirius.tf
@@ -0,0 +1,71 @@
+
+data "aws_ecr_repository" "lpa_api_mock_gateway" {
+  provider = aws.management
+  name     = "online-lpa/mockgateway"
+}
+
+data "aws_ecr_repository" "lpa_api_mock_sirius" {
+  provider = aws.management
+  name     = "online-lpa/mocksirius"
+}
+
+//-----------------------------------------------
+// mock sirius ECS Service Task Container level config
+
+locals {
+  mock_gateway = jsonencode(
+  {
+    "cpu" : 1,
+    "essential" : true,
+    "image" : "${data.aws_ecr_repository.lpa_api_mock_gateway.repository_url}:${var.container_version}",
+    "mountPoints" : [],
+    "name" : "gateway",
+    "portMappings" : [
+      {
+        "containerPort" : 7010,
+        "hostPort" : 5000,
+        "protocol" : "tcp"
+      }
+    ],
+    "volumesFrom" : [],
+    "logConfiguration" : {
+      "logDriver" : "awslogs",
+      "options" : {
+        "awslogs-group" : aws_cloudwatch_log_group.application_logs.name,
+        "awslogs-region" : "${local.region_name}",
+        "awslogs-stream-prefix" : "${local.environment}.api-web.online-lpa"
+      }
+    }
+    "environment" : [
+      { "name" : "OPG_LPA_STATUS_ENDPOINT", "value" : local.account.sirius_api_gateway_endpoint }
+    ]
+  }
+  )
+
+  mock_sirius = jsonencode(
+  {
+    "cpu" : 1,
+    "essential" : true,
+    "image" : "${data.aws_ecr_repository.lpa_api_mock_gateway.repository_url}:${var.container_version}",
+    "mountPoints" : [],
+    "name" : "mocksirius",
+    "portMappings" : [
+      {
+        "containerPort" : 7011,
+        "hostPort" : 5000,
+        "protocol" : "tcp"
+      }
+    ],
+    "volumesFrom" : [],
+    "logConfiguration" : {
+      "logDriver" : "awslogs",
+      "options" : {
+        "awslogs-group" : aws_cloudwatch_log_group.application_logs.name,
+        "awslogs-region" : "${local.region_name}",
+        "awslogs-stream-prefix" : "${local.environment}.api-web.online-lpa"
+      }
+    }
+  }
+  )
+
+}
