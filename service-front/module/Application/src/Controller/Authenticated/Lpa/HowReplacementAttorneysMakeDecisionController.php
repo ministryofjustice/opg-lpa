18c89cbe3a37942f7d8070e3e661f2d62d7acd8b
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2021-12-07 16:18:22 +0000

    fix form calls and tests

--- service-front/module/Application/src/Controller/Authenticated/Lpa/HowReplacementAttorneysMakeDecisionController.php
+++ service-front/module/Application/src/Controller/Authenticated/Lpa/HowReplacementAttorneysMakeDecisionController.php
@@ -24,7 +24,7 @@ class HowReplacementAttorneysMakeDecisionController extends AbstractLpaControlle
             $postData = $this->request->getPost();
 
             if ($postData['how'] != ReplacementAttorneyDecisions::LPA_DECISION_HOW_DEPENDS) {
-                $form->setValidationGroup('how');
+                $form->setValidationGroup(array('how'));
             }
 
             // set data for validation
