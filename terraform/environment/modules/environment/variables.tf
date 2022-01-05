b7c8885e34f9e84dcc9bfe28b0c4c2c3c6a07f64
William Falconer <williamfalconeruk@users.noreply.github.com>
2022-01-05 13:15:51 +0000

    LPAL-645 part 2: enable waf web acl association with front and admin (#774)
    * enable waf web acl association with front and admin
    
    * replace local with var

--- terraform/environment/modules/environment/variables.tf
+++ terraform/environment/modules/environment/variables.tf
@@ -16,26 +16,27 @@ variable "lambda_container_version" {
 
 variable "account" {
   type = object({
-    dr_enabled                   = bool
-    performance_platform_enabled = bool
-    pagerduty_service_name       = string
-    account_id                   = string
-    is_production                = string
-    sirius_api_gateway_endpoint  = string
-    sirius_api_gateway_arn       = string
-    deletion_protection          = bool
-    backup_retention_period      = number
-    auth_token_ttl_secs          = number
-    skip_final_snapshot          = bool
-    psql_engine_version          = string
-    psql_parameter_group_family  = string
-    aurora_enabled               = bool
-    aurora_serverless            = bool
-    aurora_instance_count        = number
-    deletion_protection          = bool
-    always_on                    = bool
-    log_retention_in_days        = number
-    account_name_short           = string
+    dr_enabled                             = bool
+    performance_platform_enabled           = bool
+    pagerduty_service_name                 = string
+    account_id                             = string
+    is_production                          = string
+    sirius_api_gateway_endpoint            = string
+    sirius_api_gateway_arn                 = string
+    deletion_protection                    = bool
+    backup_retention_period                = number
+    auth_token_ttl_secs                    = number
+    skip_final_snapshot                    = bool
+    psql_engine_version                    = string
+    psql_parameter_group_family            = string
+    aurora_enabled                         = bool
+    aurora_serverless                      = bool
+    aurora_instance_count                  = number
+    deletion_protection                    = bool
+    always_on                              = bool
+    log_retention_in_days                  = number
+    account_name_short                     = string
+    associate_alb_with_waf_web_acl_enabled = bool
     regions = map(
       object({
         region     = string
