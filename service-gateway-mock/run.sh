5358daa6bcfa51240efd68ff5ee792bf8a1baccb
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2022-01-28 16:37:46 +0000

    Changed docker files base apline image & prisim installed

--- service-gateway-mock/run.sh
+++ service-gateway-mock/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node /usr/src/prism/packages/cli/dist/index.js mock -h 0.0.0.0 -p 5011 "/app/swagger.yaml"
+node /usr/local/lib/node_modules/@stoplight/prism-cli/dist/index.js mock -h 0.0.0.0 -p 5011 "/app/swagger.yaml"
