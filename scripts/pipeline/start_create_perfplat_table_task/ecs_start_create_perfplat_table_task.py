f534f4bfa5dccf74919336d155285a35021af017
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-02-03 17:44:33 +0000

    test out in CI using shared code from lib directory

--- scripts/pipeline/start_create_perfplat_table_task/ecs_start_create_perfplat_table_task.py
+++ scripts/pipeline/start_create_perfplat_table_task/ecs_start_create_perfplat_table_task.py
@@ -1,2 +1,4 @@
+from ECSMonitor import *
 # dummy script to start with while getting this running in CI
 print("%%%%%%%%%%%%%%%%% running create perfplat table script %%%%%%%%%%%%%")
+printSomething()
