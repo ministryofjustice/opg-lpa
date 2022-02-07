c8740718dc39f7b0c411bdb1e27ddedfaa5b1866
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-02-07 10:03:40 +0000

    pull in alembic to docker container, started modifying python script

--- scripts/db/create_feedback_table.py
+++ scripts/db/create_feedback_table.py
@@ -14,7 +14,7 @@ def create_perf_feedback_table():
         Column('id', Integer, primary_key=True),
         Column('rating', String(2), nullable=False), # up to 2-letter acronynm , VS = very satisfied , etc
         Column('comment', String(1200), nullable=False),
-        Column('datetime', String(1200), nullable=False),
+        Column('datetime', DateTime(), nullable=False),
     )
     feedback.create(engine)
 
@@ -22,6 +22,10 @@ def show_perf_feedback_table_details():
   columns = inspector.get_columns('perf_feedback')
   for column in columns:
       print(column["name"], column["type"])
+
+def create_apiv2_user():
+    # using OPG_LPA_POSTGRES_APIV2 blah, create the user if not already there, and grant privileges on perfplat table
+    pass
 
 #print(inspector.get_table_names())
 
