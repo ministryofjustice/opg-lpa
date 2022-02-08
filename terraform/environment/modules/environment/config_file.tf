98920421c1afdbeb492e163e9a80474b12b37055
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-02-08 17:01:18 +0000

    indent terraform code

--- terraform/environment/modules/environment/config_file.tf
+++ terraform/environment/modules/environment/config_file.tf
@@ -6,15 +6,15 @@ resource "local_file" "environment_pipeline_tasks_config" {
 locals {
 
   environment_pipeline_tasks_config = {
-    account_id                  = var.account.account_id
-    cluster_name                = aws_ecs_cluster.online-lpa.name
-    environment                 = var.environment_name
-    front_fqdn                  = aws_route53_record.front.fqdn
-    admin_fqdn                  = aws_route53_record.admin.fqdn
-    public_facing_fqdn          = aws_route53_record.public_facing_lastingpowerofattorney.fqdn
-    tag                         = var.container_version
-    db_client_security_group_id = aws_security_group.rds-client.id
-    seeding_security_group_id   = aws_security_group.seeding_ecs_service.id
-    create_perfplat_table_security_group_id   = aws_security_group.create_perfplat_table_ecs_service.id
+    account_id                              = var.account.account_id
+    cluster_name                            = aws_ecs_cluster.online-lpa.name
+    environment                             = var.environment_name
+    front_fqdn                              = aws_route53_record.front.fqdn
+    admin_fqdn                              = aws_route53_record.admin.fqdn
+    public_facing_fqdn                      = aws_route53_record.public_facing_lastingpowerofattorney.fqdn
+    tag                                     = var.container_version
+    db_client_security_group_id             = aws_security_group.rds-client.id
+    seeding_security_group_id               = aws_security_group.seeding_ecs_service.id
+    create_perfplat_table_security_group_id = aws_security_group.create_perfplat_table_ecs_service.id
   }
 }
