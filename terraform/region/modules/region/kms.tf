b6ed8295399a1ce56234f79099786bdc769afc60
William Falconer <williamfalconeruk@users.noreply.github.com>
2022-03-24 14:31:30 +0000

    create kms key at region level (#850)

--- terraform/region/modules/region/kms.tf
+++ terraform/region/modules/region/kms.tf
@@ -19,3 +19,15 @@ resource "aws_kms_alias" "lpa_pdf_cache" {
   name          = "alias/lpa_pdf_cache-${terraform.workspace}"
   target_key_id = aws_kms_key.lpa_pdf_cache.key_id
 }
+
+resource "aws_kms_key" "cloudwatch_encryption" {
+  description             = "encryption key for cloudwatch"
+  deletion_window_in_days = 7
+  tags                    = merge(local.default_tags, local.pdf_component_tag)
+  enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "cloudwatch_encryption" {
+  name          = "alias/cloudwatch_encryption-${terraform.workspace}"
+  target_key_id = aws_kms_key.cloudwatch_encryption.key_id
+}
