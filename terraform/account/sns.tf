964089b12c7ea353a4ed202bcbe79355e7012411
William Falconer <william.falconer@digital.justice.gov.uk>
2022-03-24 09:26:13 +0000

    remove event categories as already set to all.

--- terraform/account/sns.tf
+++ terraform/account/sns.tf
@@ -23,5 +23,4 @@ resource "aws_sns_topic" "rds_events" {
 resource "aws_db_event_subscription" "rds_events" {
   name             = "${local.account_name}-rds-event-sub"
   sns_topic        = aws_sns_topic.rds_events.arn
-  event_categories = ["All"]
 }
