38427fc68887eedd206e2be847adf87e25793bd6
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-04-20 14:27:48 +0100

    unit tests for all 10 scenarios - no that 8 with payment currently all the same until we start actually changing the email template

--- service-front/module/Application/tests/Model/Service/Lpa/CommunicationTest.php
+++ service-front/module/Application/tests/Model/Service/Lpa/CommunicationTest.php
@@ -102,11 +102,64 @@ class CommunicationTest extends AbstractEmailServiceTest
         $this->assertTrue($result);
     }
 
-    public function testSendRegistrationCompleteEmailWithoutPaymentOrPersonToNotify(): void
+    public function testSendRegistrationCompleteEmailWithoutPaymentNoPersonToNotify(): void
     {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_PF,
+                'donor' => [
+                    'name' => new LongName('{"title":"Dr", "first":"Pete", "last":"Vamoose"}')
+                ]
+            ])
+        ]);
+
+        // The service is partially mocked so we don't have to mess
+        // about with expectations on the HelperPluginManager;
+        // the formatLpaId() and url() methods on the service are just
+        // proxies through the methods on that plugin manager anyway.
+        $this->service->shouldReceive('formatLpaId')
+            ->with($lpa->id)
+            ->andReturn('A111 111 1111');
+
+        $this->service->shouldReceive('url')
+            ->with(
+                'lpa/view-docs',
+                ['lpa-id' => $lpa->id],
+                ['force_canonical' => true],
+            )
+            ->andReturn('https://view.docs.url');
+
+        $this->service->shouldReceive('url')
+            ->with(
+                'lpa/date-check',
+                ['lpa-id' => $lpa->id],
+                ['force_canonical' => true],
+            )
+            ->andReturn('https://check.dates.url');
+
+        // What we expect to pass to the mail transport
+        $expectedMailParams = new MailParameters(
+            ['test@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION,
+            [
+                'donorName' => 'Dr Pete Vamoose',
+                'lpaType' => 'property and financial affairs',
+                'lpaId' => 'A111 111 1111',
+                'viewDocsUrl' => 'https://view.docs.url',
+                'checkDatesUrl' => 'https://check.dates.url',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            ->with(Matchers::equalTo($expectedMailParams));
+
+        // Call test method
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
     }
 
-    public function testSendRegistrationCompleteEmailWithPaymentAndPersonToNotify(): void
+    public function testSendRegistrationCompleteEmailWithOnlinePaymentAndPersonToNotify(): void
     {
         $lpa = new Lpa([
             'document' => new Document([
@@ -164,16 +217,404 @@ class CommunicationTest extends AbstractEmailServiceTest
     }
 
 
-    public function testSendRegistrationCompleteEmailWithPaymentNoPersonToNotify(): void
+    public function testSendRegistrationCompleteEmailWithOnlinePaymentNoPersonToNotify(): void
     {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_HW,
+                'donor' => [
+                    'name' => new LongName('{"title":"Father", "first":"Spodo", "last":"Komodo"}')
+                ]
+            ]),
+            'payment' => new Payment([
+                'amount' => '200000.00',
+                'email' => new EmailAddress(['address' => 'paymentfrom@email.com']),
+                'reference' => '12345678',
+                'date' => new DateTime('2021-09-24 07:54:00'),
+            ]),
+        ]);
+
+        // We're not testing the URLs or LPA ID formatting in this case
+        $this->service->shouldReceive('formatLpaId')
+            ->andReturn('A22222222');
+        $this->service->shouldReceive('url')
+            ->andReturn('https://some.url');
+
+        // We are testing moneyFormat()
+        $this->service->shouldReceive('moneyFormat')
+            ->with('200000.00')
+            ->andReturn('200,000.00');
+
+        // Expected data passed to send()
+        $expectedMailParams = new MailParameters(
+            ['test@email.com', 'paymentfrom@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT,
+            [
+                'donorName' => 'Father Spodo Komodo',
+                'lpaType' => 'health and welfare',
+                'lpaId' => 'A22222222',
+                'viewDocsUrl' => 'https://some.url',
+                'checkDatesUrl' => 'https://some.url',
+                'lpaTypeTitleCase' => 'Health and welfare',
+                'lpaPaymentReference' => '12345678',
+                'lpaPaymentDate' => '24 September 2021 - 8:54am',
+                'paymentAmount' => '200,000.00',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            //->with(Matchers::equalTo($expectedMailParams));
+            ->with(Mockery::on(function ($actualMailParams) use ($expectedMailParams) {
+                MatcherAssert::assertThat($expectedMailParams, Matchers::equalTo($actualMailParams));
+                return true;
+            }));
+
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
     }
 
-    public function testSendRegistrationCompleteEmailWithReducedPaymentAndPersonToNotify(): void
+    public function testSendRegistrationCompleteEmailWithReducedOnlinePaymentAndPersonToNotify(): void
     {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_HW,
+                'donor' => [
+                    'name' => new LongName('{"title":"Father", "first":"Spodo", "last":"Komodo"}')
+                ]
+            ]),
+            'payment' => new Payment([
+                'amount' => '200000.00',
+                'email' => new EmailAddress(['address' => 'paymentfrom@email.com']),
+                'reference' => '12345678',
+                'date' => new DateTime('2021-09-24 07:54:00'),
+            ]),
+        ]);
+
+        // We're not testing the URLs or LPA ID formatting in this case
+        $this->service->shouldReceive('formatLpaId')
+            ->andReturn('A22222222');
+        $this->service->shouldReceive('url')
+            ->andReturn('https://some.url');
+
+        // We are testing moneyFormat()
+        $this->service->shouldReceive('moneyFormat')
+            ->with('200000.00')
+            ->andReturn('200,000.00');
+
+        // Expected data passed to send()
+        $expectedMailParams = new MailParameters(
+            ['test@email.com', 'paymentfrom@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT,
+            [
+                'donorName' => 'Father Spodo Komodo',
+                'lpaType' => 'health and welfare',
+                'lpaId' => 'A22222222',
+                'viewDocsUrl' => 'https://some.url',
+                'checkDatesUrl' => 'https://some.url',
+                'lpaTypeTitleCase' => 'Health and welfare',
+                'lpaPaymentReference' => '12345678',
+                'lpaPaymentDate' => '24 September 2021 - 8:54am',
+                'paymentAmount' => '200,000.00',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            //->with(Matchers::equalTo($expectedMailParams));
+            ->with(Mockery::on(function ($actualMailParams) use ($expectedMailParams) {
+                MatcherAssert::assertThat($expectedMailParams, Matchers::equalTo($actualMailParams));
+                return true;
+            }));
+
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
     }
 
-    public function testSendRegistrationCompleteEmailWithPaymentNoPersonToNotify(): void
+    public function testSendRegistrationCompleteEmailWithReducedOnlinePaymentNoPersonToNotify(): void
     {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_HW,
+                'donor' => [
+                    'name' => new LongName('{"title":"Father", "first":"Spodo", "last":"Komodo"}')
+                ]
+            ]),
+            'payment' => new Payment([
+                'amount' => '200000.00',
+                'email' => new EmailAddress(['address' => 'paymentfrom@email.com']),
+                'reference' => '12345678',
+                'date' => new DateTime('2021-09-24 07:54:00'),
+            ]),
+        ]);
+
+        // We're not testing the URLs or LPA ID formatting in this case
+        $this->service->shouldReceive('formatLpaId')
+            ->andReturn('A22222222');
+        $this->service->shouldReceive('url')
+            ->andReturn('https://some.url');
+
+        // We are testing moneyFormat()
+        $this->service->shouldReceive('moneyFormat')
+            ->with('200000.00')
+            ->andReturn('200,000.00');
+
+        // Expected data passed to send()
+        $expectedMailParams = new MailParameters(
+            ['test@email.com', 'paymentfrom@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT,
+            [
+                'donorName' => 'Father Spodo Komodo',
+                'lpaType' => 'health and welfare',
+                'lpaId' => 'A22222222',
+                'viewDocsUrl' => 'https://some.url',
+                'checkDatesUrl' => 'https://some.url',
+                'lpaTypeTitleCase' => 'Health and welfare',
+                'lpaPaymentReference' => '12345678',
+                'lpaPaymentDate' => '24 September 2021 - 8:54am',
+                'paymentAmount' => '200,000.00',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            //->with(Matchers::equalTo($expectedMailParams));
+            ->with(Mockery::on(function ($actualMailParams) use ($expectedMailParams) {
+                MatcherAssert::assertThat($expectedMailParams, Matchers::equalTo($actualMailParams));
+                return true;
+            }));
+
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
+    }
+
+    public function testSendRegistrationCompleteEmailWithChequePaymentAndPersonToNotify(): void
+    {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_HW,
+                'donor' => [
+                    'name' => new LongName('{"title":"Father", "first":"Spodo", "last":"Komodo"}')
+                ]
+            ]),
+            'payment' => new Payment([
+                'amount' => '200000.00',
+                'email' => new EmailAddress(['address' => 'paymentfrom@email.com']),
+                'reference' => '12345678',
+                'date' => new DateTime('2021-09-24 07:54:00'),
+            ]),
+        ]);
+
+        // We're not testing the URLs or LPA ID formatting in this case
+        $this->service->shouldReceive('formatLpaId')
+            ->andReturn('A22222222');
+        $this->service->shouldReceive('url')
+            ->andReturn('https://some.url');
+
+        // We are testing moneyFormat()
+        $this->service->shouldReceive('moneyFormat')
+            ->with('200000.00')
+            ->andReturn('200,000.00');
+
+        // Expected data passed to send()
+        $expectedMailParams = new MailParameters(
+            ['test@email.com', 'paymentfrom@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT,
+            [
+                'donorName' => 'Father Spodo Komodo',
+                'lpaType' => 'health and welfare',
+                'lpaId' => 'A22222222',
+                'viewDocsUrl' => 'https://some.url',
+                'checkDatesUrl' => 'https://some.url',
+                'lpaTypeTitleCase' => 'Health and welfare',
+                'lpaPaymentReference' => '12345678',
+                'lpaPaymentDate' => '24 September 2021 - 8:54am',
+                'paymentAmount' => '200,000.00',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            //->with(Matchers::equalTo($expectedMailParams));
+            ->with(Mockery::on(function ($actualMailParams) use ($expectedMailParams) {
+                MatcherAssert::assertThat($expectedMailParams, Matchers::equalTo($actualMailParams));
+                return true;
+            }));
+
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
+    }
+
+
+    public function testSendRegistrationCompleteEmailWithChequePaymentNoPersonToNotify(): void
+    {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_HW,
+                'donor' => [
+                    'name' => new LongName('{"title":"Father", "first":"Spodo", "last":"Komodo"}')
+                ]
+            ]),
+            'payment' => new Payment([
+                'amount' => '200000.00',
+                'email' => new EmailAddress(['address' => 'paymentfrom@email.com']),
+                'reference' => '12345678',
+                'date' => new DateTime('2021-09-24 07:54:00'),
+            ]),
+        ]);
+
+        // We're not testing the URLs or LPA ID formatting in this case
+        $this->service->shouldReceive('formatLpaId')
+            ->andReturn('A22222222');
+        $this->service->shouldReceive('url')
+            ->andReturn('https://some.url');
+
+        // We are testing moneyFormat()
+        $this->service->shouldReceive('moneyFormat')
+            ->with('200000.00')
+            ->andReturn('200,000.00');
+
+        // Expected data passed to send()
+        $expectedMailParams = new MailParameters(
+            ['test@email.com', 'paymentfrom@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT,
+            [
+                'donorName' => 'Father Spodo Komodo',
+                'lpaType' => 'health and welfare',
+                'lpaId' => 'A22222222',
+                'viewDocsUrl' => 'https://some.url',
+                'checkDatesUrl' => 'https://some.url',
+                'lpaTypeTitleCase' => 'Health and welfare',
+                'lpaPaymentReference' => '12345678',
+                'lpaPaymentDate' => '24 September 2021 - 8:54am',
+                'paymentAmount' => '200,000.00',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            //->with(Matchers::equalTo($expectedMailParams));
+            ->with(Mockery::on(function ($actualMailParams) use ($expectedMailParams) {
+                MatcherAssert::assertThat($expectedMailParams, Matchers::equalTo($actualMailParams));
+                return true;
+            }));
+
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
+    }
+
+    public function testSendRegistrationCompleteEmailWithReducedChequePaymentAndPersonToNotify(): void
+    {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_HW,
+                'donor' => [
+                    'name' => new LongName('{"title":"Father", "first":"Spodo", "last":"Komodo"}')
+                ]
+            ]),
+            'payment' => new Payment([
+                'amount' => '200000.00',
+                'email' => new EmailAddress(['address' => 'paymentfrom@email.com']),
+                'reference' => '12345678',
+                'date' => new DateTime('2021-09-24 07:54:00'),
+            ]),
+        ]);
+
+        // We're not testing the URLs or LPA ID formatting in this case
+        $this->service->shouldReceive('formatLpaId')
+            ->andReturn('A22222222');
+        $this->service->shouldReceive('url')
+            ->andReturn('https://some.url');
+
+        // We are testing moneyFormat()
+        $this->service->shouldReceive('moneyFormat')
+            ->with('200000.00')
+            ->andReturn('200,000.00');
+
+        // Expected data passed to send()
+        $expectedMailParams = new MailParameters(
+            ['test@email.com', 'paymentfrom@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT,
+            [
+                'donorName' => 'Father Spodo Komodo',
+                'lpaType' => 'health and welfare',
+                'lpaId' => 'A22222222',
+                'viewDocsUrl' => 'https://some.url',
+                'checkDatesUrl' => 'https://some.url',
+                'lpaTypeTitleCase' => 'Health and welfare',
+                'lpaPaymentReference' => '12345678',
+                'lpaPaymentDate' => '24 September 2021 - 8:54am',
+                'paymentAmount' => '200,000.00',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            //->with(Matchers::equalTo($expectedMailParams));
+            ->with(Mockery::on(function ($actualMailParams) use ($expectedMailParams) {
+                MatcherAssert::assertThat($expectedMailParams, Matchers::equalTo($actualMailParams));
+                return true;
+            }));
+
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
+    }
+
+    public function testSendRegistrationCompleteEmailWithReducedChequePaymentNoPersonToNotify(): void
+    {
+        $lpa = new Lpa([
+            'document' => new Document([
+                'type' => Document::LPA_TYPE_HW,
+                'donor' => [
+                    'name' => new LongName('{"title":"Father", "first":"Spodo", "last":"Komodo"}')
+                ]
+            ]),
+            'payment' => new Payment([
+                'amount' => '200000.00',
+                'email' => new EmailAddress(['address' => 'paymentfrom@email.com']),
+                'reference' => '12345678',
+                'date' => new DateTime('2021-09-24 07:54:00'),
+            ]),
+        ]);
+
+        // We're not testing the URLs or LPA ID formatting in this case
+        $this->service->shouldReceive('formatLpaId')
+            ->andReturn('A22222222');
+        $this->service->shouldReceive('url')
+            ->andReturn('https://some.url');
+
+        // We are testing moneyFormat()
+        $this->service->shouldReceive('moneyFormat')
+            ->with('200000.00')
+            ->andReturn('200,000.00');
+
+        // Expected data passed to send()
+        $expectedMailParams = new MailParameters(
+            ['test@email.com', 'paymentfrom@email.com'],
+            AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT,
+            [
+                'donorName' => 'Father Spodo Komodo',
+                'lpaType' => 'health and welfare',
+                'lpaId' => 'A22222222',
+                'viewDocsUrl' => 'https://some.url',
+                'checkDatesUrl' => 'https://some.url',
+                'lpaTypeTitleCase' => 'Health and welfare',
+                'lpaPaymentReference' => '12345678',
+                'lpaPaymentDate' => '24 September 2021 - 8:54am',
+                'paymentAmount' => '200,000.00',
+            ]
+        );
+
+        $this->mailTransport->shouldReceive('send')
+            //->with(Matchers::equalTo($expectedMailParams));
+            ->with(Mockery::on(function ($actualMailParams) use ($expectedMailParams) {
+                MatcherAssert::assertThat($expectedMailParams, Matchers::equalTo($actualMailParams));
+                return true;
+            }));
+
+        $result = $this->service->sendRegistrationCompleteEmail($lpa);
+
+        $this->assertTrue($result);
     }
 
     public function testSendRegistrationCompleteEmailSendFails(): void
