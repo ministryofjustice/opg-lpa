f908c415eebebd6881b3351be09b258df98b14ea
William Falconer <williamfalconeruk@users.noreply.github.com>
2022-04-26 11:34:08 +0100

    LPAL-709 attach region Level DB Events Subscription to PagerDuty. (#896)
    * Remove Account level subscription
    
    * swap pagerduty over
    
    * Fix event subscription
    
    * create new subscription
    
    * Reset Account level as we keep for now.
    
    * restore sns topic

--- terraform/region/modules/region/pagerduty.tf
+++ terraform/region/modules/region/pagerduty.tf
@@ -2,14 +2,28 @@ data "pagerduty_service" "pagerduty_ops" {
   name = local.pager_duty_ops_service_name
 }
 
+data "pagerduty_service" "pagerduty_db_alerts" {
+  name = local.pager_duty_db_service_name
+}
+
 data "pagerduty_vendor" "cloudwatch" {
   name = "Cloudwatch"
+}
+
+data "pagerduty_vendor" "custom_events" {
+  name = "Custom Event Transformer"
 }
 
 resource "pagerduty_service_integration" "cloudwatch_integration" {
   name    = "${data.pagerduty_vendor.cloudwatch.name} ${local.region_name} Region Ops"
   service = data.pagerduty_service.pagerduty_ops.id
   vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "pagerduty_service_integration" "db_alerts_integration" {
+  name    = "${local.account_name} ${local.region_name} Region DB Alerts"
+  service = data.pagerduty_service.pagerduty_db_alerts.id
+  vendor  = data.pagerduty_vendor.custom_events.id
 }
 
 resource "aws_sns_topic_subscription" "cloudwatch_account_ops_sns_subscription" {
@@ -25,4 +39,11 @@ resource "aws_sns_topic_subscription" "cloudwatch_elasticache_alerts_sns_subscri
   protocol               = "https"
   endpoint_auto_confirms = true
   endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.cloudwatch_integration.integration_key}/enqueue"
+}
+
+resource "aws_sns_topic_subscription" "rds_events_sns_subscription" {
+  topic_arn              = aws_sns_topic.rds_events.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.db_alerts_integration.integration_key}/enqueue"
 }
