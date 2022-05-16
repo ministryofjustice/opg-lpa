1d2ce8274f2179ef3b791ddb417a3684cbe1e311
William Falconer <william.falconer@digital.justice.gov.uk>
2022-05-16 12:00:21 +0100

    Refactor: - move DNS to own module (except internal) - move config pipeline file - refactor and add out environment level module outputs - name refactoring - provider alias naming

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -2,7 +2,7 @@ module "eu-west-1" {
   source                   = "./modules/environment"
   account                  = local.account
   account_name             = local.account_name
-  environment_name         = local.environment
+  environment_name         = local.environment_name
   region_name              = "eu-west-1"
   container_version        = var.container_version
   lambda_container_version = var.lambda_container_version
@@ -10,17 +10,50 @@ module "eu-west-1" {
   management_role          = var.management_role
   default_role             = var.default_role
 
-
   providers = {
-    aws            = aws
+    aws            = aws.eu_west_1
     aws.management = aws.management
   }
 }
 
+module "eu-west-2" {
+  count                    = local.account_name == "development" && local.account.dr_enabled ? 1 : 0
+  source                   = "./modules/environment"
+  account                  = local.account
+  account_name             = local.account_name
+  environment_name         = local.environment_name
+  region_name              = "eu-west-2"
+  container_version        = var.container_version
+  lambda_container_version = var.lambda_container_version
+  pagerduty_token          = var.pagerduty_token
+  management_role          = var.management_role
+  default_role             = var.default_role
+
+  providers = {
+    aws            = aws.eu_west_2
+    aws.management = aws.management
+  }
+}
+
+module "environment_dns" {
+  source = "./modules/dns"
+  providers = {
+    aws = aws
+    aws.management = aws.management
+  }
+  account_name     = local.account_name
+  environment_name = local.environment_name
+  front_dns_name   = !local.account.dr_enabled ? module.eu-west-1.front_dns_name : module.eu-west-2[0].front_dns_name
+  front_zone_id    = !local.account.dr_enabled ? module.eu-west-1.front_zone_id : module.eu-west-2[0].front_zone_id
+  admin_dns_name   = !local.account.dr_enabled ? module.eu-west-1.admin_dns_name : module.eu-west-2[0].admin_dns_name
+  admin_zone_id    = !local.account.dr_enabled ? module.eu-west-1.admin_zone_id : module.eu-west-2[0].admin_zone_id
+
+}
+
 output "admin-domain" {
-  value = module.eu-west-1.admin-domain
+  value = module.environment_dns.admin_domain
 }
 
 output "front-domain" {
-  value = module.eu-west-1.front-domain
+  value = module.environment_dns.front_domain
 }
