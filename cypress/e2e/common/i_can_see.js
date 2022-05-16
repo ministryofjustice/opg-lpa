4114ffa6b1e439b463db96caec19013c4fca5b5a
Mabel Li <mabel.li@digital.justice.gov.uk>
2022-05-16 12:16:39 +0100

    Add Continuation Sheet 4 test for PF LPA date check

--- cypress/e2e/common/i_can_see.js
+++ cypress/e2e/common/i_can_see.js
@@ -16,11 +16,25 @@ const seeReuseDetailsLink = (shouldExist) => {
     });
 }
 
-Then('I can see fields for the donor, certificate provider, attorney, applicant', () => {
+Then('I can see fields for the PF donor, certificate provider, attorney, applicant', () => {
 		const expectedNames = ["Mr Dead Pool (the donor)", "Mr Cee Pee (certificate provider)",
 													 "Mr A Att (attorney)", "Mr Dead Pool (applicant)"]
 		cy.get("[data-cy=person-name]").then(($name) => {
-				var names = $name.map(function() {return this.innerText}).toArray();
+				const names = $name.map(function() {return this.innerText}).toArray();
 				expect(names).to.deep.eq(expectedNames);
 		})
+})
+
+Then('I can see fields for the HW donor, certificate provider, attorney, applicant', () => {
+		const expectedNames = ["Miss Rapunzel Tower (the donor)", "Mr Cee Pee (certificate provider)",
+													 "Mr A Att (attorney)", "Miss Rapunzel Tower (applicant)"]
+		cy.get("[data-cy=person-name]").then(($name) => {
+				const names = $name.map(function() {return this.innerText}).toArray();
+				expect(names).to.deep.eq(expectedNames);
+		})
+})
+
+Then('I can see a reminder on signing continuation sheet 4', () => {
+    const text = "They must have signed continuation sheet 4 after the ‘certificate provider’ has signed section 10 of the LPA form."
+    cy.get("[data-cy=continuation-sheet-info]").should("have.text", text);
 })
