34de672bd5bc334b71e9267196730afad46216b5
William Falconer <william.falconer@digital.justice.gov.uk>
2022-05-18 11:55:23 +0100

    remove data sources no longer needed here

--- terraform/environment/modules/environment/dns.tf
+++ terraform/environment/modules/environment/dns.tf
@@ -1,13 +1,3 @@
-data "aws_route53_zone" "opg_service_justice_gov_uk" {
-  provider = aws.management
-  name     = "opg.service.justice.gov.uk"
-}
-
-data "aws_route53_zone" "live_service_lasting_power_of_attorney" {
-  provider = aws.management
-  name     = "lastingpowerofattorney.service.gov.uk"
-}
-
 locals {
   dns_namespace_internal = (
     var.account_name == "development" ?
