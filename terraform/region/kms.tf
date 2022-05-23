89ec951363a7db3fd08e44bbb7a2792b13d37916
William Falconer <williamfalconeruk@users.noreply.github.com>
2022-05-23 17:11:30 +0100

    LPAL-798 add KMS aliases to London region (#942)
    * add aliases to London region
    
    * Lock file

--- terraform/region/kms.tf
+++ terraform/region/kms.tf
@@ -16,6 +16,12 @@ resource "aws_kms_replica_key" "multi_region_secrets_encryption_key_replica" {
   provider                = aws.eu-west-2
 }
 
+resource "aws_kms_alias" "multi_region_secrets_encryption_alias_replica" {
+  name          = "alias/mrk_secrets_encryption_key-${terraform.workspace}"
+  target_key_id = aws_kms_replica_key.multi_region_secrets_encryption_key_replica.key_id
+  provider      = aws.eu-west-2
+}
+
 resource "aws_kms_key" "multi_region_db_snapshot_key" {
   enable_key_rotation = true
   provider            = aws.eu-west-1
@@ -32,4 +38,10 @@ resource "aws_kms_replica_key" "multi_region_db_snapshot_key_replica" {
   deletion_window_in_days = 7
   primary_key_arn         = aws_kms_key.multi_region_db_snapshot_key.arn
   provider                = aws.eu-west-2
+}
+
+resource "aws_kms_alias" "multi_region_db_snapshot_alias_replica" {
+  name          = "alias/mrk_db_snapshot_key-${terraform.workspace}"
+  target_key_id = aws_kms_replica_key.multi_region_db_snapshot_key_replica.key_id
+  provider      = aws.eu-west-2
 }
