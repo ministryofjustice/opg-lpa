c06b235bdfb3ef4d7483668905afdb1f74881dd9
William Falconer <william.falconer@digital.justice.gov.uk>
2022-06-09 10:41:22 +0100

    Environment Level PD Hard Code

--- terraform/environment/modules/environment/pagerduty.tf
+++ terraform/environment/modules/environment/pagerduty.tf
@@ -1,3 +1,15 @@
+#temporary fix due to https://github.com/PagerDuty/terraform-provider-pagerduty/issues/523
+
+locals{
+    ops_service_id  = "PP0UDI9"
+    cloudwatch_service_id = (
+        var.account_name == "production" ?
+        "PVIXHGS" :
+        "PS99H42"
+    )
+}
+
+/*
 data "pagerduty_service" "pagerduty" {
   name = var.account.pagerduty_service_name
 }
@@ -5,19 +17,23 @@ data "pagerduty_service" "pagerduty" {
 data "pagerduty_service" "pagerduty_ops" {
   name = local.pager_duty_ops_service_name
 }
+*/
 
 data "pagerduty_vendor" "cloudwatch" {
   name = "Cloudwatch"
 }
 
+
 resource "pagerduty_service_integration" "cloudwatch_integration" {
   name    = "${data.pagerduty_vendor.cloudwatch.name} ${var.environment_name} Environment"
-  service = data.pagerduty_service.pagerduty.id
+  #service = data.pagerduty_service.pagerduty.id
+  service  = local.cloudwatch_service_id
   vendor  = data.pagerduty_vendor.cloudwatch.id
 }
 
 resource "pagerduty_service_integration" "cloudwatch_integration_ops" {
   name    = "${data.pagerduty_vendor.cloudwatch.name} ${var.environment_name} Environment Ops"
-  service = data.pagerduty_service.pagerduty_ops.id
+  #service = data.pagerduty_service.pagerduty_ops.id
+  service = local.ops_service_id
   vendor  = data.pagerduty_vendor.cloudwatch.id
 }
