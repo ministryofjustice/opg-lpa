8a39e230f71bdbd7d8370061055705b5cdba1cf7
Mabel Li <mabel.li@digital.justice.gov.uk>
2022-06-13 12:30:32 +0100

    Refactor and remove redundant HW LPA e2e tests

--- cypress/e2e/common/i_can_see.js
+++ cypress/e2e/common/i_can_see.js
@@ -20,7 +20,7 @@ Then('I cannot see continuation sheet reminders', () => {
     cy.get('[data-cy=continuation-sheet-info]').should('not.exist');
 });
 
-Then('I can see fields for the PF donor, certificate provider, attorney, applicant', () => {
+Then('I can see fields for the donor, certificate provider, attorney, applicant', () => {
     const expectedNames = ['Mr Dead Pool (the donor)', 'Mr Cee Pee (certificate provider)',
                            'Mr A Att (attorney)', 'Mr Dead Pool (applicant)']
 	    cy.get('[data-cy=person-name]').then(($name) => {
@@ -29,36 +29,13 @@ Then('I can see fields for the PF donor, certificate provider, attorney, applica
 		})
 })
 
-Then('I can see fields for the HW donor, certificate provider, attorney, applicant', () => {
-    const expectedNames = ['Miss Rapunzel Tower (the donor)', 'Mr Cee Pee (certificate provider)',
-                                                 'Mr A Att (attorney)', 'Miss Rapunzel Tower (applicant)']
-    cy.get('[data-cy=person-name]').then(($name) => {
-        const names = $name.map(function() { return this.innerText }).toArray();
-        expect(names).to.deep.eq(expectedNames);
-    })
-})
-
-Then('I can see the revelant HW continuation sheet notes', () => {
-    const text = ["As the donor cannot sign the LPA, a trusted person will need to sign 'continuation sheet 3' on the donor's behalf. The continuation sheet is included at the end of the LPA form."]
-    cy.get('[data-cy=continuation-sheet-notes]').should('have.length', 1);
-    cy.get('[data-cy=continuation-sheet-notes]').then(($note) => {
-        const notes = $note.map(function() { return this.innerText }).toArray();
-        expect(notes).to.deep.eq(text);
-    })
-})
-
 Then('I can see a reminder to sign continuation sheet 1 and 2', () => {
     const text = 'You must have signed and dated continuation sheets 1 and 2 before you signed section 9 of the LPA, or on the same day.'
     cy.get('[data-cy=continuation-sheet-info]').should('contain.text', text);
 })
 
-Then('I can see a reminder to sign continuation sheet 3 for PF', () => {
+Then('I can see a reminder to sign continuation sheet 3', () => {
     const text = 'This person must have signed continuation sheet 3 before the certificate provider has signed section 10.'
-    cy.get('[data-cy=continuation-sheet-info]').should('contain.text', text);
-})
-
-Then('I can see a reminder to sign continuation sheet 3 for HW', () => {
-    const text = 'This person must have signed continuation sheet 3 on the same day as they sign section 5 and before the certificate provider signs section 10.'
     cy.get('[data-cy=continuation-sheet-info]').should('contain.text', text);
 })
 
