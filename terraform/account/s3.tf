570e69f3f0d21fb7100821721846510a40389f28
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-06-24 11:51:22 +0100

    Correct string to set

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -68,7 +68,7 @@ data "aws_iam_policy_document" "iam_loadbalancer_kms" {
     ]
 
     principals {
-      identifiers = "delivery.logs.amazonaws.com"
+      identifiers = ["delivery.logs.amazonaws.com"]
 
       type = "Service"
     }
