bca82e96633fd69e23b987c8f05ff085efde6870
William Falconer <william.falconer@digital.justice.gov.uk>
2022-06-24 10:16:29 +0100

    to string conversion for env vars - port number

--- terraform/environment/modules/environment/ecs_seeding.tf
+++ terraform/environment/modules/environment/ecs_seeding.tf
@@ -85,7 +85,7 @@ locals {
       "environment" : [
         { "name" : "OPG_LPA_POSTGRES_NAME", "value" : "${local.db.name}" },
         { "name" : "OPG_LPA_POSTGRES_HOSTNAME", "value" : "${local.db.endpoint}" },
-        { "name" : "OPG_LPA_POSTGRES_PORT", "value" : "${local.db.port}" },
+        { "name" : "OPG_LPA_POSTGRES_PORT", "value" : "${tostring(local.db.port)}" },
         { "name" : "OPG_LPA_STACK_ENVIRONMENT", "value" : "${var.account_name}" }
       ]
   })
