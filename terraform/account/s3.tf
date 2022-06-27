9a55e245304f07e27f78cd0d59b3957be0f52268
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-06-27 13:12:54 +0100

    Add access log key data lookup to account

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -1,3 +1,7 @@
+data "aws_kms_key" "access_log_key" {
+  key_id = "alias/mrk_access_logs_lb_encryption_key-${local.account_name}"
+}
+
 data "aws_elb_service_account" "main" {
   region = "eu-west-1"
 }
