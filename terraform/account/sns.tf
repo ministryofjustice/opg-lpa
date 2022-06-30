46c431033753d41cad77941ca4bff6c4594e00ed
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-06-30 15:47:01 +0100

    Use provider-level default tags for account

--- terraform/account/sns.tf
+++ terraform/account/sns.tf
@@ -1,16 +1,16 @@
 #tfsec:ignore:AWS016 fix for now, to be followed with a CMK KMS
 resource "aws_sns_topic" "cloudwatch_to_slack_breakglass_alerts" {
   name = "CloudWatch-to-PagerDuty-${local.account_name}-Breakglass-alert"
-  tags = merge(local.default_tags, local.shared_component_tag)
+  tags = local.shared_component_tag
 }
 #tfsec:ignore:AWS016 unsupported for this type of alert
 resource "aws_sns_topic" "cloudwatch_to_slack_elasticache_alerts" {
   name = "CloudWatch-to-PagerDuty-${local.account_name}-elasticache-alert"
-  tags = merge(local.default_tags, local.front_component_tag)
+  tags = local.front_component_tag
 }
 
 #tfsec:ignore:AWS016 unsupported for this type of alert
 resource "aws_sns_topic" "cloudwatch_to_account_ops_alerts" {
   name = "CloudWatch-to-PagerDuty-${local.account_name}-account-ops-alert"
-  tags = merge(local.default_tags, local.shared_component_tag)
+  tags = local.shared_component_tag
 }
