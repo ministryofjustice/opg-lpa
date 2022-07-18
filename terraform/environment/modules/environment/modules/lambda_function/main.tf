254486dab81f47d8d103e1c0d232a1e17541056c
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-07-18 10:44:25 +0100

    Ignore (for now) remaining alerts

--- terraform/environment/modules/environment/modules/lambda_function/main.tf
+++ terraform/environment/modules/environment/modules/lambda_function/main.tf
@@ -1,3 +1,4 @@
+#tfsec:ignore:aws-lambda-enable-tracing - To be enabled along with apps
 resource "aws_lambda_function" "lambda_function" {
   function_name = var.lambda_name
   image_uri     = var.image_uri
