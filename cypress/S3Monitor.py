cfc6d79f74a34d6b7c194cca8da29e6657e8ca4b
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-07-19 10:58:47 +0100

    First pass at unifying cypress scripts
    * Remove userNumber.js (now done in Python)
    * Replicate functionality of start.sh in Python
    * Move S3Monitor into a class
    * Start S3Monitor on a thread in main script
    
    Arguments are not pulled from command line yet, but hard-coded.
    
    Also need to get the return code from cypress and use that
    as return code for whole script.

--- cypress/S3Monitor.py
+++ cypress/S3Monitor.py
@@ -1,176 +1,184 @@
 import boto3
 import json
-import os
 import quopri
 import time
 import re
 import argparse
-
-mailbox_bucket = (
-    "opg-lpa-casper-mailbox"  # this might get renamed once casper tests are turned off
-)
-activation_emails_path = "cypress/activation_emails"
-parser = argparse.ArgumentParser(
-    description="Monitor S3 bucket for emails sent during tests of Make an LPA."
-)
-parser.add_argument("-v", action="store_true", help="verbose", default=False)
-args = parser.parse_args()
+from pathlib import Path
 
 
-def assume_role_and_get_client():
-    sts = boto3.client(
-        "sts",
-        region_name="eu-west-1",
-    )
+class S3Monitor:
 
-    if os.getenv("CYPRESS_CI"):
-        print("S3Monitor starting, Assuming CI role")
-        role_arn = "arn:aws:iam::050256574573:role/opg-lpa-ci"
-    else:
-        print("S3Monitor starting, Assuming operator role")
-        role_arn = "arn:aws:iam::050256574573:role/operator"
+    MAILBOX_BUCKET = "opg-lpa-casper-mailbox"
+    ACTIVATION_EMAILS_PATH = (Path(__file__).parent / "activation_emails").resolve()
 
-    result = sts.assume_role(
-        RoleArn=role_arn,
-        RoleSessionName="session1",
-    )
+    def __init__(self, options):
+        """
+        :param options: dict with v (verbose) and c (is in CI) boolean properties
+        """
+        self.verbose = options["v"]
+        self.in_ci = options["c"]
+        self.s3Client = self.assume_role_and_get_client()
 
-    s3Client = boto3.client(
-        "s3",
-        aws_access_key_id=result["Credentials"]["AccessKeyId"],
-        aws_secret_access_key=result["Credentials"]["SecretAccessKey"],
-        aws_session_token=result["Credentials"]["SessionToken"],
-    )
+    def assume_role_and_get_client(self):
+        sts = boto3.client(
+            "sts",
+            region_name="eu-west-1",
+        )
 
-    return s3Client
+        if self.in_ci:
+            print("S3Monitor starting, Assuming CI role")
+            role_arn = "arn:aws:iam::050256574573:role/opg-lpa-ci"
+        else:
+            print("S3Monitor starting, Assuming operator role")
+            role_arn = "arn:aws:iam::050256574573:role/operator"
 
+        result = sts.assume_role(
+            RoleArn=role_arn,
+            RoleSessionName="session1",
+        )
 
-# Extract the plus part from emails of the form:
-# basename+pluspart@example.com
-def getPlusPartFromEmailAddress(email):
-    match = re.search("[^\+]\+(.+)@", email)
-    if match is None:
-        return ""
-    return match.group(1)
+        return boto3.client(
+            "s3",
+            aws_access_key_id=result["Credentials"]["AccessKeyId"],
+            aws_secret_access_key=result["Credentials"]["SecretAccessKey"],
+            aws_session_token=result["Credentials"]["SessionToken"],
+        )
 
+    # Extract the plus part from emails of the form:
+    # basename+pluspart@example.com
+    def getPlusPartFromEmailAddress(self, email):
+        match = re.search("[^\+]\+(.+)@", email)
+        if match is None:
+            return ""
+        return match.group(1)
 
-def parseBody(bodyContent, subject, thetype, linkRegex):
-    regex = "https:\/\/\S+" + linkRegex + "\/[a-zA-Z0-9]+"
+    def parseBody(self, bodyContent, subject, thetype, linkRegex):
+        regex = "https:\/\/\S+" + linkRegex + "\/[a-zA-Z0-9]+"
 
-    match = re.search(regex, bodyContent)
+        match = re.search(regex, bodyContent)
 
-    if match is not None:
-        s = match.start()
-        e = match.end()
-        activationLink = bodyContent[s:e]
-        printIfVerbose(f"{ thetype } link { activationLink }")
+        if match is not None:
+            s = match.start()
+            e = match.end()
+            activationLink = bodyContent[s:e]
+            self.printIfVerbose(f"{ thetype } link { activationLink }")
 
-        emailRegex = "To: (.+\\+.+)\\n"
+            emailRegex = "To: (.+\\+.+)\\n"
 
-        emailMatch = re.search(emailRegex, bodyContent)
-        if emailMatch is not None:
-            toEmail = emailMatch.group(1)
+            emailMatch = re.search(emailRegex, bodyContent)
+            if emailMatch is not None:
+                toEmail = emailMatch.group(1)
 
-            userId = getPlusPartFromEmailAddress(toEmail)
-            printIfVerbose(f"userId {userId}")
+                userId = self.getPlusPartFromEmailAddress(toEmail)
+                self.printIfVerbose(f"userId {userId}")
 
-            if userId != "":
-                contents = f"{toEmail[:-1]},{activationLink}"
-                filePath = f"{activation_emails_path}/{userId}.{thetype}"
-                emailFile = open(filePath, "w")
-                emailFile.write(contents)
-                emailFile.close()
-                printIfVerbose(f"wrote file for {thetype} email to {filePath}")
+                if userId != "":
+                    contents = f"{toEmail[:-1]},{activationLink}"
+                    filePath = (
+                        Path(S3Monitor.ACTIVATION_EMAILS_PATH) / f"{userId}.{thetype}"
+                    )
+                    emailFile = open(filePath, "w")
+                    emailFile.write(contents)
+                    emailFile.close()
+                    self.printIfVerbose(f"wrote file for {thetype} email to {filePath}")
+                else:
+                    self.printIfVerbose(
+                        f"could not get valid user ID from email address {toEmail}"
+                    )
             else:
-                printIfVerbose(
-                    f"could not get valid user ID from email address {toEmail}"
+                self.printIfVerbose(
+                    "unable to find email regex to derive the To: field"
                 )
         else:
-            printIfVerbose("unable to find email regex to derive the To: field")
-    else:
-        printIfVerbose(f"Message: {subject} does not match regex {regex}")
+            self.printIfVerbose(f"Message: {subject} does not match regex {regex}")
 
+    def parse_email(self, bodyContent, s3Key):
+        self.printIfVerbose("\n-------- START PARSE EMAIL")
 
-def parse_email(bodyContent, s3Key):
-    printIfVerbose("\n-------- START PARSE EMAIL")
+        activate_subject = "Subject: Activate your lasting power of attorney account"
+        reset_password_subject = "Subject: Password reset request"
+        reset_password_no_account_subject = "Subject: Request to reset password"
 
-    activate_subject = "Subject: Activate your lasting power of attorney account"
-    reset_password_subject = "Subject: Password reset request"
-    reset_password_no_account_subject = "Subject: Request to reset password"
+        if re.search(activate_subject, bodyContent, re.IGNORECASE) is not None:
+            return self.parseBody(
+                bodyContent, activate_subject, "activation", "signup\/confirm"
+            )
+        else:
+            self.printIfVerbose("email is not an activation email")
 
-    if re.search(activate_subject, bodyContent, re.IGNORECASE) is not None:
-        return parseBody(bodyContent, activate_subject, "activation", "signup\/confirm")
-    else:
-        printIfVerbose("email is not an activation email")
+        if re.search(reset_password_subject, bodyContent, re.IGNORECASE) is not None:
+            return self.parseBody(
+                bodyContent,
+                reset_password_subject,
+                "passwordreset",
+                "forgot-password\/reset",
+            )
+        else:
+            self.printIfVerbose("email is not a forgotten password email")
 
-    if re.search(reset_password_subject, bodyContent, re.IGNORECASE) is not None:
-        return parseBody(
-            bodyContent,
-            reset_password_subject,
-            "passwordreset",
-            "forgot-password\/reset",
+        # handle password resets where the account doesn't exist yet. We may need to test this too ultimately
+        if (
+            re.search(reset_password_no_account_subject, bodyContent, re.IGNORECASE)
+            is not None
+        ):
+            self.printIfVerbose(
+                "Found Password reset for a non-existent account. This shouldn't happen during tests, one explanation can be running password reset test before test that signs user up"
+            )
+            return self.write_unrecognized_file(
+                s3Key, bodyContent, "noaccountpasswordreset"
+            )
+        else:
+            self.printIfVerbose("email is not to reset password for non-active account")
+
+        # handle other emails. Ultimately, we should be testing these other emails as well
+        self.printIfVerbose(
+            "Found an email that is not an Activate or Password reset. Don't know what to do with it"
         )
-    else:
-        printIfVerbose("email is not a forgotten password email")
+        self.write_unrecognized_file(s3Key, bodyContent, "unrecognized")
 
-    # handle password resets where the account doesn't exist yet. We may need to test this too ultimately
-    if (
-        re.search(reset_password_no_account_subject, bodyContent, re.IGNORECASE)
-        is not None
-    ):
-        printIfVerbose(
-            "Found Password reset for a non-existent account. This shouldn't happen during tests, one explanation can be running password reset test before test that signs user up"
-        )
-        return write_unrecognized_file(s3Key, bodyContent, "noaccountpasswordreset")
-    else:
-        printIfVerbose("email is not to reset password for non-active account")
+        self.printIfVerbose("-------- END PARSE EMAIL\n")
 
-    # handle other emails. Ultimately, we should be testing these other emails as well
-    printIfVerbose(
-        "Found an email that is not an Activate or Password reset. Don't know what to do with it"
-    )
-    write_unrecognized_file(s3Key, bodyContent, "unrecognized")
+    def write_unrecognized_file(self, s3Key, bodyContent, filePrefix):
+        fileSuffix = s3Key[s3Key.rfind("/") + 1 :]
+        filePath = Path(S3Monitor.ACTIVATION_EMAILS_PATH) / f"{filePrefix}.{fileSuffix}"
+        emailFile = open(filePath, "w")
+        emailFile.write(bodyContent)
+        emailFile.close()
 
-    printIfVerbose("-------- END PARSE EMAIL\n")
+    def process_bucket_object(self, s3Key):
+        result = self.s3Client.get_object(Bucket=S3Monitor.MAILBOX_BUCKET, Key=s3Key)
+        bodyContent = quopri.decodestring(result["Body"].read()).decode("latin-1")
+        self.parse_email(bodyContent, s3Key)
 
+    def printIfVerbose(self, logOutput):
+        if self.verbose:
+            print(logOutput)
 
-def write_unrecognized_file(s3Key, bodyContent, filePrefix):
-    fileSuffix = s3Key[s3Key.rfind("/") + 1 :]
-    filePath = f"{activation_emails_path}/{filePrefix}.{fileSuffix}"
-    emailFile = open(filePath, "w")
-    emailFile.write(bodyContent)
-    emailFile.close()
+    def run(self):
+        seenkeys = []
 
-
-def process_bucket_object(s3Client, s3Key):
-    result = s3Client.get_object(Bucket=mailbox_bucket, Key=s3Key)
-    bodyContent = quopri.decodestring(result["Body"].read()).decode("latin-1")
-    parse_email(bodyContent, s3Key)
-
-
-def monitor_bucket(s3Client):
-    seenkeys = []
-
-    while True:
-        bucketContents = s3Client.list_objects(Bucket=mailbox_bucket)
-        if "Contents" in bucketContents:  # handle bucket being empty
-            for s3obj in s3Client.list_objects(Bucket=mailbox_bucket)["Contents"]:
-                s3Key = s3obj["Key"]
-                if not s3Key in seenkeys:
-                    process_bucket_object(s3Client, s3Key)
-                    seenkeys.append(s3Key)
-        time.sleep(5)
-
-
-def printIfVerbose(logOutput):
-    if args.v:  # if we specified verbose with -v option
-        print(logOutput)
-
-
-def main():
-    s3Client = assume_role_and_get_client()
-    monitor_bucket(s3Client)
+        while True:
+            bucketContents = self.s3Client.list_objects(Bucket=S3Monitor.MAILBOX_BUCKET)
+            if "Contents" in bucketContents:  # handle bucket being empty
+                for s3obj in self.s3Client.list_objects(
+                    Bucket=S3Monitor.MAILBOX_BUCKET
+                )["Contents"]:
+                    s3Key = s3obj["Key"]
+                    if not s3Key in seenkeys:
+                        self.process_bucket_object(s3Key)
+                        seenkeys.append(s3Key)
+            time.sleep(5)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Monitor S3 bucket for emails sent during tests of Make an LPA"
+    )
+    parser.add_argument("-v", action="store_true", help="verbose", default=False)
+    parser.add_argument(
+        "-c", action="store_true", help="set flag if running in CI", default=False
+    )
+    args = parser.parse_args()
+
+    S3Monitor(vars(args)).run()
---
+++ cypress/cypress_runner.py
@@ -0,0 +1,88 @@
+import sys
+from datetime import datetime
+from pathlib import Path
+from random import randint
+from subprocess import Popen
+from threading import Thread
+
+from S3Monitor import S3Monitor
+
+
+# environment variables to set when calling cypress:
+# CYPRESS_baseUrl={cypress_base_url}
+# CYPRESS_userNumber={cypress_user_number}
+# CYPRESS_TAGS={cypress_tags}
+# GLOB={cypress_glob}
+def build_cypress_command(command, env={}):
+    """
+    :param command: path to script to run cypress
+    :param env: dict in format:
+        {
+            "CYPRESS_userNumber": <CYPRESS_userNumber>,
+            "CYPRESS_baseUrl": <CYPRESS_baseUrl>,
+            "GLOB": <GLOB for feature files>
+            "TAGS": <CYPRESS_TAGS setting>
+        }
+    """
+    command = f"{command} run --headless --config video=false"
+    if len(env) > 0:
+        env_vars = " ".join([f'{key}="{value}"' for key, value in env.items()])
+        command = f"{command} -e {env_vars}"
+
+    return command
+
+
+# TODO parse command-line args
+def get_settings():
+    enable_s3_monitor = True
+    in_ci = False
+    verbose = False
+
+    _parent_dir = Path(__file__).parent
+    cypress_script = (
+        _parent_dir.parent / Path("node_modules/.bin/cypress-tags")
+    ).resolve()
+    cypress_glob = (_parent_dir / Path("e2e/**/*.feature")).resolve()
+
+    cypress_base_url = "https://localhost:7002"
+
+    # enable this to be set multiple times so that tests can run in parallel?
+    cypress_tags = "@Polyfills"
+
+    # one user per value of cypress_tags?
+    user_number = (
+        f"{int(datetime.timestamp(datetime.now()))}{randint(100000000, 999999999)}"
+    )
+
+    return {
+        "script": cypress_script,
+        "enable_s3_monitor": enable_s3_monitor,
+        "in_ci": in_ci,
+        "verbose": verbose,
+        "env": {
+            "CYPRESS_userNumber": user_number,
+            "CYPRESS_baseUrl": cypress_base_url,
+            "GLOB": cypress_glob,
+            "TAGS": cypress_tags,
+        },
+    }
+
+
+if __name__ == "__main__":
+    settings = get_settings()
+
+    # start S3Monitor if required (in its own thread)
+    if settings["enable_s3_monitor"]:
+        monitor = S3Monitor({"c": settings["in_ci"], "v": settings["verbose"]})
+
+        # daemon threads are killed when the main thread exits
+        Thread(target=monitor.run, daemon=True).start()
+
+    # TODO stitch scripts together
+
+    # build cypress command
+    cypress_command = build_cypress_command(settings["script"], settings["env"])
+    print(f"Will run cypress command:\n{cypress_command}")
+
+    p = Popen(cypress_command, shell=True)
+    p.wait()
