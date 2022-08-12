2c6c7496f81594f073101bb9bfc793dd0d3c3360
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-08-12 11:02:10 +0100

    Remove unused composer deps from service-front

---
+++ service-front/.unused-scanner.php
@@ -0,0 +1,20 @@
+<?php
+
+$projectPath = __DIR__ ;
+
+// Declare directories which contains php code
+$scanDirectories = [
+   $projectPath . '/module/Application/src/',
+   $projectPath . '/module/Application/config/',
+   $projectPath . '/module/Application/tests/',
+];
+
+// Optionally declare standalone files
+$scanFiles = [];
+
+return [
+   'composerJsonPath' => $projectPath . '/composer.json',
+   'vendorPath' => $projectPath . '/vendor/',
+   'scanDirectories' => $scanDirectories,
+   'scanFiles'=>$scanFiles
+];
