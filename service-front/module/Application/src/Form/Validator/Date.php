a6d0377d38a2dad099e56ef1eefca099b4053c0d
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-08-12 08:41:54 +0100

    Fix psalm errors for service-front

--- service-front/module/Application/src/Form/Validator/Date.php
+++ service-front/module/Application/src/Form/Validator/Date.php
@@ -8,12 +8,12 @@ use Exception;
 
 class Date extends DateValidator
 {
-    const EMPTY_DATE = "emptyDate";
+    public const EMPTY_DATE = "emptyDate";
 
     public function __construct($options = [])
     {
         $this->messageTemplates = array_merge($this->messageTemplates, [
-            self::EMPTY_DATE   => "Enter all the date fields",
+            self::EMPTY_DATE => "Enter all the date fields",
         ]);
 
         parent::__construct($options);
@@ -22,7 +22,11 @@ class Date extends DateValidator
     public function isValid($value)
     {
         if (is_array($value)) {
-            if (!array_key_exists('year', $value) || !array_key_exists('month', $value) || !array_key_exists('day', $value)) {
+            if (
+                !array_key_exists('year', $value) ||
+                !array_key_exists('month', $value) ||
+                !array_key_exists('day', $value)
+            ) {
                 throw new Exception('Invalid date array passed to Application\Form\Lpa\Validator\Date validator');
             }
 
@@ -45,9 +49,10 @@ class Date extends DateValidator
             $month = (int) $month;
             $year = (int) $year;
 
-            //  If possible check the date value by using the same conversion that will take place in the Dob date model
-            //  This is required to make sure that PHP doesn't 'shift' the time in an attempt to be helpful
-            //  e.g. 1980-06-015 becomes 1st June 1980 at 5am
+            // If possible check the date value by using the same
+            // conversion that will take place in the Dob date model
+            // This is required to make sure that PHP doesn't 'shift' the time in an attempt to be helpful
+            // e.g. 1980-06-015 becomes 1st June 1980 at 5am
             $dateStr = implode('-', array_reverse($value));
             $date = date_parse_from_format(DateTime::ISO8601, $dateStr);
 
@@ -57,11 +62,12 @@ class Date extends DateValidator
             }
 
             //  Year minimum value is to ensure that the value is 4 digits long
-            if (!checkdate($month, $day, $year)
+            if (
+                !checkdate($month, $day, $year)
                 || !$this->intBetweenInclusive($day, 1, 31)
                 || !$this->intBetweenInclusive($month, 1, 12)
-                || !$this->intBetweenInclusive($year, 1000, 9999)) {
-
+                || !$this->intBetweenInclusive($year, 1000, 9999)
+            ) {
                 $this->error(parent::INVALID_DATE);
                 return false;
             }
