5120f317d78ea772a53ce958924d429a1973c2c3
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-08-17 15:30:00 +0100

    Remove jQuery and lint moj.helpers.js

--- service-front/assets/js/moj/moj.helpers.js
+++ service-front/assets/js/moj/moj.helpers.js
@@ -1,65 +1,79 @@
-(function () {
-  'use strict';
+;(function () {
+  'use strict'
+
+  window.moj = window.moj || {}
+  const moj = window.moj
 
   // test for html5 storage
   moj.Helpers.hasHtml5Storage = function () {
     try {
-      return 'sessionStorage' in window && window.sessionStorage !== null;
+      return 'sessionStorage' in window && window.sessionStorage !== null
     } catch (e) {
-      return false;
+      return false
     }
-  };
+  }
 
-  // helper to check if popup is currently open
-  moj.Helpers.isPopupOpen = function () {
-    if (moj.Modules.Popup !== undefined) {
-      return moj.Modules.Popup.isOpen();
-    } else {
-      return false;
-    }
-  };
+  // check if form elements are all empty
+  moj.Helpers.hasCleanFields = function () {
+    let clean = true
+    const selector = 'input:not([type="submit"]), select:not([name*="country"]), textarea'
 
-  // check if children are all empty
-  moj.Helpers.hasCleanFields = function (wrap) {
-    var clean = true;
-    $('input:not([type="submit"]), select:not([name*="country"]), textarea', wrap).each(function () {
-      if ($(this).val() !== '') {
-        clean = false;
+    document.querySelectorAll(selector).forEach(function (element) {
+      const val = element.getAttribute('value')
+
+      if (val !== '' && val !== null) {
+        clean = false
       }
-    });
-    return clean;
-  };
+    })
 
-  // helper to return the scroll position of an element
-  moj.Helpers.scrollTo = function (e) {
-    var $target = e.target !== undefined ? $($(e.target).attr('href')) : $(e),
-        $scrollEl = moj.Helpers.isPopupOpen() ? $('#mask') : $('html, body'),
-        topPos = moj.Helpers.scrollPos($target);
+    return clean
+  }
 
-    $scrollEl
-      .animate({
-        scrollTop: topPos
-      },0)
-      .promise()
-      .done(function () {
-        $target.closest('.group').find('input, select, textarea').first().focus();
-      });
-  };
+  // cribbed from jQuery; see
+  // https://github.com/jquery/jquery/blob/d0ce00cdfa680f1f0c38460bc51ea14079ae8b07/src/offset.js#L65
+  // simplified to use window rather than document.ownerDocument.defaultView
+  // as we don't have iframes
+  moj.Helpers.getOffset = function (element) {
+    try {
+      const rect = element.getBoundingClientRect()
 
-  // helper to return the scroll position of an element
-  moj.Helpers.scrollPos = function (target) {
-    /*jshint laxbreak: true */
-    return moj.Helpers.isPopupOpen()
-              ? target.offset().top - $('#popup').offset().top
-              : target.offset().top;
-  };
+      return {
+        top: rect.top + window.pageYOffset,
+        left: rect.left + window.pageXOffset
+      }
+    } catch (e) {
+      // for browsers which don't support getBoundingClientRect()
+      return { top: 0, left: 0 }
+    }
+  }
+
+  // helper to scroll popup into view;
+  // selector is always #popup-content
+  moj.Helpers.scrollTo = function (selector) {
+    const targetElt = document.querySelector(selector)
+    const scrollElt = document.querySelector('#mask')
+    const popupElt = document.querySelector('#popup')
+
+    const topPos = moj.Helpers.getOffset(targetElt).top - moj.Helpers.getOffset(popupElt).top
+
+    scrollElt.scrollTop = topPos
+
+    // put focus into the first user input inside the target element
+    const inputs = targetElt.querySelectorAll(
+      'input:not([type=hidden]), checkbox, radio, select'
+    )
+
+    if (inputs.length > 0) {
+      inputs[0].focus()
+    }
+  }
 
   moj.Helpers.isMobileWidth = function () {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
     if (w > 640) {
-      return false;
+      return false
     } else {
-      return true;
+      return true
     }
-  };
-})();
+  }
+})()
