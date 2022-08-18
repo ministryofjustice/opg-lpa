fb977e5d10753a96a02c0b0e8d96dff62776eedd
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-08-18 14:21:55 +0100

    Better encapsulation of event handlers in moj.postcode-lookup.js
    By avoiding use of "this", we can expose fewer methods on the
    postcode lookup component API. We also remove the need to use
    _.bindall().

--- service-front/assets/js/moj/moj.modules/moj.postcode-lookup.js
+++ service-front/assets/js/moj/moj.modules/moj.postcode-lookup.js
@@ -4,32 +4,13 @@
 ;(function () {
   'use strict'
 
-  // check if form elements within the element context are all empty
-  const hasCleanFields = function (context) {
-    let clean = true
-    const selector = 'input:not([type="submit"]), select:not([name*="country"]), textarea'
+  const lpa = window.lpa || {}
+  const moj = window.moj || {}
 
-    context.querySelectorAll(selector).forEach(function (element) {
-      const val = element.getAttribute('value')
+  const makePostcodeLookup = function (wrap) {
+    const that = {}
 
-      if (val !== '') {
-        clean = false
-      }
-    })
-
-    return clean
-  }
-
-  // Define the class
-  const PostcodeLookup = function (el) {
-    _.bindAll(this, 'searchClicked', 'toggleClicked', 'changeClicked', 'resultsChanged', 'queryEnter', 'postcodeSuccess', 'postcodeError')
-    this.cacheEls(el)
-    this.bindEvents()
-    this.init()
-  }
-
-  PostcodeLookup.prototype = {
-    settings: {
+    const settings = {
       postcodeSearchUrl: '/address-lookup',
       // used to populate fields
       // key is the key name sent in response and value is name of app's field
@@ -39,132 +20,117 @@
         line3: 'address-address3',
         postcode: 'postcode'
       }
-    },
+    }
 
-    // wrap is the container element for the postcode lookup component
-    cacheEls: function (wrap) {
-      this.wrap = wrap
-      this.postalForm = this.wrap.querySelector('.js-PostcodeLookup__postal-add')
+    const postalForm = wrap.querySelector('.js-PostcodeLookup__postal-add')
 
-      // TODO remove jQuery
-      this.$wrap = $(wrap)
-      this.$form = this.$wrap.closest('form')
-      this.$postalFields = this.$wrap.find('.js-PostcodeLookup__postal-add')
+    // TODO remove jQuery
+    const $wrap = $(wrap)
+    const $postalFields = $wrap.find('.js-PostcodeLookup__postal-add')
 
-      this.searchTpl = lpa.templates['postcodeLookup.search-field']
-      this.toggleTpl = lpa.templates['postcodeLookup.address-toggle']
-      this.resultTpl = lpa.templates['postcodeLookup.search-result']
-      this.changeTpl = lpa.templates['postcodeLookup.address-change']
-      this.errorMessageTpl = lpa.templates['errors.formMessage']
-    },
+    const searchTpl = lpa.templates['postcodeLookup.search-field']
+    const toggleTpl = lpa.templates['postcodeLookup.address-toggle']
+    const resultTpl = lpa.templates['postcodeLookup.search-result']
+    const changeTpl = lpa.templates['postcodeLookup.address-change']
+    const errorMessageTpl = lpa.templates['errors.formMessage']
 
-    bindEvents: function () {
-      this.$wrap.on('click.moj.Modules.PostcodeLookup', '.js-PostcodeLookup__search-btn', this.searchClicked)
-      this.$wrap.on('click.moj.Modules.PostcodeLookup', '.js-PostcodeLookup__toggle-address', this.toggleClicked)
-      this.$wrap.on('click.moj.Modules.PostcodeLookup', '.js-PostcodeLookup__change', this.changeClicked)
-      this.$wrap.on('change.moj.Modules.PostcodeLookup', '.js-PostcodeLookup__search-results', this.resultsChanged)
-      this.$wrap.on('keydown.moj.Modules.PostcodeLookup', '.js-PostcodeLookup__query', this.queryEnter)
-    },
+    let query = ''
 
-    init: function () {
-      // prepend template to postal fields
-      this.$postalFields.before(this.searchTpl() + this.toggleTpl() + this.changeTpl()).addClass('hidden')
+    // check if form elements within the element context are all empty
+    const hasCleanFields = function () {
+      let clean = true
+      const selector = 'input:not([type="submit"]), select:not([name*="country"]), textarea'
 
-      // if all fields are empty and there are no validation messages, hide the fields
-      if (hasCleanFields(this.postalForm) && document.querySelector('.error-summary') === null) {
-        this.wrap.querySelector('[data-role=postcodelookup-search]').classList.add('hidden')
-      } else {
-        this.hideSearchForm()
-        this.toggleAddress()
+      postalForm.querySelectorAll(selector).forEach(function (element) {
+        const val = element.getAttribute('value')
+
+        if (val !== '') {
+          clean = false
+        }
+      })
+
+      return clean
+    }
+
+    // event handlers
+    const changeClicked = function (e) {
+      e.preventDefault()
+
+      wrap.querySelector('[data-role=postcodelookup-search]').classList.add('hidden')
+      wrap.querySelector('.js-PostcodeLookup__search').classList.remove('hidden')
+      if (hasCleanFields(postalForm) && document.querySelector('.error-summary') === null) {
+        wrap.querySelector('[data-role="postcodelookup-search-input"]').classList.remove('hidden')
       }
-    },
-
-    hideSearchForm: function () {
-      this.wrap.querySelector('.js-PostcodeLookup__search').classList.add('hidden')
-      this.$wrap.find('.js-PostcodeLookup__toggle-address').closest('div').addClass('hidden')
-      this.$wrap.find('.js-PostcodeLookup__change').closest('div').removeClass('hidden')
-    },
-
-    changeClicked: function (e) {
-      this.$wrap.find('.js-PostcodeLookup__change').closest('div').addClass('hidden')
-      this.wrap.querySelector('.js-PostcodeLookup__search').classList.remove('hidden')
-      if (hasCleanFields(this.postalForm) && document.querySelector('.error-summary') === null) {
-        this.$wrap.find('.js-PostcodeLookup__toggle-address').closest('div').removeClass('hidden')
-      }
-      this.wrap.querySelector('.js-PostcodeLookup__query').focus()
+      wrap.querySelector('.js-PostcodeLookup__query').focus()
       return false
-    },
+    }
 
-    searchClicked: function (e) {
+    const searchClicked = function (e) {
+      e.preventDefault()
+
       const $el = $(e.target)
-      const $searchContainer = this.$wrap.find('.js-PostcodeLookup__search')
+      const $searchContainer = $wrap.find('.js-PostcodeLookup__search')
       const $postcodeLabel = $('label[for="postcode-lookup"]')
 
       // store the current query
-      this.query = this.$wrap.find('.js-PostcodeLookup__query').val()
+      query = $wrap.find('.js-PostcodeLookup__query').val()
 
       if (!$el.hasClass('disabled')) {
-        if (this.query !== '') {
+        if (query !== '') {
           $el.spinner()
-          this.findPostcode(this.query)
+          that.findPostcode(query)
           $searchContainer.removeClass('error')
           $postcodeLabel.children('.error-message').remove()
         } else {
           $searchContainer.addClass('error')
           $postcodeLabel.children('.error-message').remove()
-          $postcodeLabel
-            .append($(this.errorMessageTpl({
-              errorMessage: 'Enter a postcode'
-            })))
+          $postcodeLabel.append(
+            errorMessageTpl({ errorMessage: 'Enter a postcode' })
+          )
         }
       }
       return false
-    },
+    }
 
-    toggleClicked: function (e) {
-      this.toggleAddress()
+    const toggleClicked = function (e) {
+      e.preventDefault()
+      that.toggleAddress()
       return false
-    },
+    }
 
-    resultsChanged: function (e) {
+    // when a postcode result (address) is selected, update address fields
+    const resultsChanged = function (e) {
       const $el = $(e.target)
-      const val = $el.val()
 
       const $selectedOption = $el.find(':selected')
 
-      $('[name*="' + this.settings.fieldMappings.line1 + '"]').val($selectedOption.data('line1'))
-      $('[name*="' + this.settings.fieldMappings.line2 + '"]').val($selectedOption.data('line2'))
-      $('[name*="' + this.settings.fieldMappings.line3 + '"]').val($selectedOption.data('line3'))
-      $('[name*="' + this.settings.fieldMappings.postcode + '"]').val($selectedOption.data('postcode')).change()
+      $('[name*="' + settings.fieldMappings.line1 + '"]').val($selectedOption.data('line1'))
+      $('[name*="' + settings.fieldMappings.line2 + '"]').val($selectedOption.data('line2'))
+      $('[name*="' + settings.fieldMappings.line3 + '"]').val($selectedOption.data('line3'))
+      $('[name*="' + settings.fieldMappings.postcode + '"]').val($selectedOption.data('postcode')).change()
 
-      this.toggleAddress()
-    },
+      that.toggleAddress()
+    }
 
-    queryEnter: function (e) {
+    // capture "Return" key presses in the search box
+    const queryEnter = function (e) {
       const code = (e.keyCode ? e.keyCode : e.which)
 
       if (code === 13) {
         e.preventDefault()
-        this.$wrap.find('.js-PostcodeLookup__search-btn').click()
+
+        // synthesise a click event on the search button
+        const event = document.createEvent('HTMLEvents')
+        event.initEvent('click', true, true)
+        wrap.querySelector('.js-PostcodeLookup__search-btn').dispatchEvent(event)
       }
-    },
+    }
 
-    findPostcode: function (query) {
-      $.ajax({
-        url: this.settings.postcodeSearchUrl,
-        data: { postcode: query },
-        dataType: 'json',
-        timeout: 10000,
-        cache: true,
-        error: this.postcodeError,
-        success: this.postcodeSuccess
-      })
-    },
-
-    postcodeError: function (jqXHR, textStatus, errorThrown) {
+    // request handling
+    const postcodeRequestError = function (jqXHR, textStatus, errorThrown) {
       let errorText = 'There is a problem: '
 
-      this.$wrap.find('.js-PostcodeLookup__search-btn').spinner('off')
+      $wrap.find('.js-PostcodeLookup__search-btn').spinner('off')
 
       if (textStatus === 'timeout') {
         errorText += 'the service did not respond in the allotted time'
@@ -172,51 +138,101 @@
         errorText += errorThrown
       }
 
-      alert(errorText)
-    },
+      window.alert(errorText)
+    }
 
-    postcodeSuccess: function (response) {
+    const postcodeRequestSuccess = function (response) {
       // not successful
       if (!response.success || response.addresses === null) {
-        const $searchContainer = this.$wrap.find('.js-PostcodeLookup__search')
+        const $searchContainer = $wrap.find('.js-PostcodeLookup__search')
         const $postcodeLabel = $('label[for="postcode-lookup"]')
 
         if (response.isPostcodeValid) {
           $searchContainer.addClass('error')
           $postcodeLabel.children('.error-message').remove()
-          $postcodeLabel
-            .append($(this.errorMessageTpl({
-              errorMessage: 'Enter a real postcode. If you live overseas, enter your address manually instead of using the postcode lookup'
-            })))
+          $postcodeLabel.append(
+            $(errorMessageTpl({
+              errorMessage: 'Enter a real postcode. If you live overseas, ' +
+                  'enter your address manually instead of using the postcode lookup'
+            }))
+          )
         } else {
-          alert('Enter a valid UK postcode')
+          // don't think this is reachable...
+          window.alert('Enter a valid UK postcode')
         }
       } else {
         // successful
+        const resultsNode = $(resultTpl({ results: response.addresses }))
+        resultsNode.on('change', resultsChanged)
 
-        if (this.$wrap.find('.js-PostcodeLookup__search-results').length > 0) {
-          this.$wrap.find('.js-PostcodeLookup__search-results').parent().replaceWith(this.resultTpl({ results: response.addresses }))
+        if ($wrap.find('.js-PostcodeLookup__search-results').length > 0) {
+          $wrap.find('.js-PostcodeLookup__search-results').parent().replaceWith(resultsNode)
         } else {
-          this.$wrap.find('.js-PostcodeLookup__search').after(this.resultTpl({ results: response.addresses }))
+          $wrap.find('.js-PostcodeLookup__search').after(resultsNode)
         }
-        this.$wrap.find('.js-PostcodeLookup__search-results').focus()
+        $wrap.find('.js-PostcodeLookup__search-results').focus()
       }
-      this.$wrap.find('.js-PostcodeLookup__search-btn').spinner('off')
-    },
+      $wrap.find('.js-PostcodeLookup__search-btn').spinner('off')
+    }
 
-    toggleAddress: function () {
-      const $search = this.$wrap.find('.js-PostcodeLookup__query')
-      const $pcode = this.$wrap.find('[name*="' + this.settings.fieldMappings.postcode + '"]')
+    // public API
+    that.init = function () {
+      // prepend template to postal fields
+      $postalFields.before(searchTpl() + toggleTpl() + changeTpl()).addClass('hidden')
+
+      // if all fields are empty and there are no validation messages, hide the fields
+      if (hasCleanFields() && document.querySelector('.error-summary') === null) {
+        wrap.querySelector('[data-role=postcodelookup-search]').classList.add('hidden')
+      } else {
+        that.hideSearchForm()
+        that.toggleAddress()
+      }
+    }
+
+    that.findPostcode = function (query) {
+      $.ajax({
+        url: settings.postcodeSearchUrl,
+        data: { postcode: query },
+        dataType: 'json',
+        timeout: 10000,
+        cache: true,
+        error: postcodeRequestError,
+        success: postcodeRequestSuccess
+      })
+    }
+
+    that.hideSearchForm = function () {
+      wrap.querySelector('.js-PostcodeLookup__search').classList.add('hidden')
+      $wrap.find('.js-PostcodeLookup__toggle-address').closest('div').addClass('hidden')
+      $wrap.find('.js-PostcodeLookup__change').closest('div').removeClass('hidden')
+    }
+
+    that.toggleAddress = function () {
+      const $search = $wrap.find('.js-PostcodeLookup__query')
+      const $pcode = $wrap.find('[name*="' + settings.fieldMappings.postcode + '"]')
+
       // populate postcode field
       if ($search.val() !== '' && $pcode.val() === '') {
         $pcode.val($search.val()).change()
       }
-      this.$postalFields.removeClass('hidden')
+
+      $postalFields.removeClass('hidden')
+
       // focus on first address field
       if ($('.js-PostcodeLookup__postal-add').parent().find('#address-search-result').length === 1) {
-        this.$postalFields.find('[name*="addr1"]').focus()
+        $postalFields.find('[name*="addr1"]').focus()
       }
     }
+
+    that.init()
+
+    // attach event handlers
+    wrap.querySelector('.js-PostcodeLookup__search-btn').addEventListener('click', searchClicked)
+    wrap.querySelector('.js-PostcodeLookup__toggle-address').addEventListener('click', toggleClicked)
+    wrap.querySelector('.js-PostcodeLookup__change').addEventListener('click', changeClicked)
+    wrap.querySelector('.js-PostcodeLookup__query').addEventListener('keydown', queryEnter)
+
+    return that
   }
 
   // Add module to LPA namespace
@@ -238,7 +254,7 @@
 
     wrap: function (element) {
       if (element.getAttribute('data-inited') === null) {
-        new PostcodeLookup(element)
+        makePostcodeLookup(element)
         element.setAttribute('data-inited', 'true')
       }
     }
