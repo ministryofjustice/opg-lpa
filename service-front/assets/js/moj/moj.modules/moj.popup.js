8dc53ed1d240a907a646acb573bb12731dd12b59
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-08-30 17:21:10 +0100

    Remove jQuery event listeners from moj.popup.js

--- service-front/assets/js/moj/moj.modules/moj.popup.js
+++ service-front/assets/js/moj/moj.modules/moj.popup.js
@@ -1,18 +1,24 @@
 // Popup module for LPA
-// Dependencies: moj, jQuery
+;(function () {
+  'use strict'
 
-(function () {
-  'use strict';
+  window.moj = window.moj || {}
+  const moj = window.moj
+
+  const lpa = window.lpa
 
   // Define the class
-  var Popup = function (options) {
-    this.settings = $.extend({}, this.defaults, options);
-    this._cacheEls();
-  };
+  const Popup = function (options) {
+    this.settings = $.extend({}, this.defaults, options)
+    this._cacheEls()
+  }
 
   Popup.prototype = {
+    keydownListener: null,
+    clickListener: null,
+
     defaults: {
-      source: $('#content'),
+      source: document.querySelector('#content'),
       placement: 'body',
       popupId: 'popup',
       ident: null,
@@ -28,43 +34,52 @@
     init: function () {},
 
     _cacheEls: function () {
-      this.$win = $(window);
-      this.$html = $('html');
-      this.$body = $('body');
-      this.$mask = $(this.settings.maskTemplate);
-      this.$popup = $(this.settings.containerTemplate);
-      this.$content = $(this.settings.contentTemplate);
-      this.$close = $(this.settings.closeTemplate);
+      this.$mask = $(this.settings.maskTemplate)
+
+      this.popup = moj.Helpers.strToHtml(this.settings.containerTemplate)
+      this.$popup = $(this.popup)
+
+      this.$content = $(this.settings.contentTemplate)
+      this.$close = $(this.settings.closeTemplate)
     },
 
     _bindEvents: function () {
-      var self = this;
+      const self = this
 
-      $('body').on('keydown.moj.Modules.Popup', function (e) {
+      this.keydownListener = document.body.addEventListener('keydown', function (e) {
         if (e.which === 27) {
-          self.close();
+          self.close()
         }
-      });
-      this.$popup.on('click.moj.Modules.Popup', '.js-popup-close, .js-cancel', function (e) {
-        e.preventDefault();
-        self.close();
-      });
+      })
+
+      this.clickListener = this.popup.addEventListener('click', function (e) {
+        if (!moj.Helpers.matchesSelector(e.target, '.js-popup-close, .js-cancel')) {
+          return true
+        }
+
+        e.preventDefault()
+        self.close()
+
+        return false
+      })
     },
 
     _unbindEvents: function () {
-      $('body').off('keydown.moj.Modules.Popup');
-      this.$popup.off('click.moj.Modules.Popup');
+      document.body.removeEventListener('keydown', this.keydownListener)
+      this.popup.removeEventListener('click', this.clickListener)
     },
 
     open: function (html, opts) {
-      var self = this;
+      const self = this
+
       // combine opts with settings for local settings
-      opts = $.extend({}, this.settings, opts);
+      opts = $.extend({}, this.settings, opts)
 
       // disable body scroll
-      $('html').addClass('noscroll');
+      document.querySelector('html').classList.add('noscroll')
+
       // hide main contents from print layout
-      $('body > *').addClass('print-hidden');
+      $('body > *').addClass('print-hidden')
 
       // Join it all together
       this.$popup.data('settings', opts)
@@ -72,121 +87,123 @@
         .addClass('popup ' + opts.ident)
         .append(this.$close)
         .append(this.$content.html(html))
-        .appendTo(this.$mask);
+        .appendTo(this.$mask)
 
       // bind event handlers
-      this._bindEvents();
+      this._bindEvents()
 
       // Place the mask in the DOM
       // If a placement has been provided, the popup is appended to that element,
       // otherwise the popup is appended to the body element.
-      $(opts.placement)[opts.placement === 'body' ? 'append' : 'after'](this.$mask);
+      $(opts.placement)[opts.placement === 'body' ? 'append' : 'after'](this.$mask)
 
       // callback func
-      if (opts.beforeOpen && typeof(opts.beforeOpen) === 'function') {
-        opts.beforeOpen();
+      if (opts.beforeOpen && typeof (opts.beforeOpen) === 'function') {
+        opts.beforeOpen()
       }
 
       // prevent tab navigation outside the lightbox
-      self.loopTabKeys(self.$popup);
+      self.loopTabKeys(self.$popup)
 
       // Fade in the mask
-      this.$mask.fadeTo(200, 1);
+      this.$mask.fadeTo(200, 1)
 
-      // Center and fase in the popup
+      // Center and phase in the popup
       this.$popup.delay(100).fadeIn(200, function () {
-        self.$popup.find('h2').attr('tabindex', -1);
-        self.$popup.find('.close a').focus(); // for accessibility
+        self.$popup.find('h2').attr('tabindex', -1)
+        self.$popup.find('.close a').focus()
 
         // callback func
-        if (opts.onOpen && typeof(opts.onOpen) === 'function') {
-          opts.onOpen();
+        if (opts.onOpen && typeof (opts.onOpen) === 'function') {
+          opts.onOpen()
         }
-      });
+      })
     },
 
     close: function () {
       // make sure there is a popup to close
       if (this.isOpen()) {
-        var self = this,
-            opts = $('#popup').data('settings'),
-            scrollPosition = $(window).scrollTop();
+        const self = this
+        const opts = $('#popup').data('settings')
+        const scrollPosition = $(window).scrollTop()
 
         self.$popup.fadeOut(400, function () {
           self.$mask.fadeOut(200, function () {
             // focus on previous element
             if (typeof opts.source !== 'undefined' && opts.source) {
-              opts.source.focus();
+              opts.source.focus()
             }
+
             // clear out any hash locations
-            window.location.hash = '';
+            window.location.hash = ''
             if (history.pushState) {
-              history.pushState('', document.title, window.location.pathname);
+              history.pushState('', document.title, window.location.pathname)
             }
 
             // callback func
-            if (opts.onClose && typeof(opts.onClose) === 'function') {
-              opts.onClose();
+            if (opts.onClose && typeof (opts.onClose) === 'function') {
+              opts.onClose()
             }
 
             // Remove the popup from the DOM
-            $(this).remove();
+            $(this).remove()
 
             // re-enable body scroll
-            $(window).scrollTop(scrollPosition);
-            $('html').removeClass('noscroll');
+            $(window).scrollTop(scrollPosition)
+            document.querySelector('html').classList.remove('noscroll')
+
             // unhide main contents from print layout
-            $('body > *').removeClass('print-hidden');
+            $('body > *').removeClass('print-hidden')
 
             // unbind event handlers
-            self._unbindEvents();
-          });
-        });
+            self._unbindEvents()
+          })
+        })
       }
     },
 
     tabFocusesOn: function (e) {
       // on tab set focus
       if (e.key === 'Tab' && !e.shiftKey) {
-        e.preventDefault();
-        e.data.element.focus();
+        e.preventDefault()
+        e.data.element.focus()
       }
     },
 
     reverseTabFocusesOn: function (e) {
       // on tab with shift held set focus
       if (e.key === 'Tab' && e.shiftKey) {
-        e.preventDefault();
-        e.data.element.focus();
+        e.preventDefault()
+        e.data.element.focus()
       }
     },
 
     loopTabKeys: function (wrap) {
-      var tabbable = 'a, area, button, input, object, select, textarea, [tabindex]';
-      this.$first = wrap.find(tabbable).filter(':first');
-      this.$last = wrap.find(tabbable).filter(':last');
+      const tabbable = 'a, area, button, input, object, select, textarea, [tabindex]'
+      this.$first = wrap.find(tabbable).filter(':first')
+      this.$last = wrap.find(tabbable).filter(':last')
 
-      this.$first.keydown({'element': this.$last}, this.reverseTabFocusesOn);
-      this.$last.keydown({'element': this.$first}, this.tabFocusesOn);
+      this.$first.keydown({ element: this.$last }, this.reverseTabFocusesOn)
+      this.$last.keydown({ element: this.$first }, this.tabFocusesOn)
     },
 
     redoLoopedTabKeys: function () {
       if (typeof this.$first !== 'undefined') {
-        this.$first.off('keydown', this.reverseTabFocusesOn);
+        this.$first.off('keydown', this.reverseTabFocusesOn)
       }
 
       if (typeof this.$last !== 'undefined') {
-        this.$last.off('keydown', this.tabFocusesOn);
+        this.$last.off('keydown', this.tabFocusesOn)
       }
 
-      this.loopTabKeys(this.$popup);
+      this.loopTabKeys(this.$popup)
     },
 
     isOpen: function () {
-      return $('#' + this.settings.popupId).length > 0;
+      return document.querySelectorAll('#' + this.settings.popupId).length > 0
     }
-  };
+  }
 
   // Add module to MOJ namespace
-  moj.Modules.Popup = new Popup();
-}());
+  moj.Modules.Popup = new Popup()
+}())
