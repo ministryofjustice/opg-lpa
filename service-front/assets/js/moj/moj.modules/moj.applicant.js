d6c5f17635298f33e2639938ab449b229ca50c9d
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-09-07 10:06:48 +0100

    Lint and fix deprecations for moj.applicant.js

--- service-front/assets/js/moj/moj.modules/moj.applicant.js
+++ service-front/assets/js/moj/moj.modules/moj.applicant.js
@@ -1,49 +1,48 @@
+/* globals $ */
 // Dependencies: moj, jQuery
+;(function () {
+  'use strict'
 
-(function () {
-    'use strict';
+  const moj = window.moj
 
-    moj.Modules.Applicant = {
+  moj.Modules.Applicant = {
+    init: function () {
+      this.selectionBehaviour()
+    },
 
-        init: function () {
-            this.selectionBehaviour();
-        },
+    selectionBehaviour: function () {
+      // Only do the following if .js-attorney-list exists
+      if ($('.js-attorney-list')[0]) {
+        // Toggle all checkboxes under Attorneys
+        $('[name="whoIsRegistering"]').on('change', function () {
+          if ($(this).val() === 'donor') {
+            //  Uncheck the attorney checkboxes and re-render the input styles
+            $('.js-attorney-list input:checkbox').prop('checked', false)
 
-        selectionBehaviour: function() {
-            // Only do the following if .js-attorney-list exists
-            if ($('.js-attorney-list')[0]) {
+            moj.Modules.Applicant.renderRegisteredByInputs()
+          }
+        })
 
-                // Toggle all checkboxes under Attorneys
-                $('[name="whoIsRegistering"]').change(function() {
-                    if ($(this).val() === 'donor') {
-                        //  Uncheck the attorney checkboxes and re-render the input styles
-                        $('.js-attorney-list input:checkbox').prop('checked', false);
+        //  If an attorney checkbox is checked then ensure that the correct radio button is selected
+        $('.js-attorney-list input').on('change', function () {
+          //  If an attorney is selected directly then trigger the radio button select
+          if ($('.js-attorney-list input').is(':checked')) {
+            $('input[name="whoIsRegistering"][value!="donor"]').prop('checked', true)
+          }
 
-                        moj.Modules.Applicant.renderRegisteredByInputs();
-                    }
-                });
+          moj.Modules.Applicant.renderRegisteredByInputs()
+        })
+      }
+    },
 
-                //  If an attorney checkbox is checked then ensure that the correct radio button is selected
-                $('.js-attorney-list input').change(function() {
-                    //  If an attorney is selected directly then trigger the radio button select
-                    if ($('.js-attorney-list input').is(':checked')) {
-                        $('input[name="whoIsRegistering"][value!="donor"]').prop('checked', true);
-                    }
+    renderRegisteredByInputs: function () {
+      //  Render the radio buttons and checkboxes as required
+      $('[name="whoIsRegistering"]').parent().removeClass('selected')
+      $('[name="whoIsRegistering"]:checked').parent().addClass('selected')
 
-                    moj.Modules.Applicant.renderRegisteredByInputs();
-                });
+      $('.js-attorney-list input').parent().removeClass('selected')
+      $('.js-attorney-list input:checked').parent().addClass('selected')
+    }
 
-            }
-        },
-
-        renderRegisteredByInputs: function () {
-            //  Render the radio buttons and checkboxes as required
-            $('[name="whoIsRegistering"]').parent().removeClass('selected');
-            $('[name="whoIsRegistering"]:checked').parent().addClass('selected');
-
-            $('.js-attorney-list input').parent().removeClass('selected');
-            $('.js-attorney-list input:checked').parent().addClass('selected');
-        }
-
-    };
-})();
+  }
+})()
