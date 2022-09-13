477e14e7cf69bae9ce4242386b668699ade47b19
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-09-13 12:10:42 +0100

    Reword and fix tests for signing on behalf of donor/applicant
    Relates to LPAL-890 and LPAL-891

--- cypress/e2e/common/i_can_see.js
+++ cypress/e2e/common/i_can_see.js
@@ -48,7 +48,7 @@ Then('I can see that the donor cannot sign', () => {
     cy.get('[data-cy=date-check-donor]').find('p').should('contain.text', donorText);
 
     const applicantName = 'A person signing on behalf of Mr Christopher Robin (applicant)'
-    const applicantText = 'This person signed section 15 of the LPA on behalf of the donor on'
+    const applicantText = 'This person signed section 15 of the LPA on behalf of the applicant on'
     cy.get('[data-cy=date-check-applicant]').find('h3').should('contain.text', applicantName);
     cy.get('[data-cy=date-check-applicant]').find('p').should('contain.text', applicantText);
 })
@@ -66,9 +66,10 @@ Then('I cannot see that the donor cannot sign', () => {
 })
 
 Then('I can see validation errors do not refer to the donor', () => {
-    const text = 'Enter the person signing on behalf of the donor\'s signature date'
-    cy.get('[data-cy=date-check-donor]').find('.error-message').should('contain.text', text);
-    cy.get('[data-cy=date-check-applicant]').find('.error-message').should('contain.text', text);
+    const donorText = 'Enter the signature date of the person signing on behalf of the donor'
+    const applicantText = 'Enter the signature date of the person signing on behalf of the applicant'
+    cy.get('[data-cy=date-check-donor]').find('.error-message').should('contain.text', donorText)
+    cy.get('[data-cy=date-check-applicant]').find('.error-message').should('contain.text', applicantText)
 })
 
 Then('I can see validation errors refer to the donor and applicant', () => {
