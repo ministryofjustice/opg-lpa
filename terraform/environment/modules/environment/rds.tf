49a3689781867313250f005cb3b1c461132331e7
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-09-21 11:18:08 +0100

    Simplify PG13 RDS config

--- terraform/environment/modules/environment/rds.tf
+++ terraform/environment/modules/environment/rds.tf
@@ -37,7 +37,7 @@ resource "aws_db_instance" "api" {
   kms_key_id                          = local.is_primary_region ? data.aws_kms_key.rds.arn : data.aws_kms_key.multi_region_db_snapshot_key.arn
   username                            = data.aws_secretsmanager_secret_version.api_rds_username.secret_string
   password                            = data.aws_secretsmanager_secret_version.api_rds_password.secret_string
-  parameter_group_name                = var.account.use_postgres13 == true ? aws_db_parameter_group.postgres13-db-params.name : aws_db_parameter_group.postgres-db-params.name
+  parameter_group_name                = aws_db_parameter_group.postgres13-db-params.name
   vpc_security_group_ids              = [aws_security_group.rds-api.id]
   auto_minor_version_upgrade          = true
   maintenance_window                  = "wed:05:00-wed:09:00"
@@ -108,34 +108,10 @@ module "api_aurora" {
   copy_tags_to_snapshot         = true
 }
 
-resource "aws_db_parameter_group" "postgres-db-params" {
-  name        = lower("postgres-db-params-${var.environment_name}")
-  description = "default postgres rds parameter group"
-  family      = var.account.psql_parameter_group_family
-  parameter {
-    name         = "log_min_duration_statement"
-    value        = "500"
-    apply_method = "immediate"
-  }
-
-  parameter {
-    name         = "log_statement"
-    value        = "all"
-    apply_method = "pending-reboot"
-  }
-
-
-  parameter {
-    name         = "rds.log_retention_period"
-    value        = "1440"
-    apply_method = "immediate"
-  }
-}
-
 resource "aws_db_parameter_group" "postgres13-db-params" {
   name        = lower("postgres13-db-params-${var.environment_name}")
   description = "default postgres13 rds parameter group"
-  family      = var.account.psql13_parameter_group_family
+  family      = var.account.psql_parameter_group_family
   parameter {
     name         = "log_min_duration_statement"
     value        = "500"
