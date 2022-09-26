51146e34c25c9efb1e5037e3f14c0fe62d9653fb
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-26 14:35:56 +0100

    use Amy's test service IDs for testing

--- service-front/module/Application/src/Model/Service/Mail/Transport/NotifyMailTransport.php
+++ service-front/module/Application/src/Model/Service/Mail/Transport/NotifyMailTransport.php
@@ -33,9 +33,13 @@ class NotifyMailTransport implements MailTransportInterface
     private $defaultTemplateMap = [
        AbstractEmailService::EMAIL_FEEDBACK => '3fb12879-7665-4ffe-a76f-ed90cde7a35d',
        AbstractEmailService::EMAIL_ACCOUNT_ACTIVATE => '32aea199-3b82-4e2d-8228-f2cd8b58c40a',
-       AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT1 => 'e462a4f3-db4a-4748-aecb-7b1b5c653e58',
-       AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_CHEQUE_PAYMENT2 => '6779a351-b53e-4267-8eb7-7f24193e3026',
-       AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_NO_PAYMENT3 => 'b84aa41d-c94e-4bb7-8747-28b9d6ed0d6c',
+       // DO NOT MERGE - temporarily switched IDs to Amy's test service for testing
+       //AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT1 => 'e462a4f3-db4a-4748-aecb-7b1b5c653e58',
+       AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_PAYMENT1 => '8f13c89f-5d0a-44f4-8302-4e7171cdccab',
+       //AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_CHEQUE_PAYMENT2 => '6779a351-b53e-4267-8eb7-7f24193e3026',
+       AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_CHEQUE_PAYMENT2 => '305d0c96-959d-4627-98c8-cf5b12637d22',
+       //AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_NO_PAYMENT3 => 'b84aa41d-c94e-4bb7-8747-28b9d6ed0d6c',
+       AbstractEmailService::EMAIL_LPA_REGISTRATION_WITH_NO_PAYMENT3 => 'dd002071-50fa-4948-98bc-1125ba335a9a',
        AbstractEmailService::EMAIL_NEW_EMAIL_ADDRESS_NOTIFY => '85a14f80-813e-4e72-8dc5-5549d958a592',
        AbstractEmailService::EMAIL_NEW_EMAIL_ADDRESS_VERIFY => '1dd980a2-deab-4a5b-802b-61566188496d',
        AbstractEmailService::EMAIL_PASSWORD_CHANGED => '856f6b93-a248-42ae-9580-5d0ff24b595e',
