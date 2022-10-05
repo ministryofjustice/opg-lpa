510a6ba55eccf932ae597fff097982e937d41c28
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-10-05 13:03:27 +0100

    Add unit tests for continuation notes Twig macro

---
+++ service-front/module/Application/tests/View/ContinuationNotesTwigMacroTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace ApplicationTest\View;
+
+use Application\Model\Service\Lpa\ContinuationSheets;
+use ApplicationTest\View\ViewModelRenderer;
+use DOMDocument;
+use DOMXpath;
+use Laminas\View\Model\ViewModel;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Opg\Lpa\DataModel\Lpa\Lpa;
+
+/**
+ * Unit tests for the Twig macro which renders continuation sheet prompts
+ * for both the "Last steps" and date check pages.
+ */
+class ContinuationNotesTwigMacroTest extends MockeryTestCase
+{
+    /** @var ViewModelRenderer */
+    private $renderer;
+
+    public function setUp(): void
+    {
+        $this->renderer = new ViewModelRenderer();
+
+        // stub out twig functions we don't care about
+        $this->renderer->addFunction('url');
+        $this->renderer->addFunction('formLinkedErrorListV2');
+        $this->renderer->addFunction('formElementErrorsV2');
+        $this->renderer->addFunction('formElement');
+        $this->renderer->addFunction('serverUrl');
+
+        // template will load the macro we're testing and call it
+        $this->renderer->loadTemplateString("
+            {% import \"application/macros.twig\" as macros %}
+            {{ macros.continuationNotes(continuationNoteKeys) }}
+        ");
+    }
+
+    // check that the macro correctly renders the notes about
+    // continuation sheet 2s
+    private function assertCS2NotesRendered($html)
+    {
+        $dom = new DOMDocument();
+
+        $dom->loadHTML($html);
+        $xpath = new DOMXpath($dom);
+
+        // check section title is in place with correct class
+        $matches = $xpath->query('//h3[@class="heading-medium"]');
+        $this->assertEquals(1, $matches->length, 'number of continuation sheet 2 headings is incorrect');
+
+        // check section title has correct content
+        $this->assertEquals(
+            'Continuation sheets',
+            trim(($matches[0])->textContent),
+            'continuation sheet 2 heading contains incorrect text'
+        );
+
+        // check explanatory paragraph is present
+        $matches = $xpath->query('//p');
+        $this->assertEquals(
+            1,
+            $matches->length,
+            'number of continuation sheet 2 explanatory paragraphs is incorrect'
+        );
+
+        // check explanatory paragraph has the correct text in it
+        $this->assertEquals(
+            'As there are extra instructions for the attorneys on the LPA, ' .
+                'the donor will need to sign continuation sheet 2. ' .
+                'The continuation sheet is included at the end of the LPA form.',
+            trim(($matches[0])->textContent),
+            'continuation sheet 2 explanatory paragraph contains incorrect text'
+        );
+    }
+
+    public function testContinuationNotesCS2WhenHasAttorneyDecisions()
+    {
+        $viewModel = new ViewModel([
+            'continuationNoteKeys' => ['HAS_ATTORNEY_DECISIONS'],
+        ]);
+
+        $html = $this->renderer->render($viewModel);
+
+        $this->assertCS2NotesRendered($html);
+    }
+
+    public function testContinuationNotesCS2WhenLongPreferencesOrInstructions()
+    {
+        $viewModel = new ViewModel([
+            'continuationNoteKeys' => ['LONG_INSTRUCTIONS_OR_PREFERENCES'],
+        ]);
+
+        $html = $this->renderer->render($viewModel);
+
+        $this->assertCS2NotesRendered($html);
+    }
+}
