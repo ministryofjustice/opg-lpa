5c780dab3f3658c862e57b121c8eb4cadb023034
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-10-12 14:41:05 +0100

    LPAL-870 Unit tests for template rendering of continuation sheet 2 guidance on "Last steps" page (#1135)
    * Update 'Last steps' text as per LPAL-870 acceptance criteria
    
    * Mark block in template for 'Last steps' template unit tests
    
    * Refactor StatusViewModelHelperTest to extract ViewModelRenderer
    
    * Refactor date check view model unit tests to use ViewModelRenderer
    
    * Extend ViewModelRenderer for tests
    
    * Enable loading a template from a string (useful for testing
    macros).
    * Enable rendering the whole template or a block within it.
    
    * Only render top para if we're not rendering the bottom one
    
    Also cleaned up the Twig template so it doesn't render loads of
    empty lines.
    
    * Add unit tests for continuation notes Twig macro
    
    * Make session timeout cypress test less precise
    
    The session timeout cypress test sets the session timeout to 302
    seconds, so that the popup can be forced to open in the test.
    However, the response being returned when the timeout is updated
    states that the session length is 301 seconds. This suggests there's
    some lag between the session length set by the request, and the
    amount of time left when the response is returned.
    
    To fix this, change the expectation that the response states 302
    seconds as the remaining session time, to expect *at most* 302
    seconds instead.
    
    * Reinstate single quotes around 'continuation sheet 2'

---
+++ service-front/module/Application/tests/View/ViewModelRenderer.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace ApplicationTest\View;
+
+use Laminas\View\Model\ViewModel;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Twig\TwigFunction;
+
+/**
+ * Test helper class to load Twig view templates,
+ * stub out any unnecessary TwigFunctions, add other
+ * TwigFunctions (if desired), and render ViewModel instances.
+ *
+ * Typically, you would want one of these for each template you're
+ * going to render in a test. Alternatively, you can load different
+ * templates with loadTemplate().
+ *
+ * This will cache the compiled Twig templates to build/twig-cache,
+ * which enables them to be included in coverage reports if desired.
+ *
+ * Example usage:
+ *
+ * $renderer = new ViewModelRenderer();
+ * $renderer->addFunction('moneyFormat', MoneyFormat::class);
+ * $renderer->loadTemplate('authenticated/lpa/complete/complete.twig');
+ */
+class ViewModelRenderer
+{
+    /** @var Environment */
+    private $renderer;
+
+    /** @var Template */
+    private $template;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->renderer = new Environment(
+            new FilesystemLoader('module/Application/view'),
+            ['cache' => 'build/twig-cache']
+        );
+    }
+
+    /**
+     * Load a Twig template from the filesystem. Note that this should be done *after*
+     * adding Twig functions with addFunction().
+     *
+     * @param string $templatePath Path to the Twig template,
+     *     relative to module/Application/view/
+     */
+    public function loadTemplate(string $templatePath)
+    {
+        $this->template = $this->renderer->load($templatePath);
+    }
+
+    /**
+     * Load a Twig template from a string. Note that this should be done *after*
+     * adding Twig functions with addFunction().
+     *
+     * @param string $template Template to load.
+     */
+    public function loadTemplateString(string $template)
+    {
+        $this->template = $this->renderer->createTemplate($template);
+    }
+
+    /**
+     * Add a TwigFunction to the renderer.
+     *
+     * @param string $name Name of the Twig function
+     * @param mixed $callable Callable object or function; if not supplied,
+     *     a no-op function is used by default
+     */
+    public function addFunction(string $name, mixed $callable = null): void
+    {
+        if (is_null($callable)) {
+            $callable = function () {
+                return 'noop';
+            };
+        }
+
+        $this->renderer->addFunction(new TwigFunction($name, $callable));
+    }
+
+    /**
+     * Render the block $block using $viewModel to populate its placeholder
+     * variables.
+     *
+     * @param ViewModel $viewModel ViewModel instance to use for populating
+     *     variable placeholders in the template
+     * @param ?string $block Block to render within the template; if null, the
+     *     whole template is rendered
+     */
+    public function render(ViewModel $viewModel, ?string $block = null): string
+    {
+        $vars = (array) $viewModel->getVariables();
+
+        if (is_null($block)) {
+            return $this->template->render($vars);
+        }
+
+        return $this->template->renderBlock($block, $vars);
+    }
+}
