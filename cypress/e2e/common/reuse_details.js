1a554d772c029fa3f26c080f407bcd11b2061eca
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-10-17 11:43:41 +0100

    fix no-unused-vars for cypress code

--- cypress/e2e/common/reuse_details.js
+++ cypress/e2e/common/reuse_details.js
@@ -18,12 +18,12 @@ const seeReuseDetailsLink = (shouldExist) => {
 
 // should('be.checked')  or not checked exists here to ensure that cypress doesn't race off
 // and carry out the next operation without making sure first that the check or uncheck has taken effect
-Then(`I opt not to re-use details`, (checkable) => {
+Then(`I opt not to re-use details`, () => {
     cy.get("[data-cy=reuse-details--1]").check().should('be.checked');
     cy.get("[data-cy=continue]").click();
 })
 
-Then(`I opt not to re-use details if lpa is a clone`, (checkable) => {
+Then(`I opt not to re-use details if lpa is a clone`, () => {
     if (Cypress.env('clonedLpa') === true) {
       cy.get("[data-cy=reuse-details--1]").check().should('be.checked');
       cy.get("[data-cy=continue]").click();
