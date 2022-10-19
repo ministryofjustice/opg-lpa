ce091e1ca30613f99c9aedc2f328cb5db5d829fb
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-10-19 17:11:37 +0100

    Fix lint error

--- cypress/e2e/common/session.js
+++ cypress/e2e/common/session.js
@@ -7,7 +7,7 @@ Then('I hack the session to have {int} seconds remaining', (seconds) => {
     url: '/session-set-expiry',
     method: 'POST',
     body: { expireInSeconds: seconds },
-    headers: { 'Content-Type': 'application/json' }
+    headers: { 'Content-Type': 'application/json' },
   };
 
   cy.request(requestOptions).then((response) => {
