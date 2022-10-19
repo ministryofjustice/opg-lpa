bca879322851be30c60ada65b7763a437c70b5c9
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-10-19 12:22:20 +0100

    Add failing unit tests for FeedbackValidator - details

--- service-api/module/Application/tests/Model/Service/Feedback/FeedbackValidatorTest.php
+++ service-api/module/Application/tests/Model/Service/Feedback/FeedbackValidatorTest.php
@@ -14,18 +14,70 @@ class FeedbackValidatorTest extends MockeryTestCase
         $this->sut = new FeedbackValidator();
     }
 
-    public function testRatingNotSet()
+    public function testRatingNotSet(): void
     {
         $this->assertFalse($this->sut->isValid([]));
     }
 
-    public function testRatingOutsideValidRange()
+    public function testRatingOutsideValidRange(): void
     {
         $this->assertFalse($this->sut->isValid(['rating' => 'foo']));
     }
 
-    public function testRatingValid()
+    public function testDetailsNotSet(): void
     {
-        $this->assertTrue($this->sut->isValid(['rating' => 'satisfied']));
+        $feedbackData = [
+            'rating' => 'satisfied',
+        ];
+
+        $this->assertFalse($this->sut->isValid($feedbackData));
+    }
+
+    public function testDetailsNullOrEmptyOrNotString(): void
+    {
+        $feedbackData = [
+            'rating' => 'satisfied',
+            'details' => null,
+        ];
+
+        $this->assertFalse($this->sut->isValid($feedbackData));
+
+        $feedbackData = [
+            'rating' => 'satisfied',
+            'details' => '',
+        ];
+
+        $this->assertFalse($this->sut->isValid($feedbackData));
+
+        $feedbackData = [
+            'rating' => 'satisfied',
+            'details' => 4,
+        ];
+
+        $this->assertFalse($this->sut->isValid($feedbackData));
+    }
+
+    public function testDetailsTooLong(): void
+    {
+        $feedbackData = [
+            'rating' => 'satisfied',
+
+            // 2001 characters, 1 character over max length for details field
+            'details' => 'a' . str_repeat('a ', 1000),
+        ];
+
+        $this->assertFalse($this->sut->isValid($feedbackData));
+    }
+
+    public function testValid(): void
+    {
+        $feedbackData = [
+            'rating' => 'satisfied',
+
+            // at max length for the details field
+            'details' => str_repeat('c ', 1000),
+        ];
+
+        $this->assertTrue($this->sut->isValid($feedbackData));
     }
 }
