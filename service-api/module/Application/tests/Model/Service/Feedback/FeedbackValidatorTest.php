97d54b341972b11051880b9186a8231260b9b064
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-10-19 14:03:00 +0100

    Add failing unit tests for FeedbackValidator - email

--- service-api/module/Application/tests/Model/Service/Feedback/FeedbackValidatorTest.php
+++ service-api/module/Application/tests/Model/Service/Feedback/FeedbackValidatorTest.php
@@ -4,6 +4,7 @@ namespace ApplicationTest\Model\Service\Feedback;
 
 use Application\Model\Service\Feedback\FeedbackValidator;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
+use str_repeat;
 
 class FeedbackValidatorTest extends MockeryTestCase
 {
@@ -69,6 +70,34 @@ class FeedbackValidatorTest extends MockeryTestCase
         $this->assertFalse($this->sut->isValid($feedbackData));
     }
 
+    public function testEmailProvidedButEmptyString(): void
+    {
+        $feedbackData = [
+            'rating' => 'satisfied',
+            'details' => 'very good',
+            'email' => '',
+        ];
+
+        $this->assertTrue(
+            $this->sut->isValid($feedbackData),
+            'empty email string should be allowed and not be validated'
+        );
+    }
+
+    public function testEmailProvidedButInvalidPattern(): void
+    {
+        $feedbackData = [
+            'rating' => 'satisfied',
+            'details' => 'very good',
+            'email' => 'notanemail',
+        ];
+
+        $this->assertFalse(
+            $this->sut->isValid($feedbackData),
+            'non-empty email string should resemble an email address'
+        );
+    }
+
     public function testValid(): void
     {
         $feedbackData = [
@@ -76,6 +105,8 @@ class FeedbackValidatorTest extends MockeryTestCase
 
             // at max length for the details field
             'details' => str_repeat('c ', 1000),
+
+            'email' => 'emilfoo@example.com',
         ];
 
         $this->assertTrue($this->sut->isValid($feedbackData));
