f3a08b3b4e63b34e396eab5556c1d34fa3289b66
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-11-03 13:34:44 +0000

    Delete Github environments on merge

---
+++ scripts/pipeline/environment_cleanup/environment_cleanup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Please provide workspaces to be removed."
+fi
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` [workspaces separated by a space]"
+  exit 0
+fi
+
+RESPONSE=(curl -X DELETE -s -o /dev/null -w "%{http_code}" -H "Accept: application/vnd.github+json" -H "Authorizartion: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/ministryofjustice/opg-lpa/environments/$1)
+
+if [ "${RESPONSE}" == "204" ]; then
+  echo "Environment $1 deleted"
+  exit 0
+else
+  echo "Environment $1 not deleted"
+  exit 1
+fi
