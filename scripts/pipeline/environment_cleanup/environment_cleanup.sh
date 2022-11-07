bf4ecfea1dfa745f7eb4d2ead2bda5f22cecae62
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-11-07 11:31:51 +0000

    Fix header typo

--- scripts/pipeline/environment_cleanup/environment_cleanup.sh
+++ scripts/pipeline/environment_cleanup/environment_cleanup.sh
@@ -9,14 +9,12 @@ if [ "$1" == "-h" ]; then
   exit 0
 fi
 
-RESPONSE=$(curl -X DELETE -v -H "Accept: application/vnd.github+json" -H "Authorizartion: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/ministryofjustice/opg-lpa/environments/$1)
+RESPONSE=$(curl -X DELETE -s -o /dev/null -w "%{http_code}" -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/ministryofjustice/opg-lpa/environments/$1)
 
-echo $RESPONSE
-
-# if [ "${RESPONSE}" == "204" ]; then
-#   echo "Environment $1 deleted"
-#   exit 0
-# else
-#   echo "Environment $1 not deleted. Github API returned ${RESPONSE}"
-#   exit 1
-# fi
+if [ "${RESPONSE}" == "204" ]; then
+  echo "Environment $1 deleted"
+  exit 0
+else
+  echo "Environment $1 not deleted. Github API returned ${RESPONSE}"
+  exit 1
+fi
