e94aff9fb129c17e1cf2f25ee54a1d289608144d
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-11-07 15:01:50 +0000

    extra click through in cypress tests

--- cypress/e2e/common/feedback.js
+++ cypress/e2e/common/feedback.js
@@ -12,12 +12,24 @@ Then(`I can find feedback buttons`, () => {
   cy.get('[data-cy=feedback-phone]');
 });
 
+Then(`I select very satisfied`, () => {
+  cy.get('[data-cy=rating-very-satisfied]').click();
+});
+
 Then(`I select satisfied`, () => {
   cy.get('[data-cy=rating-satisfied]').click();
 });
 
 Then(`I select neither satisfied or dissatisfied`, () => {
   cy.get('[data-cy=rating-neither-satisfied-or-dissatisfied]').click();
+});
+
+Then(`I select dissatisfied`, () => {
+  cy.get('[data-cy=rating-dissatisfied]').click();
+});
+
+Then(`I select very dissatisfied`, () => {
+  cy.get('[data-cy=rating-very-dissatisfied]').click();
 });
 
 Then(`I set feedback content as {string}`, (email) => {
