516f41ec20652ce3d26650e62976f0370663ee2f
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-11-22 16:34:11 +0000

    Add tfsec ignores for access logs

--- terraform/account/s3.tf
+++ terraform/account/s3.tf
@@ -47,13 +47,14 @@ data "aws_iam_policy_document" "loadbalancer_logging" {
 
 # We will keep this in for historical purposes. we need to think how far back we need this.
 #versioning not required for a logging bucket bucket logging not needed
-#encryption of ALB access logs not supported with CMK
-#tfsec:ignore:aws-s3-enable-bucket-logging  #tfsec:ignore:aws-s3-enable-versioning #tfsec:ignore:aws-s3-encryption-customer-key
+#tfsec:ignore:aws-s3-enable-bucket-logging  #tfsec:ignore:aws-s3-enable-versioning 
 resource "aws_s3_bucket" "access_log" {
   bucket = "online-lpa-${terraform.workspace}-lb-access-logs"
   acl    = "private"
 }
 
+#encryption of ALB access logs not supported with CMK
+#tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket_server_side_encryption_configuration" "access_log" {
   bucket = aws_s3_bucket.access_log.id
 
