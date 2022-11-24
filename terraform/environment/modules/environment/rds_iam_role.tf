303b206911a3518d4445005ebf2f40833cb3ef4a
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-11-24 09:59:03 +0000

    Remove default_tags

--- terraform/environment/modules/environment/rds_iam_role.tf
+++ terraform/environment/modules/environment/rds_iam_role.tf
@@ -1,7 +1,6 @@
 resource "aws_iam_role" "rds_iam" {
   assume_role_policy = data.aws_iam_policy_document.rds_iam.json
   name               = "${var.environment_name}-rds-iam"
-  tags               = local.default_tags
 }
 
 data "aws_iam_policy_document" "rds_iam" {
