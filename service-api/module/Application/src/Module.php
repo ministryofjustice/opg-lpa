eb38d4d3c9acef1e4460b05a0bf0b4dfb045b349
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-11-28 12:16:01 +0000

    More logging

--- service-api/module/Application/src/Module.php
+++ service-api/module/Application/src/Module.php
@@ -103,6 +103,8 @@ class Module
                     $token = $RdsAuthGenerator->createToken("api2-1220lpal517.cluster-cycofak3lgax.eu-west-1.rds.amazonaws.com", 'eu-west-1', 'db_userx');
                     // $token = $RdsAuthGenerator->createToken($dbconf['host'] . ":" . $dbconf['port'], 'eu-west-1', 'db_userx');
                     
+                    $this->getLogger()->info("Provider: " . var_dump($RdsAuthGenerator));
+                    $this->getLogger()->info("Provider: " . var_dump($provider));
                     $this->getLogger()->info("Token: " . var_dump($token));
 
                     return new ZendDbAdapter([
