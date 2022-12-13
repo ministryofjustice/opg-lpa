1fc93f257d40a6e695a645336b4c914d88a75eca
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-12-13 12:02:26 +0000

    LPAL-1115 Fix destroy workspace on merge to main

---
+++ scripts/pipeline/workspace_cleanup/destroy_workspace.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# A script to destroy a single workspace
+# Usage: ./destroy_workspace.sh <workspace_name>
+
+set -Eeuo pipefail
+
+print_usage() {
+  echo "Usage: `basename $0` [workspace]"
+}
+
+if [ -z "$1" ]; then
+    print_usage
+    exit 1
+fi
+
+if [ "$1" == "-h" ]; then
+  print_usage
+  exit 0
+fi
+
+workspace_name=$1
+reserved_workspaces="default production preproduction development demo ithc"
+
+for workspace in $reserved_workspaces; do
+  if [ "$workspace" == "$workspace_name" ]; then
+    echo "protected workspace: $workspace. refusing to destroy."
+    exit 1
+  fi
+done
+
+echo "cleaning up workspace $workspace_name..."
+terraform workspace select $workspace_name
+terraform init 
+terraform destroy -auto-approve
+terraform workspace select default
+terraform workspace delete $workspace_name
