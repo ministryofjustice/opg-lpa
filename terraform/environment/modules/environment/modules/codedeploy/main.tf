ce51366e22ef24c5eb601463cfd77e6f23dcb2ff
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-12-14 11:48:56 +0000

    correct polis name

--- terraform/environment/modules/environment/modules/codedeploy/main.tf
+++ terraform/environment/modules/environment/modules/codedeploy/main.tf
@@ -80,6 +80,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 resource "aws_iam_role_policy_attachment" "main" {
   role       = aws_iam_role.main.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECSLimited"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECSLimited"
 }
 
