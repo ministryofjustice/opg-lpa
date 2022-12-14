4957e1d338b601cb2c7ca0d0893406e58cd39343
Sam Wilkinson <sam.wilkinson@digital.justice.gov.uk>
2022-12-14 14:39:05 +0000

    LPAL-876 Add OpenTelemetry sidecar containers

--- terraform/environment/modules/environment/ecs_api.tf
+++ terraform/environment/modules/environment/ecs_api.tf
@@ -123,7 +123,7 @@ resource "aws_ecs_task_definition" "api" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  container_definitions    = "[${local.api_web}, ${local.api_app}, ${local.app_init_container}]"
+  container_definitions    = "[${local.api_web}, ${local.api_app}, ${local.app_init_container}, ${local.aws_otel_collector}]"
   task_role_arn            = aws_iam_role.api_task_role.arn
   execution_role_arn       = aws_iam_role.execution_role.arn
   tags                     = local.api_component_tag
