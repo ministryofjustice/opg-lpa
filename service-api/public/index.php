af5778d4612b9def08f476e69cba77b5cfe93726
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2022-12-15 15:14:01 +0000

    POC of adding opentelemetry to service-api

--- service-api/public/index.php
+++ service-api/public/index.php
@@ -1,31 +1,48 @@
 <?php
 
+require_once(__DIR__ . '/../vendor/autoload.php');
+
 use Laminas\Mvc\Application;
+use OpenTelemetry\SDK\Trace\SpanExporter\ConsoleSpanExporterFactory;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
+use OpenTelemetry\SDK\Trace\TracerProvider;
 
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
+$tracerProvider = new TracerProvider(
+    new SimpleSpanProcessor(
+        (new ConsoleSpanExporterFactory())->create()
+    )
+);
 
-// Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (__FILE__ !== $path && is_file($path)) {
-        return false;
+$tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php');
+$rootSpan = $tracer->spanBuilder('root')->startSpan();
+$rootScope = $rootSpan->activate();
+
+try {
+    // This makes our life easier when dealing with paths. Everything is relative
+    // to the application root now.
+    chdir(dirname(__DIR__));
+
+    // Run the application!
+    $app = Application::init(require_once(__DIR__ . '/../config/application.config.php'));
+
+    $app->run();
+} finally {
+    $rootSpan->end();
+    $rootScope->detach();
+}
+
+/*
+try {
+    $span1 = $tracer->spanBuilder('foo')->startSpan();
+    $scope = $span1->activate();
+    try {
+        $span2 = $tracer->spanBuilder('bar')->startSpan();
+        echo 'OpenTelemetry welcomes PHP' . PHP_EOL;
+    } finally {
+        $span2->end();
     }
-    unset($path);
+} finally {
+    $span1->end();
+    $scope->detach();
 }
-
-// Composer autoloading
-include __DIR__ . '/../vendor/autoload.php';
-
-if (! class_exists(Application::class)) {
-    throw new RuntimeException(
-        "Unable to load application.\n"
-        . "- Type `composer install` if you are developing locally.\n"
-    );
-}
-
-// Run the application!
-Application::init(require __DIR__ . '/../config/application.config.php')->run();
+*/
