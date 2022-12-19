4df5791c59e589f47b8d904eb9aef817555f41fd
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2022-12-19 15:05:33 +0000

    Add guard so events are only triggered if event manager is set

--- shared/module/MakeShared/src/Telemetry/TelemetryEventManager.php
+++ shared/module/MakeShared/src/Telemetry/TelemetryEventManager.php
@@ -10,7 +10,7 @@ use MakeShared\Telemetry\Event;
 
 class TelemetryEventManager
 {
-    public static EventManagerInterface $eventManager;
+    public static ?EventManagerInterface $eventManager = null;
 
     public static function setEventManager(EventManagerInterface $eventManager)
     {
@@ -19,12 +19,20 @@ class TelemetryEventManager
 
     public static function triggerStart(string $spanName, array $attributes = [])
     {
+        if (is_null(self::$eventManager)) {
+            return;
+        }
+
         $event = new Event(Constants::TELEMETRY_START_CHILD, $spanName, $attributes);
         self::$eventManager->triggerEvent($event);
     }
 
     public static function triggerStop(string $spanName)
     {
+        if (is_null(self::$eventManager)) {
+            return;
+        }
+
         $event = new Event(Constants::TELEMETRY_STOP_CHILD, $spanName);
         self::$eventManager->triggerEvent($event);
     }
