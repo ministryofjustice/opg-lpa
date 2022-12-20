ae0296f42e332f4e3e33ccddf230f93bbf92642b
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2022-12-20 15:16:14 +0000

    Documentation improvements

--- shared/module/MakeShared/src/Telemetry/Module.php
+++ shared/module/MakeShared/src/Telemetry/Module.php
@@ -10,6 +10,89 @@ use MakeShared\Telemetry\Event;
 use MakeShared\Telemetry\TelemetryEventManager;
 use MakeShared\Telemetry\Tracer;
 
+/**
+ * Set up event handlers for capturing trace events triggered in the code.
+ * This approach was used rather than singletons so that the tracer
+ * instance can be configured like any other Laminas service. To start a new
+ * span in code, use the TelemetryEventManager to trigger start/stop child
+ * events.
+ *
+ * To incorporate telemetry into your Laminas application, you'll need to load
+ * this module in application.config.php:
+ *
+ * return array(
+ *     'modules' => array(
+ *         // ... other modules here ...
+ *
+ *         'Telemetry',
+ *     ),
+ *     'module_listener_options' => array(
+ *         'module_paths' => array(
+ *              './module',
+ *              './vendor',
+ *
+ *              // or whatever the path is to the directory; I ended up doing this
+ *              // because the autloader refused to find this directory
+ *              'Telemetry' => __DIR__ . '/../../shared/module/MakeShared/src/Telemetry',
+ *          ),
+ *      ),
+ *
+ *      // ... other application config
+ * );
+ *
+ * You will also need to add a telemetry tracer resource to your application module,
+ * called TelemetryTracer:
+ *
+ *    class Module
+ *    {
+ *        public function getServiceConfig()
+ *        {
+ *            return [
+ *                // ... other factories ...
+ *
+ *                'factories' => [
+ *                    'TelemetryTracer' => function ($sm) {
+ *                        $telemetryConfig = $sm->get('config')['telemetry'];
+ *                        return Tracer::create($telemetryConfig);
+ *                    },
+ *                ]
+ *            ];
+ *        }
+ *    }
+ *
+ * Note that here we pass in some config which contains an exporter.url property
+ * which points to an aws-otel-collector in prod and is empty in dev (resulting
+ * in all telemetry being written to the console).
+ * This is mainly used to configure the exporter used by the Tracer. If you need
+ * more flexibility, it would be fairly easy to refactor the Tracer class into
+ * an interface, then implement a tracer with different OpenTelemetry configuration.
+ *
+ * The $this->tracer->start() call in this module sets up the root span.
+ *
+ * Then, to trace an individual piece of code, surround it like this:
+ *
+ *     use MakeShared\Telemetry\TelemetryEventManager;
+ *
+ *     TelemetryEventManager::triggerStart('DbWrapper.select', ['table' => $tableName]);
+ *
+ *     // ******* code to be traced goes here *******
+ *
+ *     TelemetryEventManager::triggerStop('DbWrapper.select');
+ *
+ * triggerStart() and triggerStop() trigger start/stop events respectively,
+ * which set up and tear down a trace span. The array passed to triggerStart()
+ * consists of key/value pairs which set attributes on the trace.
+ *
+ * Usually, triggerStart() fires an event which in turn causes
+ * a child span to be attached to the root span. However, if you start a
+ * child span B while another child span A is already active, the
+ * OT API will make B a child of A rather than root.
+ * So, to attach children to the root span, stop any other child spans before starting
+ * a new one.
+ *
+ * Tracing is automatically stopped and cleaned up by this module via
+ * $this->tracer->stop().
+ */
 class Module
 {
     private Tracer $tracer;
@@ -27,15 +110,19 @@ class Module
 
     public function onBootstrap(MvcEvent $event): void
     {
-        $this->tracer = $event->getApplication()->getServiceManager()->get('TelemetryTracer');
+        $app = $event->getApplication();
+
+        $this->tracer = $app->getServiceManager()->get('TelemetryTracer');
+
+        // Establish the root trace
         $this->tracer->start();
 
-        $eventManager = $event->getApplication()->getEventManager();
+        $eventManager = $app->getEventManager();
         $eventManager->attach(Constants::TELEMETRY_START_CHILD, [$this, 'startChild']);
         $eventManager->attach(Constants::TELEMETRY_STOP_CHILD, [$this, 'stopChild']);
         $eventManager->attach(MvcEvent::EVENT_FINISH, [$this, 'onFinish']);
 
-        // gives us a globally-accessible Laminas event manager for triggering telemetry events
+        // Gives us a globally-accessible Laminas event manager for triggering telemetry events
         TelemetryEventManager::setEventManager($eventManager);
     }
 
