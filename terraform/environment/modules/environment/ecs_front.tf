a47796ecb13f61389557146febb6937e49167b04
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2023-02-07 11:00:23 +0000

    Set per-environment telemetry sampling rate

--- terraform/environment/modules/environment/ecs_front.tf
+++ terraform/environment/modules/environment/ecs_front.tf
@@ -300,7 +300,8 @@ locals {
         { "name" : "AWS_ACCOUNT_TYPE", "value" : var.account_name },
         { "name" : "OPG_LPA_FRONT_EMAIL_TRANSPORT", "value" : "notify" },
         { "name" : "OPG_LPA_TELEMETRY_HOST", "value" : "127.0.0.1" },
-        { "name" : "OPG_LPA_TELEMETRY_PORT", "value" : "2000" }
+        { "name" : "OPG_LPA_TELEMETRY_PORT", "value" : "2000" },
+        { "name" : "OPG_LPA_TELEMETRY_REQUESTS_SAMPLED_FRACTION", "value" : "${var.account.telemetry_requests_sampled_fraction}" }
       ]
     }
   )
