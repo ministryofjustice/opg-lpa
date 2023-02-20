bb7ddfbe43202eb45f1cd7786021b8688b2f4b4a
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2023-02-20 09:54:05 +0000

    js-enabled added to className by main.js rather than inline js

--- service-front/assets/js/main.js
+++ service-front/assets/js/main.js
@@ -15,3 +15,20 @@ showHideContent.init();
 
 // Remove the no-js class
 $('body').removeClass('no-js');
+
+var dashboard = function () {
+  if ($('span[data-page=dashboard]').length === 0) {
+    return;
+  }
+};
+
+var jsEnabled = function () {
+  document.body.className = document.body.className + ' js-enabled';
+};
+
+var inline = function () {
+  dashboard();
+  jsEnabled();
+};
+
+$(document).ready(inline);
