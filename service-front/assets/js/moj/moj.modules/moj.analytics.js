a5c0ebbec03b29c715bbbd3ab09f0b98d91de84d
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2023-03-17 17:15:15 +0000

    Load GA from our code, taking cookies into account

--- service-front/assets/js/moj/moj.modules/moj.analytics.js
+++ service-front/assets/js/moj/moj.modules/moj.analytics.js
@@ -5,14 +5,32 @@
   'use strict';
 
   moj.Modules.Analytics = {
+    dataLayer: window.dataLayer || [],
+
     init: function () {
+      // only send analytics data from the main website
+      if (
+        window.location.hostname.indexOf(
+          'lastingpowerofattorney.service.gov.uk',
+        ) == -1
+      ) {
+        return;
+      }
+
       // Check if we have permission to enable tracking
       if (
-        typeof GOVUK.checkConsentCookieCategory === 'function' &&
-        GOVUK.checkConsentCookieCategory('analytics', 'usage')
+        typeof GOVUK.checkConsentCookieCategory !== 'function' ||
+        !GOVUK.checkConsentCookieCategory('analytics', 'usage')
       ) {
-        console.log('!!!!!!!! ENABLE ANALYTICS HERE');
+        return;
       }
+
+      this.gtag('js', new Date());
+      this.gtag('config', 'G-DY4BCL021L');
+    },
+
+    gtag: function () {
+      this.dataLayer.push(arguments);
     },
   };
 })();
