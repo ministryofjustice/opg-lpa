5501676d87e2158d30671abcb933049a8f2b747c
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2023-03-17 14:52:51 +0000

    Clear out existing GA initialisation

--- service-front/assets/js/moj/moj.modules/moj.analytics.js
+++ service-front/assets/js/moj/moj.modules/moj.analytics.js
@@ -4,11 +4,6 @@
 (function () {
   'use strict';
 
-  if (typeof gaConfig === 'undefined') {
-    moj.log('gaConfig not set. skipping Google Analytics tracking.');
-    return;
-  }
-
   moj.Modules.Analytics = {
     init: function () {
       this.bindEvents();
@@ -18,49 +13,7 @@
         typeof GOVUK.checkConsentCookieCategory === 'function' &&
         GOVUK.checkConsentCookieCategory('analytics', 'usage')
       ) {
-        this.setup();
-      }
-    },
-
-    bindEvents: function () {
-      moj.Events.on('Analytics.start', this.setup);
-    },
-
-    setup: function () {
-      GOVUK.Analytics.load();
-
-      // Use document.domain in dev, preview and staging so that tracking works
-      // Otherwise remove proceeding www, as only prod will append www to the URL
-      var regEx = new RegExp('^www.');
-      var cookieDomain = regEx.test(document.domain)
-        ? document.domain.replace(regEx, '.')
-        : document.domain;
-
-      // Configure profiles and make interface public
-      // for custom dimensions, virtual pageviews and events
-      GOVUK.analytics = new GOVUK.Analytics({
-        universalId: gaConfig.universalId || '',
-        cookieDomain: cookieDomain,
-        allowLinker: true,
-        allowAnchor: true,
-
-        //TODO are we tracking this within lpa
-        stripPostcodePII: true,
-        stripDatePII: true,
-      });
-
-      if (regEx.test(document.domain)) {
-        GOVUK.analytics.addLinkedTrackerDomain(gaConfig.govId, 'govuk_shared', [
-          'www.gov.uk',
-          '.payments.service.gov.uk',
-        ]);
-      }
-
-      // Track initial pageview
-      if (typeof GOVUK.pageviewOptions !== 'undefined') {
-        GOVUK.analytics.trackPageview(null, null, GOVUK.pageviewOptions);
-      } else {
-        GOVUK.analytics.trackPageview();
+        console.log('ENABLE ANALYTICS HERE');
       }
     },
   };
