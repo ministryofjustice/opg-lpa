d13ad6de8812e7167a6635aa08da1051db26984f
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2023-03-17 15:17:14 +0000

    Remove all analytics-related client-side files except our base one

--- service-front/assets/js/moj/moj.modules/moj.analytics.js
+++ service-front/assets/js/moj/moj.modules/moj.analytics.js
@@ -6,14 +6,12 @@
 
   moj.Modules.Analytics = {
     init: function () {
-      this.bindEvents();
-
       // Check if we have permission to enable tracking
       if (
         typeof GOVUK.checkConsentCookieCategory === 'function' &&
         GOVUK.checkConsentCookieCategory('analytics', 'usage')
       ) {
-        console.log('ENABLE ANALYTICS HERE');
+        console.log('!!!!!!!! ENABLE ANALYTICS HERE');
       }
     },
   };
