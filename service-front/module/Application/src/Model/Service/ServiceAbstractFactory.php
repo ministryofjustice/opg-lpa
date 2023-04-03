67e4fda576c7fcc6f4d4e7b0085b4dacc826dc9a
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2023-04-03 16:16:20 +0100

    Wire up MailTransportInterface

--- service-front/module/Application/src/Model/Service/ServiceAbstractFactory.php
+++ service-front/module/Application/src/Model/Service/ServiceAbstractFactory.php
@@ -48,6 +48,7 @@ class ServiceAbstractFactory implements AbstractFactoryInterface
             'setSessionSaveHandler' => 'SaveHandler',
             'setOrdnanceSurveyClient' => 'OrdnanceSurvey',
             'setRedisClient' => 'RedisClient',
+            'setMailTransport' => 'MailTransport',
         ],
     ];
 
