e73deb57649a6dd4a125a1864fcf4b1f4e002a63
Mabel Li <mabel.li@digital.justice.gov.uk>
2023-04-05 19:15:08 +0100

    Add logging statements as data is not streaming

--- service-front/assets/js/moj/moj.modules/moj.analytics.js
+++ service-front/assets/js/moj/moj.modules/moj.analytics.js
@@ -9,27 +9,32 @@
 
     init: function () {
       // only send analytics data from the main website
-      if (
-        window.location.hostname.indexOf(
-          '1469lpal118.development.front.lpa.opg.service.justice.gov.uk',
-        ) == -1
-      ) {
-        return;
-      }
+      console.log('func init');
+      console.log('host name: ' + window.location.hostname);
+      // if (
+      //   window.location.hostname.indexOf(
+      //     '1469lpal118.development.front.lpa.opg.service.justice.gov.uk',
+      //   ) == -1
+      // ) {
+      //   return;
+      // }
 
       // Check if we have permission to enable tracking
       if (
         typeof GOVUK.checkConsentCookieCategory !== 'function' ||
         !GOVUK.checkConsentCookieCategory('analytics', 'usage')
       ) {
+        console.log('analytics rejected - about to return');
         return;
       }
 
+      console.log('about to add tags!');
       this.gtag('js', new Date());
       this.gtag('config', 'G-1DVC295G9L');
     },
 
     gtag: function () {
+      console.log('pushing arguments');
       this.dataLayer.push(arguments);
     },
   };
