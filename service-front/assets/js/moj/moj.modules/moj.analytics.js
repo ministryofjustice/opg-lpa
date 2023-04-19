66942a308df5f33b69eb168b47725467bd1e9dd6
Mabel Li <mabel.li@digital.justice.gov.uk>
2023-04-19 10:40:18 +0100

    Use debug_mode when domain is not main site

--- service-front/assets/js/moj/moj.modules/moj.analytics.js
+++ service-front/assets/js/moj/moj.modules/moj.analytics.js
@@ -6,30 +6,33 @@
 
   moj.Modules.Analytics = {
     dataLayer: window.dataLayer || [],
+    gaId: '1DVC295G9L',
 
     init: function () {
-      // only send analytics data from the main website
-      // if (
-      //   window.location.hostname.indexOf(
-      //     '1469lpal118.development.front.lpa.opg.service.justice.gov.uk',
-      //   ) == -1
-      // ) {
-      //   return;
-      // }
-
       // Check if we have permission to enable tracking
       if (
         typeof GOVUK.checkConsentCookieCategory !== 'function' ||
         !GOVUK.checkConsentCookieCategory('analytics', 'usage')
       ) {
-        // delete session state _ga cookie
-        document.cookie =
-          '_ga_1DVC295G9L=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+        // set session state _ga cookie to expire
+        document.cookie = `_ga_${this.gaId}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
         return;
       }
 
       this.gtag('js', new Date());
-      this.gtag('config', 'G-1DVC295G9L');
+
+      // GA is in debug mode ('dev traffic') if traffic is not from the main website - ie. localhost
+      // or dev/test environments - this allows us to use a data filter to filter out dev traffic in GA
+      // and keep integration tests that check analytics cookies
+      if (
+        window.location.hostname.indexOf(
+          'lastingpowerofattorney.service.gov.uk',
+        ) == 0
+      ) {
+        this.gtag('config', `G-${this.gaId}`);
+      } else {
+        this.gtag('config', `G-${this.gaId}`, { debug_mode: true });
+      }
     },
 
     gtag: function () {
