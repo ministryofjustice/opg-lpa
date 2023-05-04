7fec7e6e29ef6d1a7474091b04124535b83918ed
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-05-04 15:57:25 +0100

    Spin up in eu-west-2

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -60,10 +60,10 @@ module "environment_dns" {
   }
   account_name     = local.account_name
   environment_name = local.environment_name
-  front_dns_name   = !local.dr_enabled ? module.eu-west-1.front_dns_name : module.eu-west-2[0].front_dns_name
-  front_zone_id    = !local.dr_enabled ? module.eu-west-1.front_zone_id : module.eu-west-2[0].front_zone_id
-  admin_dns_name   = !local.dr_enabled ? module.eu-west-1.admin_dns_name : module.eu-west-2[0].admin_dns_name
-  admin_zone_id    = !local.dr_enabled ? module.eu-west-1.admin_zone_id : module.eu-west-2[0].admin_zone_id
+  front_dns_name   = local.account.regions["eu-west-1"].is_primary ? module.eu-west-1.front_dns_name : module.eu-west-2[0].front_dns_name
+  front_zone_id    = local.account.regions["eu-west-1"].is_primary ? module.eu-west-1.front_zone_id : module.eu-west-2[0].front_zone_id
+  admin_dns_name   = local.account.regions["eu-west-1"].is_primary ? module.eu-west-1.admin_dns_name : module.eu-west-2[0].admin_dns_name
+  admin_zone_id    = local.account.regions["eu-west-1"].is_primary ? module.eu-west-1.admin_zone_id : module.eu-west-2[0].admin_zone_id
 
 }
 
