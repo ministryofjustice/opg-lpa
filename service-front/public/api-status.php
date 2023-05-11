b70b51cca0abc482832c43b01f26a5e0df5f1e0c
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-05-11 11:36:44 +0100

    update status

--- service-front/public/api-status.php
+++ service-front/public/api-status.php
@@ -1,15 +1,16 @@
 <?php
 
-$url = getenv('OPG_LPA_ENDPOINTS_API') . '/fpm-status?json';
+$url = getenv('OPG_LPA_ENDPOINTS_API') . '/fpm-status';
 
-$cURLConnection = curl_init();
+$ch = curl_init();
 
-curl_setopt($cURLConnection, CURLOPT_URL, $url);
-curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)");
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 
-$json = curl_exec($cURLConnection);
-curl_close($cURLConnection);
+$data = curl_exec($ch);
+curl_close($ch);
 
-$jsonArrayResponse - json_decode($json);
-
-echo $jsonArrayResponse;
+echo $data;
