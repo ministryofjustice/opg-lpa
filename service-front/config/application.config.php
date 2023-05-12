be344318e8e71e633cb68675be73e555f494b352
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-05-12 16:55:13 +0100

    Don't cache config

--- service-front/config/application.config.php
+++ service-front/config/application.config.php
@@ -42,7 +42,7 @@ return array(
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        'config_cache_enabled' => true,
+        // 'config_cache_enabled' => true,
         // The key used to create the configuration cache file name.
         //'config_cache_key' => $stringKey,
         // Whether or not to enable a module class map cache.
@@ -52,7 +52,7 @@ return array(
         // The key used to create the class map cache file name.
         //'module_map_cache_key' => $stringKey,
         // The path in which to cache merged configuration.
-        'cache_dir' => '/tmp/cache',
+        // 'cache_dir' => '/tmp/cache',
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
