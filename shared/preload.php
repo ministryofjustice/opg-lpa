2f29f7eda8d1f62b9a9371241ed152fff6bc9372
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-08-21 09:48:44 +0100

    Correct class map

--- shared/preload.php
+++ shared/preload.php
@@ -1,11 +1,6 @@
 <?php
-$sharedClassMap = require __DIR__ . '/vendor/composer/autoload_classmap.php';
-$sharedFileMap = array_flip($sharedClassMap);
-
 $appClassMap = require '/app/vendor/composer/autoload_classmap.php';
 $appFileMap = array_flip($appClassMap);
-
-$combinedFileMap = array_merge($appFileMap, $sharedFileMap);
 
 foreach ($combinedFileMap as $file => $class) {
     opcache_compile_file($file);
