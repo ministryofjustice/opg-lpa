d24b8e1ba896ff79373326c9ab7d42f32ba4a4ce
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-08-21 10:12:53 +0100

    Correction

--- shared/preload.php
+++ shared/preload.php
@@ -2,7 +2,7 @@
 $appClassMap = require '/app/vendor/composer/autoload_classmap.php';
 $appFileMap = array_flip($appClassMap);
 
-foreach ($combinedFileMap as $file => $class) {
+foreach ($appFileMap as $file => $class) {
     opcache_compile_file($file);
 }
 ?>
