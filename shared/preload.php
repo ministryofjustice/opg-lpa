48af32665952ded6075ce7171f478d5eba7af789
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-08-21 16:27:20 +0100

    Temporaryily set memory limit

--- shared/preload.php
+++ shared/preload.php
@@ -3,6 +3,8 @@ $directory = new RecursiveDirectoryIterator('/app');
 $fullTree = new RecursiveIteratorIterator($directory);
 $phpFiles = new RegexIterator($fullTree, '/.+((?<!Test)+\.php$)/i', RecursiveRegexIterator::GET_MATCH);
 
+ini_set('memory_limit', '-1');
+
 foreach ($phpFiles as $key => $file) {
     opcache_compile_file($file[0]);
 }
