1cf7befbd989297ffe0ab5c31de6deff2b27b0f1
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-08-23 16:45:20 +0100

    LPAL-1239 Remove breakglass alarms

--- terraform/account/cloudwatch.tf
+++ terraform/account/cloudwatch.tf
@@ -1,35 +1,3 @@
 data "aws_cloudwatch_log_group" "cloudtrail" {
   name = "online_lpa_cloudtrail_${local.account_name}"
 }
-
-resource "aws_cloudwatch_log_metric_filter" "breakglass_metric" {
-  count          = local.account.is_production ? 1 : 0
-  name           = "BreakGlassConsoleLogin"
-  pattern        = "{ ($.eventType = \"AwsConsoleSignIn\") && ($.userIdentity.arn = \"arn:aws:sts::${local.account_id}:assumed-role/breakglass/*\") }"
-  log_group_name = data.aws_cloudwatch_log_group.cloudtrail.name
-
-  metric_transformation {
-    name      = "EventCount"
-    namespace = "online-lpa/Cloudtrail"
-    value     = "1"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "account_breakglass_login_alarm" {
-  count               = local.account.is_production ? 1 : 0
-  actions_enabled     = true
-  alarm_name          = "${local.account_name} breakglass console login check"
-  alarm_actions       = [aws_sns_topic.cloudwatch_to_slack_breakglass_alerts.arn]
-  ok_actions          = [aws_sns_topic.cloudwatch_to_slack_breakglass_alerts.arn]
-  alarm_description   = "number of breakglass attempts"
-  namespace           = "online-lpa/Cloudtrail"
-  metric_name         = "EventCount"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  period              = 60
-  evaluation_periods  = 1
-  datapoints_to_alarm = 1
-  statistic           = "Sum"
-  tags                = local.shared_component_tag
-  threshold           = 1
-  treat_missing_data  = "notBreaching"
-}
--- terraform/account/sns.tf
+++ terraform/account/sns.tf
@@ -1,8 +1,3 @@
-#tfsec:ignore:aws-sns-enable-topic-encryption fix for now, to be followed with a CMK KMS
-resource "aws_sns_topic" "cloudwatch_to_slack_breakglass_alerts" {
-  name = "CloudWatch-to-PagerDuty-${local.account_name}-Breakglass-alert"
-  tags = local.shared_component_tag
-}
 #tfsec:ignore:aws-sns-enable-topic-encryption unsupported for this type of alert
 resource "aws_sns_topic" "cloudwatch_to_slack_elasticache_alerts" {
   name = "CloudWatch-to-PagerDuty-${local.account_name}-elasticache-alert"
