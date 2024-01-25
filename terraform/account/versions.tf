3b10b4ce37380ec662b172365e3896b6ad130dfa
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2024-01-25 15:16:26 +0000

    Use versions.tf file

---
+++ terraform/account/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    pagerduty = {
+      source  = "PagerDuty/pagerduty"
+      version = "~> 2.0"
+    }
+  }
+  required_version = "1.6.3"
+}
