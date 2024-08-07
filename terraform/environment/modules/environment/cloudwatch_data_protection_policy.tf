ba936273fb6f3016fab4348127291a59dc91ed9d
gillettmoj <jason.gillett@digital.justice.gov.uk>
2024-08-07 15:38:17 +0100

    Update data protection policy to include email regex (#2081)
    * Update data protection policy to include email regex
    
    * Add email address regex to data protection policy

--- terraform/environment/modules/environment/cloudwatch_data_protection_policy.tf
+++ terraform/environment/modules/environment/cloudwatch_data_protection_policy.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_data_protection_policy" "application_logs" {
 
-  log_group_name = aws_cloudwatch_log_group.application_logs.name
+  log_group_name = "${var.environment_name}_application_logs"
 
   policy_document = jsonencode({
     Name    = "data_protection_${var.environment_name}_application_logs"
@@ -10,7 +10,8 @@ resource "aws_cloudwatch_log_data_protection_policy" "application_logs" {
       {
         "Sid" : "audit-policy",
         "DataIdentifier" : [
-          "arn:aws:dataprotection::aws:data-identifier/EmailAddress"
+          "arn:aws:dataprotection::aws:data-identifier/EmailAddress",
+          "HTTPGetEmailAddress"
         ],
         "Operation" : {
           "Audit" : {
@@ -21,7 +22,8 @@ resource "aws_cloudwatch_log_data_protection_policy" "application_logs" {
       {
         "Sid" : "redact-policy",
         "DataIdentifier" : [
-          "arn:aws:dataprotection::aws:data-identifier/EmailAddress"
+          "arn:aws:dataprotection::aws:data-identifier/EmailAddress",
+          "HTTPGetEmailAddress"
         ],
         "Operation" : {
           "Deidentify" : {
@@ -29,6 +31,14 @@ resource "aws_cloudwatch_log_data_protection_policy" "application_logs" {
           }
         }
       }
-    ]
+    ],
+    "Configuration" : {
+      "CustomDataIdentifier" : [
+        {
+          "Name" : "HTTPGetEmailAddress",
+          "Regex" : "\\/create-account-success\\?email=[\\w-\\.]+%40([\\w-]+\\.)+[\\w-]{2,4}&resend=true"
+        }
+      ]
+    }
   })
 }
