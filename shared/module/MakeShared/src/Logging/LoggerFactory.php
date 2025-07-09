f69aa0a9c8edb9ed4e587ac2fc055f7d2de2ddee
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-07-09 14:09:33 +0100

    get logger out of container instead of new-ing it

--- shared/module/MakeShared/src/Logging/LoggerFactory.php
+++ shared/module/MakeShared/src/Logging/LoggerFactory.php
@@ -13,7 +13,7 @@ class LoggerFactory implements FactoryInterface
 
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null) : Logger
     {
-        $logger = new Logger('MakeAnLPALogger');
+        $logger = $container->get('Logger');
         $logger->pushHandler(new StreamHandler('php://stderr', Level::Debug  ));
         $logger->pushProcessor(new MvcEventProcessor())
             ->pushProcessor(new HeadersProcessor())
