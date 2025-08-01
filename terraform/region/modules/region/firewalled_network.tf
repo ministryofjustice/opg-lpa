e440cd45796bb2edffaacbb10967baa7d9afc017
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-08-01 11:06:23 +0100

    use PR version

--- terraform/region/modules/region/firewalled_network.tf
+++ terraform/region/modules/region/firewalled_network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source                              = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v0.2.15"
+  source                              = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v0.2.16-trimspacesfr.0"
   cidr                                = var.network_cidr_block
   enable_dns_hostnames                = true
   enable_dns_support                  = true
