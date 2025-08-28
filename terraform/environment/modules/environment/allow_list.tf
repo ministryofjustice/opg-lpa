ea8de79a990a34104f25b4d0425c2febf83f4750
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-08-28 14:08:35 +0100

    get new network resources

---
+++ terraform/environment/modules/environment/allow_list.tf
@@ -0,0 +1,3 @@
+module "allowed_ip_list" {
+  source = "github.com/ministryofjustice/terraform-aws-moj-ip-whitelist.git?ref=v2.3.2"
+}
---
+++ terraform/environment/modules/environment/network.tf
@@ -0,0 +1,63 @@
+# old network
+data "aws_vpc" "default" {
+  default = "true"
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  tags = {
+    Name = "private*"
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  tags = {
+    Name = "public*"
+  }
+}
+
+# firewalled network
+data "aws_availability_zones" "available" {
+}
+
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:application"
+    values = [data.aws_default_tags.current.tags.application]
+  }
+  filter {
+    name   = "tag:name"
+    values = ["${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.account-name}-vpc"]
+  }
+}
+
+data "aws_subnet" "application" {
+  count             = 3
+  vpc_id            = data.aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  filter {
+    name   = "tag:Name"
+    values = ["application*"]
+  }
+}
+
+data "aws_subnet" "public" {
+  count             = 3
+  vpc_id            = data.aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  filter {
+    name   = "tag:Name"
+    values = ["public*"]
+  }
+}
