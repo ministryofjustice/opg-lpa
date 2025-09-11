52fcdd619fa7d3935f2fe0d9d669770790d4e03d
Allen Annom <allen.annom@publicguardian.gov.uk>
2025-09-11 15:49:29 +0100

    updated existing radio buttons to support the new v5 govuk-frontend.js

--- service-front/assets/js/opg/govuk-init.js
+++ service-front/assets/js/opg/govuk-init.js
@@ -1,2 +1,15 @@
 import { initAll } from '/assets/v2/js/govuk-frontend.min.js';
-initAll();
+
+if (!window.__govukInited) {
+  window.__govukInited = true;
+
+  const start = () => {
+    initAll();
+    document.documentElement.dataset.govukInit = 'done';
+    document.dispatchEvent(new CustomEvent('govuk:init'));
+  };
+
+  document.readyState === 'loading'
+    ? document.addEventListener('DOMContentLoaded', start, { once: true })
+    : start();
+}
--- service-front/public/assets/v2/js/govuk-init.js
+++
@@ -1,2 +0,0 @@
-import { initAll } from "/assets/v2/js/govuk-frontend.min.js";
-initAll();
