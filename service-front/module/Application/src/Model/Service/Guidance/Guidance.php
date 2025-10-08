25297cb7ec2641aaa2071995a2cae7c3172dc55f
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-10-08 10:15:36 +0100

    tidy up, all now working

--- service-front/module/Application/src/Model/Service/Guidance/Guidance.php
+++ service-front/module/Application/src/Model/Service/Guidance/Guidance.php
@@ -20,10 +20,8 @@ class Guidance extends AbstractService
     {
         $sectionArray = [];
         $feeEffectiveDate = new DateTimeImmutable(getenv('LPA_FEE_EFFECTIVE_DATE') ?: '2025-11-17T00:00:00');
-        //$feeEffectiveDate = new DateTimeImmutable('2024-11-17T00:00:00');
         $timeNow = new DateTimeImmutable('now');
         $orderFileName = ($timeNow >= $feeEffectiveDate) ? '/order.md' : '/orderPreNov25fee.md';
-        $guidancedir = scandir(self::GUIDANCE_MARKDOWN_FOLDER );
         $lines = file(self::GUIDANCE_MARKDOWN_FOLDER . $orderFileName);
 
         foreach ($lines as $line) {
