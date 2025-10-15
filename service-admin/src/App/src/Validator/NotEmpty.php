071945f9628079e017a745ea6da21a1b7a855cce
Allen Annom <allen.annom@publicguardian.gov.uk>
2025-10-15 15:02:04 +0100

    removed NotEmpty Inheritance

--- service-admin/src/App/src/Validator/NotEmpty.php
+++ service-admin/src/App/src/Validator/NotEmpty.php
@@ -1,22 +1,77 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Validator;
 
-use Laminas\Validator\NotEmpty as ZendNotEmpty;
+use Laminas\Validator\AbstractValidator;
+use Laminas\Validator\NotEmpty as LaminasNotEmpty;
 
-/**
- * Class NotEmpty
- * @package App\Validator
- *
- * @psalm-suppress InvalidExtendClass Will be resolved in LPA-3820
- */
-class NotEmpty extends ZendNotEmpty
+final class NotEmpty extends AbstractValidator
 {
-    /**
-     * @var array<string, string>
-     */
+    public const BOOLEAN       = LaminasNotEmpty::BOOLEAN;
+    public const INTEGER       = LaminasNotEmpty::INTEGER;
+    public const FLOAT         = LaminasNotEmpty::FLOAT;
+    public const STRING        = LaminasNotEmpty::STRING;
+    public const ZERO          = LaminasNotEmpty::ZERO;
+    public const EMPTY_ARRAY   = LaminasNotEmpty::EMPTY_ARRAY;
+    public const NULL          = LaminasNotEmpty::NULL;
+    public const PHP           = LaminasNotEmpty::PHP;
+    public const SPACE         = LaminasNotEmpty::SPACE;
+    public const OBJECT        = LaminasNotEmpty::OBJECT;
+    public const OBJECT_STRING = LaminasNotEmpty::OBJECT_STRING;
+    public const OBJECT_COUNT  = LaminasNotEmpty::OBJECT_COUNT;
+    public const ALL           = LaminasNotEmpty::ALL;
+
+    public const INVALID = LaminasNotEmpty::INVALID;
+    public const IS_EMPTY = LaminasNotEmpty::IS_EMPTY;
+
+    /** @var array<string,string> */
     protected $messageTemplates = [
         self::IS_EMPTY => 'required',
         self::INVALID  => 'invalid-type',
     ];
+
+    private LaminasNotEmpty $inner;
+
+    /**
+     * @param array{type?: int} $options
+     */
+    public function __construct(array $options = [])
+    {
+        $defaultType =
+            self::BOOLEAN |
+            self::STRING |
+            self::EMPTY_ARRAY |
+            self::NULL |
+            self::SPACE |
+            self::OBJECT |
+            self::OBJECT_STRING |
+            self::OBJECT_COUNT;
+
+        $type = $options['type'] ?? $defaultType;
+
+        $this->inner = new LaminasNotEmpty(['type' => $type]);
+
+        parent::__construct($options);
+    }
+
+    public function isValid($value): bool
+    {
+        $this->setValue($value);
+
+        if (!$this->inner->isValid($value)) {
+            $messages = $this->inner->getMessages();
+
+            if (array_key_exists(self::INVALID, $messages)) {
+                $this->error(self::INVALID);
+            } else {
+                $this->error(self::IS_EMPTY);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
 }
---
+++ service-admin/test/AppTest/Validator/NotEmptyTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Validator;
+
+use App\Validator\NotEmpty;
+use PHPUnit\Framework\TestCase;
+
+final class NotEmptyTest extends TestCase
+{
+    public function validProvider(): array
+    {
+        return [
+            'simple string'          => ['foo'],
+            'numeric string'         => ['123'],
+            'zero integer allowed'   => [0],
+            'zero string allowed'    => ['0'],
+            'zero float allowed'     => [0.0],
+            'array with something'   => [[1]],
+        ];
+    }
+    public function invalidProvider(): array
+    {
+        return [
+            'empty string' => [''],
+            'spaces only'  => ['   '],
+            'null'         => [null],
+            'empty array'  => [[]],
+        ];
+    }
+
+    /** @dataProvider validProvider */
+    public function test_it_accepts_expected_values(mixed $value): void
+    {
+        $validator = new NotEmpty();
+        self::assertTrue($validator->isValid($value), 'Expected value to be considered NOT empty');
+    }
+
+    /** @dataProvider invalidProvider */
+    public function test_it_rejects_expected_values(mixed $value): void
+    {
+        $validator = new NotEmpty();
+        self::assertFalse($validator->isValid($value), 'Expected value to be considered empty');
+        self::assertNotEmpty($validator->getMessages());
+    }
+}
