a5b0aaaedbd0ef8c4810f16542f31b9f0e8a8c92
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-10-21 19:31:03 +0100

    refactor parameter groups to select the correctone from tfvars

--- terraform/environment/modules/environment/rds_parameter_groups.tf
+++ terraform/environment/modules/environment/rds_parameter_groups.tf
@@ -1,4 +1,76 @@
 # DB Parameter Groups
+
+locals {
+  psql_parameter_group_family_list = [
+    "postgres13",
+    "postgres14",
+    "postgres15",
+    "postgres16",
+    "postgres17",
+  ]
+}
+
+resource "aws_db_parameter_group" "postgres-db-params" {
+  for_each    = toset(local.psql_parameter_group_family_list)
+  name        = lower("${each.value}-db-params-${var.environment_name}")
+  description = "default ${each.value} rds parameter group"
+  family      = each.value
+  parameter {
+    name         = "log_min_duration_statement"
+    value        = "500"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "log_statement"
+    value        = "none"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "rds.log_retention_period"
+    value        = "1440"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "log_duration"
+    value        = "1"
+    apply_method = "immediate"
+  }
+}
+
+resource "aws_rds_cluster_parameter_group" "postgresql-aurora-params" {
+  for_each    = toset(local.psql_parameter_group_family_list)
+  name        = lower("${each.value}-cluster-params-${var.environment_name}")
+  description = "default ${each.value} aurora parameter group"
+  family      = "aurora-postgresql${substr(each.value, -2, 2)}"
+  parameter {
+    name         = "log_min_duration_statement"
+    value        = "500"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "log_statement"
+    value        = "none"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "log_duration"
+    value        = "1"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "rds.log_retention_period"
+    value        = "1440"
+    apply_method = "immediate"
+  }
+}
+
+##############
 resource "aws_db_parameter_group" "postgres13-db-params" {
   name        = lower("postgres13-db-params-${var.environment_name}")
   description = "default postgres13 rds parameter group"
