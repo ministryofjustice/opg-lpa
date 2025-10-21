59c2072c23b52c4da12fbab170c394fdb90df9dd
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-10-21 20:10:15 +0100

    refactor to use for_each and psql_parameter_group_family value

--- terraform/environment/refactor.tf
+++ terraform/environment/refactor.tf
@@ -2,13 +2,3 @@ moved {
   from = module.eu-west-1.module.api_aurora[0].aws_rds_cluster_parameter_group.postgres-aurora-params
   to   = module.eu-west-1.module.api_aurora[0].aws_rds_cluster_parameter_group.postgres13-aurora-params
 }
-
-moved {
-  from = module.eu-west-1.module.api_aurora[0].aws_rds_cluster_parameter_group.postgres13-aurora-params
-  to   = module.eu-west-1.aws_rds_cluster_parameter_group.postgresql13-aurora-params
-}
-
-moved {
-  from = module.eu-west-1.module.api_aurora[0].aws_rds_cluster_parameter_group.postgres14-aurora-params
-  to   = module.eu-west-1.aws_rds_cluster_parameter_group.postgresql14-aurora-params
-}
