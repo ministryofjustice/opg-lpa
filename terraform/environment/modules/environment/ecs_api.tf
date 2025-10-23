4c805ef329566fac70213cd31dd13820f628660c
jay-whitwell <jay.whitwell@publicguardian.gov.uk>
2025-10-23 11:44:48 +0100

    add missing security group

--- terraform/environment/modules/environment/ecs_api.tf
+++ terraform/environment/modules/environment/ecs_api.tf
@@ -16,6 +16,7 @@ resource "aws_ecs_service" "api" {
     security_groups = [
       aws_security_group.api_ecs_service.id,
       aws_security_group.rds-client.id,
+      aws_security_group.rds_proxy_ingress.id
     ]
     subnets          = data.aws_subnets.private.ids
     assign_public_ip = false
