8161a3af35a9be39917120cfc431523acf075552
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-10-23 16:32:51 +0100

    use correct provider for images

--- terraform/environment/modules/environment/ecs_pdf.tf
+++ terraform/environment/modules/environment/ecs_pdf.tf
@@ -83,6 +83,7 @@ data "aws_ecr_repository" "lpa_pdf_app" {
 data "aws_ecr_image" "lpa_pdf_app" {
   repository_name = data.aws_ecr_repository.lpa_pdf_app.name
   image_tag       = var.container_version
+  provider        = aws.management
 }
 
 //-----------------------------------------------
