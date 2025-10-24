9d2f320a2c49ba8e9ab1789522bb63d0cb8516ca
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2025-10-24 13:53:18 +0100

    Stop including response bodies in error messages
    We don't want to arbitraily log untrusted and potentially PII-including responses from remote servers. Instead, just include the status code and a generic error message.
    
    #patch

--- service-front/module/Application/src/Model/Service/ApiClient/Exception/ApiException.php
+++ service-front/module/Application/src/Model/Service/ApiClient/Exception/ApiException.php
@@ -35,8 +35,7 @@ class ApiException extends RuntimeException
 
             //  If there is still no message then compose a standard message
             if (is_null($message)) {
-                $message = 'HTTP:' . $this->statusCode . ' - ' .
-                (is_array($this->body) ? print_r($this->body, true) : 'Unexpected API response');
+                $message = 'HTTP:' . $this->statusCode . ' - Unexpected API response';
             }
         }
 
