761f19c35f62545eab7b347d953227c1b3e030ba
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-10-28 22:04:22 +0000

    refactor tfvars to group database attributes use rds_proxy_enabled to choose database endpoint and container definitions use operator role for local terraform development move rds proxy into a module to make it easier to enable or disable reduce number of security groups required for rds proxy use a secret in the format of an rds credential for rds proxy allow open egress for proxy to access secrets manager use aws_db_proxy_default_target_group to manage the default target group

---
+++ terraform/environment/modules/environment/modules/rds_proxy/main.tf
@@ -0,0 +1,126 @@
+data "aws_secretsmanager_secret" "api_rds_credentials" {
+  name = var.api_rds_credentials_secret_name
+}
+
+resource "aws_db_proxy" "rds_proxy" {
+  name                = lower("proxy-${var.environment_name}")
+  debug_logging       = true # this may uncover sensitive information in the logs - but it shouldn't
+  engine_family       = "POSTGRESQL"
+  idle_client_timeout = 1800
+  require_tls         = true
+  vpc_subnet_ids      = var.vpc_subnet_ids
+  vpc_security_group_ids = [
+    aws_security_group.rds_proxy.id,
+  ]
+  role_arn = aws_iam_role.rds_proxy_role.arn
+
+  auth {
+    auth_scheme               = "SECRETS"
+    description               = "Authentication for RDS Proxy"
+    iam_auth                  = "DISABLED"
+    client_password_auth_type = "POSTGRES_SCRAM_SHA_256" # pragma: allowlist secret
+    secret_arn                = data.aws_secretsmanager_secret.api_rds_credentials.arn
+  }
+}
+
+resource "aws_db_proxy_default_target_group" "rds" {
+  db_proxy_name = aws_db_proxy.rds_proxy.name
+
+  connection_pool_config {
+    connection_borrow_timeout    = 120
+    init_query                   = ""
+    max_connections_percent      = 100
+    max_idle_connections_percent = 50
+    session_pinning_filters      = []
+  }
+}
+
+resource "aws_db_proxy_target" "rds" {
+  db_proxy_name         = aws_db_proxy.rds_proxy.name
+  target_group_name     = aws_db_proxy_default_target_group.rds.name
+  db_cluster_identifier = var.db_cluster_identifier
+}
+
+resource "aws_iam_role" "rds_proxy_role" {
+  name               = lower("proxy-assume-role-${var.environment_name}")
+  assume_role_policy = data.aws_iam_policy_document.rds_proxy_assume.json
+}
+
+data "aws_iam_policy_document" "rds_proxy_assume" {
+  statement {
+    sid     = "AllowRDSServiceAssumeRole"
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["rds.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "rds_proxy_role" {
+  statement {
+    sid    = "RDSSecretsManagerAccess"
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+
+    resources = [
+      data.aws_secretsmanager_secret.api_rds_credentials.arn,
+    ]
+  }
+
+  statement {
+    sid    = "AllowRDSKMS"
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt"
+    ]
+    resources = [var.secretsmanager_encryption_key_arn]
+  }
+}
+
+resource "aws_iam_role_policy" "rds_proxy" {
+  name   = lower("rds-proxy-role-policy-${var.environment_name}")
+  role   = aws_iam_role.rds_proxy_role.id
+  policy = data.aws_iam_policy_document.rds_proxy_role.json
+}
+
+resource "aws_security_group" "rds_proxy" {
+  name                   = "rds-proxy-${var.environment_name}"
+  description            = "RDS access from RDS Proxy"
+  vpc_id                 = var.vpc_id
+  revoke_rules_on_delete = true
+}
+
+resource "aws_security_group_rule" "rds_proxy_ingress" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = var.rds_client_security_group_id
+  security_group_id        = aws_security_group.rds_proxy.id
+  description              = "Ingress from rds client"
+}
+
+resource "aws_security_group_rule" "rds_proxy_egress" {
+  type                     = "egress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = var.rds_api_security_group_id
+  security_group_id        = aws_security_group.rds_proxy.id
+  description              = "Egress To RDS"
+}
+
+resource "aws_security_group_rule" "rds_proxy_secrets_manager" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds_proxy.id
+  description       = "Egress For Secrets Manager"
+}
