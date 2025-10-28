cbcfed19bb33eba3ab5b8dc8a5fa2ee9344c20fa
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-10-28 22:26:31 +0000

    remove dif

--- terraform/environment/modules/environment/ecs_api.tf
+++ terraform/environment/modules/environment/ecs_api.tf
@@ -15,7 +15,7 @@ resource "aws_ecs_service" "api" {
   network_configuration {
     security_groups = [
       aws_security_group.api_ecs_service.id,
-      aws_security_group.rds-client.id
+      aws_security_group.rds-client.id,
     ]
     subnets          = data.aws_subnets.private.ids
     assign_public_ip = false
