6666d8eabb75a3f1de4ed1f104f1b764c09e7077
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2025-11-13 12:13:58 +0000

    LPAL-1596: Use fixed activation token in tests
    When running Cypress tests locally, base the activation/reset token on the hash of the email address rather than generating a random one. This allows the Cypress tests to navigate to the activation/reset page directly rather than having to receive a real email.
    
    This is built into the service, using a new trait that controls which generation method to use. It can only be switched on by an environment variable and, for added protection, only in dev environments.
    
    Fixes LPAL-1596 #minor

--- cypress/e2e/common/activation_email.js
+++ cypress/e2e/common/activation_email.js
@@ -1,45 +1,32 @@
+import { createHash } from 'crypto';
 import { Then } from '@badeball/cypress-cucumber-preprocessor';
 
-var link = null;
-var activation_email_path = 'cypress/activation_emails/';
-
 Then(`I use activation email to visit the link`, () => {
-  openEmailAndVisitLink('activation');
+  openEmailAndVisitLink('activation', Cypress.env('email'));
 });
 
 Then(`I use password reset email to visit the link`, () => {
-  openEmailAndVisitLink('passwordreset');
+  openEmailAndVisitLink('passwordreset', Cypress.env('email'));
 });
 
 Then(`I use activation email for {string} to visit the link`, (name) => {
-  openEmailAndVisitLink('activation', Cypress.env(name + '-identifier'));
+  openEmailAndVisitLink('activation', Cypress.env(name + '-user'));
 });
 
 Then(`I use password reset email for {string} to visit the link`, (name) => {
-  openEmailAndVisitLink('passwordreset', Cypress.env(name + '-identifier'));
+  openEmailAndVisitLink('passwordreset', Cypress.env(name + '-user'));
 });
 
 function openEmailAndVisitLink(type, identifier) {
-  if (!identifier) {
-    identifier = Cypress.env('userNumber');
+  const hash = createHash('sha1');
+  hash.update(identifier);
+  const activationToken = hash.digest('hex');
+
+  if (type === 'passwordreset') {
+    cy.visit(`/forgot-password/reset/${activationToken}`);
   }
 
-  var filename = activation_email_path + identifier + '.' + type;
-  cy.log('Trying to open: ' + filename);
-
-  cy.readFile(filename, { timeout: 100000 }).then((text) => {
-    var content = text;
-    cy.log(text);
-    cy.log('Orig Content: ' + content);
-    var contentStr = content;
-
-    cy.log(type + ' email has arrived!');
-
-    cy.log('Content: ' + contentStr);
-    console.log('Content: ');
-    console.log(contentStr);
-    link = contentStr.substring(contentStr.indexOf(',') + 1);
-    cy.log('Opening ' + type + ' link: ' + link);
-    cy.visit(link);
-  });
+  if (type === 'activation') {
+    cy.visit(`/signup/confirm/${activationToken}`);
+  }
 }
