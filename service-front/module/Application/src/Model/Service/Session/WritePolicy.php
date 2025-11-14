4e4fdddb2193a820d7c2082794818a7e4b093b0c
Allen Annom <allen.annom@publicguardian.gov.uk>
2025-11-14 10:11:20 +0000

    LPAL-1390 mezzio session handling poc

---
+++ service-front/module/Application/src/Model/Service/Session/WritePolicy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Application\Model\Service\Session;
+
+use Laminas\Http\PhpEnvironment\Request;
+
+final class WritePolicy
+{
+    public function __construct(private readonly ?Request $request = null)
+    {
+    }
+
+    public function allowsWrite(): bool
+    {
+        /*
+         * If the Laminas X-SessionReadOnly header is present, do not allow session writes.
+         */
+        if ($this->request && $this->request->getHeaders()->has('X-SessionReadOnly')) {
+            return false;
+        }
+
+        /*
+         * Mezzio does not have Laminas\Http\PhpEnvironment\Request, so also check the global $_SERVER array
+         * for the presence of the HTTP_X_SESSIONREADONLY key.
+         */
+        if (!empty($_SERVER['HTTP_X_SESSIONREADONLY'])) {
+            return false;
+        }
+
+        return true;
+    }
+}
