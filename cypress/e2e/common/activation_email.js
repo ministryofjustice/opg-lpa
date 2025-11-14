139988bab8888d4fd1df14517bb2c933945d5fc8
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2025-11-14 09:32:12 +0000

    Replace crypto with jssha
    I was using the crypto node package, but since changing our preprocessor it's no longer available. Instead, install an npm package that computes SHA hashes and use that instead.
    
    #patch

--- cypress/e2e/common/activation_email.js
+++ cypress/e2e/common/activation_email.js
@@ -1,5 +1,5 @@
-import { createHash } from 'crypto';
 import { Then } from '@badeball/cypress-cucumber-preprocessor';
+import jsSHA from 'jssha';
 
 Then(`I use activation email to visit the link`, () => {
   openEmailAndVisitLink('activation', Cypress.env('email'));
@@ -17,10 +17,11 @@ Then(`I use password reset email for {string} to visit the link`, (name) => {
   openEmailAndVisitLink('passwordreset', Cypress.env(name + '-user'));
 });
 
-function openEmailAndVisitLink(type, identifier) {
-  const hash = createHash('sha1');
-  hash.update(identifier);
-  const activationToken = hash.digest('hex');
+async function openEmailAndVisitLink(type, identifier) {
+  const sha1Obj = new jsSHA('SHA-1', 'TEXT', { encoding: 'UTF8' });
+  sha1Obj.update(identifier);
+
+  const activationToken = sha1Obj.getHash('HEX');
 
   if (type === 'passwordreset') {
     cy.visit(`/forgot-password/reset/${activationToken}`);
