27c4749d6f1e325342ef0563d7ad0dc0a991c3bf
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-11-17 16:33:18 +0000

    fixes/removals

--- terraform/environment/modules/environment/ecs_api.tf
+++ terraform/environment/modules/environment/ecs_api.tf
@@ -341,7 +341,6 @@ locals {
         { name = "OPG_LPA_COMMON_PDF_QUEUE_URL", value = "https://sqs.${var.region_name}.amazonaws.com/${var.account.account_id}/lpa-pdf-queue-${var.environment_name}.fifo" },
         { name = "OPG_LPA_TELEMETRY_HOST", value = "127.0.0.1" },
         { name = "OPG_LPA_TELEMETRY_PORT", value = "2000" },
-        { name = "LPA_FEE_EFFECTIVE_DATE", value = "2025-11-17T00:00:00" }
       ]
     }
   )
