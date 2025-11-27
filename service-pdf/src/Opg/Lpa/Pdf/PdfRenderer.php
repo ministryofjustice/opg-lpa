fcf2d2cf880c96ec3df71e956791f0ca2225b01f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-11-27 14:52:31 +0000

    reset mkdir

--- service-pdf/src/Opg/Lpa/Pdf/PdfRenderer.php
+++ service-pdf/src/Opg/Lpa/Pdf/PdfRenderer.php
@@ -85,7 +85,7 @@ class PdfRenderer implements LoggerAwareInterface
                 'path' => $templatePathOnDisk,
             ]);
 
-            // mkdir($templatePathOnDisk, 0777, true);
+            mkdir($templatePathOnDisk, 0777, true);
         }
 
         foreach (glob($assetsConfig['source_template_path'] . '/*.pdf') as $pdfSource) {
