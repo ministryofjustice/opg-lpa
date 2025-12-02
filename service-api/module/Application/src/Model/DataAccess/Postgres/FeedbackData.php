7c25367a7abfa80d359f7e376811a9ef90599b84
Alex Saunders <acsauk@gmail.com>
2025-12-02 17:23:37 +0000

    LPAL-1632 share logging setup between services

--- service-api/module/Application/src/Model/DataAccess/Postgres/FeedbackData.php
+++ service-api/module/Application/src/Model/DataAccess/Postgres/FeedbackData.php
@@ -6,7 +6,6 @@ use DateTime;
 use MakeShared\Logging\LoggerTrait;
 use Traversable;
 use Laminas\Db\Sql\Predicate\Operator;
-use Application\Model\DataAccess\Postgres\AbstractBase;
 use Application\Model\DataAccess\Repository\Feedback as FeedbackRepository;
 
 class FeedbackData extends AbstractBase implements FeedbackRepository\FeedbackRepositoryInterface
