aacd35efcb98dc9bc76f5ed1d40b7551067157f6
Allen Annom <allen.annom@publicguardian.gov.uk>
2025-12-09 11:19:48 +0000

    LPAL-1643 using TemplateRendererInterface

--- service-front/module/Application/src/Handler/CookiesHandlerFactory.php
+++ service-front/module/Application/src/Handler/CookiesHandlerFactory.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace Application\Handler;
 
 use Laminas\Http\Request as HttpRequest;
+use Mezzio\Template\TemplateRendererInterface;
 use Psr\Container\ContainerInterface;
-use Twig\Environment as TwigEnvironment;
 
 class CookiesHandlerFactory
 {
     public function __invoke(ContainerInterface $container): CookiesHandler
     {
         return new CookiesHandler(
-            $container->get(TwigEnvironment::class),
+            $container->get(TemplateRendererInterface::class),
             $container->get('FormElementManager'),
             $container->get(HttpRequest::class),
         );
