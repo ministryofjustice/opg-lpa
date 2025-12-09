97ea46dafc8dbf8c0d111a0f2427e313a67a9609
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-12-09 11:42:48 +0000

    add migration container definition with overrides

--- terraform/environment/modules/environment/ecs_api.tf
+++ terraform/environment/modules/environment/ecs_api.tf
@@ -131,7 +131,7 @@ resource "aws_ecs_task_definition" "api" {
   network_mode             = "awsvpc"
   cpu                      = 512
   memory                   = 1024
-  container_definitions    = "[${local.api_web}, ${local.api_app}, ${local.aws_otel_collector}, ${local.pgbouncer}]"
+  container_definitions    = "[${local.api_web}, ${local.api_app}, ${local.aws_otel_collector}, ${local.api_migrations}, ${local.pgbouncer}]"
   task_role_arn            = var.ecs_iam_task_roles.api.arn
   execution_role_arn       = var.ecs_execution_role.arn
   tags                     = local.api_component_tag
@@ -266,6 +266,90 @@ locals {
     }
   )
 
+  api_migrations = jsonencode(
+    {
+      cpu                    = 1,
+      essential              = true,
+      readonlyRootFilesystem = true,
+      image                  = "${data.aws_ecr_repository.lpa_api_app.repository_url}@${data.aws_ecr_image.lpa_api_app.image_digest}",
+      name                   = "app",
+      mountPoints = [
+        {
+          containerPath = "/tmp",
+          sourceVolume  = "app_tmp"
+          readOnly      = false
+        }
+      ],
+      portMappings = [
+        {
+          containerPort = 9000,
+          hostPort      = 9000,
+          protocol      = "tcp"
+        }
+      ],
+      healthCheck = {
+        command     = ["CMD", "/usr/local/bin/health-check.sh"],
+        startPeriod = 90,
+        interval    = 10,
+        timeout     = 15,
+        retries     = 3
+      },
+      containerOverrides = [
+        {
+          name    = "migrations",
+          command = ["/bin/sh", "-c", "/usr/local/bin/db-migrations.sh"]
+        }
+      ],
+      volumesFrom = [],
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.application_logs.name,
+          awslogs-region        = var.region_name,
+          awslogs-stream-prefix = "${var.environment_name}.api-app.online-lpa"
+        }
+      },
+      dependsOn = [
+        {
+          containerName = "pgbouncer",
+          condition     = "HEALTHY"
+        }
+      ],
+      secrets = [
+        { name = "OPG_LPA_API_NOTIFY_API_KEY", valueFrom = "/aws/reference/secretsmanager/${data.aws_secretsmanager_secret.opg_lpa_api_notify_api_key.name}" },
+        { name = "OPG_LPA_POSTGRES_USERNAME", valueFrom = "/aws/reference/secretsmanager/${data.aws_secretsmanager_secret.api_rds_username.name}" },
+        { name = "OPG_LPA_POSTGRES_PASSWORD", valueFrom = "/aws/reference/secretsmanager/${data.aws_secretsmanager_secret.api_rds_password.name}" },
+        { name = "OPG_LPA_COMMON_ACCOUNT_CLEANUP_NOTIFICATION_RECIPIENTS", valueFrom = "/aws/reference/secretsmanager/${data.aws_secretsmanager_secret.opg_lpa_common_account_cleanup_notification_recipients.name}" },
+        { name = "OPG_LPA_COMMON_ADMIN_ACCOUNTS", valueFrom = "/aws/reference/secretsmanager/${data.aws_secretsmanager_secret.opg_lpa_common_admin_accounts.name}" }
+      ],
+      environment = [
+        { name = "OPG_NGINX_SERVER_NAMES", value = "api api-${var.environment_name}.${var.account_name} localhost 127.0.0.1" },
+        { name = "OPG_LPA_POSTGRES_HOSTNAME", value = "127.0.0.1" },
+        { name = "OPG_LPA_POSTGRES_PORT", value = "6432" },
+        { name = "OPG_LPA_POSTGRES_NAME", value = module.api_aurora[0].name },
+        { name = "OPG_LPA_PROCESSING_STATUS_ENDPOINT", value = var.account.sirius_api_gateway_endpoint },
+        { name = "OPG_LPA_API_TRACK_FROM_DATE", value = local.track_from_date },
+        { name = "OPG_LPA_STACK_NAME", value = var.environment_name },
+        { name = "OPG_DOCKER_TAG", value = var.container_version },
+        { name = "OPG_LPA_STACK_ENVIRONMENT", value = var.account_name },
+        { name = "OPG_LPA_COMMON_APPLICATION_LOG_PATH", value = "/var/log/app/application.log" },
+        { name = "OPG_LPA_AUTH_TOKEN_GENERATION", value = var.account_name == "production" ? "random" : "hash" },
+        { name = "OPG_LPA_AUTH_TOKEN_TTL", value = tostring(var.account.auth_token_ttl_secs) },
+        { name = "OPG_LPA_COMMON_DYNAMODB_ENDPOINT", value = "" },
+        { name = "OPG_LPA_COMMON_CRONLOCK_DYNAMODB_TABLE", value = aws_dynamodb_table.lpa-locks.name },
+        { name = "OPG_LPA_COMMON_SESSION_DYNAMODB_TABLE", value = aws_dynamodb_table.lpa-sessions.name },
+        { name = "OPG_LPA_COMMON_API_DYNAMODB_TABLE", value = aws_dynamodb_table.lpa-properties.name },
+        { name = "OPG_PHP_POOL_CHILDREN_MAX", value = "25" },
+        { name = "OPG_NGINX_SSL_HSTS_AGE", value = "31536000" },
+        { name = "OPG_NGINX_SSL_FORCE_REDIRECT", value = "TRUE" },
+        { name = "OPG_LPA_COMMON_RESQUE_REDIS_HOST", value = "redisback" },
+        { name = "OPG_LPA_COMMON_PDF_CACHE_S3_BUCKET", value = data.aws_s3_bucket.lpa_pdf_cache.bucket },
+        { name = "OPG_LPA_COMMON_PDF_QUEUE_URL", value = "https://sqs.${var.region_name}.amazonaws.com/${var.account.account_id}/lpa-pdf-queue-${var.environment_name}.fifo" },
+        { name = "OPG_LPA_TELEMETRY_HOST", value = "127.0.0.1" },
+        { name = "OPG_LPA_TELEMETRY_PORT", value = "2000" }
+      ]
+    }
+  )
   api_app = jsonencode(
     {
       cpu                    = 1,
@@ -294,6 +378,12 @@ locals {
         timeout     = 15,
         retries     = 3
       },
+      containerOverrides = [
+        {
+          name    = "migrations",
+          command = ["php-fpm"]
+        }
+      ],
       volumesFrom = [],
       logConfiguration = {
         logDriver = "awslogs",
@@ -307,6 +397,10 @@ locals {
         {
           containerName = "pgbouncer",
           condition     = "HEALTHY"
+        },
+        {
+          containerName = "app_migrations",
+          condition     = "STOPPED"
         }
       ],
       secrets = [
