0952fbb53589e32473e1ab44f47975cc2d091298
Allen Annom <allen.annom@publicguardian.gov.uk>
2025-12-10 14:17:49 +0000

    LPAL-1642 wip

--- service-front/module/Application/src/Handler/FeedbackThanksHandler.php
+++ service-front/module/Application/src/Handler/FeedbackThanksHandler.php
@@ -5,18 +5,17 @@ declare(strict_types=1);
 namespace Application\Handler;
 
 use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Twig\Environment as TwigEnvironment;
 
 class FeedbackThanksHandler implements RequestHandlerInterface
 {
-    private TwigEnvironment $twig;
-
-    public function __construct(TwigEnvironment $twig)
+    private TemplateRendererInterface $renderer;
+    public function __construct(TemplateRendererInterface $renderer)
     {
-        $this->twig = $twig;
+        $this->renderer = $renderer;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -24,16 +23,13 @@ class FeedbackThanksHandler implements RequestHandlerInterface
         $queryParams   = $request->getQueryParams();
         $returnTarget  = $queryParams['returnTarget'] ?? '';
 
-        // Matches: $returnTarget = urldecode($this->params()->fromQuery('returnTarget'));
         $returnTarget = urldecode((string) $returnTarget);
 
-        // Matches: if (empty($returnTarget)) { $returnTarget = $this->url()->fromRoute('home'); }
         if (empty($returnTarget)) {
-            // No UrlHelper yet – just send them "home" at '/'
             $returnTarget = '/';
         }
 
-        $html = $this->twig->render(
+        $html = $this->renderer->render(
             'application/general/feedback/thanks.twig',
             [
                 'returnTarget' => $returnTarget,
