29951d2b63a25ef0f8e4490c8c2a35e751fe4ebe
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-12-10 16:46:53 +0000

    still need this in test

--- service-front/module/Application/tests/View/StatusViewModelHelperTest.php
+++ service-front/module/Application/tests/View/StatusViewModelHelperTest.php
@@ -50,6 +50,7 @@ final class StatusViewModelHelperTest extends MockeryTestCase
         ]);
 
         $this->renderer = new ViewModelRenderer();
+        $this->renderer->addFunction('formatLpaId');
         $this->renderer->loadTemplate('application/authenticated/lpa/status/index.twig');
     }
 
