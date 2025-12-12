c1e441111ba6ea64b872a2d2a2f37d96ffde67ee
Allen Annom <allen.annom@publicguardian.gov.uk>
2025-12-12 10:13:40 +0000

    LPAL-1687 refactored FormLinkedErrorListV2.php into twig func and removed html logic from helper

--- service-front/module/Application/src/View/Twig/AppFunctionsExtension.php
+++ service-front/module/Application/src/View/Twig/AppFunctionsExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Application\View\Twig;
 
 use Application\Model\FormFlowChecker;
+use Laminas\Form\Form;
 use MakeShared\DataModel\Lpa\Lpa;
 use Application\View\Helper\Traits\ConcatNamesTrait;
 use Twig\Extension\AbstractExtension;
@@ -24,6 +25,7 @@ class AppFunctionsExtension extends AbstractExtension
         return [
             new TwigFunction('applicant_names', [$this, 'applicantNames']),
             new TwigFunction('final_check_accessible', [$this, 'finalCheckAccessible']),
+            new TwigFunction('form_linked_errors', [$this, 'formLinkedErrors']),
         ];
     }
 
@@ -55,5 +57,33 @@ class AppFunctionsExtension extends AbstractExtension
     public function finalCheckAccessible(Lpa $lpa): bool
     {
         return FormFlowChecker::isFinalCheckAccessible($lpa);
+    }
+
+    public function formLinkedErrors(Form $form): array
+    {
+        $errors = [];
+
+        foreach ($form->getMessages() as $field => $messages) {
+            $this->walkMessages($errors, (string) $field, $messages);
+        }
+
+        return $errors;
+    }
+
+    private function walkMessages(array &$errors, string $field, mixed $messages): void
+    {
+        if (is_array($messages)) {
+            foreach ($messages as $message) {
+                $this->walkMessages($errors, $field, $message);
+            }
+            return;
+        }
+
+        if (is_string($messages) && $messages !== '') {
+            $errors[] = [
+                'field'   => $field,
+                'message' => $messages,
+            ];
+        }
     }
 }
