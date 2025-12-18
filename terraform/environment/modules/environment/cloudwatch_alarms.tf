ee5d9383039aba3fbc7cd9994ad6183a4d743947
radhia shah <radhia.shah@justice.gov.uk>
2025-12-18 16:05:52 +0000

    Adding 5xx metric anomaly alarm- updating return data rules

--- terraform/environment/modules/environment/cloudwatch_alarms.tf
+++ terraform/environment/modules/environment/cloudwatch_alarms.tf
@@ -28,7 +28,7 @@ resource "aws_cloudwatch_metric_alarm" "front_5xx_anomaly" {
   comparison_operator       = "GreaterThanUpperThreshold"
   evaluation_periods        = 2
   threshold_metric_id       = "ad1"
-  alarm_description         = "Anomaly detection in 5xx Errors for ${var.environment_name}"
+  alarm_description         = "Anomaly detection in 5xx Errors returned to front users for ${var.environment_name}"
   datapoints_to_alarm       = 2
   insufficient_data_actions = []
   treat_missing_data        = "notBreaching"
@@ -42,12 +42,13 @@ resource "aws_cloudwatch_metric_alarm" "front_5xx_anomaly" {
     return_data = true
   }
   metric_query {
-    id = "m1"
+    id          = "m1"
+    return_data = false
     metric {
       metric_name = "HTTPCode_Target_5XX_Count"
       namespace   = "AWS/ApplicationELB"
       period      = 60
-      stat        = "Average"
+      stat        = "Sum"
       dimensions = {
         LoadBalancer = trimprefix(split(":", aws_lb.front.arn)[5], "loadbalancer/")
       }
