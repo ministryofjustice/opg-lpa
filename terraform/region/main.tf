3d7e431d8c1039b7778e8c88fb3fe6def4ba35e4
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-12-30 12:40:20 +0000

    use non-overlapping CIDR ranges for new network

--- terraform/region/main.tf
+++ terraform/region/main.tf
@@ -1,9 +1,10 @@
 #manual only right now - take this out later.
 module "eu-west-1" {
-  source             = "./modules/region"
-  account            = local.account
-  account_name       = local.account_name
-  network_cidr_block = "10.162.0.0/16"
+  source                    = "./modules/region"
+  account                   = local.account
+  account_name              = local.account_name
+  firewalled_vpc_cidr_range = local.account.firewalled_vpc_cidr_ranges.eu_west_1
+  network_cidr_block        = "10.162.0.0/16"
   providers = {
     aws            = aws
     aws.management = aws.management
@@ -11,11 +12,12 @@ module "eu-west-1" {
 }
 
 module "eu-west-2" {
-  source             = "./modules/region"
-  count              = local.account.dr_enabled && local.account_name == "development" ? 1 : 0
-  account            = local.account
-  account_name       = local.account_name
-  network_cidr_block = "10.162.0.0/16"
+  source                    = "./modules/region"
+  count                     = local.account.dr_enabled && local.account_name == "development" ? 1 : 0
+  account                   = local.account
+  account_name              = local.account_name
+  firewalled_vpc_cidr_range = local.account.firewalled_vpc_cidr_ranges.eu_west_2
+  network_cidr_block        = "10.162.0.0/16"
   providers = {
     aws            = aws.eu-west-2
     aws.management = aws.management_eu_west_2
