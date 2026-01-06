a132ab74373272631f791b724c8ab66485fc964d
radhia shah <radhia.shah@justice.gov.uk>
2026-01-06 17:41:17 +0000

    Cleaning up restore testing resources to save

--- terraform/environment/modules/rds_cross_region_backup/sns.tf
+++ terraform/environment/modules/rds_cross_region_backup/sns.tf
@@ -1,3 +1,4 @@
+# BACKUP FAILURE NOTIFICATIONS
 data "aws_sns_topic" "rds_events" {
   name = "${var.account_name}-${data.aws_region.current.region}-rds-events"
 }
@@ -33,18 +34,18 @@ resource "aws_sns_topic_policy" "aws_backup_failures" {
   policy = data.aws_iam_policy_document.aws_backup_sns.json
 }
 
-# # Restore testing failure notifications
-# resource "aws_sns_topic" "restore_testing_events" {
-#   name = "${var.account_name}-${data.aws_region.current.region}-restore-testing-events"
+# # RESTORE FAILURE NOTIFICATIONS
+# data "aws_sns_topic" "restore_failure_events" {
+#   name = "${var.account_name}-${data.aws_region.current.region}_restore_failure_events"
 # }
 
-# resource "aws_restore_testing_notifications" "restore_testing_failures" {
+# resource "aws_backup_vault_notifications" "restore_failures" {
 #   backup_vault_name   = aws_backup_vault.main.name
-#   sns_topic_arn       = data.aws_sns_topic.restore_testing_events.arn
-#   backup_vault_events = ["RESTORE_JOB_FAILED"]
+#   sns_topic_arn       = data.aws_sns_topic.restore_failure_events.arn
+#   backup_vault_events = ["RESTORE_JOB_FAILED", "RESTORE_JOB_COMPLETED"]
 # }
 
-# data "aws_iam_policy_document" "aws_backup_sns" {
+# data "aws_iam_policy_document" "aws_restore_failure_sns" {
 #   statement {
 #     actions = [
 #       "SNS:Publish",
@@ -58,13 +59,13 @@ resource "aws_sns_topic_policy" "aws_backup_failures" {
 #     }
 
 #     resources = [
-#       data.aws_sns_topic.restore_testing_events.arn
+#       data.aws_sns_topic.restore_failure_events.arn
 #     ]
 
 #   }
 # }
 
-# resource "aws_sns_topic_policy" "aws_restore_testing_failures" {
-#   arn    = data.aws_sns_topic.restore_testing_events.arn
-#   policy = data.aws_iam_policy_document.aws_backup_sns.json
+# resource "aws_sns_topic_policy" "aws_restore_failure_" {
+#   arn    = data.aws_sns_topic.restore_failure_events.arn
+#   policy = data.aws_iam_policy_document.aws_restore_failure_sns.json
 # }
