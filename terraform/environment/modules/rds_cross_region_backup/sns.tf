b144dd30c4c716109eb24b7e66794d863540cc5a
radhia shah <radhia.shah@justice.gov.uk>
2026-01-06 18:18:42 +0000

    SNS and eventbridge rule added for restore_job_failures

--- terraform/environment/modules/rds_cross_region_backup/sns.tf
+++ terraform/environment/modules/rds_cross_region_backup/sns.tf
@@ -9,63 +9,35 @@ resource "aws_backup_vault_notifications" "aws_backup_failures" {
   backup_vault_events = ["BACKUP_JOB_FAILED", "COPY_JOB_FAILED"]
 }
 
-data "aws_iam_policy_document" "aws_backup_sns" {
+resource "aws_backup_vault_notifications" "restore_job_failures" {
+  backup_vault_name   = aws_backup_vault.main.name
+  sns_topic_arn       = data.aws_sns_topic.rds_events.arn
+  backup_vault_events = ["RESTORE_JOB_FAILED", "RESTORE_JOB_COMPLETED"]
+}
+
+data "aws_iam_policy_document" "aws_rds_events_sns" {
   statement {
-    actions = [
-      "SNS:Publish",
-    ]
-
-    effect = "Allow"
-
+    actions = ["SNS:Publish"]
+    effect  = "Allow"
+    sid     = "AllowAwsBackupToPublish"
     principals {
       type        = "Service"
       identifiers = ["backup.amazonaws.com"]
     }
 
-    resources = [
-      data.aws_sns_topic.rds_events.arn,
-    ]
-
+    resources = [data.aws_sns_topic.rds_events.arn]
+  }
+  statement {
+    sid     = "AllowEventBridgeToPublish"
+    actions = ["SNS:Publish"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
   }
 }
-
-resource "aws_sns_topic_policy" "aws_backup_failures" {
+resource "aws_sns_topic_policy" "rds_events_policy" {
   arn    = data.aws_sns_topic.rds_events.arn
-  policy = data.aws_iam_policy_document.aws_backup_sns.json
+  policy = data.aws_iam_policy_document.aws_rds_events_sns.json
 }
-
-# # RESTORE FAILURE NOTIFICATIONS
-# data "aws_sns_topic" "restore_failure_events" {
-#   name = "${var.account_name}-${data.aws_region.current.region}_restore_failure_events"
-# }
-
-# resource "aws_backup_vault_notifications" "restore_failures" {
-#   backup_vault_name   = aws_backup_vault.main.name
-#   sns_topic_arn       = data.aws_sns_topic.restore_failure_events.arn
-#   backup_vault_events = ["RESTORE_JOB_FAILED", "RESTORE_JOB_COMPLETED"]
-# }
-
-# data "aws_iam_policy_document" "aws_restore_failure_sns" {
-#   statement {
-#     actions = [
-#       "SNS:Publish",
-#     ]
-
-#     effect = "Allow"
-
-#     principals {
-#       type        = "Service"
-#       identifiers = ["backup.amazonaws.com"]
-#     }
-
-#     resources = [
-#       data.aws_sns_topic.restore_failure_events.arn
-#     ]
-
-#   }
-# }
-
-# resource "aws_sns_topic_policy" "aws_restore_failure_" {
-#   arn    = data.aws_sns_topic.restore_failure_events.arn
-#   policy = data.aws_iam_policy_document.aws_restore_failure_sns.json
-# }
