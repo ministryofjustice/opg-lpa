2a1da6723645fcf4c250ad03307bad55ba84dc2d
radhia shah <radhia.shah@justice.gov.uk>
2026-01-06 17:41:17 +0000

    Cleaning up restore testing resources to save

--- terraform/environment/modules/rds_cross_region_backup/restore_testing.tf
+++ terraform/environment/modules/rds_cross_region_backup/restore_testing.tf
@@ -1,7 +1,7 @@
 
 # Iam role policy
-resource "aws_iam_role" "backup_restore_testing_role" {
-  name = "${var.environment_name}_backup_restore_testing_role"
+resource "aws_iam_role" "restore_testing_role" {
+  name = "${var.environment_name}_restore_testing_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -17,15 +17,15 @@ resource "aws_iam_role" "backup_restore_testing_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "backup_restore_testing_role" {
-  role       = aws_iam_role.backup_restore_testing_role.name
+resource "aws_iam_role_policy_attachment" "restore_testing_role" {
+  role       = aws_iam_role.restore_testing_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores"
 }
 
 
 # Restore testing plan
-resource "aws_backup_restore_testing_plan" "backup_restore_testing_plan" {
-  name = aws_backup_restore_testing.plan.backup_restore_testing_plan.name
+resource "aws_backup_restore_testing_plan" "restore_testing_plan" {
+  name = "${var.environment_name}_restore_testing_plan"
 
   schedule_expression          = "cron(0 12 ? * * *)" # Daily at 12:00
   schedule_expression_timezone = "UTC"
@@ -34,22 +34,23 @@ resource "aws_backup_restore_testing_plan" "backup_restore_testing_plan" {
     algorithm      = "LATEST_WITHIN_WINDOW"
     include_vaults = [aws_backup_vault.main.arn]
     # include_vaults = ["production_eu-west-1_aurora_backup_vault"]
-    recovery_point_types  = ["CONTINUOUS"]
+    recovery_point_types  = ["CONTINUOUS", "SNAPSHOT"]
     selection_window_days = 7
   }
 }
 
 # Restore testing selection
-resource "aws_backup_restore_testing_selection" "backup_restore_testing_selection" {
-  name                      = "${var.environment_name}_backup_restore_selection"
-  restore_testing_plan_name = "${var.environment_name}_backup_restore_testing_plan"
-  iam_role_arn              = aws_iam_role.backup_restore_testing_role.arn
-  restore_metadata_overrides = {
-    DBClusterIdentifier = "${var.environment_name}_restored_testing_cluster"
-    DBSubnetGroupName   = "${var.environment_name}_restored_testing_subnet_group"
-    VpcSecurityGroupIds = [aws_security_group.restored_testing_sg.id]
-    # TODO - NEEDS TO BE discussed- restored clusters cannot be created in the same VPC as the source cluster
-  }
+resource "aws_backup_restore_testing_selection" "restore_testing_selection" {
+  name                      = "${var.environment_name}_restore_testing_selection"
+  restore_testing_plan_name = aws_backup_restore_testing_plan.restore_testing_plan.name
+  iam_role_arn              = aws_iam_role.restore_testing_role.arn
+
+  # restore_metadata_overrides = {
+  #   DBClusterIdentifier = "${var.environment_name}_restored_testing_cluster"
+  #   DBSubnetGroupName   = "${var.environment_name}_restored_testing_subnet_group"
+  #   VpcSecurityGroupIds = [aws_security_group.restored_testing_sg.id]
+  #   # TODO - NEEDS TO BE discussed- restored clusters shouldnt be created in the same VPC as the source cluster (according to docs and best practices)
+  # }
 
   protected_resource_type = "Aurora"
   protected_resource_arns = [var.source_cluster_arn]
