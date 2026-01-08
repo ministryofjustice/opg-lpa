1bdd1305d14616e5ad0349963dc000ed70df2d94
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-01-08 13:28:20 +0000

    add db subnet group and use it

--- terraform/environment/modules/environment/vpc_data_sources.tf
+++ terraform/environment/modules/environment/vpc_data_sources.tf
@@ -1,8 +1,9 @@
 locals {
-  vpc_id          = var.account.firewalled_networks_enabled ? data.aws_vpc.main.id : data.aws_vpc.default.id                                         #tflint-ignore: terraform_unused_declarations
-  lb_subnet_ids   = var.account.firewalled_networks_enabled ? [for subnet in data.aws_subnet.lb : subnet.id] : data.aws_subnets.public.ids           #tflint-ignore: terraform_unused_declarations
-  app_subnet_ids  = var.account.firewalled_networks_enabled ? [for subnet in data.aws_subnet.application : subnet.id] : data.aws_subnets.private.ids #tflint-ignore: terraform_unused_declarations
-  data_subnet_ids = var.account.firewalled_networks_enabled ? [for subnet in data.aws_subnet.data : subnet.id] : data.aws_subnets.private.ids        #tflint-ignore: terraform_unused_declarations
+  vpc_id               = var.account.firewalled_networks_enabled ? data.aws_vpc.main.id : data.aws_vpc.default.id                                         #tflint-ignore: terraform_unused_declarations
+  lb_subnet_ids        = var.account.firewalled_networks_enabled ? [for subnet in data.aws_subnet.lb : subnet.id] : data.aws_subnets.public.ids           #tflint-ignore: terraform_unused_declarations
+  app_subnet_ids       = var.account.firewalled_networks_enabled ? [for subnet in data.aws_subnet.application : subnet.id] : data.aws_subnets.private.ids #tflint-ignore: terraform_unused_declarations
+  data_subnet_ids      = var.account.firewalled_networks_enabled ? [for subnet in data.aws_subnet.data : subnet.id] : data.aws_subnets.private.ids        #tflint-ignore: terraform_unused_declarations
+  db_subnet_group_name = var.account.firewalled_networks_enabled ? aws_db_subnet_group.main.name : "data-persistence-subnet-default"                      #tflint-ignore: terraform_unused_declarations
 }
 
 
