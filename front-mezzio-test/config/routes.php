efbf49100a94f332790f4e1e18d4a4c1a3c2417d
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-01-15 11:55:01 +0000

    initial mezzio skeleton

---
+++ front-mezzio-test/config/routes.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Handler\HomePageHandler;
+use App\Handler\PingHandler;
+use Mezzio\Application;
+use Mezzio\MiddlewareFactory;
+use Psr\Container\ContainerInterface;
+
+/**
+ * laminas-router route configuration
+ *
+ * @see https://docs.laminas.dev/laminas-router/
+ *
+ * Setup routes with a single request method:
+ *
+ * $app->get('/', App\Handler\HomePageHandler::class, 'home');
+ * $app->post('/album', App\Handler\AlbumCreateHandler::class, 'album.create');
+ * $app->put('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.put');
+ * $app->patch('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.patch');
+ * $app->delete('/album/:id', App\Handler\AlbumDeleteHandler::class, 'album.delete');
+ *
+ * Or with multiple request methods:
+ *
+ * $app->route('/contact', App\Handler\ContactHandler::class, ['GET', 'POST', ...], 'contact');
+ *
+ * Or handling all request methods:
+ *
+ * $app->route('/contact', App\Handler\ContactHandler::class)->setName('contact');
+ *
+ * or:
+ *
+ * $app->route(
+ *     '/contact',
+ *     App\Handler\ContactHandler::class,
+ *     Mezzio\Router\Route::HTTP_METHOD_ANY,
+ *     'contact'
+ * );
+ */
+
+return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
+    $app->get('/', HomePageHandler::class, 'home');
+    $app->get('/api/ping', PingHandler::class, 'api.ping');
+};
