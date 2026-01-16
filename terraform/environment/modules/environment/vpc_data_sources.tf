d7930fadafa2bd82ce56fd077c7f18f5c7778275
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-01-16 16:02:37 +0000

    db_subnet_group doesn't toggle, it's separate

--- terraform/environment/modules/environment/vpc_data_sources.tf
+++ terraform/environment/modules/environment/vpc_data_sources.tf
@@ -120,5 +120,5 @@ data "aws_nat_gateway" "main" {
 # TODO: this should in region and referenced by name or datasource
 resource "aws_db_subnet_group" "main" {
   name_prefix = lower("main-${var.environment_name}")
-  subnet_ids  = local.data_subnet_ids
+  subnet_ids  = data.aws_subnet.data
 }
