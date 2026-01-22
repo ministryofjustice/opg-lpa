2ff86234fb0677cce5498e62563bb4ff73f44c6b
radhia shah <radhia.shah@justice.gov.uk>
2026-01-22 17:28:39 +0000

    LPAL-1883 - adding backup_account vars - incomplete

--- terraform/environment/modules/rds_cross_region_backup/iam.tf
+++ terraform/environment/modules/rds_cross_region_backup/iam.tf
@@ -1,33 +1,4 @@
-data "aws_iam_policy_document" "aurora_cluster_backup_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["backup.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_kms_key" "destination_rds_snapshot_key" {
-  provider = aws.destination
-  key_id   = "arn:aws:kms:${var.destination_region_name}:${data.aws_caller_identity.current.account_id}:alias/${var.key_alias}"
-}
-
-data "aws_kms_key" "source_rds_snapshot_key" {
-  key_id = "arn:aws:kms:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:alias/${var.key_alias}"
-}
-
-data "aws_iam_policy_document" "aurora_backup_role" {
-  statement {
-    actions = ["kms:Encrypt", "kms:Decrypt", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:DescribeKey"]
-
-    resources = [
-      data.aws_kms_key.source_rds_snapshot_key.arn,
-      data.aws_kms_key.destination_rds_snapshot_key.arn,
-    ]
-  }
-}
 
 resource "aws_iam_role" "aurora_backup_role" {
   name               = "${var.environment_name}_aurora_cluster_backup_role"
--- terraform/environment/modules/rds_cross_region_backup/main.tf
+++ terraform/environment/modules/rds_cross_region_backup/main.tf
@@ -1,55 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
-
-data "aws_region" "secondary" {
-  provider = aws.destination
-}
-
-# backup plans
-resource "aws_backup_plan" "main" {
-  name = "${var.environment_name}_aurora_backup_plan"
-  rule {
-    completion_window   = 10080
-    recovery_point_tags = {}
-    rule_name           = "DailyBackups"
-    schedule            = "cron(0 6 ? * * *)" // Run at 6am UTC every day
-    start_window        = 480
-    target_vault_name   = aws_backup_vault.main.name
-
-    lifecycle {
-      cold_storage_after = var.daily_backup_cold_storage
-      delete_after       = var.daily_backup_deletion
-    }
-    copy_action {
-      destination_vault_arn = aws_backup_vault.secondary.arn
-
-      lifecycle {
-        delete_after = var.daily_backup_deletion
-      }
-    }
-  }
-  rule {
-    completion_window   = 10080
-    recovery_point_tags = {}
-    rule_name           = "Monthly"
-    schedule            = "cron(0 6 1 * ? *)" // Run at 6am UTC on the first day of every month
-    start_window        = 480
-    target_vault_name   = aws_backup_vault.main.name
-
-    lifecycle {
-      cold_storage_after = var.monthly_backup_cold_storage
-      delete_after       = var.monthly_backup_deletion
-    }
-    copy_action {
-      destination_vault_arn = aws_backup_vault.secondary.arn
-
-      lifecycle {
-        delete_after = var.monthly_backup_deletion
-      }
-    }
-  }
-}
 resource "aws_backup_vault" "main" {
   name        = "${var.environment_name}_${data.aws_region.current.region}_aurora_backup_vault"
   kms_key_arn = data.aws_kms_key.source_rds_snapshot_key.arn
@@ -67,3 +18,9 @@ resource "aws_backup_selection" "main" {
   iam_role_arn = aws_iam_role.aurora_backup_role.arn
   resources    = [var.source_cluster_arn]
 }
+
+resource "aws_backup_vault" "backup_account" {
+  provider = aws.backup_account
+  name     = "${var.environment_name}_${data.aws_region.current.region}_make_backup_account_vault"
+}
+# TODO = Make name identifiable to make as others using account id
