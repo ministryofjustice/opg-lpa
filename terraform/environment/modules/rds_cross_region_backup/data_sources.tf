e9f310e89df2c21a33abff6c08a8a20693370537
radhia shah <radhia.shah@justice.gov.uk>
2026-01-26 09:36:55 +0000

    LPAL-1883 - building backup account iam policies

--- terraform/environment/modules/rds_cross_region_backup/data_sources.tf
+++ terraform/environment/modules/rds_cross_region_backup/data_sources.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "aurora_backup_role" {
     resources = [
       data.aws_kms_key.source_rds_snapshot_key.arn,
       data.aws_kms_key.destination_rds_snapshot_key.arn,
-      # data.aws_kms_key.backup_account_key.arn,
+      data.aws_kms_key.backup_account_key.arn,
     ]
   }
 }
@@ -29,62 +29,4 @@ data "aws_kms_key" "destination_rds_snapshot_key" {
 
 data "aws_kms_key" "source_rds_snapshot_key" {
   key_id = "arn:aws:kms:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:alias/${var.key_alias}"
-}
-
-
-data "aws_iam_policy_document" "backup_account_key" {
-  provider = aws.backup_account
-  statement {
-    sid    = "Enable Root KMS Permissions"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.backup_account.id}:root"]
-    }
-    actions = [
-      "kms:*",
-    ]
-    resources = [
-      "*",
-    ]
-  }
-  statement {
-    sid    = "Allow Key to be used for Encryption"
-    effect = "Allow"
-    resources = [
-      "arn:aws:kms:*:${data.aws_caller_identity.current.account_id}:key/*"
-    ]
-    actions = [
-      "kms:Encrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey",
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = var.encryption_roles
-    }
-  }
-  statement {
-    sid    = "Allow Key to be used for Decryption"
-    effect = "Allow"
-    resources = [
-      "arn:aws:kms:*:${data.aws_caller_identity.current.account_id}:key/*"
-    ]
-    actions = [
-      "kms:Decrypt",
-      "kms:DescribeKey",
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = var.decryption_roles
-    }
-  }
-}
-
-# TODO - LIMIT SCOPE TO PERMISSIONS. CONFIRM IF NEEDED
-data "aws_caller_identity" "backup_account" {
-  provider = aws.backup_account
 }
