89d31214756afe3d7f4a91994e942013f3fc3dd0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-01-27 11:21:35 +0000

    add comments to id those for cloudshell

--- terraform/region/modules/region/firewalled_network.tf
+++ terraform/region/modules/region/firewalled_network.tf
@@ -55,13 +55,14 @@ data "aws_route_tables" "firewalled_network_application" {
 module "vpc_endpoints" {
   source = "./modules/vpc_endpoints"
   interface_endpoint_names = [
-    "codecatalyst.git",
-    "codecatalyst.packages",
+    "codecatalyst.git",      # for cloudshell
+    "codecatalyst.packages", # for cloudshell
     "ec2",
     "ecr.api",
     "ecr.dkr",
-    "ecs-agent",
-    "ecs",
+    "ecs-agent",     # for cloudshell
+    "ecs-telemetry", # for cloudshell
+    "ecs",           # for cloudshell
     "events",
     "execute-api",
     "kms",
@@ -71,7 +72,7 @@ module "vpc_endpoints" {
     "secretsmanager",
     "sqs",
     "ssm",
-    "ssmmessages",
+    "ssmmessages", # for cloudshell
     "xray",
   ]
   vpc_id                          = module.network.vpc.id
