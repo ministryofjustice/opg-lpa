b60a08a4957239000aad70b56dfa7ab0aa2548a1
radhia shah <radhia.shah@justice.gov.uk>
2026-01-28 10:11:20 +0000

    cross account kms key module vars updated

--- terraform/region/kms.tf
+++ terraform/region/kms.tf
@@ -4,8 +4,8 @@ module "aws_backup_cross_account_key" {
   administrator_roles = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
   decryption_roles    = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
   encryption_roles    = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
-  usage_services      = []
-  description         = ""
+  usage_services      = "backup.*.amazonaws.com"
+  description         = "Encryption keys for Make an LPA backups copied into the backup account"
   alias               = "opg-lpa-${local.account_name}-aws-backup-key"
   providers = {
     aws = aws.backup
