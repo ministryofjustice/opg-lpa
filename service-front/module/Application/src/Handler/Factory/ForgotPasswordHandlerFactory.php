1475a903bf81ec445d78e49e74b6c99d02d60ef0
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-01-29 09:51:43 +0000

    WIP

---
+++ service-front/module/Application/src/Handler/Factory/ForgotPasswordHandlerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Handler\Factory;
+
+use Application\Handler\ForgotPasswordHandler;
+use Application\Model\Service\User\Details as UserService;
+use Laminas\Form\FormElementManager;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Container\ContainerInterface;
+
+class ForgotPasswordHandlerFactory
+{
+    public function __invoke(ContainerInterface $container): ForgotPasswordHandler
+    {
+        return new ForgotPasswordHandler(
+            $container->get(TemplateRendererInterface::class),
+            $container->get(FormElementManager::class),
+            $container->get(UserService::class),
+            $container->get(UrlHelper::class),
+        );
+    }
+}
