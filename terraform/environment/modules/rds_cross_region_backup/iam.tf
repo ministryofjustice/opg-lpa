3d5a170c23fcd39323baa3a794ced57d9f6d0add
radhia shah <radhia.shah@justice.gov.uk>
2026-02-02 15:04:36 +0000

    iam permissions for backup role added back into env

---
+++ terraform/environment/modules/rds_cross_region_backup/iam.tf
@@ -0,0 +1,30 @@
+data "aws_kms_key" "destination_rds_snapshot_key" {
+  provider = aws.destination
+  key_id   = "arn:aws:kms:${var.destination_region_name}:${data.aws_caller_identity.current.account_id}:alias/${var.key_alias}"
+}
+
+data "aws_kms_key" "source_rds_snapshot_key" {
+  key_id = "arn:aws:kms:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:alias/${var.key_alias}"
+}
+
+data "aws_iam_policy_document" "aurora_backup_role" {
+  statement {
+    actions = ["kms:Encrypt", "kms:Decrypt", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:DescribeKey"]
+
+    resources = [
+      data.aws_kms_key.source_rds_snapshot_key.arn,
+      data.aws_kms_key.destination_rds_snapshot_key.arn,
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "kms_aurora_backup_role" {
+  role       = aws_iam_role.aurora_backup_role.name
+  policy_arn = aws_iam_policy.kms_aurora_backup_role.arn
+}
+
+resource "aws_iam_policy" "kms_aurora_backup_role" {
+  name        = "kms_aurora_backup_role"
+  description = "KMS policy for aurora backup role"
+  policy      = data.aws_iam_policy_document.aurora_backup_role.json
+}
