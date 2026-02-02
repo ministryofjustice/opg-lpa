b4b4470ec8cdf456dc671b7da8561fd2d55f108b
radhia shah <radhia.shah@justice.gov.uk>
2026-02-02 15:59:31 +0000

    aws backup roles and permissions added back into rdc cross account backup module in env

--- terraform/environment/modules/rds_cross_region_backup/iam.tf
+++ terraform/environment/modules/rds_cross_region_backup/iam.tf
@@ -28,3 +28,24 @@ resource "aws_iam_policy" "kms_aurora_backup_role" {
   description = "KMS policy for aurora backup role"
   policy      = data.aws_iam_policy_document.aurora_backup_role.json
 }
+
+resource "aws_iam_role" "aurora_backup_role" {
+  name               = "aurora_cluster_backup_role"
+  assume_role_policy = data.aws_iam_policy_document.aurora_cluster_backup_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "aurora_backup_role" {
+  role       = aws_iam_role.aurora_backup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+}
+
+data "aws_iam_policy_document" "aurora_cluster_backup_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["backup.amazonaws.com"]
+    }
+  }
+}
