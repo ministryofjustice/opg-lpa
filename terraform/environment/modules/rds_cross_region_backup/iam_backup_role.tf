adcfe995bedb8a592a83322de0c1c87558d7f1b3
radhia shah <radhia.shah@justice.gov.uk>
2026-02-03 11:08:37 +0000

    backup role iam policies updated

--- terraform/environment/modules/rds_cross_region_backup/iam_backup_role.tf
+++ terraform/environment/modules/rds_cross_region_backup/iam_backup_role.tf
@@ -1,17 +1,7 @@
-resource "aws_iam_role" "aurora_backup_role" {
-  name               = "${var.environment_name}_aurora_cluster_backup_role"
-  assume_role_policy = data.aws_iam_policy_document.aurora_cluster_backup_role.json
+data "aws_iam_role" "aurora_backup_role" {
+  name = "aurora_cluster_backup_role"
 }
 
-data "aws_iam_policy_document" "aurora_cluster_backup_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["backup.amazonaws.com"]
-    }
-  }
-}
 
 data "aws_iam_policy_document" "aurora_backup_role" {
   statement {
@@ -25,18 +15,14 @@ data "aws_iam_policy_document" "aurora_backup_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "aurora_backup_role" {
-  role       = aws_iam_role.aurora_backup_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-}
 
 resource "aws_iam_policy" "aurora_backup_resources" {
-  name        = "aurora_backup_role"
+  name        = "${var.environment_name}_aurora_backup_role_policy"
   description = "Policies for aurora backup role"
   policy      = data.aws_iam_policy_document.aurora_backup_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "aurora_backup_resources" {
-  role       = aws_iam_role.aurora_backup_role.name
+  role       = data.aws_iam_role.aurora_backup_role.name
   policy_arn = aws_iam_policy.aurora_backup_resources.arn
 }
