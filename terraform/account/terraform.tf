35d19b8d1f6e95531f647bfe5fc30bd825e41d19
radhia shah <radhia.shah@justice.gov.uk>
2026-02-09 12:45:40 +0000

    testing tf linting error - commenting out unused declration errors

--- terraform/account/terraform.tf
+++ terraform/account/terraform.tf
@@ -22,62 +22,62 @@ provider "aws" {
   }
 }
 
-provider "aws" {
-  alias  = "eu-west-1"
-  region = "eu-west-1"
-  default_tags {
-    tags = local.default_tags
-  }
-  assume_role {
-    role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
-    session_name = "terraform-session"
-  }
-}
+# provider "aws" {
+#   alias  = "eu-west-1"
+#   region = "eu-west-1"
+#   default_tags {
+#     tags = local.default_tags
+#   }
+#   assume_role {
+#     role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
+#     session_name = "terraform-session"
+#   }
+# }
 
-provider "aws" {
-  region = "eu-west-2"
-  alias  = "eu-west-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
-    session_name = "terraform-session"
-  }
-}
+# provider "aws" {
+#   region = "eu-west-2"
+#   alias  = "eu-west-2"
+#   assume_role {
+#     role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
+#     session_name = "terraform-session"
+#   }
+# }
 
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-  default_tags {
-    tags = local.default_tags
-  }
-  assume_role {
-    role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
-    session_name = "terraform-session"
-  }
-}
+# provider "aws" {
+#   alias  = "us_east_1"
+#   region = "us-east-1"
+#   default_tags {
+#     tags = local.default_tags
+#   }
+#   assume_role {
+#     role_arn     = "arn:aws:iam::${local.account_id}:role/${var.default_role}"
+#     session_name = "terraform-session"
+#   }
+# }
 
-provider "aws" {
-  region = "eu-west-1"
-  alias  = "management"
-  default_tags {
-    tags = local.default_tags
-  }
-  assume_role {
-    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
-    session_name = "terraform-session"
-  }
-}
+# provider "aws" {
+#   region = "eu-west-1"
+#   alias  = "management"
+#   default_tags {
+#     tags = local.default_tags
+#   }
+#   assume_role {
+#     role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+#     session_name = "terraform-session"
+#   }
+# }
 
-provider "aws" {
-  region = "eu-west-1"
-  alias  = "legacy-lpa"
-  default_tags {
-    tags = local.default_tags
-  }
-  assume_role {
-    role_arn     = "arn:aws:iam::550790013665:role/${var.default_role}"
-    session_name = "terraform-session"
-  }
-}
+# provider "aws" {
+#   region = "eu-west-1"
+#   alias  = "legacy-lpa"
+#   default_tags {
+#     tags = local.default_tags
+#   }
+#   assume_role {
+#     role_arn     = "arn:aws:iam::550790013665:role/${var.default_role}"
+#     session_name = "terraform-session"
+#   }
+# }
 
 
 provider "pagerduty" {
