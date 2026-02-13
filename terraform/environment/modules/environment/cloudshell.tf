c3b95d73bd48a20826d53af68909be1f007559b0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-02-13 14:45:43 +0000

    remove rule

--- terraform/environment/modules/environment/cloudshell.tf
+++ terraform/environment/modules/environment/cloudshell.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "cloudshell" {
   }
 }
 
-resource "aws_security_group_rule" "cloudshell_egress" {
-  type      = "egress"
-  from_port = 5432 #TODO: can we restrict this to postgres 5432
-  to_port   = 5432 #TODO: can we restrict this to postgres 5432
-  protocol  = "-1"
-  #tfsec:ignore:aws-ec2-no-public-egress-sgr - anything out
-  cidr_blocks       = ["0.0.0.0/0"] #TODO: can we restrict this to postgres
-  security_group_id = aws_security_group.api_ecs_service.id
-  description       = "Cloudshell open egress"
-}
+# resource "aws_security_group_rule" "cloudshell_egress" {
+#   type      = "egress"
+#   from_port = 5432 #TODO: can we restrict this to postgres 5432
+#   to_port   = 5432 #TODO: can we restrict this to postgres 5432
+#   protocol  = "-1"
+#   #tfsec:ignore:aws-ec2-no-public-egress-sgr - anything out
+#   cidr_blocks       = ["0.0.0.0/0"] #TODO: can we restrict this to postgres
+#   security_group_id = aws_security_group.api_ecs_service.id
+#   description       = "Cloudshell open egress"
+# }
