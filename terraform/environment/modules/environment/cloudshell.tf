289aff8f6db30cdb7a15b1a9f0eaf543cb0daf42
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-02-13 13:26:20 +0000

    differentiate sgs

--- terraform/environment/modules/environment/cloudshell.tf
+++ terraform/environment/modules/environment/cloudshell.tf
@@ -1,5 +1,6 @@
 resource "aws_security_group" "cloudshell" {
   name_prefix = "${terraform.workspace}-cloudshell"
+  description = "Security group for Cloudshell"
   vpc_id      = data.aws_vpc.main.id
   lifecycle {
     create_before_destroy = true
