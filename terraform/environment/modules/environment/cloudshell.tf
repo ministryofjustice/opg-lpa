5a95f750814be22779346f823479d976d3bd351b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-02-13 11:36:19 +0000

    add cloudshell security group

---
+++ terraform/environment/modules/environment/cloudshell.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "cloudshell" {
+  name_prefix = "${terraform.workspace}-cloudshell"
+  vpc_id      = data.aws_vpc.main.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "api_ecs_service_egress" {
+  type      = "egress"
+  from_port = 0 #TODO: can we restrict this to postgres 5432
+  to_port   = 0 #TODO: can we restrict this to postgres 5432
+  protocol  = "-1"
+  #tfsec:ignore:aws-ec2-no-public-egress-sgr - anything out
+  cidr_blocks       = ["0.0.0.0/0"] #TODO: can we restrict this to postgres
+  security_group_id = aws_security_group.api_ecs_service.id
+  description       = "Cloudshell open egress"
+}
