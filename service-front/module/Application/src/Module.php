52e8e01dc0b40fe4e9282dc1a50426b545bd4ac7
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-02-16 11:34:18 +0000

    WIP

--- service-front/module/Application/src/Module.php
+++ service-front/module/Application/src/Module.php
@@ -5,6 +5,8 @@ namespace Application;
 use Alphagov\Pay\Client as GovPayClient;
 use Application\Handler\AboutYouHandler;
 use Application\Handler\Factory\AboutYouHandlerFactory;
+use Application\Handler\ChangeEmailAddressHandler;
+use Application\Handler\Factory\ChangeEmailAddressHandlerFactory;
 use Application\Handler\Factory\HomeRedirectHandlerFactory;
 use Application\Handler\HomeHandler;
 use Application\Adapter\DynamoDbKeyValueStore;
@@ -440,6 +442,7 @@ class Module implements FormElementProviderInterface
                 RegisterHandler::class => RegisterHandlerFactory::class,
                 ResendActivationEmailHandler::class => ResendActivationEmailHandlerFactory::class,
                 ConfirmRegistrationHandler::class => ConfirmRegistrationHandlerFactory::class,
+                ChangeEmailAddressHandler::class => ChangeEmailAddressHandlerFactory::class,
             ], // factories
             'initializers' => [
                 function (ServiceLocatorInterface $container, $instance) {
