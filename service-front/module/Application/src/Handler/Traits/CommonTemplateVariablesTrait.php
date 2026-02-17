3cd6b6bd42efa6ecdc2d0a985b13babef2816377
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-02-17 10:40:51 +0000

    added test and changed get attribute to match userdetails listener middleware as that sets user_details

--- service-front/module/Application/src/Handler/Traits/CommonTemplateVariablesTrait.php
+++ service-front/module/Application/src/Handler/Traits/CommonTemplateVariablesTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Application\Handler\Traits;
 
+use Application\Listener\Attribute;
 use Psr\Http\Message\ServerRequestInterface;
 
 trait CommonTemplateVariablesTrait
@@ -11,7 +12,8 @@ trait CommonTemplateVariablesTrait
     public function getTemplateVariables(ServerRequestInterface $request): array
     {
         return [
-            'signedInUser' => $request->getAttribute('signedInUser'),
+            // Template expects 'signedInUser', request attribute is 'userDetails'
+            'signedInUser' => $request->getAttribute(Attribute::USER_DETAILS),
             'secondsUntilSessionExpires' => $request->getAttribute('secondsUntilSessionExpires'),
         ];
     }
