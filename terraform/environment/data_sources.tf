5d93a4a009b8c82497329f24c4eaeb6e5b6a423b
radhia shah <radhia.shah@justice.gov.uk>
2026-02-24 09:27:00 +0000

    isolate new encryption key to dev only - test dev cross account backup - set to 10am

--- terraform/environment/data_sources.tf
+++ terraform/environment/data_sources.tf
@@ -1,1 +1,9 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_kms_key" "aurora_new_key" {
+  key_id = "alias/opg-lpa-${local.account_name}-rds-encryption-key"
+}
+
+data "aws_kms_key" "aurora_default_key" {
+  key_id = "alias/aws/rds"
+}
