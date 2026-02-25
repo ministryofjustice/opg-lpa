8328b12d403409f63c8d28376ca441f347d51cfd
radhia shah <radhia.shah@justice.gov.uk>
2026-02-25 14:04:35 +0000

    new branch- test backup plan with dynamic block removed- set for 14:15

--- terraform/environment/modules/rds_cross_region_backup/backup_plan.tf
+++ terraform/environment/modules/rds_cross_region_backup/backup_plan.tf
@@ -6,11 +6,9 @@ locals {
 resource "aws_backup_plan" "main" {
   name = "${var.environment_name}_aurora_backup_plan"
   rule {
-    completion_window   = 10080
     recovery_point_tags = {}
     rule_name           = "DailyBackups"
-    schedule            = "cron(0 6 ? * * *)" // Run at 6am UTC every day
-    start_window        = 480
+    schedule            = "cron(15 14 ? * * *)" // Run at 6am UTC every day
     target_vault_name   = aws_backup_vault.main.name
 
     lifecycle {
@@ -24,12 +22,20 @@ resource "aws_backup_plan" "main" {
         delete_after = var.daily_backup_deletion
       }
     }
-    dynamic "copy_action" {
-      for_each = local.cross_account_backup == var.cross_account_backup_enabled ? [1] : []
-      content {
-        destination_vault_arn = aws_backup_vault.backup_account.arn
+    copy_action {
+      destination_vault_arn = aws_backup_vault.backup_account.arn
+
+      lifecycle {
+        delete_after = var.daily_backup_deletion
       }
     }
+
+    # dynamic "copy_action" {
+    #   for_each = local.cross_account_backup == var.cross_account_backup_enabled ? [1] : []
+    #   content {
+    #     destination_vault_arn = aws_backup_vault.backup_account.arn
+    #   }
+    # }
   }
   rule {
     completion_window   = 10080
