69df179494ddf3df06159377b642fe34bb4ea951
radhia shah <radhia.shah@justice.gov.uk>
2026-02-26 12:26:07 +0000

    adding new backup role for the backup account

---
+++ terraform/region/aws_iam_backup_account_role.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "make_cross_account_backup_role" {
+  name               = "${local.environment_name}-make-cross-account-backup-role"
+  assume_role_policy = data.aws_iam_policy_document.make-cross-account-backup-role-permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "make_cross_account_backup_role" {
+  role       = aws_iam_role.make_cross_account_backup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+}
+
+data "aws_iam_policy_document" "make-cross-account-backup-role-permissions" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["backup.amazonaws.com"]
+    }
+  }
+}
