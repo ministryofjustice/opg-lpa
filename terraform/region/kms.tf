c489cab4223da454a3ad426c81859046d1b51167
radhia shah <radhia.shah@justice.gov.uk>
2026-02-26 16:13:13 +0000

    changes to kms keys for cross account backup - adding new source key and backup key

--- terraform/region/kms.tf
+++ terraform/region/kms.tf
@@ -1,8 +1,42 @@
 module "aws_backup_cross_account_key" {
-  source = "./modules/kms_key"
+  source             = "git::https://github.com/ministryofjustice/opg-terraform-aws-kms-key.git?ref=v0.0.5"
+  description        = "Encryption keys for Make an LPA backups copied into the backup account"
+  alias              = "opg-lpa-${local.account_name}-aws-backup-key"
+  primary_region     = "eu-west-1"
+  replicas_to_create = []
+  providers = {
+    aws = aws.backup
+  }
+  usage_services = ["backup.*.amazonaws.com"]
+
+  administrator_roles = [
+    "arn:aws:iam::${data.aws_caller_identity.backup.account_id}:role/breakglass",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/opg-lpa-ci",
+  ]
+  decryption_roles = [
+    "arn:aws:iam::${data.aws_caller_identity.backup.account_id}:role/breakglass",
+    aws_iam_role.make_cross_account_backup_role.arn,
+  ]
+  encryption_roles = [
+    "arn:aws:iam::${data.aws_caller_identity.backup.account_id}:role/breakglass",
+    aws_iam_role.make_cross_account_backup_role.arn,
+  ]
+}
+
+module "aws_backup_source_account_key" {
+  source             = "git::https://github.com/ministryofjustice/opg-terraform-aws-kms-key.git?ref=v0.0.5"
+  description        = "Encryption keys for Make an LPA backups copied into the backup account"
+  alias              = "opg-lpa-${local.account_name}-aws-backup-source-account-key"
+  primary_region     = "eu-west-1"
+  replicas_to_create = ["eu-west-2"]
+  providers = {
+    aws = aws.eu-west-1
+  }
+  usage_services = ["backup.*.amazonaws.com"]
 
   administrator_roles = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/opg-lpa-ci",
   ]
   decryption_roles = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass",
@@ -12,13 +46,8 @@ module "aws_backup_cross_account_key" {
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass",
     aws_iam_role.aurora_backup_role.arn,
   ]
-  usage_services = ["backup.*.amazonaws.com"]
-  description    = "Encryption keys for Make an LPA backups copied into the backup account"
-  alias          = "opg-lpa-${local.account_name}-aws-backup-key"
-  providers = {
-    aws = aws.backup
-  }
 }
+
 #
 module "aurora_database_encryption_key" {
   source             = "git::https://github.com/ministryofjustice/opg-terraform-aws-kms-key.git?ref=v0.0.5"
@@ -46,12 +75,14 @@ module "aurora_database_encryption_key" {
     "-seeding-task-role",
     "-api-task-role",
     aws_iam_role.aurora_backup_role.arn,
+    aws.iam_role.make_cross_account_backup_role.arn,
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass",
   ]
   decryption_role_patterns = [
     "-seeding-task-role",
     "-api-task-role",
     aws_iam_role.aurora_backup_role.arn,
+    aws.iam_role.make_cross_account_backup_role.arn,
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass",
   ]
 }
