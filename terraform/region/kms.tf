0e556c9fa45a8a66cdf111206720767b2729413f
radhia shah <radhia.shah@justice.gov.uk>
2026-02-26 17:46:38 +0000

    kms key grant roles updated

--- terraform/region/kms.tf
+++ terraform/region/kms.tf
@@ -21,6 +21,9 @@ module "aws_backup_cross_account_key" {
     "arn:aws:iam::${data.aws_caller_identity.backup.account_id}:role/breakglass",
     aws_iam_role.make_cross_account_backup_role.arn,
   ]
+  grant_roles = [
+    "arn:aws:iam::${data.aws_caller_identity.backup.account_id}:role/breakglass"
+  ]
 }
 
 module "aws_backup_source_account_key" {
@@ -46,9 +49,11 @@ module "aws_backup_source_account_key" {
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass",
     aws_iam_role.aurora_backup_role.arn,
   ]
+  grant_roles = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"
+  ]
 }
 
-#
 module "aurora_database_encryption_key" {
   source             = "git::https://github.com/ministryofjustice/opg-terraform-aws-kms-key.git?ref=v0.0.5"
   description        = "Customer managed encryption key for Aurora RDS database"
