ea0a9f60982bda6a1e487d107e3f73dc6988f69d
radhia shah <radhia.shah@justice.gov.uk>
2026-03-04 14:54:55 +0000

    env level changes to backup module - data sources, provider config and vault paths/names updated

--- terraform/environment/modules/rds_cross_region_backup/data_sources.tf
+++ terraform/environment/modules/rds_cross_region_backup/data_sources.tf
@@ -16,17 +16,3 @@ data "aws_kms_key" "cross_account_backup_key" {
   provider = aws.backup
   key_id   = "arn:aws:kms:${data.aws_region.current.region}:${data.aws_caller_identity.backup.account_id}:alias/${var.key_alias}"
 }
-
-# old kms keys for aws backup - to be removed once all backup resources have been migrated to the new ke
-# data "aws_kms_key" "destination_rds_snapshot_key" {
-#   provider = aws.destination
-#   key_id   = "arn:aws:kms:${var.destination_region_name}:${data.aws_caller_identity.current.account_id}:alias/${var.key_alias}"
-# }
-
-# data "aws_kms_key" "source_rds_snapshot_key" {
-#   key_id = "arn:aws:kms:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:alias/${var.key_alias}"
-# }
-
-# data "aws_region" "secondary" {
-#   provider = aws.destination
-# }
