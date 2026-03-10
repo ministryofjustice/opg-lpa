549f8b9bb9ccdbef9268772fa89914e933d27466
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-03-10 13:15:48 +0000

    updated admin to display user id and searching via user id

---
+++ service-admin/src/App/src/Validator/EmailOrUserId.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Validator;
+
+use Laminas\Validator\AbstractValidator;
+
+class EmailOrUserId extends AbstractValidator
+{
+    public const INVALID = 'invalid';
+
+    protected array $messageTemplates = [
+        self::INVALID => 'Enter a valid email address or user ID',
+    ];
+
+    public function isValid($value): bool
+    {
+        $this->setValue($value);
+
+        $value = trim($value);
+
+        if (str_contains($value, '@')) {
+            $emailValidator = new Email();
+            return $emailValidator->isValid($value);
+        }
+
+        if (preg_match('/^[0-9a-zA-Z]+$/', $value)) {
+            return true;
+        }
+
+        $this->error(self::INVALID);
+        return false;
+    }
+}
