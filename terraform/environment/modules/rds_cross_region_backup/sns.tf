ac36fd3be6597899e3f1efaf69467d3de5172ac0
radhia shah <radhia.shah@justice.gov.uk>
2026-03-11 13:08:28 +0000

    LPAL-1943| Enable backup vault lock for new vaults to test config. Only scoped to dev workspace and account

--- terraform/environment/modules/rds_cross_region_backup/sns.tf
+++ terraform/environment/modules/rds_cross_region_backup/sns.tf
@@ -1,37 +1,37 @@
-# data "aws_sns_topic" "rds_events" {
-#   name = "${var.account_name}-${data.aws_region.current.region}-rds-events"
-# }
+data "aws_sns_topic" "rds_events" {
+  name = "${var.account_name}-${data.aws_region.current.region}-rds-events"
+}
 
-# resource "aws_backup_vault_notifications" "aws_backup_job_failures" {
-#   backup_vault_name   = aws_backup_vault.main.name
-#   sns_topic_arn       = data.aws_sns_topic.rds_events.arn
-#   backup_vault_events = ["BACKUP_JOB_FAILED", "COPY_JOB_FAILED", "RESTORE_JOB_FAILED"]
-# }
+resource "aws_backup_vault_notifications" "aws_backup_job_failures" {
+  backup_vault_name   = aws_backup_vault.backup_primary.name
+  sns_topic_arn       = data.aws_sns_topic.rds_events.arn
+  backup_vault_events = ["BACKUP_JOB_FAILED", "COPY_JOB_FAILED", "RESTORE_JOB_FAILED"]
+}
 
-# data "aws_iam_policy_document" "aws_rds_events_sns" {
-#   statement {
-#     actions = ["SNS:Publish"]
-#     effect  = "Allow"
-#     sid     = "AllowAwsBackupToPublish"
-#     principals {
-#       type        = "Service"
-#       identifiers = ["backup.amazonaws.com"]
-#     }
+data "aws_iam_policy_document" "aws_rds_events_sns" {
+  statement {
+    actions = ["SNS:Publish"]
+    effect  = "Allow"
+    sid     = "AllowAwsBackupToPublish"
+    principals {
+      type        = "Service"
+      identifiers = ["backup.amazonaws.com"]
+    }
 
-#     resources = [data.aws_sns_topic.rds_events.arn]
-#   }
-#   statement {
-#     sid     = "AllowEventBridgeToPublish"
-#     actions = ["SNS:Publish"]
-#     effect  = "Allow"
-#     principals {
-#       type        = "Service"
-#       identifiers = ["events.amazonaws.com"]
-#     }
-#     resources = [data.aws_sns_topic.rds_events.arn]
-#   }
-# }
-# resource "aws_sns_topic_policy" "rds_events_policy" {
-#   arn    = data.aws_sns_topic.rds_events.arn
-#   policy = data.aws_iam_policy_document.aws_rds_events_sns.json
-# }
+    resources = [data.aws_sns_topic.rds_events.arn]
+  }
+  statement {
+    sid     = "AllowEventBridgeToPublish"
+    actions = ["SNS:Publish"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+    resources = [data.aws_sns_topic.rds_events.arn]
+  }
+}
+resource "aws_sns_topic_policy" "rds_events_policy" {
+  arn    = data.aws_sns_topic.rds_events.arn
+  policy = data.aws_iam_policy_document.aws_rds_events_sns.json
+}
