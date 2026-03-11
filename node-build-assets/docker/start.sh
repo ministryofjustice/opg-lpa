5ca33108f7b3068816e7e7ae582249976ca3900a
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-03-11 10:21:44 +0000

    back to how it was

--- node-build-assets/docker/start.sh
+++ node-build-assets/docker/start.sh
@@ -3,5 +3,5 @@ cd /service-front
 export PATH=/service-front/node_modules/.bin/:$PATH
 npm ci --ignore-scripts
 # TODO needs to become watch
-grunt build_js_dev build_css
+grunt build_js_dev build_css watch
 npm run build:js
