29adad800df769e0ba5611ef10460f70ab956a06
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-03-11 16:56:37 +0000

    gruntfile needs to be renamed to cjs to be compatible with changes to package.json

--- node-build-assets/docker/start.sh
+++ node-build-assets/docker/start.sh
@@ -3,5 +3,6 @@ cd /service-front
 export PATH=/service-front/node_modules/.bin/:$PATH
 npm ci --ignore-scripts
 # TODO needs to become watch
-grunt build_js_dev build_css watch
-npm run build:js
+#npm run clean
+grunt --gruntfile Gruntfile.cjs build_js_dev build_css watch
+#npm run build:js
