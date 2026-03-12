40750171e9f3335e15aa273a3d663ddbe1ff19fc
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-03-12 09:49:46 +0000

    WIP build locally js with esbuild, css still with grunt. Note -had to turn watch off, this will need to go back on later

--- node-build-assets/docker/start.sh
+++ node-build-assets/docker/start.sh
@@ -4,5 +4,5 @@ export PATH=/service-front/node_modules/.bin/:$PATH
 npm ci --ignore-scripts
 # TODO needs to become watch
 #npm run clean
-grunt --gruntfile Gruntfile.cjs build_js_dev build_css watch
-#npm run build:js
+grunt --gruntfile Gruntfile.cjs build_css 
+npm run build:js
