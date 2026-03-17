8d310be9e3f26470112b33812260478a2c296b8b
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-03-17 16:13:18 +0000

    fix build and run js version of build to build js files

--- node-build-assets/docker/start.sh
+++ node-build-assets/docker/start.sh
@@ -2,5 +2,5 @@
 cd /service-front
 export PATH=/service-front/node_modules/.bin/:$PATH
 npm ci --ignore-scripts
-grunt --gruntfile Gruntfile.cjs build_js build_css watch
-#npm run build:js
+grunt --gruntfile Gruntfile.cjs build_css 
+node ./build.js
