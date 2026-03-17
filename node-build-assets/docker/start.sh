c82b6244f40c1f7630f449487c1e9f608b3167c8
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-03-17 10:20:38 +0000

    local run

--- node-build-assets/docker/start.sh
+++ node-build-assets/docker/start.sh
@@ -3,5 +3,5 @@ cd /service-front
 export PATH=/service-front/node_modules/.bin/:$PATH
 npm ci --ignore-scripts
 # TODO needs to become watch
-grunt --gruntfile Gruntfile.cjs build_css watch
+grunt --gruntfile Gruntfile.cjs build_js build_css watch
 #npm run build:js
