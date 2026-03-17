19026974949647c4fa13a0e301eb636c7cfd676c
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-03-17 10:10:19 +0000

    WIP go back to grunt running everything to ensure all working after the "dust is settled"

--- node-build-assets/docker/start.sh
+++ node-build-assets/docker/start.sh
@@ -3,6 +3,5 @@ cd /service-front
 export PATH=/service-front/node_modules/.bin/:$PATH
 npm ci --ignore-scripts
 # TODO needs to become watch
-#npm run clean
-grunt --gruntfile Gruntfile.cjs build_css 
-npm run build:js
+grunt --gruntfile Gruntfile.cjs build_css watch
+#npm run build:js
