64440cd618ff921865d122ca4fefbe37f5e2ceed
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-03-23 15:23:31 +0000

    LPAL-1818 wip

--- service-front/module/Application/config/module.routes.php
+++ service-front/module/Application/config/module.routes.php
@@ -22,6 +22,7 @@ use Application\Handler\Lpa\DonorAddHandler;
 use Application\Handler\Lpa\DonorEditHandler;
 use Application\Handler\Lpa\DonorIndexHandler;
 use Application\Handler\Lpa\ReuseDetailsHandler;
+use Application\Handler\Lpa\WhenLpaStartsHandler;
 use Application\Handler\StatusesHandler;
 use Application\Handler\TermsChangedHandler;
 use Application\Listener\TermsAndConditionsListener;
@@ -1200,8 +1201,15 @@ return [
                         'options' => [
                             'route'    => '/when-lpa-starts',
                             'defaults' => [
-                                'controller' => 'Authenticated\Lpa\WhenLpaStartsController',
-                                'action'     => 'index',
+                                'controller' => PipeSpec::class,
+                                'middleware' => new PipeSpec(
+                                    RouteMatchMiddleware::class,
+                                    AuthenticationListener::class,
+                                    UserDetailsListener::class,
+                                    TermsAndConditionsListener::class,
+                                    LpaLoaderMiddleware::class,
+                                    WhenLpaStartsHandler::class,
+                                ),
                             ],
                         ],
                     ],
---
+++ service-front/module/Application/src/Handler/Lpa/WhenLpaStartsHandler.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Handler\Lpa;
+
+use Application\Handler\Traits\CommonTemplateVariablesTrait;
+use Application\Helper\MvcUrlHelper;
+use Application\Middleware\RequestAttribute;
+use Application\Model\FormFlowChecker;
+use Application\Model\Service\Lpa\Application as LpaApplicationService;
+use Fig\Http\Message\RequestMethodInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\RedirectResponse;
+use Laminas\Form\FormElementManager;
+use MakeShared\DataModel\Lpa\Document\Decisions\PrimaryAttorneyDecisions;
+use MakeShared\DataModel\Lpa\Lpa;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use RuntimeException;
+
+class WhenLpaStartsHandler implements RequestHandlerInterface
+{
+    use CommonTemplateVariablesTrait;
+
+    public function __construct(
+        private readonly TemplateRendererInterface $renderer,
+        private readonly FormElementManager $formElementManager,
+        private readonly LpaApplicationService $lpaApplicationService,
+        private readonly MvcUrlHelper $urlHelper,
+    ) {
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        /** @var Lpa $lpa */
+        $lpa = $request->getAttribute(RequestAttribute::LPA);
+
+        /** @var FormFlowChecker $flowChecker */
+        $flowChecker = $request->getAttribute(RequestAttribute::FLOW_CHECKER);
+
+        $currentRoute = (string) $request->getAttribute(RequestAttribute::CURRENT_ROUTE);
+
+        $form = $this->formElementManager->get(
+            'Application\Form\Lpa\WhenLpaStartsForm',
+            ['lpa' => $lpa]
+        );
+
+        $primaryAttorneyDecisions = $lpa->document->primaryAttorneyDecisions;
+
+        if (strtoupper($request->getMethod()) === RequestMethodInterface::METHOD_POST) {
+            $postData = $request->getParsedBody() ?? [];
+            if (!is_array($postData)) {
+                $postData = [];
+            }
+
+            $form->setData($postData);
+
+            if ($form->isValid()) {
+                if (!$primaryAttorneyDecisions instanceof PrimaryAttorneyDecisions) {
+                    $primaryAttorneyDecisions = new PrimaryAttorneyDecisions();
+                    $lpa->document->primaryAttorneyDecisions = $primaryAttorneyDecisions;
+                }
+
+                /** @var array $formData */
+                $formData = $form->getData();
+                $whenToStart = $formData['when'];
+
+                if ($primaryAttorneyDecisions->when !== $whenToStart) {
+                    $primaryAttorneyDecisions->when = $whenToStart;
+
+                    $setOk = $this->lpaApplicationService->setPrimaryAttorneyDecisions(
+                        $lpa,
+                        $primaryAttorneyDecisions
+                    );
+
+                    if (!$setOk) {
+                        throw new RuntimeException(
+                            'API client failed to set when LPA starts for id: ' . $lpa->id
+                        );
+                    }
+                }
+
+                $nextRoute = $flowChecker->nextRoute($currentRoute);
+
+                return new RedirectResponse(
+                    $this->urlHelper->generate(
+                        $nextRoute,
+                        ['lpa-id' => $lpa->id],
+                        $flowChecker->getRouteOptions($nextRoute)
+                    )
+                );
+            }
+        } else {
+            if ($primaryAttorneyDecisions instanceof PrimaryAttorneyDecisions) {
+                $form->bind($primaryAttorneyDecisions->flatten());
+            }
+        }
+
+        $html = $this->renderer->render(
+            'application/authenticated/lpa/when-lpa-starts/index.twig',
+            array_merge(
+                $this->getTemplateVariables($request),
+                ['form' => $form]
+            )
+        );
+
+        return new HtmlResponse($html);
+    }
+}
