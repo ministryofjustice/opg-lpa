8f61509d588c318808b89b03c4233c10dc9b5ea8
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-03-24 14:17:56 +0000

    css and js done by esbuild

--- node-build-assets/docker/start.sh
+++ node-build-assets/docker/start.sh
@@ -2,5 +2,5 @@
 cd /service-front
 export PATH=/service-front/node_modules/.bin/:$PATH
 npm ci --ignore-scripts
-grunt --gruntfile Gruntfile.cjs build_css
-node ./build.js
+npm run build:css
+npm run build:js
