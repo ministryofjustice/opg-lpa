6d0262e683d2d5453bbe77ba9559282b437ac2c0
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-03-31 10:50:43 +0100

    LPAL-1803 refactored fee reduction controller to handler

--- service-front/module/Application/config/module.routes.php
+++ service-front/module/Application/config/module.routes.php
@@ -25,6 +25,7 @@ use Application\Handler\Lpa\DeleteLpaHandler;
 use Application\Handler\Lpa\DonorAddHandler;
 use Application\Handler\Lpa\DonorEditHandler;
 use Application\Handler\Lpa\DonorIndexHandler;
+use Application\Handler\Lpa\FeeReductionHandler;
 use Application\Handler\Lpa\ReuseDetailsHandler;
 use Application\Handler\Lpa\WhenLpaStartsHandler;
 use Application\Handler\StatusesHandler;
@@ -1101,8 +1102,8 @@ return [
                         'options' => [
                             'route'    => '/fee-reduction',
                             'defaults' => [
-                                'controller' => 'Authenticated\Lpa\FeeReductionController',
-                                'action'     => 'index',
+                                'controller' => PipeSpec::class,
+                                'middleware' => RouteMiddlewareHelper::addMiddleware(FeeReductionHandler::class, []),
                             ],
                         ],
                     ],
