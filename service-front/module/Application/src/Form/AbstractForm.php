0081514c25b23775eb9c01a0ff93187c31be88ca
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-04-01 11:41:56 +0100

    LPAL-1980

--- service-front/module/Application/src/Form/AbstractForm.php
+++ service-front/module/Application/src/Form/AbstractForm.php
@@ -2,6 +2,7 @@
 
 namespace Application\Form;
 
+use Laminas\Form\Element\Textarea;
 use Laminas\Form\Form;
 
 /**
@@ -28,21 +29,30 @@ abstract class AbstractForm extends Form
      */
     protected function addToInputFilter(array $inputData)
     {
-        // Only add the StripTags and StringTrim filters for
-        // non-password fields; if used for password fields, these
-        // filters strip leading/trailing spaces and remove anything that
-        // looks like HTML, such as <>
-        if (!in_array($inputData['name'], ['password', 'password_confirm', 'password_current'])) {
-            // Merge additional input filters for non-password fields
+        $fieldName = $inputData['name'];
+
+        // Password fields: no StripTags or StringTrim, as these filters
+        // strip leading/trailing spaces and remove anything that looks
+        // like HTML, such as <>
+        $isPasswordField = in_array($fieldName, ['password', 'password_confirm', 'password_current']);
+
+        // Textarea fields: no StripTags, as the filter treats characters
+        // like < and > as HTML tags and strips everything from < to the
+        // next > (or end of string), destroying legitimate user input.
+        // Output escaping in templates (Twig auto-escape) handles XSS.
+        $isTextareaField = $this->has($fieldName) && $this->get($fieldName) instanceof Textarea;
+
+        if (!$isPasswordField) {
+            $filters = [];
+
+            if (!$isTextareaField) {
+                $filters[] = ['name' => 'Laminas\Filter\StripTags'];
+            }
+
+            $filters[] = ['name' => 'Laminas\Filter\StringTrim'];
+
             $inputData = array_merge_recursive([
-                'filters'  => [
-                    [
-                        'name' => 'Laminas\Filter\StripTags'
-                    ],
-                    [
-                        'name' => 'Laminas\Filter\StringTrim'
-                    ],
-                ]
+                'filters' => $filters,
             ], $inputData);
         }
 
