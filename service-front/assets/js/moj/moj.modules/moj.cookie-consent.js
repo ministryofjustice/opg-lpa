6e5f5c9e8a37805091b05f5f45cffbfaf27a826d
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-04-14 15:55:26 +0100

    make google analytics be conditional on having accepted cookies

--- service-front/assets/js/moj/moj.modules/moj.cookie-consent.js
+++ service-front/assets/js/moj/moj.modules/moj.cookie-consent.js
@@ -115,6 +115,10 @@
       if (usage) {
         // enable analytics and fire off a pageview
         moj.Events.trigger('Analytics.start');
+        let s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.src = 'https://www.googletagmanager.com/gtag/js?id=G-DY4BCL021L"';
+        document.getElementsByTagName('head')[0].appendChild(s);
       }
     },
 
