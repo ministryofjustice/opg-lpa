9795f1e5720fb452d8a8dc55510794a84e4810e0
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-04-15 11:10:10 +0100

    fix

--- service-front/assets/js/moj/moj.modules/moj.cookie-consent.js
+++ service-front/assets/js/moj/moj.modules/moj.cookie-consent.js
@@ -114,11 +114,11 @@
 
       if (usage) {
         // enable analytics and fire off a pageview
-        moj.Events.trigger('Analytics.start');
         let s = document.createElement('script');
         s.type = 'text/javascript';
         s.src = 'https://www.googletagmanager.com/gtag/js?id=G-DY4BCL021L"';
         document.getElementsByTagName('head')[0].appendChild(s);
+        moj.Events.trigger('Analytics.start');
       }
     },
 
