2f6691ad1d7b9485764ee51d709029e70c355efe
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2026-05-06 14:48:48 +0100

    cypress use data-cy for popup

--- cypress/e2e/common/i_can_see_popup.js
+++ cypress/e2e/common/i_can_see_popup.js
@@ -2,9 +2,9 @@ import { Then } from '@badeball/cypress-cucumber-preprocessor';
 
 Then(`I can see popup`, () => {
   // in ideal world we should look for data-cy=popup but this is not simple to implement
-  cy.get('#popup');
+  cy.get('[data-cy=popup]');
 });
 
 Then(`I cannot see popup`, () => {
-  cy.get('#popup').should('not.exist');
+  cy.get('[data-cy=popup]').should('not.exist');
 });
