f8b2e5184cbf5bc4ddd41ef918cf2d6672a5cf5f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-05-07 09:31:08 +0100

    codecatalyst only supports the full access endpoint policy

--- terraform/region/modules/region/modules/vpc_endpoints/main.tf
+++ terraform/region/modules/region/modules/vpc_endpoints/main.tf
@@ -140,6 +140,9 @@ locals {
     "ecs-telemetry",
     "ecs",
     "ssmmessages",
+  ])
+
+  codecatalyst_endpoints = toset([
     "codecatalyst.packages",
     "codecatalyst.git",
   ])
@@ -155,6 +158,18 @@ resource "aws_vpc_endpoint" "cloudshell" {
   security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
   subnet_ids          = var.application_subnets_id
   policy              = data.aws_iam_policy_document.allow_account_access.json
+  tags                = { Name = "cloudshell-${each.value}-private" }
+}
+
+resource "aws_vpc_endpoint" "codecatalyst" {
+  provider            = aws.region
+  for_each            = local.codecatalyst_endpoints
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${data.aws_region.current.region}.${each.value}"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
+  subnet_ids          = var.application_subnets_id
   tags                = { Name = "cloudshell-${each.value}-private" }
 }
 
