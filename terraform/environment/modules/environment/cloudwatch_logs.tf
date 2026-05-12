a533b188b1ffc0dcf60a3624e0d21a0f21af34f2
radhia shah <radhia.shah@justice.gov.uk>
2026-05-12 12:10:05 +0100

    Environment changes - adding the new kms key for application log encryption

--- terraform/environment/modules/environment/cloudwatch_logs.tf
+++ terraform/environment/modules/environment/cloudwatch_logs.tf
@@ -1,7 +1,8 @@
-#tfsec:ignore:aws-cloudwatch-log-group-customer-key encryption of logs too expensive
+
 resource "aws_cloudwatch_log_group" "application_logs" {
   name              = "${var.environment_name}_application_logs"
   retention_in_days = var.account.log_retention_in_days
+  kms_key_id        = data.aws_kms_alias.application_log_group_encryption_key_alias.arn
 
   tags = merge(
     local.shared_component_tag,
