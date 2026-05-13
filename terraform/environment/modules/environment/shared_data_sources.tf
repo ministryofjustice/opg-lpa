5e301f8f67f7e30c39252ccd6319eb6051ac7fe5
radhia shah <radhia.shah@justice.gov.uk>
2026-05-13 16:25:39 +0100

    env changes accidently placed on region pr - remove

--- terraform/environment/modules/environment/shared_data_sources.tf
+++ terraform/environment/modules/environment/shared_data_sources.tf
@@ -66,8 +66,3 @@ data "aws_availability_zones" "aws_zones" {
 }
 
 data "aws_default_tags" "current" {}
-
-# multi region encryption key alias
-data "aws_kms_alias" "application_log_group_encryption_key_alias" {
-  name = "alias/opg-lpa-${var.account_name}-application-log-group-encryption-key"
-}
