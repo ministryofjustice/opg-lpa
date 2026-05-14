d71223edccb1e71835e9defca193da5912d026a8
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-05-14 14:19:25 +0100

    remove permissions to deprecated keys

--- terraform/environment/modules/environment/shared_data_sources.tf
+++ terraform/environment/modules/environment/shared_data_sources.tf
@@ -41,17 +41,17 @@ data "aws_iam_role" "ecs_autoscaling_service_role" {
   name = "AWSServiceRoleForApplicationAutoScaling_ECSService"
 }
 
-data "aws_kms_alias" "secrets_encryption_alias" {
-  name = "alias/secrets_encryption_key-${var.account_name}"
-}
+# data "aws_kms_alias" "secrets_encryption_alias" {
+#   name = "alias/secrets_encryption_key-${var.account_name}"
+# }
 
 data "aws_kms_alias" "multi_region_secrets_encryption_alias" {
   name = "alias/opg-lpa-${var.account_name}-secrets-manager-encryption-key"
 }
 
-data "aws_kms_alias" "old_multi_region_secrets_encryption_alias" {
-  name = "alias/mrk_secrets_encryption_key-${var.account_name}"
-}
+# data "aws_kms_alias" "old_multi_region_secrets_encryption_alias" {
+#   name = "alias/mrk_secrets_encryption_key-${var.account_name}"
+# }
 
 data "aws_region" "current" {}
 
