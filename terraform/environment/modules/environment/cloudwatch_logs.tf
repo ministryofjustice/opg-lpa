597e19032623eedf9aff812e80b376bf595909ce
radhia shah <radhia.shah@justice.gov.uk>
2026-05-14 11:11:53 +0100

    env changes - adding cmk key policy to allow cloudwatch logs encryption

--- terraform/environment/modules/environment/cloudwatch_logs.tf
+++ terraform/environment/modules/environment/cloudwatch_logs.tf
@@ -1,5 +1,7 @@
 #tfsec:ignore:aws-cloudwatch-log-group-customer-key encryption of logs too expensive
 resource "aws_cloudwatch_log_group" "application_logs" {
+  depends_on = [aws_kms_key_policy.application_log_group_cloudwatch_logs]
+
   name              = "${var.environment_name}_application_logs"
   retention_in_days = var.account.log_retention_in_days
   kms_key_id        = data.aws_kms_alias.application_log_group_encryption_alias.target_key_arn
@@ -73,4 +75,40 @@ resource "aws_cloudwatch_query_definition" "migrations" {
   | filter @logStream like "migrations"
   |limit 10000
   EOF
+}
+
+data "aws_iam_policy_document" "application_log_group_cloudwatch_logs" {
+  statement {
+    sid    = "AllowCloudWatchLogsService"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*",
+    ]
+    resources = ["*"]
+    condition {
+      test     = "ArnLike"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+    }
+  }
+  statement {
+    effect = "Allow"
+    sid    = "AllowKMSKeyUsageForCloudWatchLogs"
+    resources = [
+      data.aws_kms_alias.application_log_group_encryption_alias.target_key_arn,
+    ]
+  }
+}
+
+resource "aws_kms_key_policy" "application_log_group_cloudwatch_logs" {
+  key_id = data.aws_kms_alias.application_log_group_encryption_alias.target_key_id
+  policy = data.aws_iam_policy_document.application_log_group_cloudwatch_logs.json
 }
