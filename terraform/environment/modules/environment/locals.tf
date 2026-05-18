25fcf81d64267facd8ba00c2e89df21cb068b923
radhia shah <radhia.shah@justice.gov.uk>
2026-05-18 11:32:23 +0100

    Adding custom sid for cloudwatch encryption key policies to test custom policies

--- terraform/environment/modules/environment/locals.tf
+++ terraform/environment/modules/environment/locals.tf
@@ -11,8 +11,6 @@ locals {
   admin_dns                   = "admin.lpa"
   pager_duty_ops_service_name = "Make a Lasting Power of Attorney Ops Monitoring"
   region_name                 = var.account.regions[data.aws_region.current.region].region
-  application_log_group_name  = "${var.environment_name}_application_logs"
-
   shared_component_tag = {
     component = "shared"
   }
