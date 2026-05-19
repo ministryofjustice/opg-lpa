b2edeb1e29106209fadc7b42e9f207917a94860a
radhia shah <radhia.shah@justice.gov.uk>
2026-05-19 10:09:25 +0100

    Adding custom policies for application log group encryption key

---
+++ terraform/region/kms_policies_custom.tf
@@ -0,0 +1,42 @@
+
+# custom application log group encryption key policy document
+
+data "aws_iam_policy_document" "application_log_group_kms_policy" {
+  statement {
+    sid    = "Allow root to manage application log key policies"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow CloudWatch access to application log encryption key"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*",
+    ]
+    resources = ["*"]
+
+    condition {
+      test     = "ArnLike"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = ["arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:*"]
+    }
+  }
+}
