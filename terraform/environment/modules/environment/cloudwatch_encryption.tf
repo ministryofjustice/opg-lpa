dacabf612dfe56fc8b9b7e4cba58f246a84f6df4
radhia shah <radhia.shah@justice.gov.uk>
2026-05-19 18:38:54 +0100

    env changes - adding application logs encryption key to cloudwatch logs

--- terraform/environment/modules/environment/cloudwatch_encryption.tf
+++ terraform/environment/modules/environment/cloudwatch_encryption.tf
@@ -1,6 +1,4 @@
 resource "aws_cloudwatch_log_group" "application_logs" {
-  depends_on = [aws_kms_key_policy.application_log_group_cloudwatch_logs]
-
   name              = "${var.environment_name}_application_logs"
   retention_in_days = var.account.log_retention_in_days
   kms_key_id        = data.aws_kms_alias.application_log_group_encryption_alias.target_key_arn
@@ -11,44 +9,4 @@ resource "aws_cloudwatch_log_group" "application_logs" {
       "Name" = "${var.environment_name}_application_logs"
     },
   )
-}
-
-resource "aws_kms_key_policy" "application_log_group_cloudwatch_logs" {
-  key_id = data.aws_kms_alias.application_log_group_encryption_alias.target_key_id
-  policy = data.aws_iam_policy_document.application_log_group_cloudwatch_logs.json
-}
-
-
-data "aws_iam_policy_document" "application_log_group_cloudwatch_logs" {
-  statement {
-    sid    = "Allow root to manage key policies"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-    }
-
-    actions   = ["kms:*"]
-    resources = ["*"]
-  }
-
-  statement {
-    sid    = "Allow CloudWatch access"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
-    }
-
-    actions   = []
-    resources = ["*"]
-
-    condition {
-      test     = "ArnLike"
-      variable = "kms:EncryptionContext:aws:logs:arn"
-      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"]
-    }
-  }
 }
