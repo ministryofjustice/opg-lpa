5163669f2a9311a27601924737e79502f4767be2
radhia shah <radhia.shah@justice.gov.uk>
2026-05-19 14:23:29 +0100

    region changes- adding custom kms policies and updating kms module version to v1.0.0

--- terraform/region/kms_policies_custom.tf
+++ terraform/region/kms_policies_custom.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "application_log_group_kms_policy" {
 
     principals {
       type        = "Service"
-      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+      identifiers = ["logs.${data.aws_region.current.region}.amazonaws.com"]
     }
 
     actions = [
