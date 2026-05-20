a635f7a0764fd45a8a80a4683dabc4ac4b5fbb97
radhia shah <radhia.shah@justice.gov.uk>
2026-05-20 16:42:53 +0100

    env - attaching application log encryption key to cloudwatch logs

--- terraform/environment/modules/environment/cloudwatch_logs.tf
+++ terraform/environment/modules/environment/cloudwatch_logs.tf
@@ -1,3 +1,15 @@
+resource "aws_cloudwatch_log_group" "application_logs" {
+  name              = "${var.environment_name}_application_logs"
+  retention_in_days = var.account.log_retention_in_days
+  kms_key_id        = data.aws_kms_alias.application_log_group_encryption_alias.target_key_arn
+
+  tags = merge(
+    local.shared_component_tag,
+    {
+      "Name" = "${var.environment_name}_application_logs"
+    },
+  )
+}
 resource "aws_cloudwatch_query_definition" "error_insight_query" {
   name            = "${var.environment_name}/error insight query"
   log_group_names = [aws_cloudwatch_log_group.application_logs.name]
