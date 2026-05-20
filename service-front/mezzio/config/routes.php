bb7523219afe8f3bc1adb0db2c3ab16b3e293f6a
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-05-20 09:51:45 +0100

    LPAL-2045 removed redundant files added phpunit test and cypress test for homehandler updated various config files

--- service-front/mezzio/config/routes.php
+++ service-front/mezzio/config/routes.php
@@ -3,14 +3,10 @@
 declare(strict_types=1);
 
 use App\Handler\HomeHandler;
-use App\Handler\HomePageHandler;
-use App\Handler\PingHandler;
 use Mezzio\Application;
 use Mezzio\MiddlewareFactory;
 use Psr\Container\ContainerInterface;
 
 return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
-    $app->get('/', HomePageHandler::class, 'home');
-    $app->get('/api/ping', PingHandler::class, 'api.ping');
     $app->get('/home', HomeHandler::class, 'application.home');
 };
