9eafc6c48e864457e970d5addeeae7820232a14c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-06-11 11:53:48 +0100

    allow resolver logs kms key to be used for eu-west-2 resources

--- terraform/region/kms_policies_custom.tf
+++ terraform/region/kms_policies_custom.tf
@@ -22,7 +22,10 @@ data "aws_iam_policy_document" "application_log_group_kms_policy" {
     condition {
       test     = "ArnLike"
       variable = "kms:EncryptionContext:aws:logs:arn"
-      values   = ["arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:*"]
+      values = [
+        "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:*",
+        "arn:aws:logs:${data.aws_region.eu_west_2.region}:${data.aws_caller_identity.current.account_id}:log-group:*",
+      ]
     }
   }
 }
