496868ac089ad209b5e8c838e8879508d897f144
radhia shah <radhia.shah@justice.gov.uk>
2026-06-24 17:35:30 +0100

    removing count condition from front_mezzio_app ecr & image

--- terraform/environment/modules/environment/ecs_front.tf
+++ terraform/environment/modules/environment/ecs_front.tf
@@ -128,21 +128,19 @@ data "aws_ecr_image" "lpa_front_app" {
 }
 
 data "aws_ecr_repository" "lpa_front_mezzio_app" {
-  count    = var.mezzio_frontend_enabled ? 1 : 0
   provider = aws.management
   name     = "online-lpa/front_mezzio_app"
 }
 
 data "aws_ecr_image" "lpa_front_mezzio_app" {
-  count           = var.mezzio_frontend_enabled ? 1 : 0
-  repository_name = one(data.aws_ecr_repository.lpa_front_mezzio_app).name
+  repository_name = data.aws_ecr_repository.lpa_front_mezzio_app.name
   image_tag       = var.container_version
   provider        = aws.management
 }
 locals {
 
   current_front_app_image = "${data.aws_ecr_repository.lpa_front_app.repository_url}@${data.aws_ecr_image.lpa_front_app.image_digest}"
-  mezzio_front_app_image  = var.mezzio_frontend_enabled ? "${one(data.aws_ecr_repository.lpa_front_mezzio_app).repository_url}@${one(data.aws_ecr_image.lpa_front_mezzio_app).image_digest}" : null
+  mezzio_front_app_image  = var.mezzio_frontend_enabled ? "${data.aws_ecr_repository.lpa_front_mezzio_app.repository_url}@${data.aws_ecr_image.lpa_front_mezzio_app.image_digest}" : null
   front_app_image         = var.mezzio_frontend_enabled ? local.mezzio_front_app_image : local.current_front_app_image
 
 }
