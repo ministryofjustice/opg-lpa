e9f530a8abec9b1ecf245b9c53b8ac0d08ee8b80
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-06-25 15:36:53 +0100

    temporarily remove id providers

--- terraform/environment/cognito_client.tf
+++ terraform/environment/cognito_client.tf
@@ -14,12 +14,13 @@ locals {
 }
 
 resource "aws_cognito_user_pool_client" "make_a_lasting_power_of_attorney_admin" {
-  provider                             = aws.identity
-  name                                 = "${local.environment_name}-admin-auth"
-  user_pool_id                         = local.admin_cognito_user_pool_id
-  allowed_oauth_flows                  = ["code"]
-  allowed_oauth_scopes                 = ["openid"]
-  supported_identity_providers         = ["EntraID"]
+  provider                     = aws.identity
+  name                         = "${local.environment_name}-admin-auth"
+  user_pool_id                 = local.admin_cognito_user_pool_id
+  allowed_oauth_flows          = ["code"]
+  allowed_oauth_scopes         = ["openid"]
+  supported_identity_providers = []
+  # supported_identity_providers         = ["EntraID"]
   allowed_oauth_flows_user_pool_client = true
   explicit_auth_flows = [
     "ALLOW_CUSTOM_AUTH",
