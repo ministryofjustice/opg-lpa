04881e50c9eef7c09929b4c7f27be54339decb1b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-06-29 13:45:38 +0100

    conditionally enable cognito authentication

--- terraform/environment/modules/environment/variables.tf
+++ terraform/environment/modules/environment/variables.tf
@@ -14,6 +14,7 @@ variable "account" {
     log_retention_in_days                  = number
     account_name_short                     = string
     associate_alb_with_waf_web_acl_enabled = bool
+    admin_cognito_auth_enabled             = bool
     database = object({
       cluster_identifier                 = string
       aurora_cross_region_backup_enabled = bool
