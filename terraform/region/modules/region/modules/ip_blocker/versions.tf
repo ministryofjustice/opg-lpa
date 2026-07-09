2872c7691a78220e6dc95b208bf6f927355e6286
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-09 21:05:22 +0100

    upgrade kms modules, use dynamodb key and upgrade providers

--- terraform/region/modules/region/modules/ip_blocker/versions.tf
+++ terraform/region/modules/region/modules/ip_blocker/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.8"
+    }
   }
   required_version = "1.14.3"
 }
