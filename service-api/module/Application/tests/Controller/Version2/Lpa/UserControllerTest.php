802489d16017a526b530601311abe678988af8e6
Allen Annom <allen.annom@publicguardian.gov.uk>
2026-07-10 10:39:31 +0100

    fixed broken tests

--- service-api/module/Application/tests/Controller/Version2/Lpa/UserControllerTest.php
+++ service-api/module/Application/tests/Controller/Version2/Lpa/UserControllerTest.php
@@ -173,7 +173,7 @@ class UserControllerTest extends AbstractControllerTestCase
             'type' => 'http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html',
             'title' => 'Internal Server Error',
             'status' => 500,
-            'detail' => 'Unable to process request'
+            'detail' => 'error'
         ], $response->toArray());
     }
 
