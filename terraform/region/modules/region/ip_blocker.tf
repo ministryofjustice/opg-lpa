e6d6b95b090c11e969390c92e9a080f3fa6c20c6
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-13 22:58:18 +0100

    only turn off scheduler, always deploy resources

--- terraform/region/modules/region/ip_blocker.tf
+++ terraform/region/modules/region/ip_blocker.tf
@@ -1,6 +1,5 @@
 module "ip_blocker" {
   source                       = "./modules/ip_blocker"
-  count                        = var.account.web_application_firewall.waf_ip_blocking_enabled ? 1 : 0
   dynamodb_kms_key_arn         = var.dynamodb_kms_key_arn
   application_logs_kms_key_arn = var.application_logs_kms_key_arn
   lambda_function_aws_iam_role = var.aws_iam_roles.ip_blocker
