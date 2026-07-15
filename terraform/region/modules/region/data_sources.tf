66d534a071b0793855125d0138136275f826d7cc
radhia shah <radhia.shah@justice.gov.uk>
2026-07-15 14:37:55 +0100

    settiing transit encryption mode with auth token and update strategy

--- terraform/region/modules/region/data_sources.tf
+++ terraform/region/modules/region/data_sources.tf
@@ -3,3 +3,11 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_default_tags" "current" {}
+
+data "aws_secretsmanager_secret" "elasticache_auth_token" {
+  name = "${var.account_name}/elasticache_auth_token"
+}
+
+data "aws_secretsmanager_secret_version" "elasticache_auth_token" {
+  secret_id = data.aws_secretsmanager_secret.elasticache_auth_token.id
+}
