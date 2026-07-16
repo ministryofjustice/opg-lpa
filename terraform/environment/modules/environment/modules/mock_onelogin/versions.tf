c7d40f6b1bbec51958204e464adf282b7272ae9f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-16 08:06:36 +0100

    modules should specify minimum version not absolute version

--- terraform/environment/modules/environment/modules/mock_onelogin/versions.tf
+++ terraform/environment/modules/environment/modules/mock_onelogin/versions.tf
@@ -1,9 +1,13 @@
 terraform {
+  required_version = ">= 1.5.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 6.28.0"
+      configuration_aliases = [
+        aws.region,
+      ]
     }
   }
-  required_version = "1.14.3"
 }
