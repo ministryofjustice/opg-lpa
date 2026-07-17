76744b8b23d699118cd56623f350a8319880dc87
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-17 10:56:04 +0100

    add dns for mock onelogin

--- terraform/environment/modules/dns/variables.tf
+++ terraform/environment/modules/dns/variables.tf
@@ -18,6 +18,16 @@ variable "admin_zone_id" {
   description = "admin route 53 zone id"
 }
 
+variable "mock_onelogin_dns_name" {
+  type        = string
+  description = "mock onelogin service dns name"
+}
+
+variable "mock_onelogin_zone_id" {
+  type        = string
+  description = "mock onelogin route 53 zone id"
+}
+
 variable "account_name" {
   type        = string
   description = "aws account name"
