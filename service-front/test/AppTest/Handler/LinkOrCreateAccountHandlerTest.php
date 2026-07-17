e962c596bf7fdeb34029f3fa1f45c448ac483a0c
Joshua Hawxwell <m@hawx.me>
2026-07-17 11:30:09 +0100

    Add page to choose whether to link or create a Make account

---
+++ service-front/test/AppTest/Handler/LinkOrCreateAccountHandlerTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Handler\Lpa;
+
+use App\Form\User\LinkOrCreateAccountForm;
+use App\Handler\LinkOrCreateAccountHandler;
+use App\Middleware\CsrfValidationMiddleware;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\RedirectResponse;
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\Uri;
+use Laminas\Form\FormElementManager;
+use Mezzio\Template\TemplateRendererInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class LinkOrCreateAccountHandlerTest extends TestCase
+{
+    private TemplateRendererInterface&MockObject $renderer;
+    private FormElementManager&MockObject $formElementManager;
+    private LinkOrCreateAccountForm $form;
+    private LinkOrCreateAccountHandler $handler;
+
+    protected function setUp(): void
+    {
+        $this->renderer = $this->createMock(TemplateRendererInterface::class);
+        $this->formElementManager = $this->createMock(FormElementManager::class);
+
+        $this->form = new LinkOrCreateAccountForm();
+        $this->form->init();
+
+        $this->formElementManager->method('get')->willReturn($this->form);
+
+        $this->handler = new LinkOrCreateAccountHandler(
+            $this->renderer,
+            $this->formElementManager,
+        );
+    }
+
+    private function createRequest(
+        string $method = 'GET',
+        array $postData = [],
+    ): ServerRequest {
+        $request = (new ServerRequest())
+            ->withMethod($method)
+            ->withUri(new Uri('/link-or-create-account'))
+            ->withAttribute(CsrfValidationMiddleware::TOKEN_ATTRIBUTE, 'test-token');
+
+        if ($method === 'POST') {
+            $request = $request->withParsedBody($postData);
+        }
+
+        return $request;
+    }
+
+    public function testGetRendersForm(): void
+    {
+        $this->renderer->expects($this->once())
+            ->method('render')
+            ->with(
+                'application/authenticated/linking/link-or-create-account.twig',
+                $this->callback(fn(array $vars) => isset($vars['form']) && $vars['csrfToken'] === 'test-token'),
+            )
+            ->willReturn('<html>form</html>');
+
+        $response = $this->handler->handle($this->createRequest('GET', []));
+
+        $this->assertInstanceOf(HtmlResponse::class, $response);
+    }
+
+    public function testPostWithInvalidFormRendersForm(): void
+    {
+        $this->renderer->method('render')->willReturn('<html>form with errors</html>');
+
+        $response = $this->handler->handle(
+            $this->createRequest('POST', [])
+        );
+
+        $this->assertInstanceOf(HtmlResponse::class, $response);
+    }
+
+    public static function validProvider(): array
+    {
+        return [
+            ['link', 'TODO-link-account'],
+            ['create', 'TODO-create-account'],
+        ];
+    }
+
+    #[DataProvider('validProvider')]
+    public function testPostWithValidFormRedirectsToCorrectUrl(string $value, string $redirectUrl): void
+    {
+        $response = $this->handler->handle(
+            $this->createRequest('POST', ['choice' => $value])
+        );
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $location = $response->getHeaderLine('Location');
+        $this->assertEquals($redirectUrl, $location);
+    }
+}
