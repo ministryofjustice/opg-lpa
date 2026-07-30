52855c3a451d95f04e3ae9bb274629f30acd83c9
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-30 09:28:06 +0100

    allow ecr to use resources in management account

--- terraform/region/modules/region/modules/vpc_endpoints/main.tf
+++ terraform/region/modules/region/modules/vpc_endpoints/main.tf
@@ -35,8 +35,6 @@ locals {
   interface_endpoint = toset([
     "ec2",
     "ec2messages",
-    "ecr.api",
-    "ecr.dkr",
     "events",
     "execute-api",
     "kms",
@@ -47,6 +45,10 @@ locals {
     "sqs",
     "ssm",
     "xray",
+  ])
+  ecr_endpoints = toset([
+    "ecr.api",
+    "ecr.dkr",
   ])
 }
 
@@ -77,10 +79,48 @@ resource "aws_vpc_endpoint_policy" "private" {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         },
         Action = [
+          "${each.value}:*"
+        ],
+        Resource = [
+          "arn:aws:${each.value}::${data.aws_caller_identity.current.account_id}:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_vpc_endpoint" "ecr" {
+  provider = aws.region
+  for_each = local.ecr_endpoints
+
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${data.aws_region.current.region}.${each.value}"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
+  subnet_ids          = var.application_subnets_id
+  tags                = { Name = "${each.value}-private" }
+}
+
+resource "aws_vpc_endpoint_policy" "ecr" {
+  provider        = aws.region
+  for_each        = local.ecr_endpoints
+  vpc_endpoint_id = aws_vpc_endpoint.private[each.value].id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowAll",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action = [
           "${startswith(each.value, "ecr") ? "ecr" : each.value}:*"
         ],
         Resource = [
-          "arn:aws:${startswith(each.value, "ecr") ? "ecr" : each.value}::${data.aws_caller_identity.current.account_id}:*"
+          "arn:aws:${startswith(each.value, "ecr") ? "ecr" : each.value}::${data.aws_caller_identity.current.account_id}:*",
+          "arn:aws:${startswith(each.value, "ecr") ? "ecr" : each.value}::${var.management_account_id}:*"
         ]
       }
     ]
