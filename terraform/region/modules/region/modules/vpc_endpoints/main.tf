d516e211e9acf8e6860253d292ed4f16eccfe42d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-30 10:19:27 +0100

    refactor endpoint groups into their own files to make it easier to work with

--- terraform/region/modules/region/modules/vpc_endpoints/main.tf
+++ terraform/region/modules/region/modules/vpc_endpoints/main.tf
@@ -46,10 +46,6 @@ locals {
     "ssm",
     "xray",
   ])
-  ecr_endpoints = toset([
-    "ecr.api",
-    "ecr.dkr",
-  ])
 }
 
 resource "aws_vpc_endpoint" "private" {
@@ -83,44 +79,6 @@ resource "aws_vpc_endpoint_policy" "private" {
         ],
         Resource = [
           "arn:aws:${each.value}::${data.aws_caller_identity.current.account_id}:*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_vpc_endpoint" "ecr" {
-  provider = aws.region
-  for_each = local.ecr_endpoints
-
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${data.aws_region.current.region}.${each.value}"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-  security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
-  subnet_ids          = var.application_subnets_id
-  tags                = { Name = "${each.value}-private" }
-}
-
-resource "aws_vpc_endpoint_policy" "ecr" {
-  provider        = aws.region
-  for_each        = local.ecr_endpoints
-  vpc_endpoint_id = aws_vpc_endpoint.private[each.value].id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowAll",
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action = [
-          "${startswith(each.value, "ecr") ? "ecr" : each.value}:*"
-        ],
-        Resource = [
-          "arn:aws:${startswith(each.value, "ecr") ? "ecr" : each.value}::${data.aws_caller_identity.current.account_id}:*",
-          "arn:aws:${startswith(each.value, "ecr") ? "ecr" : each.value}::${var.management_account_id}:*"
         ]
       }
     ]
@@ -186,56 +144,4 @@ data "aws_iam_policy_document" "s3_bucket_access" {
       identifiers = ["*"]
     }
   }
-}
-
-# for cloudshell
-locals {
-  cloudshell_endpoints = toset([
-    "ecs-agent",
-    "ecs-telemetry",
-    "ecs",
-    "ssmmessages",
-  ])
-
-  codecatalyst_endpoints = toset([
-    "codecatalyst.packages",
-    "codecatalyst.git",
-  ])
-}
-
-resource "aws_vpc_endpoint" "cloudshell" {
-  provider            = aws.region
-  for_each            = local.cloudshell_endpoints
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${data.aws_region.current.region}.${each.value}"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-  security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
-  subnet_ids          = var.application_subnets_id
-  policy              = data.aws_iam_policy_document.allow_account_access.json
-  tags                = { Name = "cloudshell-${each.value}-private" }
-}
-
-resource "aws_vpc_endpoint" "codecatalyst" {
-  provider            = aws.region
-  for_each            = var.codecatalyst_endpoints_enabled ? local.codecatalyst_endpoints : []
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${data.aws_region.current.region}.${each.value}"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-  security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
-  subnet_ids          = var.application_subnets_id
-  tags                = { Name = "cloudshell-${each.value}-private" }
-}
-
-resource "aws_vpc_endpoint" "cloudshell_codecatalyst_global" {
-  count               = var.codecatalyst_endpoints_enabled ? 1 : 0
-  provider            = aws.region
-  vpc_id              = var.vpc_id
-  service_name        = "aws.api.global.codecatalyst"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-  security_group_ids  = aws_security_group.vpc_endpoints_private[*].id
-  subnet_ids          = var.application_subnets_id
-  tags                = { Name = "cloudshell-aws.api.global.codecatalyst-private" }
 }
