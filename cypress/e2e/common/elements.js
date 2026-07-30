92fd4a86d694ec522a0e222f25b8bc92fdb7ec38
Alex Saunders <acsauk@gmail.com>
2026-07-30 16:59:31 +0100

    LPAL-2356 add basic user + LPA fixture endpoint behind feature flag

--- cypress/e2e/common/elements.js
+++ cypress/e2e/common/elements.js
@@ -168,7 +168,9 @@ Then('the instructions expandable element should be present and closed', () => {
 
 Then('I can see myself', () => {
   cy.get('table').within(() => {
-    cy.contains('Shared Space Test User');
-    cy.contains('seeded_test_user_shared@publicguardian.gov.uk');
+      cy.get('@fixtureUserEmail').then((fixtureUserEmail) => {
+        cy.contains(fixtureUserEmail);
+        cy.contains('Fixture User');
+      });
   });
 });
--- service-front/src/App/src/Feature.php
+++ service-front/src/App/src/Feature.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App;
 
+use function getenv;
+
 enum Feature: string
 {
-    case OneLogin = 'ONELOGIN_ENABLED';
-    case SharedSpace = 'SHARED_SPACES_ENABLED';
+    case OneLogin        = 'ONELOGIN_ENABLED';
+    case SharedSpace     = 'SHARED_SPACES_ENABLED';
+    case CypressFixtures = 'CYPRESS_FIXTURES_ENABLED';
 
     public function isEnabled(): bool
     {
