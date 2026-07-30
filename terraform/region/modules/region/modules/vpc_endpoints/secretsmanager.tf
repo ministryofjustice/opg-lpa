8b0f562046f3490ba75d30d476edc7bc6a5426e3
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-07-30 13:25:21 +0100

    lock secrets manager to account

--- terraform/region/modules/region/modules/vpc_endpoints/secretsmanager.tf
+++ terraform/region/modules/region/modules/vpc_endpoints/secretsmanager.tf
@@ -1,15 +1,5 @@
-locals {
-  logging_endpoints = toset([
-    "logs",
-    "xray",
-    "rum",
-  ])
-}
-
-resource "aws_vpc_endpoint" "logging" {
-  provider = aws.region
-  for_each = local.logging_endpoints
-
+resource "aws_vpc_endpoint" "secretsmanager" {
+  provider            = aws.region
   vpc_id              = var.vpc_id
   service_name        = "com.amazonaws.${data.aws_region.current.region}.${each.value}"
   vpc_endpoint_type   = "Interface"
@@ -19,10 +9,9 @@ resource "aws_vpc_endpoint" "logging" {
   tags                = { Name = "${each.value}-private" }
 }
 
-resource "aws_vpc_endpoint_policy" "logging" {
+resource "aws_vpc_endpoint_policy" "secretsmanager" {
   provider        = aws.region
-  for_each        = local.logging_endpoints
-  vpc_endpoint_id = aws_vpc_endpoint.logging[each.value].id
+  vpc_endpoint_id = aws_vpc_endpoint.secretsmanager.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -33,10 +22,10 @@ resource "aws_vpc_endpoint_policy" "logging" {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         },
         Action = [
-          "${each.value}:*"
+          "secretsmanager:*"
         ],
         Resource = [
-          "arn:aws:${each.value}::${data.aws_caller_identity.current.account_id}:*",
+          "arn:aws:secretsmanager::${data.aws_caller_identity.current.account_id}:*",
         ]
       }
     ]
