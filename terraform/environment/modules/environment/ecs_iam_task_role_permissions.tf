0ff863e56d2070a2abeb3047334729e5827b7a38
radhia shah <radhia.shah@justice.gov.uk>
2026-08-03 18:15:32 +0100

    re-adding old lpa pdf s3 key to ecs task roles in case to manage safe deployment of new keys

--- terraform/environment/modules/environment/ecs_iam_task_role_permissions.tf
+++ terraform/environment/modules/environment/ecs_iam_task_role_permissions.tf
@@ -85,6 +85,7 @@ data "aws_iam_policy_document" "api_permissions_role" {
     ]
     resources = [
       data.aws_s3_bucket.lpa_pdf_cache.arn,
+      data.aws_kms_key.lpa_pdf_cache.arn,
       data.aws_kms_alias.pdf_cache_s3_encryption_key.target_key_arn,
       data.aws_kms_alias.elasticache_encryption_key.target_key_arn,
       data.aws_kms_alias.dynamodb_encryption_key.target_key_arn,
@@ -99,6 +100,7 @@ data "aws_iam_policy_document" "api_permissions_role" {
       "kms:GenerateDataKey",
     ]
     resources = [
+      data.aws_kms_key.lpa_pdf_sqs.arn,
       data.aws_kms_alias.pdf_sqs_encryption_key.target_key_arn,
       data.aws_kms_alias.dynamodb_encryption_key.target_key_arn,
     ]
@@ -234,6 +236,7 @@ data "aws_iam_policy_document" "front_permissions_role" {
     ]
     resources = [
       data.aws_s3_bucket.lpa_pdf_cache.arn,
+      data.aws_kms_key.lpa_pdf_cache.arn,
       data.aws_kms_alias.pdf_cache_s3_encryption_key.target_key_arn,
       data.aws_kms_alias.elasticache_encryption_key.target_key_arn,
       data.aws_kms_alias.dynamodb_encryption_key.target_key_arn,
@@ -326,6 +329,7 @@ data "aws_iam_policy_document" "pdf_permissions_role" {
     ]
     resources = [
       data.aws_s3_bucket.lpa_pdf_cache.arn,
+      data.aws_kms_key.lpa_pdf_cache.arn,
       data.aws_kms_alias.pdf_cache_s3_encryption_key.target_key_arn,
       data.aws_kms_alias.elasticache_encryption_key.target_key_arn,
       data.aws_kms_alias.dynamodb_encryption_key.target_key_arn,
@@ -339,6 +343,7 @@ data "aws_iam_policy_document" "pdf_permissions_role" {
       "kms:GenerateDataKey",
     ]
     resources = [
+      data.aws_kms_key.lpa_pdf_sqs.arn,
       data.aws_kms_alias.pdf_sqs_encryption_key.target_key_arn,
     ]
   }
