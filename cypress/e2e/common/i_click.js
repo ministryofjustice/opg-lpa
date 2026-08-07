96fc60216f583790f6c42609debc3752d7d8c653
Joshua Hawxwell <m@hawx.me>
2026-08-07 09:40:01 +0100

    Change to look more like the shared space prototype

--- cypress/e2e/common/i_click.js
+++ cypress/e2e/common/i_click.js
@@ -38,6 +38,10 @@ Then(`I click element marked {string}`, (text) => {
   cy.contains(text).click();
 });
 
+Then(`I click link {string}`, (text) => {
+  cy.contains("a", text).click();
+});
+
 Then(`I click {string} for LPA ID {int}`, (clickable, LpaId) => {
   cy.get('[data-cy=lpa-' + LpaId + ']')
     .find('[data-cy=' + clickable + ']')
