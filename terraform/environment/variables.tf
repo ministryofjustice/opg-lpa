da60bddd12d16dea07bbe86a31f7d58a167b569d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-08-19 17:02:44 +0100

    use boundaried ci role where they exist

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -5,6 +5,12 @@ variable "pagerduty_token" {
 }
 
 variable "default_role" {
+  default     = "opg-lpa-ci"
+  type        = string
+  description = "The default role to use to create resources"
+}
+
+variable "boundaried_default_role" {
   default     = "opg-lpa-ci-boundary"
   type        = string
   description = "The default role to use to create resources"
