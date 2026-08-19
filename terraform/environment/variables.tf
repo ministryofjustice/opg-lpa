ca5af14e06844809ba46bd8b2739cd4abd7d3e6e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-08-19 16:08:50 +0100

    use boundaried ci role in environment

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -5,13 +5,13 @@ variable "pagerduty_token" {
 }
 
 variable "default_role" {
-  default     = "opg-lpa-ci"
+  default     = "opg-lpa-ci-boundary"
   type        = string
   description = "The default role to use to create resources"
 }
 
 variable "management_role" {
-  default     = "opg-lpa-ci"
+  default     = "opg-lpa-ci-boundary"
   type        = string
   description = "The default role to use to create resources in the management account"
 }
