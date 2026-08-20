46d542992c15f996310aba78ba064f5b01ab8d86
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-08-20 16:06:37 +0100

    don't add permission boundaries for backup account resources

--- terraform/region/aws_iam_backup_account_role.tf
+++ terraform/region/aws_iam_backup_account_role.tf
@@ -1,8 +1,7 @@
 resource "aws_iam_role" "make_cross_account_backup_role" {
-  provider             = aws.backup
-  name                 = "${local.environment_name}-make-cross-account-backup-role"
-  permissions_boundary = data.aws_iam_policy.default_boundary.arn
-  assume_role_policy   = data.aws_iam_policy_document.make-cross-account-backup-role-permissions.json
+  provider           = aws.backup
+  name               = "${local.environment_name}-make-cross-account-backup-role"
+  assume_role_policy = data.aws_iam_policy_document.make-cross-account-backup-role-permissions.json
 }
 
 resource "aws_iam_role_policy_attachment" "make_cross_account_backup_role" {
