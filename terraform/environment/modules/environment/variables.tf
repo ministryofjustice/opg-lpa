2b6dea176aac186590aa8559ac6b4af2ab6305b7
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-09-08 11:29:41 +0100

    allow ur to be accessed by the public too

--- terraform/environment/modules/environment/variables.tf
+++ terraform/environment/modules/environment/variables.tf
@@ -14,6 +14,7 @@ variable "environment" {
     log_retention_in_days                  = number
     account_name_short                     = string
     associate_alb_with_waf_web_acl_enabled = bool
+    public_access_enabled                  = bool
     cognito = object({
       admin_cognito_auth_enabled = bool
     })
