bcb58c513cbdd945e38fa9cb268d5c532fc9c2d9
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-09-10 12:23:24 +0100

    add nginx health check for api and admin ecs services

--- terraform/environment/modules/environment/ecs_api.tf
+++ terraform/environment/modules/environment/ecs_api.tf
@@ -222,6 +222,13 @@ locals {
           },
         ]
       },
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost:8080/nginx-health || exit 1"],
+        startPeriod = 30,
+        interval    = 15,
+        timeout     = 10,
+        retries     = 3
+      },
       logConfiguration = {
         logDriver = "awslogs",
         options = {
