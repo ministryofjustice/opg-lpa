64c7f03057ec9c483adf1a57e0d1b6d5be1895c6
Andrew Pearce <6545556+andrewpearce-digital@users.noreply.github.com>
2026-09-10 15:27:07 +0000

    LPAL-2428 - add nginx healthcheck (#3784)
    * add healthcheck for nginx
    
    * add nginx healthcheck to service front web
    
    * don't remove curl
    
    * add healthcheck
    
    * add nginx health check for api and admin ecs services
    
    * keep curl on the web containers
    
    * test using /dev/tcp for healthchecks
    
    * remove test

--- terraform/environment/modules/environment/ecs_front.tf
+++ terraform/environment/modules/environment/ecs_front.tf
@@ -37,8 +37,8 @@ resource "aws_ecs_service" "front" {
   }
 
   timeouts {
-    create = var.environment_name == "production" ? "20m" : "10m"
-    update = var.environment_name == "production" ? "20m" : "6m"
+    create = var.environment_name == "production" ? "20m" : "4m"
+    update = var.environment_name == "production" ? "20m" : "4m"
   }
 
   depends_on = [aws_lb.front]
@@ -177,6 +177,13 @@ locals {
           mountOptions  = ["uid=101", "gid=101"]
         },
       ]
+    },
+    healthCheck = {
+      command     = ["CMD-SHELL", "curl -f http://localhost:8080/nginx-health || exit 1"],
+      startPeriod = 30,
+      interval    = 15,
+      timeout     = 10,
+      retries     = 3
     },
     logConfiguration = {
       logDriver = "awslogs",
