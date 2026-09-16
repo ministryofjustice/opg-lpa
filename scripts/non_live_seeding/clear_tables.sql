d41b9fc21f0f385e46045d29a71e0152e3589d57
Joshua Hawxwell <m@hawx.me>
2026-09-16 08:23:42 +0000

    Make seeding truncate shared spaces tables and fix error in ur script (#3798)

--- scripts/non_live_seeding/clear_tables.sql
+++ scripts/non_live_seeding/clear_tables.sql
@@ -3,3 +3,6 @@ TRUNCATE TABLE public.users RESTART identity CASCADE;
 TRUNCATE TABLE public.applications RESTART identity CASCADE;
 TRUNCATE TABLE public.deletion_log RESTART identity CASCADE;
 TRUNCATE TABLE public.feedback RESTART identity CASCADE;
+TRUNCATE TABLE public.shared_space_members RESTART identity CASCADE;
+TRUNCATE TABLE public.shared_space_invites RESTART identity CASCADE;
+TRUNCATE TABLE public.shared_space RESTART identity CASCADE;
