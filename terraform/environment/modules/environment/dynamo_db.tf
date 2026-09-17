896e670e08e1d1a5a98d0808e5f18de6e667ff24
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-09-17 15:36:48 +0100

    don't provision sessions tables

--- terraform/environment/modules/environment/dynamo_db.tf
+++ terraform/environment/modules/environment/dynamo_db.tf
@@ -36,27 +36,3 @@ resource "aws_dynamodb_table" "lpa-properties" {
 
   tags = local.dynamodb_component_tag
 }
-
-# #tfsec:ignore:aws-dynamodb-enable-recovery
-# resource "aws_dynamodb_table" "lpa-sessions" {
-#   name         = "lpa-sessions-${var.environment_name}"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "id"
-
-#   attribute {
-#     name = "id"
-#     type = "S"
-#   }
-
-#   ttl {
-#     attribute_name = "expires"
-#     enabled        = true
-#   }
-
-#   server_side_encryption {
-#     enabled     = true
-#     kms_key_arn = data.aws_kms_alias.dynamodb_encryption_key.target_key_arn
-#   }
-
-#   tags = local.dynamodb_component_tag
-# }
